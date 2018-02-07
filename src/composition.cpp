6d30a9f35d13b79f111597c0cffe54f2dee5a361
Simon Heybrock <simon.heybrock@esss.se>
2018-02-07 10:37:50 +0100

    Extend composition example with calls to different algorithm types.

--- src/composition.cpp
+++ src/composition.cpp
@@ -1,7 +1,11 @@
 #include <vector>
 #include <memory>
 
+#include <boost/tti/has_static_member_function.hpp>
+
 using Histogram = double;
+using SpectrumInfo = std::vector<double>;
+using IncidentWavelength = double;
 
 class Histograms {
 public:
@@ -15,6 +19,7 @@ public:
   void setHistograms(Histograms &&histograms) {
     m_histograms = std::move(histograms);
   }
+  const T &metadata() const { return m_metadata; }
 
 private:
   Histograms m_histograms;
@@ -22,12 +27,45 @@ private:
 };
 
 Histograms rebin(const Histograms &histograms) { return Histograms{}; }
+Histograms convertUnits(const Histograms &histograms,
+                        const SpectrumInfo &spectrumInfo) {
+  return Histograms{};
+}
+Histograms convertUnits(const Histograms &histograms,
+                        const IncidentWavelength &wavelength) {
+  return Histograms{};
+}
+
+BOOST_TTI_HAS_STATIC_MEMBER_FUNCTION(exec);
 
 template <class T> class Algorithm {
 public:
-  template <class U> Workspace<U> execute(const Workspace<U> &ws) {
+  // Enabled if T needs only histograms for processing
+  // sizeof(U) is a workaround, because we must use U in the enable_if clause.
+  template <class U>
+  Workspace<U>
+  execute(const Workspace<U> &ws,
+          typename std::enable_if<
+              sizeof(U) &&
+              has_static_member_function_exec<
+                  T, Histograms,
+                  boost::mpl::vector<const Histograms &>>::value>::type * =
+              nullptr) {
     auto out(ws);
     out.setHistograms(T::exec(ws.histograms()));
+    return out;
+  }
+
+  // Enabled if T needs histograms and metadata for processing
+  template <class U>
+  Workspace<U> execute(
+      const Workspace<U> &ws,
+      typename std::enable_if<has_static_member_function_exec<
+          T, Histograms,
+          boost::mpl::vector<const Histograms &, const U &>>::value>::type * =
+          nullptr) {
+    auto out(ws);
+    out.setHistograms(T::exec(ws.histograms(), ws.metadata()));
     return out;
   }
 };
@@ -38,14 +76,45 @@ struct Rebin {
   }
 };
 
+struct ConvertUnits {
+  static Histograms exec(const Histograms &histograms,
+                         const SpectrumInfo &spectrumInfo) {
+    return convertUnits(histograms, spectrumInfo);
+  }
+  static Histograms exec(const Histograms &histograms,
+                         const IncidentWavelength &wavelength) {
+    return convertUnits(histograms, wavelength);
+  }
+};
+
 int main() {
   Histograms hists;
   auto rebinnedHists = rebin(hists);
 
-  Workspace<int> ws;
+  Workspace<SpectrumInfo> ws;
+  Workspace<IncidentWavelength> ws2;
   // We would like to call `rebin` on `ws`, which will obviously not work:
   // auto rebinnedWs = rebin(ws);
 
+  // Wrapping in `Algorithm` does what we need:
   Algorithm<Rebin> alg;
   auto rebinnedWs = alg.execute(ws);
+  auto rebinnedWs2 = alg.execute(ws2);
+
+  // Composition can be handled via templated Algorithm::execute, actual
+  // algorithms do not need templates. Note in particular that ws and ws2 are
+  // *different types*, nevertheless `Rebin` works with both!
+  Algorithm<ConvertUnits> alg2;
+  auto convertedWs = alg2.execute(rebinnedWs);
+  auto convertedWs2 = alg2.execute(rebinnedWs2);
 }
+
+// TODO
+// - How should output workspace generation work in general?
+// - How should we pass other arguments?
+// - Iterators?
+// - Instead of passing indivudual arguments, should we create a temporay
+//   forwarding workspace of "minimal" type, containing only required
+//   information?
+// - How can we use properties instead of arguments and return values in
+//   Algorithm::execute?
