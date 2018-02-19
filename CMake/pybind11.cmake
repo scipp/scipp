74601837058036f5a75bb037bdee9e543fa2e177
Simon Heybrock <simon.heybrock@esss.se>
2018-02-19 15:18:26 +0100

    Add pybind11 as ExternalProject.

---
+++ CMake/pybind11.cmake
@@ -0,0 +1,12 @@
+# Download and unpack pybind11 at configure time
+configure_file(${CMAKE_SOURCE_DIR}/CMake/pybind11.in
+               ${CMAKE_BINARY_DIR}/pybind11-download/CMakeLists.txt)
+
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+                  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/pybind11-download )
+
+execute_process(COMMAND ${CMAKE_COMMAND} --build .
+                  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/pybind11-download )
+
+add_subdirectory(${CMAKE_BINARY_DIR}/pybind11-src
+                 ${CMAKE_BINARY_DIR}/pybind11-src)
