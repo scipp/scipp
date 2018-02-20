de4cde5d15c81b8e380a798be3867e5d87e8c6e2
Simon Heybrock <simon.heybrock@esss.se>
2018-02-20 10:56:26 +0100

    Remove example relying on implicit cast.

--- src/prototype/prototype.cpp
+++ src/prototype/prototype.cpp
@@ -14,7 +14,6 @@ public:
   typename std::vector<Data>::iterator end() { return m_data.end(); }
   Logs &logs() { return m_logs; }
   const Logs &logs() const { return m_logs; }
-  operator Logs &() { return m_logs; }
 
 private:
   std::vector<Data> m_data;
@@ -86,34 +85,11 @@ Ws callWithConstructor(Ws ws, const Args &... args) {
   return ws;
 }
 
-// Case 2b : Alg needs constructor arguments, apply is not static, using
-// implicit type cast of Workspace to Logs.
-BOOST_TTI_HAS_MEMBER_FUNCTION(apply);
-template <class Alg, class Enable, class Ws, class... Args> struct Apply;
-template <class Alg, class Ws, class... Args>
-struct Apply<Alg, typename std::enable_if<
-                      !has_member_function_apply<
-                          Alg, void, boost::mpl::vector<Logs &>>::value>::type,
-             Ws, Args...> {
-  static Ws run(Ws ws, const Args &... args) {
-    Alg alg(ws, args...);
-    for (auto &item : ws)
-      alg.apply(item);
-    return ws;
-  }
-};
-
-template <class Alg, class Ws, class... Args>
-Ws call2(Ws ws, const Args &... args) {
-  return Apply<Alg, void, Ws, Args...>::run(ws, args...);
-}
-
 int main() {
   Workspace<Histogram> ws;
   ws = call<Scale>(std::move(ws), 2.3);
   ws = call<ClearLogs>(std::move(ws));
   Workspace<EventList> eventWs;
-  eventWs = call2<FilterByLogValue>(std::move(eventWs), "temp1", 274.0, 275.0);
   eventWs = callWithConstructor<FilterByLogValue>(std::move(eventWs), "temp1",
                                                   274.0, 275.0);
 }
