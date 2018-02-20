5bcaa388acfe9304ba25cb857db00c3922b9bb87
Simon Heybrock <simon.heybrock@esss.se>
2018-02-20 11:17:24 +0100

    Consolidate construction/calling mechanism.

--- src/prototype/prototype.cpp
+++ src/prototype/prototype.cpp
@@ -1,7 +1,6 @@
 #include <cstdint>
 
-#include <boost/tti/has_member_function.hpp>
-#include <boost/tti/has_static_member_function.hpp>
+#include <boost/tti/has_function.hpp>
 
 #include "algorithms.h"
 #include "data.h"
@@ -23,66 +22,64 @@ private:
   Logs m_logs;
 };
 
-// Case 1: No state, apply is static method of Alg
-BOOST_TTI_HAS_STATIC_MEMBER_FUNCTION(apply);
-template <class Alg, class Enable, class Ws, class... Args> struct ApplyStatic;
+// Part 2: Call apply functions
+BOOST_TTI_HAS_FUNCTION(apply);
+template <class Alg, class Enable, class Ws, class... Args> struct Apply;
 template <class Alg, class Ws, class... Args>
-struct ApplyStatic<Alg,
-                   typename std::enable_if<has_static_member_function_apply<
-                       Alg, void, boost::mpl::vector<Logs &>>::value>::type,
-                   Ws, Args...> {
-  static Ws run(Ws ws, const Args &... args) {
-    Alg::apply(ws.logs(), args...);
+struct Apply<Alg, typename std::enable_if<has_function_apply<
+                      Alg, void, boost::mpl::vector<Logs &>>::value>::type,
+             Ws, Args...> {
+  static Ws run(const Alg &alg, Ws ws, const Args &... args) {
+    alg.apply(ws.logs(), args...);
     return ws;
   }
 };
 template <class Alg, class Ws, class... Args>
-struct ApplyStatic<Alg,
-                   typename std::enable_if<
-                       !has_static_member_function_apply<
-                           Alg, void, boost::mpl::vector<Logs &>>::value>::type,
-                   Ws, Args...> {
-  static Ws run(Ws ws, const Args &... args) {
+struct Apply<Alg, typename std::enable_if<
+                      !has_function_apply<
+                          Alg, void, boost::mpl::vector<Logs &>>::value>::type,
+             Ws, Args...> {
+  static Ws run(const Alg &alg, Ws ws, const Args &... args) {
     // Can do multi-threading, basic logging, and cancellation here.
     for (auto &item : ws)
-      Alg::apply(item, args...);
+      alg.apply(item, args...);
     return ws;
+  }
+};
+
+template <class Alg, class Ws, class... Args>
+Ws callInstance(const Alg &alg, Ws ws, const Args &... args) {
+  return Apply<Alg, void, Ws, Args...>::run(alg, ws, args...);
+}
+
+// Part 1: Create algorithm instance and forward to call helper.
+template <class Alg, class Enable, class Ws, class... Args>
+struct ConstructAndApply;
+template <class Alg, class Ws, class... Args>
+struct ConstructAndApply<
+    Alg, typename std::enable_if<
+             std::is_trivially_default_constructible<Alg>::value>::type,
+    Ws, Args...> {
+  static Ws run(const Ws &ws, const Args &... args) {
+    Alg alg;
+    // Alg constructor does not need arguments, pass all arguments to apply.
+    return callInstance<Alg>(alg, std::move(ws), args...);
+  }
+};
+template <class Alg, class Ws, class... Args>
+struct ConstructAndApply<Alg, typename std::enable_if<std::is_constructible<
+                                  Alg, Logs, Args...>::value>::type,
+                         Ws, Args...> {
+  static Ws run(const Ws &ws, const Args &... args) {
+    Alg alg(ws.logs(), args...);
+    // Alg constructor consumed the arguments, pass only workspace.
+    return callInstance<Alg>(alg, std::move(ws));
   }
 };
 
 template <class Alg, class Ws, class... Args>
 Ws call(Ws ws, const Args &... args) {
-  return ApplyStatic<Alg, void, Ws, Args...>::run(ws, args...);
-}
-
-// Case 2: Constructor needs arguments.
-template <class Alg, class Enable, class Ws, class... Args> struct Construct;
-template <class Alg, class Ws, class... Args>
-struct Construct<Alg,
-                 typename std::enable_if<
-                     std::is_trivially_default_constructible<Alg>::value>::type,
-                 Ws, Args...> {
-  static Alg run(const Ws &ws, const Args &... args) {
-    Alg alg;
-    return alg;
-  }
-};
-template <class Alg, class Ws, class... Args>
-struct Construct<Alg, typename std::enable_if<std::is_constructible<
-                          Alg, Logs, Args...>::value>::type,
-                 Ws, Args...> {
-  static Alg run(const Ws &ws, const Args &... args) {
-    Alg alg(ws.logs(), args...);
-    return alg;
-  }
-};
-
-template <class Alg, class Ws, class... Args>
-Ws callWithConstructor(Ws ws, const Args &... args) {
-  const auto alg = Construct<Alg, void, Ws, Args...>::run(ws, args...);
-  for (auto &item : ws)
-    alg.apply(item);
-  return ws;
+  return ConstructAndApply<Alg, void, Ws, Args...>::run(ws, args...);
 }
 
 int main() {
@@ -90,6 +87,5 @@ int main() {
   ws = call<Scale>(std::move(ws), 2.3);
   ws = call<ClearLogs>(std::move(ws));
   Workspace<EventList> eventWs;
-  eventWs = callWithConstructor<FilterByLogValue>(std::move(eventWs), "temp1",
-                                                  274.0, 275.0);
+  eventWs = call<FilterByLogValue>(std::move(eventWs), "temp1", 274.0, 275.0);
 }
