f49213827b3f3a14b08983323a3f693c8f215e93
Simon Heybrock <simon.heybrock@esss.se>
2018-03-22 15:34:11 +0100

    Progress on thoughts regarding type erased dataset iteration.

--- src/type-erased-prototype/dataset_iterator.h
+++ src/type-erased-prototype/dataset_iterator.h
@@ -22,11 +22,37 @@ class DatasetIterator {
               dataset.get<std::vector<std::remove_const_t<Ts>>>()...)) {}
 
     template <class T> T &get() {
-      // Works as long as all columsn share the same dimensions.
+      // Works as long as all columns share the same dimensions.
+      // Where should we fail if T has extra dimensions? Default iterator: iterate everything (exception: TOF if requested). If not iterating everything: must request slice of columns
       return std::get<std::vector<T> &>(m_columns)[m_index];
     }
 
-    void increment() { ++m_index; }
+    // Increment each dimension separately?
+    // May not want to iterate all (in particular TOF!). Can we request that by having Histogram (or similar) as requested type, instead of item type? That is, Dataset could contain std::vector<BinEdge>, std::vector<Value>, std::vector<Error> but we ask for ther plural versions, and thus make it skip that iteration?
+    // We know only at runtime which dimensions contribute to each column. How do we set this up at runtime?
+    // - iterate dimensions in certain order, incrementing index with dimensions
+    // - for each column, go though list of its dimension and compute index based on above set of indices
+    // Is this very expensive? Is that an issue if we do not iterate the innermost (TOF) index?
+    void increment() {
+      ++m_index;
+      // full multi dimensional version:
+      // unknown size since arbitrary dimensions possible (but could be put on stack since it is always small):
+      std::vector<gsl::index> index;
+      std::array<std::vector<gsl::index>, sizeof...(Ts)> columnDimensions;
+      // 1. Increment index as a normal multi-dimensional index.
+      // 2. compute column index:
+      std::array<gsl::index, sizeof...(Ts)> columnIndices;
+      for (auto i : columnIndices) {
+        i = 0;
+        gsl::index prev_dimension = -1;
+        for (const auto dimension : columnDimensions) {
+          if (prev_dimension >= 0)
+            i *= m_dimensions[prev_dimension];
+          prev_dimensions = dimension;
+          i += index[dimension];
+        }
+      }
+    }
 
   private:
     gsl::index m_index;
--- src/type-erased-prototype/test/dataset_iterator_test.cpp
+++ src/type-erased-prototype/test/dataset_iterator_test.cpp
@@ -29,3 +29,38 @@ TEST(DatasetIterator, get) {
   it.increment();
   ASSERT_EQ(it.get<double>(), 3.2);
 }
+
+TEST(DatasetIterator, notes) {
+  Dataset d(std::vector<double>(1), std::vector<int>(1));
+  d.addDimension("tof", 10);
+  d.extendAlongDimension(ColumnType::Doubles, "tof");
+  d.addDimension("spec", 10);
+  d.extendAlongDimension(ColumnType::Doubles, "spec");
+  d.extendAlongDimension(ColumnType::Ints, "spec");
+
+  // ok
+  DatasetIterator<double> it(d, 0);
+  // should throw, because int has less dimensions and is not const
+  DatasetIterator<double, int> it(d, 0);
+  // ok
+  DatasetIterator<double, const int> it(d, 0);
+  // ok, int can be non-const since slab says "do not iterate tof".
+  // This is a common case so we may want a shorthand notation for this.
+  // We also need a way to have multiple columns of the same type, X,Y,E of a
+  // histogram are all double currently! If we have a type for, e.g., BinEdges
+  // wrapping a std::vector<double> we can use that (but template argument would
+  // not be item type anymore!). Can we use a column ID instead? Since the
+  // iterator is templated we cannot support custom types anyway.
+  // Unit handling?? Can/should a slab have a (runtime) unit? Could used by call
+  // wrapper to create output workspace (cannot *set* based on it due to
+  // conflict with multi threading, but can *check*)? (do we even need call
+  // wrappers, or should things be handled based on Dataset and
+  // transformations?).
+  DatasetIterator<slab<double, Dimension::Tof>, int> it(d, 0);
+  // iterate over items that are slabs of doubles with a spectrum dimension. In
+  // this case iteration is over tof.
+  // A slab is a bit like gsl::range, i.e., a view into a vector but may have a
+  // spread to support multi-dimensional slices.
+  DatasetIterator<slab<double, Dimension::Spectrum>,
+                  slab<const int, Dimension::Spectrum>> it(d, 0);
+}
