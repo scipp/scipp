63375d709220e1781e166754c2615b6598ab0bff
Simon Heybrock <simon.heybrock@esss.se>
2018-06-06 13:14:36 +0200

    Easier DataArray init.

--- src/type-erased-prototype/data_array.h
+++ src/type-erased-prototype/data_array.h
@@ -1,8 +1,11 @@
 #ifndef DATA_ARRAY_H
 #define DATA_ARRAY_H
 
+#include <type_traits>
+
 #include "cow_ptr.h"
 #include "index.h"
+#include "variable.h"
 
 class DataArrayConcept {
 public:
@@ -24,7 +27,7 @@ public:
   T m_model;
 };
 
-class DataArray{
+class DataArray {
 public:
   template <class T>
   DataArray(uint32_t id, T object)
@@ -36,6 +39,25 @@ public:
   gsl::index size() const { return m_object->size(); }
   void resize(const gsl::index size) { m_object.access().resize(size); }
 
+  uint32_t type() const { return m_type; }
+
+  template <class Tag> const variable_type_t<Tag> &get() const {
+    return cast<std::remove_const_t<variable_type_t<Tag>>>();
+  }
+
+  template <class Tag>
+  variable_type_t<Tag> &
+  get(std::enable_if_t<std::is_const<Tag>::value> * = nullptr) {
+    return const_cast<const DataArray *>(this)->get<Tag>();
+  }
+
+  template <class Tag>
+  variable_type_t<Tag> &
+  get(std::enable_if_t<!std::is_const<Tag>::value> * = nullptr) {
+    return cast<std::remove_const_t<variable_type_t<Tag>>>();
+  }
+
+private:
   template <class T> const T &cast() const {
     return dynamic_cast<const DataArrayModel<T> &>(*m_object).m_model;
   }
@@ -44,12 +66,18 @@ public:
     return dynamic_cast<DataArrayModel<T> &>(m_object.access()).m_model;
   }
 
-  uint32_t type() const { return m_type; }
-
-private:
   const uint32_t m_type;
   cow_ptr<DataArrayConcept> m_object;
 };
 
+template <class Tag, class... Args> DataArray makeDataArray(Args &&... args) {
+  return DataArray(Tag::type_id,
+                   variable_type_t<Tag>(std::forward<Args>(args)...));
+}
+
+template <class Tag, class T>
+DataArray makeDataArray(std::initializer_list<T> values) {
+  return DataArray(Tag::type_id, variable_type_t<Tag>(values));
+}
 
 #endif // DATA_ARRAY_H
--- src/type-erased-prototype/test/data_array_test.cpp
+++ src/type-erased-prototype/test/data_array_test.cpp
@@ -2,20 +2,30 @@
 #include <vector>
 
 #include "data_array.h"
+#include "variable.h"
 
 TEST(DataArray, construct) {
-  ASSERT_NO_THROW(DataArray a(0, std::vector<double>(2)));
+  ASSERT_NO_THROW(makeDataArray<Variable::Value>(2));
+  const auto a = makeDataArray<Variable::Value>(2);
+  const auto &data = a.get<Variable::Value>();
+  EXPECT_EQ(data.size(), 2);
 }
 
 TEST(DataArray, sharing) {
-  const DataArray a1(0, std::vector<double>(2));
+  const auto a1 = makeDataArray<Variable::Value>(2);
   const auto a2(a1);
-  ASSERT_EQ(&a1.cast<std::vector<double>>(), &a2.cast<std::vector<double>>());
+  EXPECT_EQ(&a1.get<Variable::Value>(), &a2.get<Variable::Value>());
 }
 
 TEST(DataArray, copy) {
-  const DataArray a1(0, std::vector<double>(2));
+  const auto a1 = makeDataArray<Variable::Value>({1.1, 2.2});
+  const auto &data1 = a1.get<Variable::Value>();
+  EXPECT_EQ(data1[0], 1.1);
+  EXPECT_EQ(data1[1], 2.2);
   auto a2(a1);
-  // Calls the non-const cast version, so a2 make a copy.
-  ASSERT_NE(&a1.cast<std::vector<double>>(), &a2.cast<std::vector<double>>());
+  EXPECT_EQ(&a1.get<Variable::Value>(), &a2.get<const Variable::Value>());
+  EXPECT_NE(&a1.get<Variable::Value>(), &a2.get<Variable::Value>());
+  const auto &data2 = a2.get<Variable::Value>();
+  EXPECT_EQ(data2[0], 1.1);
+  EXPECT_EQ(data2[1], 2.2);
 }
