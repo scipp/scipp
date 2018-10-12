2210da842a6449415ea74aa4a1643834732f09f2
Simon Heybrock <simon.heybrock@esss.se>
2018-10-12 12:41:20 +0200

    Thread safe now?

--- test/copy_on_write_with_views_test.cpp
+++ test/copy_on_write_with_views_test.cpp
@@ -5,64 +5,98 @@
 /// National Laboratory, and European Spallation Source ERIC.
 #include <gtest/gtest.h>
 
+#include <omp.h>
 #include <gsl/gsl_util>
 #include <memory>
+#include <mutex>
 #include <vector>
 
 #include "../benchmark/legacy_cow_ptr.h"
 
 template <class T> class BufferManager {
 public:
-  BufferManager(const gsl::index size) : m_data(std::make_unique<T>(size)) {}
+  BufferManager(const gsl::index size)
+      : m_data(std::make_shared<cow_ptr<T>>(std::make_unique<T>(size))) {}
+  BufferManager(const BufferManager &other)
+      : m_data(std::make_shared<cow_ptr<T>>(*other.m_data)) {}
 
   const T &data() const { return *m_data; }
   T &mutableData() { return m_data.access(); }
 
-  cow_ptr<cow_ptr<T>> getForReading() const { return m_data; }
-  cow_ptr<cow_ptr<T>> getForWriting() {
+  std::pair<std::shared_ptr<cow_ptr<T>>, const T &> getForReading() const {
+    // Two things need to be protected by mutex:
+    // 1. The outer pointer may not be copied and reassigned at the same time.
+    // 2. The inner pointer may not be copied and used with `access()` at the
+    // same time.
+
+    // Without this I get bad data in copy!
+    std::lock_guard<std::mutex> lock{m_mutex};
+    return {m_data, **m_data};
+  }
+
+  std::pair<std::shared_ptr<cow_ptr<T>>, T &> getForWriting() {
     // If buffer owner (m_data) is unique this will simply behave like a normal
     // copy-on-write (the inner call to access()). If there are multiple buffer
     // owners it first copies the owner (the outer access()) to ensure that
     // other owners can still read in a safe way.
-    // TODO Need lock here?
-    if (!m_data->unique())
-      m_data.access().access();
-    return m_data;
+    // Note the difference to using cow_ptr<cow_ptr>: Here we copy the outer
+    // based on the ref count of the *inner* pointer!
+    if (!m_data->unique()) {
+      std::lock_guard<std::mutex> lock{m_mutex};
+      if (!m_data->unique()) {
+        //fprintf(stderr, "copy owner\n");
+        std::atomic_store(&m_data, std::make_shared<cow_ptr<T>>(*m_data));
+        //m_data = std::make_shared<cow_ptr<T>>(*m_data);
+      }
+    }
+    std::lock_guard<std::mutex> lock{m_mutex};
+    return {m_data, m_data->access()};
   }
 
 private:
-  cow_ptr<cow_ptr<T>> m_data;
+  std::shared_ptr<cow_ptr<T>> m_data;
+  mutable std::mutex m_mutex;
 };
 
 template <class T> class VariableView {
 public:
   VariableView(const gsl::index size)
-      : m_bufferManager(std::make_unique<BufferManager<T>>(size)) {
-  }
+      : m_bufferManager(std::make_unique<BufferManager<T>>(size)) {}
 
   VariableView(const VariableView &other)
-      : m_bufferManager(std::make_shared<BufferManager<T>>(
-            *other.m_bufferManager)) {}
+      : m_bufferManager(
+            std::make_shared<BufferManager<T>>(*other.m_bufferManager)) {}
 
   VariableView makeView() { return VariableView(m_bufferManager); }
 
   const T &data() const {
-    m_bufferKeepAlive = m_bufferManager->getForReading();
-    return **m_bufferKeepAlive;
+    // Do we need to call this every time or is doing it on construction
+    // sufficient? Main view would never see updates of writes to slices!
+    // How can we make reading from a single VariableView thread safe then? Is
+    // it ok as long as no one writes?
+    auto handle = m_bufferManager->getForReading();
+    m_bufferKeepAlive = handle.first;
+    return handle.second;
   }
-  T &mutableData() { 
-    m_bufferKeepAlive = m_bufferManager->getForWriting();
-    return *m_bufferManager; }
+  T &mutableData() {
+    // Dropping old buffer. This is strictly speaking not necessary but can
+    // avoid unneccessary copies of the buffer owner (not the buffer itself) in
+    // BufferManager.
+    m_bufferKeepAlive = nullptr;
+    auto handle = m_bufferManager->getForWriting();
+    m_bufferKeepAlive = handle.first;
+    return handle.second;
+  }
 
 private:
   VariableView(std::shared_ptr<BufferManager<T>> bufferManager)
       : m_bufferManager(bufferManager) {}
 
   std::shared_ptr<BufferManager<T>> m_bufferManager;
-  cow_ptr<cow_ptr<T>> m_bufferKeepAlive;
+  mutable std::shared_ptr<cow_ptr<T>> m_bufferKeepAlive;
 };
 
-TEST(VariableView, copy_and_view) {
+TEST(VariableView, read_write) {
   VariableView<std::vector<double>> v(4);
 
   // Read
@@ -72,27 +106,116 @@ TEST(VariableView, copy_and_view) {
   // Write
   v.mutableData()[0] = 1.0;
   EXPECT_EQ(v.data()[0], 1.0);
+}
+
+TEST(VariableView, copy) {
+  VariableView<std::vector<double>> v(4);
+  v.mutableData()[0] = 1.0;
 
   // Copy does not see changes
   auto copy(v);
+  EXPECT_EQ(copy.data()[0], 1.0);
   v.mutableData()[0] = 2.0;
   EXPECT_EQ(copy.data()[0], 1.0);
+}
+
+TEST(VariableView, view) {
+  VariableView<std::vector<double>> v(4);
+  v.mutableData()[0] = 1.0;
 
   // View sees changes.
   auto view = v.makeView();
+  EXPECT_EQ(view.data()[0], 1.0);
+  v.mutableData()[0] = 2.0;
   EXPECT_EQ(view.data()[0], 2.0);
-  v.mutableData()[0] = 3.0;
-  EXPECT_EQ(view.data()[0], 3.0);
+}
+
+TEST(VariableView, copy_and_view_write) {
+  VariableView<std::vector<double>> v(4);
+  v.mutableData()[0] = 1.0;
+
+  auto copy(v);
+  auto view = v.makeView();
+
+  v.mutableData()[0] = 2.0;
+
+  EXPECT_EQ(copy.data()[0], 1.0);
+  EXPECT_EQ(view.data()[0], 2.0);
+}
+
+TEST(VariableView, copy_and_view_write_to_copy) {
+  VariableView<std::vector<double>> v(4);
+  v.mutableData()[0] = 1.0;
+
+  auto copy(v);
+  auto view = v.makeView();
+
+  copy.mutableData()[0] = 2.0;
+
+  EXPECT_EQ(view.data()[0], 1.0);
+  EXPECT_EQ(v.data()[0], 1.0);
+}
+
+TEST(VariableView, copy_and_view_write_to_view) {
+  VariableView<std::vector<double>> v(4);
+  v.mutableData()[0] = 1.0;
+
+  auto copy(v);
+  auto view = v.makeView();
+
+  view.mutableData()[0] = 2.0;
+
+  EXPECT_EQ(copy.data()[0], 1.0);
+  EXPECT_EQ(v.data()[0], 2.0);
+}
+
+TEST(VariableView, single_owner_read_then_write) {
+  VariableView<std::vector<double>> v(4);
+  v.mutableData()[0] = 1.0;
+
+  const auto *addr = v.data().data();
+  EXPECT_EQ(v.data()[0], 1.0);
+  v.mutableData()[0] = 2.0;
+
+  EXPECT_EQ(v.data()[0], 2.0);
+  EXPECT_EQ(v.data().data(), addr);
+}
+
+TEST(VariableView, multiple_owners_read_then_write) {
+  VariableView<std::vector<double>> v(4);
+  v.mutableData()[0] = 1.0;
+
+  auto copy(v);
+
+  EXPECT_EQ(v.data()[0], 1.0);
+  v.mutableData()[0] = 2.0;
+
+  EXPECT_EQ(copy.data()[0], 1.0);
+  EXPECT_EQ(v.data()[0], 2.0);
 }
 
 TEST(VariableView, thread_safety) {
-  VariableView<std::vector<double>> v1(4);
-  v1.mutableData() = {1.0, 2.0, 3.0, 4.0};
+  const gsl::index chunks = 12345;
+  const gsl::index chunk_size = 321;
+  const gsl::index size = chunk_size * chunks;
+  for (int repeat = 0; repeat < 5120; ++repeat) {
+    VariableView<std::vector<double>> v(size);
+    auto copy(v);
+#pragma omp parallel for num_threads(49)
+    for (gsl::index chunk = 0; chunk < chunks; ++chunk) {
+      auto view = v.makeView();
+      view.data();
+      auto &data = view.mutableData();
+      for (gsl::index i = 0; i < chunk_size; ++i) {
+        data[chunk * chunk_size + i] = chunk * chunk_size + i;
+      }
+    }
 
-  auto copy(v1);
-
-  auto v2 = v1.makeView();
-  auto v3 = v1.makeView();
-
-  //
+    const auto &data = v.data();
+    for (gsl::index i = 0; i < size; ++i)
+      ASSERT_EQ(data[i], i);
+    const auto &copy_data = copy.data();
+    for (gsl::index i = 0; i < size; ++i)
+      ASSERT_EQ(copy_data[i], 0.0);
+  }
 }
