dc778bb2a0a68ef6f539e08febab649ed9cf94ac
Simon Heybrock <simon.heybrock@esss.se>
2018-10-12 08:40:25 +0200

    Incomplete attempt to make views thread safe.

--- test/copy_on_write_with_views_test.cpp
+++ test/copy_on_write_with_views_test.cpp
@@ -18,34 +18,57 @@ public:
   const T &data() const { return *m_data; }
   T &mutableData() { return m_data.access(); }
 
+  std::shared_ptr<cow_ptr<T>> getForReading() const { return m_data; }
+  std::shared_ptr<cow_ptr<T>> getForWriting() {
+    if(m_data.unique()) {
+      // Call *inner* access to copy buffer.
+      m_data->access();
+    } else {
+      if (m_data->unique()) {
+        // If buffer is unique, do nothing.
+      } else {
+        // More than one view, may not call access() for current buffer so we
+        // first copy the buffer owner.
+        m_data.access().access();
+      }
+    }
+    return m_data;
+  }
+
 private:
-  cow_ptr<T> m_data;
+  std::shared_ptr<cow_ptr<T>> m_data;
 };
 
 template <class T> class VariableView {
 public:
   VariableView(const gsl::index size)
-      : m_bufferManager(std::make_unique<BufferManager<std::vector<T>>>(size)) {
+      : m_bufferManager(std::make_unique<BufferManager<T>>(size)) {
   }
 
   VariableView(const VariableView &other)
-      : m_bufferManager(std::make_shared<BufferManager<std::vector<T>>>(
+      : m_bufferManager(std::make_shared<BufferManager<T>>(
             *other.m_bufferManager)) {}
 
   VariableView makeView() { return VariableView(m_bufferManager); }
 
-  const std::vector<T> &data() const { return m_bufferManager->data(); }
-  std::vector<T> &mutableData() { return m_bufferManager->mutableData(); }
+  const T &data() const {
+    m_bufferKeepAlive = m_bufferManager->getForReading();
+    return **m_bufferKeepAlive;
+  }
+  T &mutableData() { 
+    m_bufferKeepAlive = m_bufferManager->getForWriting();
+    return *m_bufferManager; }
 
 private:
-  VariableView(std::shared_ptr<BufferManager<std::vector<T>>> bufferManager)
+  VariableView(std::shared_ptr<BufferManager<T>> bufferManager)
       : m_bufferManager(bufferManager) {}
 
-  std::shared_ptr<BufferManager<std::vector<T>>> m_bufferManager;
+  std::shared_ptr<BufferManager<T>> m_bufferManager;
+  std::shared_ptr<cow_ptr<T>> m_bufferKeepAlive;
 };
 
 TEST(VariableView, copy_and_view) {
-  VariableView<double> v(4);
+  VariableView<std::vector<double>> v(4);
 
   // Read
   ASSERT_EQ(v.data().size(), 4);
@@ -65,4 +88,16 @@ TEST(VariableView, copy_and_view) {
   EXPECT_EQ(view.data()[0], 2.0);
   v.mutableData()[0] = 3.0;
   EXPECT_EQ(view.data()[0], 3.0);
+}
+
+TEST(VariableView, thread_safety) {
+  VariableView<std::vector<double>> v1(4);
+  v1.mutableData() = {1.0, 2.0, 3.0, 4.0};
+
+  auto copy(v1);
+
+  auto v2 = v1.makeView();
+  auto v3 = v1.makeView();
+
+  //
 }
