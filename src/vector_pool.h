09e86d58eb7bbb948674ad5dd4c8d780ae6284c8
Simon Heybrock <simon.heybrock@esss.se>
2018-10-30 11:05:41 +0100

    VectorPool: Deallocate in background thread.

--- src/vector_pool.h
+++ src/vector_pool.h
@@ -1,21 +1,29 @@
 #ifndef VECTOR_POOL_H
 #define VECTOR_POOL_H
 
+#include <thread>
+
 #include <gsl/gsl_util>
 
 #include "vector.h"
 
 template <class T> class VectorPool {
 public:
+  ~VectorPool() {
+    if (m_backgroundDealloc.joinable())
+      m_backgroundDealloc.join();
+  }
+
   Vector<T> get(const gsl::index size) {
     std::lock_guard<std::mutex> g(m_mutex);
     auto it = std::find_if(
         m_pool.begin(), m_pool.end(),
         [size](const Vector<T> &vec) { return vec.size() == size; });
     if (it != m_pool.end()) {
-      Vector<T> vec(std::move(*it));
-    //fprintf(stderr, "Reusing vector of size %lu from pool. Pool size is now %lu\n", vec.size(), m_pool.size()-1);
-      m_pool.erase(it);
+      Vector<T> vec;
+      std::swap(vec, *it);
+      // fprintf(stderr, "Reusing vector of size %lu from pool. Pool size is now
+      // %lu\n", vec.size(), m_pool.size()-1);
       return vec;
     }
     return Vector<T>(size);
@@ -23,15 +31,27 @@ public:
 
   void put(Vector<T> &&vec) {
     std::lock_guard<std::mutex> g(m_mutex);
-    if(m_pool.size() > 16)
-      m_pool.clear();
-    //fprintf(stderr, "Added vector of size %lu to pool. Pool size is now %lu\n", vec.size(), m_pool.size()+1);
-    m_pool.emplace_back(std::move(vec));
+    // fprintf(stderr, "Added vector of size %lu to pool. Pool size is now
+    // %lu\n", vec.size(), m_pool.size()+1);
+    auto it =
+        std::find_if(m_pool.begin(), m_pool.end(),
+                     [](const Vector<T> &vec) { return vec.size() == 0; });
+    if (it != m_pool.end()) {
+      std::swap(vec, *it);
+    } else {
+      std::swap(vec, m_pool[m_lastEvicted]);
+      if (m_backgroundDealloc.joinable())
+        m_backgroundDealloc.join();
+      m_backgroundDealloc = std::thread([v = std::move(vec)]() mutable {});
+      m_lastEvicted = (m_lastEvicted - 1 + m_pool.size()) % m_pool.size();
+    }
   }
 
 private:
   mutable std::mutex m_mutex;
-  mutable std::vector<Vector<T>> m_pool;
+  mutable std::vector<Vector<T>> m_pool{std::vector<Vector<T>>(16)};
+  gsl::index m_lastEvicted{15};
+  std::thread m_backgroundDealloc;
 };
 
 template <class T> inline VectorPool<T> &vectorPoolInstance() {
