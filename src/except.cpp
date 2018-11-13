d1a7522c6e604dad2304828bdbf2d32b3141839a
Simon Heybrock <simon.heybrock@esss.se>
2018-11-13 14:23:39 +0100

    Add missing source file.

---
+++ src/except.cpp
@@ -0,0 +1,61 @@
+#include "except.h"
+#include "dimensions.h"
+
+namespace dataset {
+
+std::string to_string(const Dim dim) {
+  switch (dim) {
+  case Dim::Invalid:
+    return "<invalid>";
+  case Dim::Event:
+    return "Dim::Event";
+  case Dim::Tof:
+    return "Dim::Tof";
+  case Dim::MonitorTof:
+    return "Dim::MonitorTof";
+  case Dim::Spectrum:
+    return "Dim::Spectrum";
+  case Dim::Monitor:
+    return "Dim::Monitor";
+  case Dim::Run:
+    return "Dim::Run";
+  case Dim::Detector:
+    return "Dim::Detector";
+  case Dim::Q:
+    return "Dim::Q";
+  case Dim::X:
+    return "Dim::X";
+  case Dim::Y:
+    return "Dim::Y";
+  case Dim::Z:
+    return "Dim::Z";
+  case Dim::Polarization:
+    return "Dim::Polarization";
+  case Dim::Temperature:
+    return "Dim::Temperature";
+  case Dim::Time:
+    return "Dim::Time";
+  case Dim::DetectorScan:
+    return "Dim::DetectorScan";
+  case Dim::Component:
+    return "Dim::Component";
+  case Dim::Row:
+    return "Dim::Row";
+  default:
+    return "<unknown dimension>";
+  }
+}
+
+std::string to_string(const Dimensions &dims) {
+  if (dims.empty())
+    return "{}";
+  std::string s = "{{";
+  for (int32_t i = 0; i < dims.ndim(); ++i)
+    s += to_string(dims.labels()[i]) + ", " + std::to_string(dims.shape()[i]) +
+         "}, {";
+  s.resize(s.size() - 3);
+  s += '}';
+  return s;
+}
+
+} // namespace dataset
