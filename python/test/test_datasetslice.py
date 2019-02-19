b07a9c4baca74f3302eccaa23ee278a8c79362a8
Owen Arnold <owen.arnold@stfc.ac.uk>
2019-02-19 15:29:46 +0000

    Update test_datasetslice.py

--- python/test/test_datasetslice.py
+++ python/test/test_datasetslice.py
@@ -16,9 +16,9 @@ class TestDatasetSlice(unittest.TestCase):
         ds_slice = self._d["a"]
         self.assertEqual(type(ds_slice), dataset.DatasetView)
         # We should have just one data variable
-        self.assertEqual(1, len([var for var in ds_slice if var.is_data]), 1)
+        self.assertEqual(1, len([var for var in ds_slice if var.is_data]))
         # We should have just one coord variable
-        self.assertEqual(1, len([var for var in ds_slice if var.is_coord]), 1)
+        self.assertEqual(1, len([var for var in ds_slice if var.is_coord]))
         self.assertEqual(2, len(ds_slice))
 
     def test_repr(self):
