0e4b602e7821c68ae7cc3ff462f6809cefdf43ce
Igor Gudich <igor.gudich@esss.se>
2019-03-04 10:56:56 +0100

    Add test for slicing from back and omitting ranges

--- python/test/test_datasetslice.py
+++ python/test/test_datasetslice.py
@@ -21,6 +21,27 @@ class TestDatasetSlice(unittest.TestCase):
         self.assertEqual(1, len([var for var in ds_slice if var.is_coord]))
         self.assertEqual(2, len(ds_slice))
 
+    def test_slice_back_ommit_range(self):
+        sl = d[ds.Dim.X, 1:-1][ds.Data.Value, "a"].numpy
+        ref = np. array([1, 2, 3, 4, 5, 6, 7, 8], dtype=np.int64)
+        self.assertEqual(ref.shape, sl.shape)
+        self.self.assertEqual(np.allclose(sl.numpy, ref), True)
+        # omitting range end
+        sl = d[ds.Dim.X, 1:][ds.Data.Value, "b"].numpy
+        ref = np. array([1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=np.int64)
+        self.assertEqual(ref.shape, sl.shape)
+        self.self.assertEqual(np.allclose(sl.numpy, ref), True)
+        # omitting range begin
+        sl = d[ds.Dim.X, :-1][ds.Data.Value, "a"].numpy
+        ref = np. array([0, 1, 2, 3, 4, 5, 6, 7, 8], dtype=np.int64)
+        self.assertEqual(ref.shape, sl.shape)
+        self.self.assertEqual(np.allclose(sl.numpy, ref), True)
+        # omitting range both begin and end
+        sl = d[ds.Dim.X, :][ds.Data.Value, "b"].numpy
+        ref = np. array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=np.int64)
+        self.assertEqual(ref.shape, sl.shape)
+        self.self.assertEqual(np.allclose(sl.numpy, ref), True)
+
 
 if __name__ == '__main__':
     unittest.main()
