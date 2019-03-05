1e87cd29f8c99072598ec73da8aa8c6ac9c27fb2
Owen Arnold <owen.arnold@stfc.ac.uk>
2019-03-05 11:14:30 +0000

    Add inplace binary operations to test

--- python/test/test_variableslice.py
+++ python/test/test_variableslice.py
@@ -2,6 +2,7 @@ import unittest
 
 from dataset import *
 import numpy as np
+import operator
 
 
 class TestVariableSlice(unittest.TestCase):
@@ -9,23 +10,27 @@ class TestVariableSlice(unittest.TestCase):
     def setUp(self):
         self._a = Variable(Data.Value,[Dim.X], np.arange(1,10,dtype=float)) 
         self._b = Variable(Data.Value,[Dim.X], np.arange(1,10,dtype=float)) 
-        self._a_slice = self._a[Dim.X, :]
-        self._b_slice = self._b[Dim.X, :]
         
     def test_type(self):
-        data_slice = self._a_slice 
-        self.assertEqual(type(data_slice), dataset.VariableSlice)
+        variable_slice = self._a[Dim.X, :]
+        self.assertEqual(type(variable_slice), dataset.VariableSlice)
 
     def test_variable_type(self):
         coord_var = Variable(Coord.X, [Dim.X], np.arange(10))
         coord_slice = coord_var[Dim.X, :] 
         self.assertTrue(coord_slice.is_coord)
+
+    def _apply_test_op(self, op, a, b, data):
+        op(a,b)
+        # Assume numpy operations are correct as comparitor
+        op(data,b.numpy)
+        self.assertTrue(np.array_equal(a.numpy, data))
     
     def test_binary_operations(self):
-        a = self._a_slice 
-        b = self._b_slice 
+        a = self._a[Dim.X, :]
+        b = self._b[Dim.X, :]
 
-        data = a.numpy
+        data = np.copy(a.numpy)
         c = a + b
         self.assertEqual(type(c), dataset.Variable)
         self.assertTrue(np.array_equal(c.numpy, data+data))
@@ -35,6 +40,15 @@ class TestVariableSlice(unittest.TestCase):
         self.assertTrue(np.array_equal(c.numpy, data*data))
         c = a / b
         self.assertTrue(np.array_equal(c.numpy, data/data))
+       
+        self._apply_test_op(operator.iadd, a, b, data)
+        self._apply_test_op(operator.isub, a, b, data)
+        self._apply_test_op(operator.imul, a, b, data)
+        self._apply_test_op(operator.itruediv, a, b, data)
+        
+        
+
+        #var[Dim.X, 1:3] += var2
     
 
 if __name__ == '__main__':
