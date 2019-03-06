616876785d41a5ec08b21febe310fc0156c654b0
Neil Vaytet <neil.vaytet@esss.se>
2019-03-06 16:33:45 +0100

    - Checking for the number of dimensions is now done on the Value to be plotted, not on the entire dataset - Swapped x and y axes in plot_image - If axes are requested in a 2D image that would essentially flip the image, then the transpose of the Value is plotted - Bin edges are now handled in plot_image

--- python/dataset_plotting.py
+++ python/dataset_plotting.py
@@ -94,7 +94,7 @@ def plot_1d(input_data, logx=False, logy=False, logxy=False, bars=False):
         for v in tobeplotted:
 
             # Check that data is 1D
-            if len(v[0].dimensions.labels) > 1:
+            if len(v[0].dimensions) > 1:
                 raise RuntimeError("Can only plot 1D data with plot_1d.")
 
             # Define y
@@ -115,7 +115,7 @@ def plot_1d(input_data, logx=False, logy=False, logxy=False, bars=False):
             x = coord.numpy
             # Check for bin edges
             if nx == ny + 1:
-                x = centers_to_edges(x)
+                x = edges_to_centers(x)
             xlab = "{} [{}]".format(coord.name,coord.unit)
 
             # Define trace
@@ -153,7 +153,20 @@ def plot_1d(input_data, logx=False, logy=False, logxy=False, bars=False):
 # variable are returned.
 def plot_image(input_data, axes=None, contours=False, plot=True):
 
-    ndim = len(input_data.dimensions())
+    values = []
+    coords = []
+    for var in input_data:
+        if var.is_coord:
+            coords.append(var)
+        elif var.is_data:
+            values.append(var)
+
+    if len(values) > 1:
+        raise RuntimeError("More than one Data.Value found! Please use e.g."
+                           " plot_image(dataset.subset('sample'))"
+                           " to select only a single Value.")
+
+    ndim = len(values[0].dimensions)
     if axes is not None:
         naxes = len(axes)
     else:
@@ -163,43 +176,45 @@ def plot_image(input_data, axes=None, contours=False, plot=True):
     # way to protect against this.
     if (ndim > 1) and (((ndim < 3) or ((ndim < 5) and not plot)) or (naxes == 2)):
 
-        values = []
-        coords = []
-        for var in input_data:
-            if var.is_coord:
-                coords.append(var)
-            elif var.is_data:
-                values.append(var)
-
-        if len(values) > 1:
-            raise RuntimeError("More than one Data.Value found! Please use e.g."
-                               " plot_image(dataset.subset('sample'))"
-                               " to select only a single Value.")
-
         # Note the order of the axes here: the outermost [1] dimension is the
         # fast dimension and is plotted along x, while the inner (slow)
         # dimension [0] is plotted along y.
         if axes is None:
-            xcoord = input_data[dimensionCoord(values[0].dimensions.labels[1])]
-            ycoord = input_data[dimensionCoord(values[0].dimensions.labels[0])]
-        else:
-            x_not_found = True
-            y_not_found = True
-            for c in coords:
-                dimcoord = dimensionCoord(c.dimensions.labels[0])
-                if dimcoord == axes[1]:
-                    xcoord = input_data[dimcoord]
-                    x_not_found = False
-                if dimcoord == axes[0]:
-                    ycoord = input_data[dimcoord]
-                    y_not_found = False
-            if x_not_found:
-                raise RuntimeError("Requested x coordinate {} was not found".format(axes[0]))
-            if y_not_found:
-                raise RuntimeError("Requested y coordinate {} was not found".format(axes[1]))
+            axes = [dimensionCoord(values[0].dimensions.labels[0]),
+                    dimensionCoord(values[0].dimensions.labels[1])]
+
+        xcoord = input_data[axes[1]]
+        ycoord = input_data[axes[0]]
 
         x = xcoord.numpy
         y = ycoord.numpy
+        z = values[0].numpy
+
+        # Check for bin edges
+        # TODO: find a better way to handle edges. Currently, we convert from
+        # edges to centers and then back to edges afterwards inside the plotly
+        # object. This is not optimal and could lead to precision loss issues.
+        zdims = values[0].dimensions
+        nz = zdims.shape
+        ydims = ycoord.dimensions
+        ny = ydims.shape
+        xdims = xcoord.dimensions
+        nx = xdims.shape
+        if nx[0] == nz[1] + 1:
+            x = edges_to_centers(x)
+        if ny[0] == nz[0] + 1:
+            y = edges_to_centers(y)
+
+        # Check if dimensions of arrays agree, if not, try to plot the transpose
+        xshape = np.shape(x)
+        yshape = np.shape(y)
+        zshape = np.shape(z)
+        if (xshape[0] != zshape[1]) or (yshape[0] != zshape[0]):
+            z = z.T
+            zshape = np.shape(z)
+            if (xshape[0] != zshape[1]) or (yshape[0] != zshape[0]):
+                raise RuntimeError("Dimensions of x and y arrays to not match "
+                                   "that of the Value array.")
 
         layout = dict(
             xaxis = dict(title = "{} [{}]".format(xcoord.name, xcoord.unit)),
@@ -214,7 +229,7 @@ def plot_image(input_data, axes=None, contours=False, plot=True):
             data = [dict(
                 x = centers_to_edges(x),
                 y = centers_to_edges(y),
-                z = values[0].numpy,
+                z = z,
                 type = plot_type,
                 colorscale = 'Viridis',
                 colorbar=dict(
@@ -244,7 +259,17 @@ def plot_sliceviewer(input_data):
               "on this system")
         return
 
-    ndim = len(input_data.dimensions())
+    # Check input dataset
+    value_list = []
+    for var in input_data:
+        if var.is_data:
+            value_list.append(var)
+    if len(value_list) > 1:
+        raise RuntimeError("More than one Data.Value found! Please use e.g."
+                           " plot_sliceviewer(dataset.subset('sample'))"
+                           " to select only a single Value.")
+
+    ndim = len(value_list[0].dimensions)
     if (ndim > 2) and (ndim < 5):
 
         # Use the machinery in plot_image to make the slices
@@ -304,6 +329,9 @@ def plot_sliceviewer(input_data):
 
 #===============================================================================
 
-def centers_to_edges(x):
-
+def edges_to_centers(x):
     return 0.5 * (x[1:] + x[:-1])
+
+def centers_to_edges(x):
+    e = edges_to_centers(x)
+    return np.concatenate([[2.0*x[0]-e[0]],e,[2.0*x[-1]-e[-1]]])
