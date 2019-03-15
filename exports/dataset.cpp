699e2026cc468de6991ef999dd3a399aec8af897
Neil Vaytet <neil.vaytet@esss.se>
2019-03-15 14:47:50 +0100

    Added __setitem__ for VariableSlice rows

--- exports/dataset.cpp
+++ exports/dataset.cpp
@@ -676,6 +676,11 @@ PYBIND11_MODULE(dataset, m) {
            py::arg("data"), py::arg("dtype") = py::dtype::of<Empty>())
       .def(py::init<const VariableSlice &>())
       .def("__getitem__", detail::pySlice<Variable>, py::keep_alive<0, 1>())
+      .def("__setitem__",
+           [](Variable &self, const std::tuple<Dim, py::slice> &index,
+              const VariableSlice &other) {
+             detail::pySlice(self, index).assign(other);
+           })
       .def("copy", [](const Variable &self) { return self; })
       .def("__copy__", [](Variable &self) { return Variable(self); })
       .def("__deepcopy__",
@@ -781,6 +786,11 @@ PYBIND11_MODULE(dataset, m) {
              return slice;
            },
            py::keep_alive<0, 1>())
+      .def("__setitem__",
+           [](VariableSlice &self, const std::tuple<Dim, py::slice> &index,
+              const VariableSlice &other) {
+             detail::pySlice(self, index).assign(other);
+           })
       .def("__setitem__", &detail::setVariableSlice)
       .def("__setitem__", &detail::setVariableSliceRange)
       .def("copy", [](const VariableSlice &self) { return Variable(self); })
