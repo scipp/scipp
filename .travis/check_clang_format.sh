88a1e40657d771ba6ee1dc52eeb70c1615b60ef1
Neil Vaytet <neil.vaytet@esss.se>
2019-04-09 11:20:38 +0200

    Fixed check clang format file

--- .travis/check_clang_format.sh
+++ .travis/check_clang_format.sh
@@ -1,16 +1,14 @@
-if [ ! -z ${CLANGFORMAT+x} ] && [ "$CLANGFORMAT" == "ON" ]; then
+#!/bin/sh
 
-    find core scippy test units -type f -name '*.h' -o -name '*.cpp' \
-    | xargs -I{} clang-format-5.0 -i -style=file {};
+find core scippy test units -type f -name '*.h' -o -name '*.cpp' | xargs -I{} clang-format-5.0 -i -style=file {};
 
-    dirty=$(git ls-files --modified);
+dirty=$(git ls-files --modified);
 
-    if [[ $dirty ]]; then
-        echo "Clang format FAILED on the following files:";
-        echo $dirty;
-        exit 1;
-    else
-        echo "Clang format [ OK ]";
-        exit 0;
-    fi
+if [[ $dirty ]]; then
+  echo "Clang format FAILED on the following files:";
+  echo $dirty;
+  exit 1;
+else
+  echo "Clang format [ OK ]";
+  exit 0;
 fi
