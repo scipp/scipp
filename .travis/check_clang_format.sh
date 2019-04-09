555a50d0c2557256a723816ef19626b8c516b429
Neil Vaytet <neil.vaytet@esss.se>
2019-04-09 11:51:27 +0200

    fixed clangformat check script

--- .travis/check_clang_format.sh
+++ .travis/check_clang_format.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-find core scippy test units -type f -name '*.h' -o -name '*.cpp' | xargs -I{} clang-format-5.0 -i -style=file {};
+# Check that the source conforms to the clang-format style
 
+# Find clang-format executable
+CLANG_FORMAT_EXE=clang-format-5.0
+if [ -z "$(which $CLANG_FORMAT_EXE)" ]; then
+  echo "Cannot find ${CLANG_FORMAT_EXE} executable"
+  exit 1
+else
+  ${CLANG_FORMAT_EXE} --version
+fi
+
+# Perform formatting
+find core scippy test units -type f -name '*.h' -o -name '*.cpp' | xargs -I{} ${CLANG_FORMAT_EXE} -i -style=file {};
 dirty=$(git ls-files --modified);
 
-if [[ $dirty ]]; then
+if [ -z "$dirty" ]; then
+    echo "Clang format [ OK ]";
+    exit 0;
+else
   echo "Clang format FAILED on the following files:";
   echo $dirty;
   exit 1;
-else
-  echo "Clang format [ OK ]";
-  exit 0;
 fi
