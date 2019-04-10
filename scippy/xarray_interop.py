1ff8e62ca160eceac27e3d2f5de57187a8e11d6f
Neil Vaytet <neil.vaytet@esss.se>
2019-04-10 13:34:12 +0200

    formatted some unused python files

--- scippy/xarray_interop.py
+++ scippy/xarray_interop.py
@@ -1,4 +1,5 @@
-from dataset import *
+import holoplot
+import scippy as sp
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
@@ -6,42 +7,44 @@ import matplotlib.pyplot as plt
 lx = 200
 ly = 300
 lz = 400
-d = Dataset()
-dims = Dimensions()
-dims.add(Dim.X, lx)
-dims.add(Dim.Y, ly)
-dims.add(Dim.Z, lz)
+d = sp.Dataset()
+dims = sp.Dimensions()
+dims.add(sp.Dim.X, lx)
+dims.add(sp.Dim.Y, ly)
+dims.add(sp.Dim.Z, lz)
 
-dimsX = Dimensions()
-dimsY = Dimensions()
-dimsZ = Dimensions()
-dimsX.add(Dim.X, lx)
-dimsY.add(Dim.Y, 2*ly)
-dimsZ.add(Dim.Z, lz)
+dimsX = sp.Dimensions()
+dimsY = sp.Dimensions()
+dimsZ = sp.Dimensions()
+dimsX.add(sp.Dim.X, lx)
+dimsY.add(sp.Dim.Y, 2 * ly)
+dimsZ.add(sp.Dim.Z, lz)
 
-d.insert(Coord.X, dimsX, np.arange(lx))
-d.insert(Coord.Z, dimsZ, np.arange(lz))
-d.insert(Data.Value, "name", dims, np.random.random(lx*ly*lz))
+d.insert(sp.Coord.X, dimsX, np.arange(lx))
+d.insert(sp.Coord.Z, dimsZ, np.arange(lz))
+d.insert(sp.Data.Value, "name", dims, np.random.random(lx * ly * lz))
 
-d = concatenate(Dim.Y, d, d)
-d.insert(Coord.Y, dimsY, np.arange(2*ly))
+d = sp.concatenate(sp.Dim.Y, d, d)
+d.insert(sp.Coord.Y, dimsY, np.arange(2 * ly))
+
 
 def Dataset_iter(self):
-    yield self[Coord.X]
-    yield self[Coord.Y]
-    yield self[Coord.Z]
-    yield self[Data.Value, 'name']
+    yield self[sp.Coord.X]
+    yield self[sp.Coord.Y]
+    yield self[sp.Coord.Z]
+    yield self[sp.Data.Value, 'name']
 
-setattr(Dataset, "__iter__", Dataset_iter)
 
-dim_name = {Dim.X:'X', Dim.Y:'Y', Dim.Z:'Z'}
+setattr(sp.Dataset, "__iter__", sp.Dataset_iter)
+
+dim_name = {sp.Dim.X: 'X', sp.Dim.Y: 'Y', sp.Dim.Z: 'Z'}
+
 
 def as_xarray(dataset):
     ds = xr.Dataset()
-    data = dict()
-    coords = dict()
+
     def xarray_name(var):
-        names = {0:'X', 1:'Y', 2:'Z', 34:'Value'}
+        names = {0: 'X', 1: 'Y', 2: 'Z', 34: 'Value'}
         if var.is_coord:
             return names[var.type]
         else:
@@ -49,9 +52,11 @@ def as_xarray(dataset):
     for var in dataset:
         print(var.dimensions.labels)
         if var.is_coord:
-            ds.coords[xarray_name(var)] = ([dim_name[dim] for dim in var.dimensions.labels], var.numpy)
+            ds.coords[xarray_name(var)] = \
+                ([dim_name[dim] for dim in var.dimensions.labels], var.numpy)
         else:
-            ds[xarray_name(var)] = ([dim_name[dim] for dim in var.dimensions.labels], var.numpy)
+            ds[xarray_name(var)] = \
+                ([dim_name[dim] for dim in var.dimensions.labels], var.numpy)
     return ds
 
 
@@ -59,13 +64,11 @@ ds = as_xarray(d)
 ds *= 1.5
 
 print(ds['Value:name'][10])
-ds['Value:name'][10, 5:10,5:10].plot()
+ds['Value:name'][10, 5:10, 5:10].plot()
 plt.savefig('test.png')
 
 # pip3 install hvplot holoplot
-import hvplot.xarray
-import holoplot
-sliceview = ds['Value:name'].hvplot.image(groupby='Z',height=1000,width=1000)
+sliceview = ds['Value:name'].hvplot.image(groupby='Z', height=1000, width=1000)
 holoplot.show(sliceview)
 
 # Key differences to xarray:
