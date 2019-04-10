2d87481bb6c6ce1ff12467e3e3b2f0fd3cca5e51
Neil Vaytet <neil.vaytet@esss.se>
2019-04-10 15:25:27 +0200

    plot imports

--- scippy/__init__.py
+++ scippy/__init__.py
@@ -5,9 +5,10 @@
 # National Laboratory, and European Spallation Source ERIC.
 from .scippy import *
 from .table import table
-try:
-    from .plot import plot
-except ImportError:
-    print("Warning: the plotting module for Scippy was not imported. Check "
-          "that plotly is installed on your system. You can still use Scippy "
-          "without its plotting functionality enabled.")
+from .plot import plot
+# try:
+#     from .plot import plot
+# except ImportError:
+#     print("Warning: the plotting module for Scippy was not imported. Check "
+#           "that plotly is installed on your system. You can still use Scippy "
+#           "without its plotting functionality enabled.")
