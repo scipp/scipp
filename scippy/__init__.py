3469e2f2a0fd587ba766232500933182b0808076
Neil Vaytet <neil.vaytet@esss.se>
2019-04-10 11:40:18 +0200

    formatted test_variable.py

--- scippy/__init__.py
+++ scippy/__init__.py
@@ -8,6 +8,6 @@ from .table import table
 try:
     from .plot import plot
 except ImportError:
-    print("Warning: the plotting module for Scippy was not imported. Check that"
-          "plotly is installed on your system. You can still use Scippy "
+    print("Warning: the plotting module for Scippy was not imported. Check "
+          "that plotly is installed on your system. You can still use Scippy "
           "without its plotting functionality enabled.")
