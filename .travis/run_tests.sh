087d4feda7319f87ca733418965c18d54cbec3b0
= <owen.arnold@stfc.ac.uk>
2019-04-12 15:54:08 +0100

    simplify test execution

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -7,14 +7,6 @@ cd build
 cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DPYTHON_EXECUTABLE=/usr/bin/python3 -DCMAKE_INSTALL_PREFIX=../install ..
 make -j2 install
 
-# Units tests
-./units/test/scipp-units-test
-
-# Core tests
-./core/test/scipp-core-test
-
-# Python tests
 python3 -m pip install -r ../scippy/requirements.txt
 export PYTHONPATH=$PYTHONPATH:../install
-cd ../scippy
-python3 -m unittest discover test
+ctest -VV
