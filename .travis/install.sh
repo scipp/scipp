5cb8886573c0399eeef151d23aae72ee499e14f8
Igor Gudich <igor.gudich@esss.se>
2019-04-24 11:13:48 +0200

    Check if the old one is working

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # C++17
+sudo apt-get install -qq g++-7
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
 sudo add-apt-repository -y ppa:mhier/libboost-latest
 sudo apt update
 sudo apt install libboost1.67-dev
