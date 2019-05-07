9a5709b4945cc676516a469e5cdfe1e7744e87b5
Simon Heybrock <simon.heybrock@esss.se>
2019-05-07 13:44:35 +0200

    Remove tags.h and other unused files.

--- core/tag_util.h
+++ core/tag_util.h
@@ -5,8 +5,6 @@
 #ifndef TAG_UTIL_H
 #define TAG_UTIL_H
 
-#include "tags.h"
-
 namespace scipp::core {
 
 /**
@@ -23,41 +21,6 @@ template <typename... T> constexpr auto make_array(T &&... values) {
       std::forward<T>(values)...};
 }
 
-template <template <class> class Callable, class... Tags, class... Args>
-static auto call(const std::tuple<Tags...> &, const Tag tag, Args &&... args) {
-  auto funcs = make_array(Callable<Tags>::apply...);
-  std::array<Tag, sizeof...(Tags)> tags{Tags{}...};
-  for (size_t i = 0; i < tags.size(); ++i)
-    if (tags[i].value() == tag.value())
-      return funcs[i](std::forward<Args>(args)...);
-  throw std::runtime_error("Unsupported tag type.");
-}
-
-/// Apply Callable<T> to args, for any tag T in Tags, determined by the runtime
-/// tag given by `tag`.
-template <class... Tags> struct Call {
-  template <template <class> class Callable, class... Args>
-  static auto apply(const Tag tag, Args &&... args) {
-    return call<Callable>(std::tuple<Tags...>{}, tag,
-                          std::forward<Args>(args)...);
-  }
-};
-
-namespace detail {
-template <class... Tags> constexpr auto makeTags(const std::tuple<Tags...> &) {
-  return std::make_tuple(detail::TagImpl<Tags>{}...);
-}
-} // namespace detail
-
-/// Apply Callable<T> to args, for arbitrary tag T, determined by the runtime
-/// tag given by `tag`.
-template <template <class> class Callable, class... Args>
-auto callForAnyTag(const Tag tag, Args &&... args) {
-  return call<Callable>(detail::makeTags(detail::Tags{}), tag,
-                        std::forward<Args>(args)...);
-}
-
-// Same as `call` for tags, but based on dtype.
 template <template <class> class Callable, class... Ts, class... Args>
 static auto callDType(const std::tuple<Ts...> &, const DType dtype,
                       Args &&... args) {
