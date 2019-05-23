d49c82e80f6d7c57155c9ef15d1b6bbe47c895a6
Simon Heybrock <simon.heybrock@esss.se>
2019-05-23 11:39:46 +0200

    Some doxygen.

--- units/include/scipp/units/dimension.h
+++ units/include/scipp/units/dimension.h
@@ -9,6 +9,11 @@
 
 namespace scipp::units {
 
+/// Macro to declare available dimension labels in a particular namespace.
+///
+/// The arguments passed to the macro are converted into enum labels. `Invalid`
+/// is a reserved name and is automatically added to the list. This also defines
+/// a `to_string` function for the defined enum.
 #define SCIPP_UNITS_DECLARE_DIMENSIONS(...)                                    \
   enum class Dim : uint16_t { __VA_ARGS__, Invalid };                          \
                                                                                \
@@ -19,6 +24,22 @@ namespace scipp::units {
                                                                                \
   std::string to_string(const Dim dim);
 
+/// Macro to define dimension labels.
+///
+/// To be used in combination with SCIPP_UNITS_DECLARE_DIMENSIONS. To be placed
+/// in a cpp file. The only argument is the namespace name that the declare
+/// macro was placed in.
+///
+/// Internally the main part of this macro is doing some compile-time
+/// manipulation to auto-generate string labels for each dimension enum value.
+/// This works as follows:
+/// 1. A string with the comma-separated list of all enum values (excluding the
+///    Dim:: prefix) is defined in the header by the define macro.
+/// 2. The constexpr method std::string_view::find gives the start and end
+///    position we the Nth label, we is stored in a std::string_view.
+/// 3. At runtime the numeric enum value is used to lookup the correct string
+///    view, which points to the correct subsection of the full string listing
+///    all labels.
 #define SCIPP_UNITS_DEFINE_DIMENSIONS(MODULE)                                  \
   namespace MODULE {                                                           \
   namespace {                                                                  \
@@ -37,6 +58,7 @@ namespace scipp::units {
     const auto count = get_pos(index + 1) - pos - 2;                           \
     return names.substr(pos, count);                                           \
   }                                                                            \
+                                                                               \
   template <size_t... Is>                                                      \
   constexpr auto make_dim_names(std::index_sequence<Is...>) {                  \
     return std::array{make_dim_name(Is)...};                                   \
