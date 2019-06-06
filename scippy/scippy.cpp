ddfe0562021e2dbc7539eb62e964d7684ec3cbd3
Simon Heybrock <simon.heybrock@esss.se>
2019-06-06 15:30:47 +0200

    First steps for scippy.Dataset.

--- scippy/scippy.cpp
+++ scippy/scippy.cpp
@@ -6,12 +6,14 @@
 
 namespace py = pybind11;
 
+void init_dataset(py::module &);
 void init_dimensions(py::module &);
 void init_dtype(py::module &);
 void init_units_neutron(py::module &);
 void init_variable(py::module &);
 
 PYBIND11_MODULE(scippy, m) {
+  init_dataset(m);
   init_dimensions(m);
   init_dtype(m);
   init_units_neutron(m);
