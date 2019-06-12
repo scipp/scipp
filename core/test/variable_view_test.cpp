a8400262d9b07e47990a86aa0e3121041aa6c023
Simon Heybrock <simon.heybrock@esss.se>
2019-06-12 07:16:20 +0200

    Systematic tests for VariableView.

--- core/test/variable_view_test.cpp
+++ core/test/variable_view_test.cpp
@@ -3,6 +3,9 @@
 #include <gtest/gtest.h>
 
 #include <numeric>
+#include <vector>
+
+#include "test_macros.h"
 
 #include "variable_view.h"
 
@@ -101,4 +104,129 @@ TEST(VariableView, subview) {
   EXPECT_EQ(*it++, 2.0);
   EXPECT_EQ(*it++, 4.0);
   EXPECT_EQ(*it++, 4.0);
+}
+
+auto range(const scipp::index end) {
+  std::vector<int32_t> data(end);
+  std::iota(data.begin(), data.end(), 0);
+  return data;
+}
+
+TEST(VariableViewTest, broadcast_inner) {
+  Dimensions dims{Dim::X, 2};
+  Dimensions target({Dim::X, Dim::Y}, {2, 3});
+  EXPECT_TRUE(equals(VariableView(range(2).data(), 0, target, dims),
+                     {0, 0, 0, 1, 1, 1}));
+}
+
+TEST(VariableViewTest, broadcast_outer) {
+  Dimensions dims{Dim::X, 2};
+  Dimensions target({Dim::Y, Dim::X}, {3, 2});
+  EXPECT_TRUE(equals(VariableView(range(2).data(), 0, target, dims),
+                     {0, 1, 0, 1, 0, 1}));
+}
+
+TEST(VariableViewTest, broadcast_interior) {
+  Dimensions dims{{Dim::X, Dim::Z}, {2, 2}};
+  Dimensions target({Dim::X, Dim::Y, Dim::Z}, {2, 3, 2});
+  EXPECT_TRUE(equals(VariableView(range(4).data(), 0, target, dims),
+                     {0, 1, 0, 1, 0, 1, 2, 3, 2, 3, 2, 3}));
+}
+
+TEST(VariableViewTest, broadcast_inner_and_outer) {
+  Dimensions dims{Dim::Y, 2};
+  Dimensions target({Dim::X, Dim::Y, Dim::Z}, {2, 2, 3});
+  EXPECT_TRUE(equals(VariableView(range(2).data(), 0, target, dims),
+                     {0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1}));
+}
+
+TEST(VariableViewTest, transpose_2d) {
+  Dimensions dims{{Dim::X, Dim::Y}, {2, 3}};
+  Dimensions target({Dim::Y, Dim::X}, {3, 2});
+  EXPECT_TRUE(equals(VariableView(range(6).data(), 0, target, dims),
+                     {0, 3, 1, 4, 2, 5}));
+}
+
+TEST(VariableViewTest, transpose_3d_yx) {
+  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  Dimensions target({Dim::Y, Dim::X, Dim::Z}, {3, 2, 4});
+  EXPECT_TRUE(equals(VariableView(range(24).data(), 0, target, dims),
+                     {0,  1,  2,  3,  12, 13, 14, 15, 4,  5,  6,  7,
+                      16, 17, 18, 19, 8,  9,  10, 11, 20, 21, 22, 23}));
+}
+
+TEST(VariableViewTest, transpose_3d_zy) {
+  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  Dimensions target({Dim::X, Dim::Z, Dim::Y}, {2, 4, 3});
+  EXPECT_TRUE(equals(VariableView(range(24).data(), 0, target, dims),
+                     {0,  4,  8,  1,  5,  9,  2,  6,  10, 3,  7,  11,
+                      12, 16, 20, 13, 17, 21, 14, 18, 22, 15, 19, 23}));
+}
+
+TEST(VariableViewTest, transpose_3d_zx) {
+  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  Dimensions target({Dim::Z, Dim::Y, Dim::X}, {4, 3, 2});
+  EXPECT_TRUE(equals(VariableView(range(24).data(), 0, target, dims),
+                     {0, 12, 4, 16, 8,  20, 1, 13, 5, 17, 9,  21,
+                      2, 14, 6, 18, 10, 22, 3, 15, 7, 19, 11, 23}));
+}
+
+TEST(VariableViewTest, transpose_3d_zxy) {
+  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  Dimensions target({Dim::Z, Dim::X, Dim::Y}, {4, 2, 3});
+  EXPECT_TRUE(equals(VariableView(range(24).data(), 0, target, dims),
+                     {0, 4, 8,  12, 16, 20, 1, 5, 9,  13, 17, 21,
+                      2, 6, 10, 14, 18, 22, 3, 7, 11, 15, 19, 23}));
+}
+
+TEST(VariableViewTest, collapse_inner) {
+  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  Dimensions target({Dim::X, Dim::Y}, {2, 3});
+  EXPECT_TRUE(equals(VariableView(range(24).data(), 0, target, dims),
+                     {0, 4, 8, 12, 16, 20}));
+  // This is a typical use for the offset parameter.
+  EXPECT_TRUE(equals(VariableView(range(24).data(), 3, target, dims),
+                     {3, 7, 11, 15, 19, 23}));
+}
+
+TEST(VariableViewTest, collapse_interior) {
+  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  Dimensions target({Dim::X, Dim::Z}, {2, 4});
+  EXPECT_TRUE(equals(VariableView(range(24).data(), 0, target, dims),
+                     {0, 1, 2, 3, 12, 13, 14, 15}));
+  EXPECT_TRUE(equals(VariableView(range(24).data(), 4, target, dims),
+                     {4, 5, 6, 7, 16, 17, 18, 19}));
+}
+
+TEST(VariableViewTest, collapse_outer) {
+  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  Dimensions target({Dim::Y, Dim::Z}, {3, 4});
+  EXPECT_TRUE(equals(VariableView(range(24).data(), 0, target, dims),
+                     {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}));
+}
+
+TEST(VariableViewTest, collapse_inner_and_outer) {
+  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  Dimensions target({Dim::Y}, {3});
+  EXPECT_TRUE(
+      equals(VariableView(range(24).data(), 0, target, dims), {0, 4, 8}));
+}
+
+TEST(VariableViewTest, collapse_inner_two) {
+  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  Dimensions target({Dim::X}, {2});
+  EXPECT_TRUE(equals(VariableView(range(24).data(), 0, target, dims), {0, 12}));
+}
+
+TEST(VariableViewTest, collapse_outer_two) {
+  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  Dimensions target({Dim::Z}, {4});
+  EXPECT_TRUE(
+      equals(VariableView(range(24).data(), 0, target, dims), {0, 1, 2, 3}));
+}
+
+TEST(VariableViewTest, collapse_all) {
+  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  Dimensions target;
+  EXPECT_TRUE(equals(VariableView(range(24).data(), 0, target, dims), {0}));
 }
