5c9b2376710821e8e5370ec4b42c72fa359f13ab
Neil Vaytet <neil.vaytet@esss.se>
2019-06-25 15:21:54 +0200

    collapse now working for any number of dimensions

--- scippy/src/scippy/plot.py
+++ scippy/src/scippy/plot.py
@@ -205,7 +205,7 @@ def plot_1d(input_data, logx=False, logy=False, logxy=False, axes=None,
 
 def plot_collapse(input_data, dim=None, name=None, filename=None, **kwargs):
     """
-    Collapse higher dimensions into a 1D plot
+    Collapse higher dimensions into a 1D plot.
     """
 
     dims = input_data.dims
@@ -214,9 +214,11 @@ def plot_collapse(input_data, dim=None, name=None, filename=None, **kwargs):
 
     # Gather list of dimensions that are to be collapsed
     slice_dims = []
+    volume = 1
     for l in labs:
         if l != dim:
             slice_dims.append(l)
+            volume *= dims[l]
 
     # Create temporary Dataset
     ds = sp.Dataset()
@@ -224,18 +226,54 @@ def plot_collapse(input_data, dim=None, name=None, filename=None, **kwargs):
     # A dictionary to hold the DataProxy objects
     data = dict()
 
-    # Go through the dims that need to be collapsed
+    # Go through the dims that need to be collapsed, and create an array that
+    # holds the range of indices for each dimension
+    # Say we have [Dim.Y, 5], and [Dim.Z, 3], then dim_list will contain
+    # [[0, 1, 2, 3, 4], [0, 1, 2]]
+    dim_list = []
     for l in slice_dims:
-        for i in range(dims[l]):
-            key = "{}-{}".format(str(l), i)
-            ds[key] = sp.Variable([dim], values=input_data[l, i].values)
-            data[key] = ds[key]
+         dim_list.append(np.arange(dims[l], dtype=np.int32))
+    # Next create a grid of indices
+    # grid will contain
+    # [ [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]],
+    #   [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2]] ]
+    grid = np.meshgrid(*[x for x in dim_list])
+    # Reshape the grid to have a 2D array of length volume, i.e.
+    # [ [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4],
+    #   [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2] ]
+    res = np.reshape(grid, (len(slice_dims), volume))
+    # Now make a master array which also includes the dimension labels, i.e.
+    # [ [Dim.Y, Dim.Y, Dim.Y, Dim.Y, Dim.Y, Dim.Y, Dim.Y, Dim.Y, Dim.Y, Dim.Y, Dim.Y, Dim.Y, Dim.Y, Dim.Y, Dim.Y],
+    #   [    0,     1,     2,     3,     4,     0,     1,     2,     3,     4,     0,     1,     2,     3,     4],
+    #   [Dim.Z, Dim.Z, Dim.Z, Dim.Z, Dim.Z, Dim.Z, Dim.Z, Dim.Z, Dim.Z, Dim.Z, Dim.Z, Dim.Z, Dim.Z, Dim.Z, Dim.Z],
+    #   [    0,     0,     0,     0,     0,     1,     1,     1,     1,     1,     2,     2,     2,     2,     2] ]
+    slice_list = []
+    for i, l in enumerate(slice_dims):
+        slice_list.append([l] * volume)
+        slice_list.append(res[i])
+    # Finally reshape the master array to look like
+    # [ [[Dim.Y, 0], [Dim.Z, 0]], [[Dim.Y, 1], [Dim.Z, 0]], [[Dim.Y, 2], [Dim.Z, 0]],
+    #   [[Dim.Y, 3], [Dim.Z, 0]], [[Dim.Y, 4], [Dim.Z, 0]], [[Dim.Y, 0], [Dim.Z, 1]],
+    #   [[Dim.Y, 1], [Dim.Z, 1]], [[Dim.Y, 2], [Dim.Z, 1]], [[Dim.Y, 3], [Dim.Z, 1]],
+    # ...
+    # ]
+    slice_list = np.reshape(np.transpose(slice_list), (volume, len(slice_dims), 2))
+
+    # Extract each entry from the slice_list, make temperary dataset and add to
+    # input dictionary for plot_1d
+    for line in slice_list:
+        ds_temp = input_data
+        key = ""
+        for s in line:
+            ds_temp = ds_temp[s[0], s[1]]
+            key += "{}-{}-".format(str(s[0]), s[1])
+        ds[key] = sp.Variable([dim], values=ds_temp.values)
+        data[key] = ds[key]
 
     # Send the newly created dictionary of DataProxy to the plot_1d function
     plot_1d(data, **kwargs)
 
     return
-
 
 # =============================================================================
 
