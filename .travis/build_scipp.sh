6e561cda4a0e9d8e16285cc80dd1d2c9d946051d
Neil Vaytet <neil.vaytet@esss.se>
2019-06-26 13:39:07 +0200

    check where directories are located

--- .travis/build_scipp.sh
+++ .travis/build_scipp.sh
@@ -6,3 +6,5 @@ mkdir -p install
 cd build
 cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DPYTHON_EXECUTABLE=/usr/bin/python3 -DCMAKE_INSTALL_PREFIX=../install ..
 make -j2 install
+
+echo $(pwd)
--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -6,6 +6,7 @@
 # cd build
 # cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DPYTHON_EXECUTABLE=/usr/bin/python3 -DCMAKE_INSTALL_PREFIX=../install ..
 # make -j2 install
+echo $(pwd)
 
 # Units tests
 ./units/test/scipp-units-test
