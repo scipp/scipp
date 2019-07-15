68abf82e5262df940411bda28b915afc38ae59a8
Dan Nixon <dan@dan-nixon.com>
2019-07-15 09:41:54 +0100

    Add some Conda configuration
    See https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/use-conda-with-travis-ci.html#the-travis-yml-file

--- .travis/install_conda.sh
+++ .travis/install_conda.sh
@@ -6,5 +6,9 @@ wget 'https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh' -O 
 # Install Miniconda
 bash 'miniconda.sh' -b -p "$HOME/miniconda"
 
+conda config \
+  --set always_yes yes \
+  --set changeps1 no
+
 # Install build and deploy dependencies
-conda install --yes conda-build anaconda
+conda install conda-build anaconda
