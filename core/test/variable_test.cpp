55587f51d68542a78bff9c3eba6509f9a2be6fe9
Owen Arnold <owen.arnold@stfc.ac.uk>
2019-07-16 10:38:47 +0100

    Use custom type
    Custom type meets minimum duck-typing requirements

--- core/test/variable_test.cpp
+++ core/test/variable_test.cpp
@@ -91,21 +91,27 @@ TEST(Variable, span_references_Variable) {
   EXPECT_EQ(observer[0], 1.0);
 }
 
-using OnDemandType = int16_t; // Not instanitated in cpp.
+struct CustomType {
+public:
+  CustomType() {}
+  CustomType(const int &) {}
+  bool operator==(const CustomType &) const { return true; }
+};
+
 // Instantiate Variable type. Test template instantiation with custom type
 // argument.
-INSTANTIATE_VARIABLE(OnDemandType)
+INSTANTIATE_VARIABLE(CustomType)
 // Instantiate VariableConstProxy type. Test template instantiation with
 // custom type argument.
-INSTANTIATE_SLICEVIEW(OnDemandType)
+INSTANTIATE_SLICEVIEW(CustomType)
 
 TEST(Variable, use_custom_templates) {
-  auto input_values = std::initializer_list<OnDemandType>{1, 2};
+  auto input_values = std::initializer_list<CustomType>{1, 2};
   auto var = Variable{{Dim::X, 2}, input_values};
   // Check for bad cast or other built-in implicit type assumptions
-  EXPECT_NO_THROW(var.values<OnDemandType>());
+  EXPECT_NO_THROW(var.values<CustomType>());
   VariableConstProxy slice = var.slice(Slice(Dim::X, 0));
-  EXPECT_NO_THROW(slice.values<OnDemandType>());
+  EXPECT_NO_THROW(slice.values<CustomType>());
 }
 
 class Variable_comparison_operators : public ::testing::Test {
