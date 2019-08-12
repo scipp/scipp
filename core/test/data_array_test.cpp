a1e9e905fa9efa33ecc323836aa55f5ab332be65
Simon Heybrock <simon.heybrock@esss.se>
2019-08-12 06:38:43 +0200

    Add DataArray unit test.

--- core/test/data_array_test.cpp
+++ core/test/data_array_test.cpp
@@ -17,3 +17,19 @@ TEST(DataArrayTest, construct) {
   DataArray array(dataset["data_xyz"]);
   EXPECT_EQ(array, dataset["data_xyz"]);
 }
+
+TEST(DataArrayTest, sum_dataset_columns_via_DataArray) {
+  DatasetFactory3D factory;
+  auto dataset = factory.make();
+
+  DataArray array(dataset["data_zyx"]);
+  auto sum = array + dataset["data_xyz"];
+
+  dataset["data_zyx"] += dataset["data_xyz"];
+
+  // Direction compare fails since binary operation do not propagate attributes.
+  EXPECT_NE(sum, dataset["data_zyx"]);
+
+  dataset.setData("sum", sum);
+  EXPECT_EQ(dataset["sum"], dataset["data_zyx"]);
+}
