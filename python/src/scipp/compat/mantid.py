7f2013cfde73c236046db5c4e135acf7c3d31a2c
Neil Vaytet <neil.vaytet@esss.se>
2019-08-19 11:59:57 +0200

    converter now seems to work

--- python/src/scipp/compat/mantid.py
+++ python/src/scipp/compat/mantid.py
@@ -1,4 +1,4 @@
-import scipp as sp
+import scipp as sc
 
 
 def get_pos(pos):
@@ -6,77 +6,85 @@ def get_pos(pos):
 
 
 def convert_instrument(dataset, ws):
-    compInfo = sp.Dataset()
-    compInfo[sp.Coord.Position] = ([sp.Dim.Component], (2,))
+    compInfo = sc.Dataset(
+        {'position': sc.Variable(dims=[sc.Dim.Row], shape=(2,),
+                                 dtype=sc.dtype.vector_3_double,
+                                 unit=sc.units.m)})
     # Current assumption: 0 is source, 1 is sample
     sourcePos = ws.componentInfo().sourcePosition()
     samplePos = ws.componentInfo().samplePosition()
-    compInfo[sp.Coord.Position].data[0] = get_pos(sourcePos)
-    compInfo[sp.Coord.Position].data[1] = get_pos(samplePos)
-    dataset[sp.Coord.ComponentInfo] = ([], compInfo)
+    compInfo['position'].values[0] = get_pos(sourcePos)
+    compInfo['position'].values[1] = get_pos(samplePos)
+    dataset.labels['component_info'] = sc.Variable(dtype=sc.dtype.Dataset)
+    dataset.labels['component_info'].value = compInfo
 
 
 def initPosSpectrumNo(d, nHist, ws):
     try:
-        d[sp.Coord.Position] = ([sp.Dim.Position], [get_pos(
-            ws.spectrumInfo().position(j)) for j in range(nHist)])
+        d.coords[sc.Dim.Position] = sc.Variable(
+            [sc.Dim.Position], values=[
+                get_pos(ws.spectrumInfo().position(j)) for j in range(nHist)])
     except RuntimeError:
         pass
-    d[sp.Coord.SpectrumNumber] = (
-        [sp.Dim.Position],
-        [ws.getSpectrum(j).getSpectrumNo() for j in range(nHist)])
+    d.labels["SpectrumNumber"] = sc.Variable(
+        [sc.Dim.Position],
+        values=[ws.getSpectrum(j).getSpectrumNo() for j in range(nHist)])
 
 
 def ConvertWorkspace2DToDataset(ws, name):
-    d = sp.Dataset()
+    d = sc.Dataset()
     cb = ws.isCommonBins()
     nHist = ws.getNumberHistograms()
     convert_instrument(d, ws)
     initPosSpectrumNo(d, nHist, ws)
 
     # TODO More cases?
-    tag = sp.Coord.DeltaE if ws.getAxis(
-        0).getUnit().unitID() == 'DeltaE' else sp.Coord.Tof
-    dim = sp.Dim.DeltaE if ws.getAxis(
-        0).getUnit().unitID() == 'DeltaE' else sp.Dim.Tof
+    # tag = sc.Dim.EnergyTransfer if ws.getAxis(
+    #     0).getUnit().unitID() == 'DeltaE' else sc.Dim.Tof
+    dim = sc.Dim.EnergyTransfer if ws.getAxis(
+        0).getUnit().unitID() == 'DeltaE' else sc.Dim.Tof
     if cb:
-        d[tag] = ([dim], ws.readX(0))
+        d.coords[dim] = sc.Variable([dim], values=ws.readX(0))
     else:
-        d[tag] = ([sp.Dim.Position, dim],
-                  (ws.getNumberHistograms(), len(ws.readX(0))))
+        d.coords[dim] = sc.Variable([sc.Dim.Position, dim],
+                                    shape=(ws.getNumberHistograms(),
+                                           len(ws.readX(0))))
         for i in range(ws.getNumberHistograms()):
-            d[tag][sp.Dim.Position, i] = ws.readX(i)
-
-    d[sp.Data.Value, name] = ([sp.Dim.Position, dim],
-                              (ws.getNumberHistograms(), len(ws.readY(0))))
-    d[sp.Data.Variance, name] = (
-        [sp.Dim.Position, dim], (ws.getNumberHistograms(), len(ws.readE(0))))
+            d.coords[dim][sc.Dim.Position, i].values = ws.readX(i)
 
     # TODO Use unit information in workspace, if available.
-    d[sp.Data.Value, name].unit = sp.units.counts
-    d[sp.Data.Variance, name].unit = sp.units.counts * sp.units.counts
+    d[name] = sc.Variable([sc.Dim.Position, dim],
+                          shape=(ws.getNumberHistograms(), len(ws.readY(0))),
+                          unit=sc.units.counts)
 
     for i in range(ws.getNumberHistograms()):
-        d[sp.Data.Value, name][sp.Dim.Position, i] = ws.readY(i)
-        d[sp.Data.Variance, name][sp.Dim.Position,
-                                  i] = ws.readE(i) * ws.readE(i)
+        d[name][sc.Dim.Position, i].values = ws.readY(i)
+        d[name][sc.Dim.Position, i].variances = ws.readE(i) * ws.readE(i)
 
     return d
 
 
 def ConvertEventWorkspaceToDataset(ws, name, drop_pulse_times):
-    d = sp.Dataset()
+    d = sc.Dataset()
     nHist = ws.getNumberHistograms()
     convert_instrument(d, ws)
     initPosSpectrumNo(d, nHist, ws)
 
-    d[sp.Data.Events, name] = ([sp.Dim.Position], (nHist,))
-    for i, e in enumerate(d[sp.Data.Events, name].data):
-        e[sp.Data.Tof] = ([sp.Dim.Event], ws.getSpectrum(i).getTofs())
+    coords = sc.Variable([sc.Dim.Position, sc.Dim.Tof],
+                         values=[nHist, sc.Dimensions.Sparse])
+    if not drop_pulse_times:
+        labs = sc.Variable([sc.Dim.Position, sc.Dim.Tof],
+                           values=[nHist, sc.Dimensions.Sparse])
+    for i in range(nHist):
+        coords[sc.Dim.Position, i].values = ws.getSpectrum(i).getTofs()
         if not drop_pulse_times:
             pt = ws.getSpectrum(i).getPulseTimes()
-            e[sp.Data.PulseTime] = ([sp.Dim.Event],
-                                    [p.total_nanoseconds() for p in pt])
+            labs[sc.Dim.Position, i].values = np.asarray([p.total_nanoseconds() for p in pt])
+
+    coords_and_labs = {"coords": {sc.Dim.Tof: coords}}
+    if not drop_pulse_times:
+        coords_and_labs["labels"] = {"pulse_times": labs}
+    d[name] = sc.DataArray(**coords_and_labs)
     return d
 
 # Pulse times have a Mantid-specific format so the conversion is very slow.
