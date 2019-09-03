37645e2aa5c6876751a9877d69e7faeafb565243
Simon Heybrock <simon.heybrock@esss.se>
2019-09-03 14:00:06 +0200

    Support python in-place binary for items of coords/labels/attrs.
    This requires __setitem__ supporint proxies.

--- core/include/scipp/core/dataset.h
+++ core/include/scipp/core/dataset.h
@@ -598,7 +598,7 @@ public:
     return slice(slice1, slice2).slice(slice3);
   }
 
-  void set(const typename Base::key_type key, Variable var) {
+  void set(const typename Base::key_type key, const VariableConstProxy &var) {
     if (!m_parent || !Base::m_slices.empty())
       throw std::runtime_error(
           "Cannot add coord/labels/attr field to a slice.");
