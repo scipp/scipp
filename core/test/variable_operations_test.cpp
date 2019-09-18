d757ac3f783451b3cce2973ca769b0a7cc265710
Igor Gudich <igor.gudich@esss.se>
2019-09-18 09:11:05 +0200

    Confuse ubsan

--- core/test/variable_operations_test.cpp
+++ core/test/variable_operations_test.cpp
@@ -946,19 +946,9 @@ TEST(VariableTest, divide_with_variance) {
   EXPECT_DOUBLE_EQ(q.variances<double>()[1], expected.variances<double>()[1]);
 }
 
-int dummy_func() {
-  int a[8] = {0};
-  int b[8] = {0};
-  b[1] = 1;
-  for (int i = 0, x = 0, y = 0; i <= 8; ++i) // uh oh
-  {
-    // This loop looks like it only reads / writes inside b
-    b[i] += x + y;
-    y = x;
-    x = b[i];
-  }
-  // a has been altered, b[8] is actually a[0]
-  return a[0];
+int dummy_func(int arg = 1) {
+  int k = 0x7fffffff;
+  k += arg;
 }
 
 TEST(ASAN, asan_fail_test) { int a = dummy_func(); }
