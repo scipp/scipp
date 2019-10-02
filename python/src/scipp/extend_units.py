ffd65a4450196f97a5f7fb3657c4be2bcd1856ca
Igor Gudich <igor.gudich@esss.se>
2019-10-02 12:44:01 +0200

    Add comments

--- python/src/scipp/extend_units.py
+++ python/src/scipp/extend_units.py
@@ -9,9 +9,6 @@ def __rmul(self, scalar):
         return self.__rmul(scalar, scalar.dtype)
 
 
-setattr(Unit, '__rmul__', __rmul)
-
-
 def __rtruediv(self, scalar):
     tp = type(scalar)
     if tp in [float, int]:
@@ -20,6 +17,12 @@ def __rtruediv(self, scalar):
         return self.__rtruediv(scalar, scalar.dtype)
 
 
+# add magic python methods to Unit class
+# it is done here (on python side) because
+# there is no proper way to do this in pybind11
 setattr(Unit, '__rtruediv__', __rtruediv)
+setattr(Unit, '__rmul__', __rmul)
 
+# forbid numpy to apply ufuncs to unit
+# wrong behavior in scalar * unit otherwise
 setattr(Unit, "__array_ufunc__", None)
