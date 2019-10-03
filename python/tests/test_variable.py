499e4f600454e9444a5958431cb0af4f33e58f27
Igor Gudich <igor.gudich@esss.se>
2019-10-03 16:18:17 +0200

    Add test

--- python/tests/test_variable.py
+++ python/tests/test_variable.py
@@ -551,11 +551,14 @@ def test_make_variable_from_unit_scalar_mult_div():
     var.unit = sp.units.m**(-1)
     assert var == 0.0 / sp.units.m
 
-    # TODO change next string with normal constructor
-    # for 0D Variable the it introduced
-    v = sp.Variable([sp.Dim.X], values=np.array([0]), dtype=np.float32)
-    var = sp.Variable(v[sp.Dim.X, 0])
+    var = sp.Variable(np.float32())
     var.unit = sp.units.m
     assert var == np.float32(0.0) * sp.units.m
     var.unit = sp.units.m**(-1)
     assert var == np.float32(0.0) / sp.units.m
+
+
+def test_construct_0d_numpy():
+    v = sp.Variable([sp.Dim.X], values=np.array([0]), dtype=np.float32)
+    var = sp.Variable(v[sp.Dim.X, 0])
+    assert var == sp.Variable(np.float32())
