cdf893218edf3eef9c4d411d01f15bc097338a0c
Neil Vaytet <neil.vaytet@esss.se>
2019-10-06 23:32:02 +0200

    still trying to upgrade pip

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -7,5 +7,5 @@ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 pyenv global system 3.7
 sudo apt-get install -qq clang-format-5.0 python3-pip ninja-build
-python3 -m pip install --upgrade pip
+sudo pip3 install --upgrade pip
 python3 -m pip install flake8
