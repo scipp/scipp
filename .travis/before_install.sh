0651ce86236178ea852438275822d738fa5c0e0b
Neil Vaytet <neil.vaytet@esss.se>
2019-10-06 23:26:00 +0200

    try upgrading pip before install

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -7,4 +7,5 @@ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 pyenv global system 3.7
 sudo apt-get install -qq clang-format-5.0 python3-pip ninja-build
+python3 -m pip install --upgrade pip
 python3 -m pip install flake8
