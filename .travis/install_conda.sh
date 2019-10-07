501ba5aa90622b334d24c9bcb98f97cc7f0f7f72
Neil Vaytet <neil.vaytet@esss.se>
2019-10-07 14:41:20 +0200

    try sourcing conda

--- .travis/install_conda.sh
+++ .travis/install_conda.sh
@@ -7,10 +7,11 @@ wget "https://repo.continuum.io/miniconda/$1" -O 'miniconda.sh'
 
 # Install Miniconda
 bash 'miniconda.sh' -b -p "$HOME/miniconda"
-
-$HOME/miniconda/bin/conda config \
+source "$HOME/miniconda/etc/profile.d/conda.sh"
+conda config \
   --set always_yes yes \
   --set changeps1 no
+conda update -q conda
 
 # Install build and deploy dependencies
-$HOME/miniconda/bin/conda install -c conda-forge conda-build anaconda
+conda install -c conda-forge conda-build anaconda
--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -19,7 +19,10 @@ make -j2 install all-tests
 ./neutron/test/scipp-neutron-test
 
 # Python tests
-$HOME/miniconda/bin/conda install --file ../python/requirements.txt
+conda create -q -n test-environment -f ../python/requirements.txt
+conda activate test-environment
+# conda install --file ../python/requirements.txt
+# conda env create -f environment.yml
 # python3 -m pip install -r ../python/requirements.txt
 export PYTHONPATH=$PYTHONPATH:../install
 cd ../python
