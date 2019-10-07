b9ee41c1c187bdc82ed41d54228e227c49ebb133
Neil Vaytet <neil.vaytet@esss.se>
2019-10-07 23:31:34 +0200

    trying to fix pyenv

--- .travis/install_linux_toolchain.sh
+++ .travis/install_linux_toolchain.sh
@@ -3,5 +3,7 @@
 set -xe
 
 # C++17
-sudo apt-get install -qq g++-7 libomp-dev
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
+sudo apt-get install -qq g++ libomp-dev
+# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
+# sudo apt-get install -qq g++-7 libomp-dev
+# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
