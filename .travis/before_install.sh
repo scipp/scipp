6c3f60e3f4d93a80aa0a758470d7b267019ba1d7
Neil Vaytet <neil.vaytet@esss.se>
2019-10-08 11:15:30 +0200

    cleanup

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,6 +5,5 @@ set -xe
 # C++17
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
-# pyenv global system 3.7
 sudo apt-get install -qq clang-format-5.0 python3-pip ninja-build
 python3 -m pip install flake8
--- .travis/install_linux_toolchain.sh
+++ .travis/install_linux_toolchain.sh
@@ -4,6 +4,3 @@ set -xe
 
 # C++17
 sudo apt-get install -qq g++ libomp-dev
-# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
-# sudo apt-get install -qq g++-7 libomp-dev
-# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
