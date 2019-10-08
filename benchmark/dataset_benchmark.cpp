81d7c2c6c199ef68e55fdf5958965ad85c4857e1
Dan Nixon <dan@dan-nixon.com>
2019-10-08 12:47:25 +0100

    Benchmark Dataset item access
    Benchmarks item access from a Dataset and slice of Dataset.
    
    Re #558

--- benchmark/dataset_benchmark.cpp
+++ benchmark/dataset_benchmark.cpp
@@ -7,6 +7,11 @@
 
 #include "scipp/core/dataset.h"
 
+// Length of a string that can be stored with SSO
+constexpr auto SHORT_STRING_LENGTH = 6;
+// Length of a string that cannot take advantage of SSO (i.e. must be allocated)
+constexpr auto LONG_STRING_LENGTH = 32;
+
 using namespace scipp;
 using namespace scipp::core;
 
@@ -52,8 +57,8 @@ template <class Gen> static void BM_Dataset_coords(benchmark::State &state) {
     d.coords();
   }
 }
-BENCHMARK_TEMPLATE(BM_Dataset_coords, Generate2D<6>);
-BENCHMARK_TEMPLATE(BM_Dataset_coords, Generate6D<6>);
+BENCHMARK_TEMPLATE(BM_Dataset_coords, Generate2D<SHORT_STRING_LENGTH>);
+BENCHMARK_TEMPLATE(BM_Dataset_coords, Generate6D<SHORT_STRING_LENGTH>);
 
 template <class Gen> static void BM_Dataset_labels(benchmark::State &state) {
   const auto d = Gen()();
@@ -61,10 +66,10 @@ template <class Gen> static void BM_Dataset_labels(benchmark::State &state) {
     d.labels();
   }
 }
-BENCHMARK_TEMPLATE(BM_Dataset_labels, Generate2D<6>);
-BENCHMARK_TEMPLATE(BM_Dataset_labels, Generate2D<32>);
-BENCHMARK_TEMPLATE(BM_Dataset_labels, Generate6D<6>);
-BENCHMARK_TEMPLATE(BM_Dataset_labels, Generate6D<32>);
+BENCHMARK_TEMPLATE(BM_Dataset_labels, Generate2D<SHORT_STRING_LENGTH>);
+BENCHMARK_TEMPLATE(BM_Dataset_labels, Generate2D<LONG_STRING_LENGTH>);
+BENCHMARK_TEMPLATE(BM_Dataset_labels, Generate6D<SHORT_STRING_LENGTH>);
+BENCHMARK_TEMPLATE(BM_Dataset_labels, Generate6D<LONG_STRING_LENGTH>);
 
 struct SliceX {
   auto operator()(const Dataset &d) { return d.slice({Dim::X, 20, 90}); }
@@ -82,6 +87,12 @@ struct SliceXYQz {
   }
 };
 
+struct SliceZXY {
+  auto operator()(const Dataset &d) {
+    return d.slice({Dim::Z, 5, 95}, {Dim::X, 20, 90}, {Dim::Y, 30, 60});
+  }
+};
+
 template <class Gen, class Slice>
 static void BM_Dataset_coords_slice(benchmark::State &state) {
   const auto d = Gen()();
@@ -90,11 +101,16 @@ static void BM_Dataset_coords_slice(benchmark::State &state) {
     s.coords();
   }
 }
-BENCHMARK_TEMPLATE(BM_Dataset_coords_slice, Generate2D<6>, SliceX);
-BENCHMARK_TEMPLATE(BM_Dataset_coords_slice, Generate2D<6>, SliceXY);
-BENCHMARK_TEMPLATE(BM_Dataset_coords_slice, Generate6D<6>, SliceX);
-BENCHMARK_TEMPLATE(BM_Dataset_coords_slice, Generate6D<6>, SliceXY);
-BENCHMARK_TEMPLATE(BM_Dataset_coords_slice, Generate6D<6>, SliceXYQz);
+BENCHMARK_TEMPLATE(BM_Dataset_coords_slice, Generate2D<SHORT_STRING_LENGTH>,
+                   SliceX);
+BENCHMARK_TEMPLATE(BM_Dataset_coords_slice, Generate2D<SHORT_STRING_LENGTH>,
+                   SliceXY);
+BENCHMARK_TEMPLATE(BM_Dataset_coords_slice, Generate6D<SHORT_STRING_LENGTH>,
+                   SliceX);
+BENCHMARK_TEMPLATE(BM_Dataset_coords_slice, Generate6D<SHORT_STRING_LENGTH>,
+                   SliceXY);
+BENCHMARK_TEMPLATE(BM_Dataset_coords_slice, Generate6D<SHORT_STRING_LENGTH>,
+                   SliceXYQz);
 
 template <class Gen, class Slice>
 static void BM_Dataset_labels_slice(benchmark::State &state) {
@@ -104,15 +120,92 @@ static void BM_Dataset_labels_slice(benchmark::State &state) {
     s.labels();
   }
 }
-BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate2D<6>, SliceX);
-BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate2D<32>, SliceX);
-BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate2D<6>, SliceXY);
-BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate2D<32>, SliceXY);
-BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<6>, SliceX);
-BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<32>, SliceX);
-BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<6>, SliceXY);
-BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<32>, SliceXY);
-BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<6>, SliceXYQz);
-BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<32>, SliceXYQz);
+BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate2D<SHORT_STRING_LENGTH>,
+                   SliceX);
+BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate2D<LONG_STRING_LENGTH>,
+                   SliceX);
+BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate2D<SHORT_STRING_LENGTH>,
+                   SliceXY);
+BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate2D<LONG_STRING_LENGTH>,
+                   SliceXY);
+BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<SHORT_STRING_LENGTH>,
+                   SliceX);
+BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<LONG_STRING_LENGTH>,
+                   SliceX);
+BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<SHORT_STRING_LENGTH>,
+                   SliceXY);
+BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<LONG_STRING_LENGTH>,
+                   SliceXY);
+BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<SHORT_STRING_LENGTH>,
+                   SliceXYQz);
+BENCHMARK_TEMPLATE(BM_Dataset_labels_slice, Generate6D<LONG_STRING_LENGTH>,
+                   SliceXYQz);
+
+template <int NameLen> struct GenerateWithDataItems {
+  Dataset operator()(const int itemCount = 5, const int size = 100) {
+    Dataset d;
+    for (auto i = 0; i < itemCount; ++i) {
+      d.setData(std::to_string(i) + std::string(NameLen, 'i'),
+                makeVariable<double>(
+                    {{Dim::X, size}, {Dim::Y, size}, {Dim::Z, size}}));
+    }
+    return d;
+  }
+};
+
+template <class Gen>
+static void BM_Dataset_item_access(benchmark::State &state) {
+  const auto d = Gen()();
+  const auto name = d.begin()->second.name();
+  for (auto _ : state) {
+    d[name];
+  }
+}
+BENCHMARK_TEMPLATE(BM_Dataset_item_access,
+                   GenerateWithDataItems<SHORT_STRING_LENGTH>);
+BENCHMARK_TEMPLATE(BM_Dataset_item_access,
+                   GenerateWithDataItems<LONG_STRING_LENGTH>);
+
+template <class Gen>
+static void BM_Dataset_iterate_items(benchmark::State &state) {
+  const auto d = Gen()(state.range(0));
+  for (auto _ : state) {
+    for (const auto & [ name, item ] : d) {
+      benchmark::DoNotOptimize(name);
+      benchmark::DoNotOptimize(item);
+    }
+  }
+  state.counters["IterationItemRate"] = benchmark::Counter(
+      state.range(0), benchmark::Counter::kIsIterationInvariantRate);
+}
+BENCHMARK_TEMPLATE(BM_Dataset_iterate_items,
+                   GenerateWithDataItems<SHORT_STRING_LENGTH>)
+    ->Range(1 << 2, 1 << 8);
+BENCHMARK_TEMPLATE(BM_Dataset_iterate_items,
+                   GenerateWithDataItems<LONG_STRING_LENGTH>)
+    ->Range(1 << 2, 1 << 8);
+
+template <class Gen, class Slice>
+static void BM_Dataset_iterate_slice_items(benchmark::State &state) {
+  const auto d = Gen()(state.range(0));
+  const auto s = Slice()(d);
+  for (auto _ : state) {
+    for (const auto & [ name, item ] : s) {
+      benchmark::DoNotOptimize(name);
+      benchmark::DoNotOptimize(item);
+    }
+  }
+  state.counters["IterationItemRate"] = benchmark::Counter(
+      state.range(0), benchmark::Counter::kIsIterationInvariantRate);
+}
+BENCHMARK_TEMPLATE(BM_Dataset_iterate_slice_items,
+                   GenerateWithDataItems<SHORT_STRING_LENGTH>, SliceX)
+    ->Range(1 << 2, 1 << 8);
+BENCHMARK_TEMPLATE(BM_Dataset_iterate_slice_items,
+                   GenerateWithDataItems<SHORT_STRING_LENGTH>, SliceXY)
+    ->Range(1 << 2, 1 << 8);
+BENCHMARK_TEMPLATE(BM_Dataset_iterate_slice_items,
+                   GenerateWithDataItems<SHORT_STRING_LENGTH>, SliceZXY)
+    ->Range(1 << 2, 1 << 8);
 
 BENCHMARK_MAIN();
