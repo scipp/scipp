d25b65a45b68abf02d294ef37e91019d523fa92c
Neil Vaytet <neil.vaytet@esss.se>
2019-10-10 12:24:09 +0200

    trying to revert travis config to not use conda everywhere

--- .travis/install_conda.sh
+++ .travis/install_conda.sh
@@ -7,7 +7,10 @@ wget "https://repo.continuum.io/miniconda/$1" -O 'miniconda.sh'
 
 # Install Miniconda
 bash 'miniconda.sh' -b -p "$HOME/miniconda"
+
 conda config \
   --set always_yes yes \
   --set changeps1 no
-conda update -q conda
+
+# Install build and deploy dependencies
+conda install -c conda-forge conda-build anaconda
