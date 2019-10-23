8c127f9a556c0ab244e5e4741ad5b9594f4c98c3
Dan Nixon <dan@dan-nixon.com>
2019-10-23 10:57:17 +0100

    WIP: move some builds to Docker

--- .travis/before_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-# C++17
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update -qq
-sudo apt-get install -qq clang-format-6.0 python3-pip ninja-build
-python3 -m pip install flake8
