5336c9c1d582dc954efdebc423ea49578d6a6f66
Simon Heybrock <simon.heybrock@esss.se>
2019-11-01 10:54:36 +0100

    Extend element_array to be more useful.

--- core/include/scipp/core/element_array.h
+++ core/include/scipp/core/element_array.h
@@ -7,9 +7,15 @@
 
 #include <memory>
 
+#include "scipp/common/index.h"
 #include "scipp/core/aligned_allocator.h"
 
 namespace scipp::core::detail {
+
+/// Replacement for C++20 std::make_unique_default_init
+template <class T> auto make_unique_default_init(const scipp::index size) {
+  return std::unique_ptr<T>(new std::remove_extent_t<T>[size]);
+}
 
 template <class T> class element_array {
 public:
@@ -18,23 +24,41 @@ public:
     resize_no_init(std::distance(first, last));
     std::copy(first, last, data());
   }
+  element_array(element_array &&other) = default;
+  element_array(const element_array &other)
+      : element_array(other.data(), other.data() + other.size()) {}
+  element_array &operator=(element_array &&other) = default;
+  element_array &operator=(const element_array &other) {
+    return *this = element_array(other.data(), other.data() + other.size());
+  }
 
   explicit operator bool() const noexcept { return m_data.operator bool(); }
   scipp::index size() const noexcept { return m_size; }
   const T *data() const noexcept { return m_data.get(); }
   T *data() noexcept { return m_data.get(); }
 
+  void reset() noexcept {
+    m_data.reset();
+    m_size = -1;
+  }
+
   void resize_no_init(const scipp::index new_size) {
     if (new_size != size()) {
-      m_data = std::make_unique<T[]>(new_size);
+      m_data = make_unique_default_init<T[]>(new_size);
       m_size = new_size;
     }
+  }
+
+  void resize(const scipp::index new_size) {
+    m_data = std::make_unique<T[]>(new_size);
+    m_size = new_size;
   }
 
 private:
   scipp::index m_size{-1};
   std::unique_ptr<T[]> m_data;
 };
-}
+
+} // namespace scipp::core::detail
 
 #endif // SCIPP_CORE_ELEMENT_ARRAY_H
--- core/test/element_array_test.cpp
+++ core/test/element_array_test.cpp
@@ -8,20 +8,91 @@
 
 using scipp::core::detail::element_array;
 
-TEST(ElementArrayTest, construct_default) {
-  element_array<double> x;
-  ASSERT_FALSE(x);
-  ASSERT_EQ(x.size(), -1);
-  ASSERT_EQ(x.data(), nullptr);
+static auto make_element_array() {
+  std::vector<double> v{1.1, 2.2, 3.3};
+  return element_array<float>(v.begin(), v.end());
 }
 
-TEST(ElementArrayTest, construct_iterators) {
-  std::vector<double> v{1.1, 2.2, 3.3};
-  element_array<float> x(v.begin(), v.end());
+template <class T> void check_element_array(const element_array<T> x) {
   ASSERT_TRUE(x);
   ASSERT_EQ(x.size(), 3);
   ASSERT_NE(x.data(), nullptr);
   ASSERT_EQ(x.data()[0], 1.1f);
   ASSERT_EQ(x.data()[1], 2.2f);
   ASSERT_EQ(x.data()[2], 3.3f);
+}
+
+template <class T> void check_null_element_array(const element_array<T> x) {
+  ASSERT_FALSE(x);
+  ASSERT_EQ(x.size(), -1);
+  ASSERT_EQ(x.data(), nullptr);
+}
+
+TEST(ElementArrayTest, construct_default) {
+  element_array<double> x;
+  check_null_element_array(x);
+}
+
+TEST(ElementArrayTest, construct_iterators) {
+  auto x = make_element_array();
+  check_element_array(x);
+}
+
+TEST(ElementArrayTest, construct_move) {
+  auto x = make_element_array();
+  const auto ptr = x.data();
+  auto y(std::move(x));
+  ASSERT_FALSE(x);
+  ASSERT_EQ(y.data(), ptr);
+  check_element_array(y);
+}
+
+TEST(ElementArrayTest, construct_copy) {
+  auto x = make_element_array();
+  auto y(x);
+  ASSERT_TRUE(x);
+  check_element_array(y);
+}
+
+TEST(ElementArrayTest, assign_move) {
+  auto x = make_element_array();
+  const auto ptr = x.data();
+  element_array<float> y;
+  y = std::move(x);
+  ASSERT_FALSE(x);
+  ASSERT_EQ(y.data(), ptr);
+  check_element_array(y);
+}
+
+TEST(ElementArrayTest, assign_copy) {
+  auto x = make_element_array();
+  element_array<float> y;
+  y = x;
+  ASSERT_TRUE(x);
+  check_element_array(y);
+}
+
+TEST(ElementArrayTest, reset) {
+  auto x = make_element_array();
+  x.reset();
+  check_null_element_array(x);
+}
+
+TEST(ElementArrayTest, resize) {
+  auto x = make_element_array();
+  x.resize(2);
+  ASSERT_TRUE(x);
+  ASSERT_EQ(x.size(), 2);
+  ASSERT_NE(x.data(), nullptr);
+  ASSERT_EQ(x.data()[0], 0.0f);
+  ASSERT_EQ(x.data()[1], 0.0f);
+}
+
+TEST(ElementArrayTest, resize_no_init) {
+  auto x = make_element_array();
+  x.resize_no_init(2);
+  ASSERT_TRUE(x);
+  ASSERT_EQ(x.size(), 2);
+  ASSERT_NE(x.data(), nullptr);
+  // Data values could be anything.
 }
