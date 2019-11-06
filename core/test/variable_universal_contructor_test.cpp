de47f4d95c91225123127d32fd39a111f31c69e2
Igor Gudich <igor.gudich@esss.se>
2019-11-06 10:38:07 +0100

    split tests

--- core/test/variable_universal_contructor_test.cpp
+++ core/test/variable_universal_contructor_test.cpp
@@ -13,7 +13,8 @@
 
 using namespace scipp;
 using namespace scipp::core;
-TEST(VariableUniversalConstructorTest, universal_make_variable) {
+
+TEST(VariableUniversalConstructorTest, dimensions_unit_basic) {
   auto variable = Variable(dtype<float>, Dimensions{{Dim::X, Dim::Y}, {2, 3}},
                            units::Unit(units::kg));
 
@@ -25,37 +26,67 @@ TEST(VariableUniversalConstructorTest, universal_make_variable) {
   auto otherVariable =
       Variable(dtype<float>, Dimensions{{Dim::X, Dim::Y}, {2, 3}});
   variable.setUnit(units::dimensionless);
-
   EXPECT_EQ(variable, otherVariable);
+
+  auto oneMore = Variable(dtype<float>, units::Unit(units::dimensionless),
+                          Dimensions{{Dim::X, Dim::Y}, {2, 3}});
+  EXPECT_EQ(oneMore, variable);
+}
+
+TEST(VariableUniversalConstructorTest, type_construcors_mix) {
+  auto v1 = Variable(dtype<float>, Dimensions{{Dim::X, Dim::Y}, {2, 1}},
+                     Values({1.5f, 3.6f}), Variances({2.0, 3.0}));
+  auto v2 = Variable(dtype<float>, Dimensions{{Dim::X, Dim::Y}, {2, 1}},
+                     Values({1.5, 3.6}), Variances({2, 3}));
+  auto v3 = Variable(units::Unit(), Dimensions{{Dim::X, Dim::Y}, {2, 1}},
+                     Values<float>({1.5f, 3.6}),
+                     Variances<float>(std::optional<Vector<float>>()));
+  v3.setVariances(Vector<float>{2, 3});
+  EXPECT_EQ(v1, v2);
+  EXPECT_EQ(v1, v3);
+
+  v2 = Variable(dtype<float>, Variances({2.0, 3.0}),
+                Dimensions{{Dim::X, Dim::Y}, {2, 1}}, Values({1.5f, 3.6f}));
+  EXPECT_EQ(v1, v2);
+}
+
+TEST(VariableUniversalConstructorTest, no_copy_on_matched_types) {
   auto data = Vector<double>{1.0, 4.5, 2.7, 5.0, 7.0, 6.7};
+  auto val = Values(Vector<double>(data));
+  auto valAddr = &(*val.data)[0];
+  auto var = Variances(Vector<double>(data));
+  auto varAddr = &(*var.data)[0];
 
-  {
-    auto val = Values<double>(Vector<double>(data));
-    auto valAddr = &(*val.data)[0];
-    auto var = Variances<double>(std::optional(Vector<double>(data)));
-    auto varAddr = &(*var.data)[0];
+  auto variable =
+      Variable(dtype<double>, Dimensions{{Dim::X, Dim::Y}, {2, 3}},
+               std::move(val), units::Unit(units::kg), std::move(var));
 
-    variable = Variable(dtype<double>, Dimensions{{Dim::X, Dim::Y}, {2, 3}},
-                        std::move(val), units::Unit(units::kg), std::move(var));
+  auto vval = variable.values<double>();
+  auto vvar = variable.variances<double>();
+  EXPECT_TRUE(equals(vval, data));
+  EXPECT_TRUE(equals(vvar, data));
+  EXPECT_EQ(&vval[0], valAddr);
+  EXPECT_EQ(&vvar[0], varAddr);
+}
 
-    auto vval = variable.values<double>();
-    auto vvar = variable.variances<double>();
-    EXPECT_TRUE(equals(vval, data));
-    EXPECT_TRUE(equals(vvar, data));
-    EXPECT_EQ(&vval[0], valAddr);
-    EXPECT_EQ(&vvar[0], varAddr);
-  }
+TEST(VariableUniversalConstructorTest, convertable_types) {
+  auto data = Vector<double>{1.0, 4.5, 2.7, 5.0, 7.0, 6.7};
+  auto val = Values(Vector<double>(data));
+  auto var = Variances(Vector<double>(data));
+  auto variable =
+      Variable(dtype<int64_t>, Dimensions{{Dim::X, Dim::Y}, {2, 3}},
+               std::move(val), units::Unit(units::kg), std::move(var));
 
-  {
-    auto val = Values<double>(Vector<double>(data));
-    auto var = Variances<double>(std::optional(Vector<double>(data)));
-    variable = Variable(dtype<int64_t>, Dimensions{{Dim::X, Dim::Y}, {2, 3}},
-                        std::move(val), units::Unit(units::kg), std::move(var));
+  EXPECT_EQ(variable.dtype(), dtype<int64_t>);
+  EXPECT_TRUE(equals(variable.values<int64_t>(),
+                     Vector<int64_t>(data.begin(), data.end())));
+  EXPECT_TRUE(equals(variable.variances<int64_t>(),
+                     Vector<int64_t>(data.begin(), data.end())));
+}
 
-    EXPECT_EQ(variable.dtype(), dtype<int64_t>);
-    EXPECT_TRUE(equals(variable.values<int64_t>(),
-                       Vector<int64_t>(data.begin(), data.end())));
-    EXPECT_TRUE(equals(variable.variances<int64_t>(),
-                       Vector<int64_t>(data.begin(), data.end())));
-  }
+TEST(VariableUniversalConstructorTest, unconvertable_types) {
+  EXPECT_THROW(Variable(dtype<Eigen::Vector3d>,
+                        Dimensions{{Dim::X, Dim::Y}, {2, 1}},
+                        Values({1.5f, 3.6f}), Variances({2.0, 3.0})),
+               std::logic_error);
 }
