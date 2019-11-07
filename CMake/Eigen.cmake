2998ff4851f4a2a44d774fdd8282310fe2091f9d
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2019-11-07 15:45:02 +0000

    ExternalProject errors will now fail the CMake Configure
    - Added missing exit code checks on multiple external project calls

--- CMake/Eigen.cmake
+++ CMake/Eigen.cmake
@@ -3,7 +3,17 @@ configure_file(${CMAKE_SOURCE_DIR}/CMake/Eigen.in
   ${CMAKE_BINARY_DIR}/Eigen-download/CMakeLists.txt)
 
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/Eigen-download )
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/Eigen-download
+  RESULTS_VARIABLE result)
+
+if(result)
+  message(FATAL_ERROR "CMake step for Eigen failed: ${result}")
+endif()
 
 execute_process(COMMAND ${CMAKE_COMMAND} --build .
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/Eigen-download )
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/Eigen-download
+  RESULTS_VARIABLE result)
+
+if(result)
+  message(FATAL_ERROR "Build step for Eigen failed: ${result}")
+endif()
