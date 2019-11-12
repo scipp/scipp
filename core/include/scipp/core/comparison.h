ba423dfcec944305d09cfbe6536913e3a670e5ac
Dan Nixon <dan@dan-nixon.com>
2019-11-12 09:33:50 +0000

    Resolve ambiguous overloads in transform lambdas

--- core/include/scipp/core/comparison.h
+++ core/include/scipp/core/comparison.h
@@ -27,27 +27,27 @@ bool is_approx(const VariableConstProxy &a, const VariableConstProxy &b,
   std::atomic_flag mismatch = ATOMIC_FLAG_INIT;
   transform_in_place<pair_self_t<T>>(
       aa, b,
-      scipp::overloaded{[&](const T va, const T vb) {
-                          if (std::abs(va - vb) >= tol)
-                            mismatch.test_and_set();
-                        },
-                        [&](const T va, const auto &vb) {
-                          if (std::abs(va - vb.value) >= tol)
-                            mismatch.test_and_set();
-                        },
-                        [&](const auto &va, const T vb) {
-                          if (std::abs(va.value - vb) >= tol)
-                            mismatch.test_and_set();
-                        },
-                        [&](const auto &va, const auto &vb) {
-                          if ((std::abs(va.value - vb.value) >= tol) ||
-                              (std::abs(va.variance - vb.variance) >= tol))
-                            mismatch.test_and_set();
-                        },
-                        [&](units::Unit &ua, const units::Unit &ub) {
-                          if (ua != ub)
-                            mismatch.test_and_set();
-                        }});
+      scipp::overloaded{
+          [&](const auto &va, const auto &vb) {
+            if constexpr (detail::is_ValueAndVariance_v<
+                              std::decay_t<decltype(va)>> !=
+                          detail::is_ValueAndVariance_v<
+                              std::decay_t<decltype(vb)>>) {
+              mismatch.test_and_set();
+            } else if constexpr (detail::is_ValueAndVariance_v<
+                                     std::decay_t<decltype(va)>>) {
+              if ((std::abs(va.value - vb.value) >= tol) ||
+                  (std::abs(va.variance - vb.variance) >= tol))
+                mismatch.test_and_set();
+            } else {
+              if (std::abs(va - vb) >= tol)
+                mismatch.test_and_set();
+            }
+          },
+          [&](units::Unit &ua, const units::Unit &ub) {
+            if (ua != ub)
+              mismatch.test_and_set();
+          }});
   return !mismatch.test_and_set();
 }
 
