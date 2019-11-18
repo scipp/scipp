2077c7dfcff093991deeaed2f5d92971d8ddb75a
Neil Vaytet <neil.vaytet@esss.se>
2019-11-18 14:13:54 +0100

    cleanup

--- python/src/scipp/plot/plot_1d.py
+++ python/src/scipp/plot/plot_1d.py
@@ -21,9 +21,10 @@ def plot_1d(input_data, backend=None, logx=False, logy=False, logxy=False,
     """
     Plot a 1D spectrum.
 
-    Input is a dictionary containing a list of DataProxy.
+    Input is a Dataset containing one or more Variables.
     If the coordinate of the x-axis contains bin edges, then a bar plot is
     made.
+    If the data contains more than one dimensions, sliders are added.
 
     TODO: find a more general way of handling arguments to be sent to plotly,
     probably via a dictionay of arguments
@@ -35,7 +36,6 @@ def plot_1d(input_data, backend=None, logx=False, logy=False, logxy=False,
     data = []
     for i, (name, var) in enumerate(sorted(input_data)):
 
-        # xlab, ylab, x, y = get_1d_axes(var, axes, name)
         ax = var.dims
         if var.variances is not None:
             err = np.sqrt(var.variances)
@@ -45,96 +45,56 @@ def plot_1d(input_data, backend=None, logx=False, logy=False, logxy=False,
         ymin = min(ymin, np.nanmin(var.values - err))
         ymax = max(ymax, np.nanmax(var.values + err))
 
-        # nx = x.shape[0]
-        # ny = y.shape[0]
-        # histogram = False
-        # if nx == ny + 1:
-        #     histogram = True
-
         # Define trace
-        # trace = dict(x=x, y=y, name=name, type="scattergl")
         trace = dict(name=name, type="scattergl")
-        # if histogram:
-        #     trace["line"] = {"shape": "hv"}
-        #     trace["y"] = np.concatenate((trace["y"], [0.0]))
-        #     trace["fill"] = "tozeroy"
-        #     trace["mode"] = "lines"
-        # print(color)
-        # print(color[i])
         if color is not None:
             trace["marker"] = {"color": color[i]}
-        # # Include variance if present
-        # if var.variances is not None:
-        #     err_dict = dict(
-        #             type="data",
-        #             array=np.sqrt(var.variances),
-        #             visible=True,
-        #             color=color[i])
-        #     if histogram:
-        #         trace2 = dict(x=edges_to_centers(x), y=y, showlegend=False,
-        #                       type="scattergl", mode="markers",
-        #                       error_y=err_dict,
-        #                       marker={"color": color[i]})
-        #         data.append(trace2)
-        #     else:
-        #         trace["error_y"] = err_dict
-
         data.append(trace)
 
     if axes is None:
         axes = ax
 
-    dy = 0.05*(ymax - ymin)
     layout = dict(
-        yaxis=dict(range=[ymin-dy, ymax+dy]),
+        xaxis=dict(),
+        yaxis=dict(),
         showlegend=True,
         legend=dict(x=0.0, y=1.15, orientation="h"),
         height=config.height,
         width=config.width
     )
-    # if histogram:
-    #     layout["barmode"] = "overlay"
     if logx or logxy:
         layout["xaxis"]["type"] = "log"
     if logy or logxy:
         layout["yaxis"]["type"] = "log"
+        [ymin, ymax] = np.log10([ymin, ymax])
+    dy = 0.05*(ymax - ymin)
+    layout["yaxis"]["range"] = [ymin-dy, ymax+dy]
 
-    sv = Slicer1d(data=data, layout=layout, input_data=input_data, axes=axes, color=color)
-
-
+    sv = Slicer1d(data=data, layout=layout, input_data=input_data, axes=axes,
+                  color=color)
     render_plot(static_fig=sv.fig, interactive_fig=sv.box, backend=backend,
                 filename=filename)
 
-    # fig = go.Figure(data=data, layout=layout)
-    # render_plot(static_fig=fig, interactive_fig=fig, backend=backend,
-    #             filename=filename)
     return
-
 
 
 class Slicer1d(Slicer):
 
     def __init__(self, data, layout, input_data, axes, color):
 
-        super().__init__(input_data=input_data, axes=axes, button_options=['X'])
+        super().__init__(input_data=input_data, axes=axes,
+                         button_options=['X'])
 
         self.color = color
         self.fig = go.FigureWidget(layout=layout)
 
         self.traces = dict()
         for i, (name, var) in enumerate(sorted(self.input_data)):
-            # Define trace
-            # trace = dict(x=x, y=y, name=name, type="scattergl")
             trace = dict(name=name, type="scattergl")
             if color is not None:
                 trace["marker"] = {"color": color[i]}
             self.traces[name] = i
             self.fig.add_trace(trace)
-
-        # self.fig = go.FigureWidget(data=data, layout=layout)
-        # self.fields = []
-        # for i, (name, var) in enumerate(sorted(self.input_data)):
-        #     self.fields.append((name, i))
 
         # Disable buttons
         for key, button in self.buttons.items():
@@ -145,32 +105,30 @@ class Slicer1d(Slicer):
         self.update_histograms()
 
         self.keep_buttons = dict()
-        self.make_keep_button()
+        if self.ndim > 1:
+            self.make_keep_button()
 
-        self.mbox = [self.fig] + self.vbox# + [widgets.HBox(self.keep_buttons[0])]
+        # vbox contains the original sliders and buttons. In mbox, we include
+        # the keep trace buttons.
+        self.mbox = [self.fig] + self.vbox
         for key, val in self.keep_buttons.items():
             self.mbox.append(widgets.HBox(val))
-        # print(self.mbox)
         self.box = widgets.VBox(self.mbox)
         self.box.layout.align_items = 'center'
 
         return
 
     def make_keep_button(self):
-        drop = widgets.Dropdown(options=self.traces.keys(),
-                                description='', layout={'width': 'initial'})
-        but = widgets.Button(
-                    description="Keep",
-                    disabled=False,
-                    button_style="",
-                    layout={'width': "70px"})
+        drop = widgets.Dropdown(options=self.traces.keys(), description='',
+                                layout={'width': 'initial'})
+        but = widgets.Button(description="Keep", disabled=False,
+                             button_style="", layout={'width': "70px"})
+        # Generate a random color. TODO: should we initialise the seed?
         r = lambda: random.randint(0,255)
-        col = widgets.ColorPicker(
-    concise=True,
-    description='',
-    value='#%02X%02X%02X' % (r(),r(),r()),
-    disabled=False
-)
+        col = widgets.ColorPicker(concise=True, description='',
+                                  value='#%02X%02X%02X' % (r(),r(),r()),
+                                  disabled=False)
+        # Make a unique id
         key = str(id(but))
         setattr(but, "id", key)
         setattr(col, "id", key)
@@ -179,16 +137,8 @@ class Slicer1d(Slicer):
         self.keep_buttons[key] = [drop, but, col]
         return
 
-
-
     def update_buttons(self, owner, event, dummy):
-
-        # toggle_slider = False
-        # if not self.slider[owner.dim_str].disabled:
-        #     toggle_slider = True
-        #     self.slider[owner.dim_str].disabled = True
         for key, button in self.buttons.items():
-            # if (button.value == owner.value) and (key != owner.dim_str):
             if key == owner.dim_str:
                 self.slider[key].disabled = True
                 button.disabled = True
@@ -196,14 +146,6 @@ class Slicer1d(Slicer):
                 self.slider[key].disabled = False
                 button.value = None
                 button.disabled = False
-        #         if self.slider[key].disabled:
-        #             button.value = owner.old_value
-        #         else:
-        #             button.value = None
-        #         button.old_value = button.value
-        #         if toggle_slider:
-        #             self.slider[key].disabled = False
-        # owner.old_value = owner.value
         self.update_axes(owner.dim_str)
         self.update_slice(None)
         self.update_histograms()
@@ -211,104 +153,33 @@ class Slicer1d(Slicer):
         self.keep_buttons = dict()
         self.make_keep_button()
         self.mbox = [self.fig] + self.vbox
-        # print(self.keep_buttons)
         for k, b in self.keep_buttons.items():
             self.mbox.append(widgets.HBox(b))
         self.box.children=tuple(self.mbox)
-        # for i in range(len(self.fig.data)):
-        #     histogram = False
-        #     trace = self.fig.data[i]
-        #     if len(trace.x) == len(trace.y) + 1:
-        #         histogram = True
-        #     if histogram:
-        #         trace["line"] = {"shape": "hv"}
-        #         trace["y"] = np.concatenate((trace["y"], [0.0]))
-        #         trace["fill"] = "tozeroy"
-        #         trace["mode"] = "lines"
-        #     else:
-        #         trace["line"] = None
-        #         # trace["y"] = np.concatenate((trace["y"], [0.0]))
-        #         trace["fill"] = None
-        #         trace["mode"] = None
-
         return
 
     def update_axes(self, dim_str):
         self.fig.data = self.fig.data[:len(self.input_data)]
         self.fig.update_traces(x=self.slider_x[dim_str].values)
-        # for i in range(len(self.fig.data)):
-        #     self.fig.data[i].x = self.slider_x[dim_str].values
-
-        # # Go through the buttons and select the right coordinates for the axes
-        # for key, button in self.buttons.items():
-        #     if self.slider[key].disabled:
-        #         but_val = button.value.lower()
-        #         # for i in range(1 + self.show_variances):
-        #         #     if self.rasterize:
-        #         #         self.fig.data[i][but_val] = \
-        #         #             self.slider_x[key].values[[0, -1]]
-        #         #     else:
-        #         self.fig.data[i]["x"] = self.slider_x[key].values
-        #         # if self.surface3d:
-        #         #     self.fig.layout.scene1["{}axis_title".format(
-        #         #         but_val)] = axis_label(self.slider_x[key],
-        #         #                                name=self.slider_labels[key])
-        #         #     if self.show_variances:
-        #         #         self.fig.layout.scene2["{}axis_title".format(
-        #         #             but_val)] = axis_label(
-        #         #                 self.slider_x[key],
-        #         #                 name=self.slider_labels[key])
-        #         # else:
-        #         #     if self.show_variances:
-        #         #         func = getattr(self.fig, 'update_{}axes'.format(
-        #         #             but_val))
-        #         #         for i in range(2):
-        #         #             func(title_text=axis_label(
-        #         #                 self.slider_x[key],
-        #         #                 name=self.slider_labels[key]),
-        #         #                 row=1, col=i+1)
-        #         #     else:
-        #         # axis_str = "{}axis".format(but_val)
         self.fig.layout["xaxis"]["title"] = axis_label(
             self.slider_x[dim_str], name=self.slider_labels[dim_str])
-
         return
 
     # Define function to update slices
     def update_slice(self, change):
         # The dimensions to be sliced have been saved in slider_dims
         for i, (name, var) in enumerate(sorted(self.input_data)):
-            # print(name)
             vslice = var
             # Slice along dimensions with active sliders
-            # button_dims = [None, None]
-            # print("start slicing")
             for key, val in self.slider.items():
-                # print(key, val)
                 if not val.disabled:
                     if i == 0:
                         self.lab[key].value = str(
                             self.slider_x[key].values[val.value])
-                    # print("slicing along", val.dim, val.value)
                     vslice = vslice[val.dim, val.value]
-                # else:
-                #     button_dims[self.buttons[key].value.lower() == "y"] = val.dim
-
             self.fig.data[i].y = vslice.values
-            # nx = len(self.fig.data[i].y)
-            # ny = y.shape[0]
-            # histogram = False
-            # if nx == ny + 1:
-            #     histogram = True
             if var.variances is not None:
                 self.fig.data[i]["error_y"].array = np.sqrt(vslice.variances)
-
-        # # Check if dimensions of arrays agree, if not, plot the transpose
-        # slice_dims = vslice.dims
-        # transp = slice_dims == button_dims
-        # self.update_z2d(vslice.values, transp, self.cb["log"], 0)
-        # if self.show_variances:
-        #     self.update_z2d(vslice.variances, transp, self.cb["log"], 1)
         return
 
     def update_histograms(self):
@@ -316,27 +187,17 @@ class Slicer1d(Slicer):
             histogram = False
             trace = self.fig.data[i]
             if len(trace.x) == len(trace.y) + 1:
-            #     histogram = True
-            # if histogram:
                 trace["line"] = {"shape": "hvh"}
                 trace["x"] = 0.5 * (trace["x"][:-1] + trace["x"][1:])
-                # trace["x"] = np.concatenate(([trace["x"][0]-], 0.5 * (trace["x"][:-1] + trace["x"][1:]), [trace["x"][-1]]))
-                # trace["y"] = np.concatenate(([0.0], trace["y"], [0.0]))
-                # print(trace["error_y"].array)
-                # if trace["error_y"].array is not None:
-                #     trace["error_y"].array = np.concatenate(([0.0], trace["error_y"].array, [0.0]))
                 trace["fill"] = "tozeroy"
                 trace["mode"] = "lines"
             else:
                 trace["line"] = None
-                # trace["y"] = np.concatenate((trace["y"], [0.0]))
                 trace["fill"] = None
                 trace["mode"] = None
         return
 
     def keep_remove_trace(self, owner):
-        # print(owner)
-        # print(dir(owner))
         if owner.description == "Keep":
             self.keep_trace(owner)
         elif owner.description == "Remove":
@@ -344,73 +205,36 @@ class Slicer1d(Slicer):
         return
 
     def keep_trace(self, owner):
-        # owner.value
         lab = self.keep_buttons[owner.id][0].value
-        # print(self.traces[self.keep_buttons[owner.id][0].value])
-        # setattr(owner, "trace_number", len(self.fig.data))
         self.fig.add_trace(self.fig.data[self.traces[lab]])
-        # self.fig.data[owner.value]
-        self.fig.data[-1]["marker"]["color"] = self.keep_buttons[owner.id][2].value
+        self.fig.data[-1]["marker"]["color"] = self.keep_buttons[
+            owner.id][2].value
         self.fig.data[-1]["showlegend"] = False
         self.fig.data[-1]["meta"] = owner.id
-        # lab = 
         for key, val in self.slider.items():
             if not val.disabled:
                 lab = "{},{}:{}".format(lab, key, val.value)
-        # self.fig.data[-1]["name"] = lab
-        # print(lab)
-        self.keep_buttons[owner.id][0] = widgets.Label(value=lab, layout={'width': "150px"})
+        self.keep_buttons[owner.id][0] = widgets.Label(
+            value=lab, layout={'width': "initial"}, title=lab)
         self.make_keep_button()
         owner.description = "Remove"
         self.mbox = [self.fig] + self.vbox
         for k, b in self.keep_buttons.items():
-            # print(k, b)
             self.mbox.append(widgets.HBox(b))
         self.box.children=tuple(self.mbox)
-        # # self.box.children = widgets.VBox(self.mbox)
-        # self.box.children=tuple(list(self.box.children) + [widgets.HBox(self.keep_buttons[-1])])
-        # self.box.children=tuple([self.fig] + self.vbox + [widgets.HBox(tuple(b) for b in self.keep_buttons)])
-
         return
 
     def remove_trace(self, owner):
-        # del owner
-        # # owner.value
-        # print(self.keep_buttons.keys())
-        # print("removing", owner.id)
-        # print(owner.trace_number)
         del self.keep_buttons[owner.id]
         data = []
         for tr in self.fig.data:
-        # for i in range(len(self.fig.data)):
             if tr.meta != owner.id:
                 data.append(tr)
         self.fig.data = data
-        # lab = self.keep_buttons[owner.id][0].value
-        # print(self.traces[self.keep_buttons[owner.id][0].value])
-        # self.fig.add_trace(self.fig.data[self.traces[lab]])
-        # print(self.traces[self.keep_buttons[owner.number][0].value])
-        # self.fig.add_trace(self.fig.data[self.traces[lab]])
-        # # self.fig.data[owner.value]
-        # self.fig.data[-1]["marker"]["color"] = self.keep_buttons[owner.number][2].value
-        # self.fig.data[-1]["showlegend"] = False
-        # # lab = 
-        # for key, val in self.slider.items():
-        #         if not val.disabled:
-        #             lab = "{},{}:{}".format(lab, key, val.value)
-        # # self.fig.data[-1]["name"] = lab
-        # print(lab)
-        # self.keep_buttons[-1][0] = widgets.Label(value=lab, layout={'width': "150px"})
-        # self.make_keep_button()
         self.mbox = [self.fig] + self.vbox
         for k, b in self.keep_buttons.items():
-            # print(k, b)
             self.mbox.append(widgets.HBox(b))
         self.box.children=tuple(self.mbox)
-        # # self.box.children = widgets.VBox(self.mbox)
-        # self.box.children=tuple(list(self.box.children) + [widgets.HBox(self.keep_buttons[-1])])
-        # self.box.children=tuple([self.fig] + self.vbox + [widgets.HBox(tuple(b) for b in self.keep_buttons)])
-
         return
 
     def update_trace_color(self, change):
@@ -418,5 +242,3 @@ class Slicer1d(Slicer):
             if tr.meta == change["owner"].id:
                 tr["marker"]["color"] = change["new"]
                 return
-
-
