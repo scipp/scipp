a81546a86ecf7eb423920a6688982ef0a116e8be
Igor Gudich <igor.gudich@esss.se>
2019-11-18 09:59:27 +0100

    remove unneeded header include

--- core/include/scipp/core/variable.h
+++ core/include/scipp/core/variable.h
@@ -18,7 +18,6 @@
 #include "scipp/core/except.h"
 #include "scipp/core/slice.h"
 #include "scipp/core/string.h"
-#include "scipp/core/value_and_variance.h"
 #include "scipp/core/values_and_variances_strong_types.h"
 #include "scipp/core/variable_view.h"
 #include "scipp/units/unit.h"
