778394ce7dc97b6751827f225dda404b38519b5d
Neil Vaytet <neil.vaytet@esss.se>
2019-11-19 13:40:29 +0100

    plotting notebook

--- python/src/scipp/plot/plot.py
+++ python/src/scipp/plot/plot.py
@@ -8,7 +8,7 @@ from .._scipp import core as sc
 
 
 def plot(input_data, collapse=None, backend=None, color=None, projection=None,
-         **kwargs):
+         axes=None, **kwargs):
     """
     Wrapper function to plot any kind of dataset
     """
@@ -26,7 +26,7 @@ def plot(input_data, collapse=None, backend=None, color=None, projection=None,
     # Search through the variables and group the 1D datasets that have
     # the same coordinates and units.
     # tobeplotted is a dict that holds three items:
-    # [number_of_dimensions, Dataset, color_list].
+    # [number_of_dimensions, Dataset, color_list, axes].
     tp = type(input_data)
     if tp is sc.DataProxy or tp is sc.DataArray:
         ds = sc.Dataset()
@@ -44,26 +44,24 @@ def plot(input_data, collapse=None, backend=None, color=None, projection=None,
     for name, var in sorted(input_data):
         ndims = len(var.dims)
         sp_dim = var.sparse_dim
-        # col = None
+        ax = axes
         if ndims == 1 or projection == "1d" or projection == "1D":
             # Construct a key from the dimensions
-            key = "{}.".format(str(var.dims))
+            if axes is not None:
+                # Check if we are dealing with a dict mapping dimensions to
+                # labels
+                if isinstance(axes, dict):
+                    key = axes[var.dims[0]]
+                    ax = [key]
+                else:
+                    key = "{}.".format(str(axes))
+            else:
+                key = "{}.".format(str(var.dims))
             # Add unit to key
             if sp_dim is not None:
                 key = "{}{}".format(key, str(var.coords[sp_dim].unit))
             else:
                 key = "{}{}".format(key, str(var.unit))
-            # if auto_color:
-            #     col = get_color(index=color_count)
-            # elif isinstance(color, list):
-            #     col = color[color_count]
-            #     if isinstance(col, int):
-            #         col = get_color(index=col)
-            # elif isinstance(color, int):
-            #     col = get_color(index=color)
-            # else:
-            #     col = color
-            # color_count += 1
         else:
             key = name
 
@@ -80,27 +78,30 @@ def plot(input_data, collapse=None, backend=None, color=None, projection=None,
         color_count += 1
 
         if key not in tobeplotted.keys():
-            tobeplotted[key] = [ndims, sc.Dataset(), []]
-        tobeplotted[key][1][name] = input_data[name]
-        tobeplotted[key][2].append(col)
+            tobeplotted[key] = dict(ndims=ndims, dataset=sc.Dataset(),
+                                    colors=[], axes=ax)
+        tobeplotted[key]["dataset"][name] = input_data[name]
+        tobeplotted[key]["colors"].append(col)
         sparse_dim[key] = sp_dim
 
     # Plot all the subsets
     output = dict()
     for key, val in tobeplotted.items():
         if collapse is not None:
-            output[key] = plot_collapse(input_data=val[1],
+            output[key] = plot_collapse(input_data=val["dataset"],
                                         dim=collapse,
                                         backend=backend,
+                                        axes=val["axes"],
                                         **kwargs)
         else:
-            output[key] = dispatch(input_data=val[1],
+            output[key] = dispatch(input_data=val["dataset"],
                                    name=key,
-                                   ndim=val[0],
+                                   ndim=val["ndims"],
                                    backend=backend,
-                                   color=val[2],
+                                   color=val["colors"],
                                    sparse_dim=sparse_dim[key],
                                    projection=projection,
+                                   axes=val["axes"],
                                    **kwargs)
 
     if backend == "matplotlib":
