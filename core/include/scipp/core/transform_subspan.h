3b9fe4c98663627539d2e7bba11fe2244c1e1455
Simon Heybrock <simon.heybrock@esss.se>
2019-11-20 14:26:22 +0100

    Doxygen.

--- core/include/scipp/core/transform_subspan.h
+++ core/include/scipp/core/transform_subspan.h
@@ -2,14 +2,34 @@
 // Copyright (c) 2019 Scipp contributors (https://github.com/scipp)
 /// @file
 /// @author Simon Heybrock
-#ifndef SCIPP_CORE_TRANSFORM_RESIZE_H
-#define SCIPP_CORE_TRANSFORM_RESIZE_H
+#ifndef SCIPP_CORE_TRANSFORM_SUBSPAN_H
+#define SCIPP_CORE_TRANSFORM_SUBSPAN_H
 
 #include "scipp/core/subspan_view.h"
 #include "scipp/core/transform.h"
 
 namespace scipp::core {
 
+/// Non-element-wise transform.
+///
+/// This is a specialized version of transform, handling the case of inputs (and
+/// output) that differ along one of their dimensions. Applications are mixing
+/// of sparse and dense data, as well as operations that change the length of a
+/// dimension (such as rebin). The syntax for the user-provided operator is
+/// special and differs from that of `transform` and `transform_in_place`, and
+/// also the tuple of supported type combinations is special:
+/// 1. The overload for the transform of the unit is as for `transform`, i.e.,
+///    returns the new unit.
+/// 2. The overload handling the data has one extra argument. This additional
+///    (first) argument is the "out" argument, as used in `transform_in_place`.
+/// 3. The tuple of supported type combinations must include the type of the out
+///    argument as the first type in the inner tuples. Currently all supported
+///    combinations must have the same output type.
+/// 4. The output type and the type of non-sparse inputs that depend on `dim`
+///    must be specified as `span<T>`. The user-provided lambda is called with a
+///    span of values for these arguments.
+/// 5. Use the flag transform_flags::expect_variance_arg<0> to control whether
+///    the output should have variances or not.
 template <class Types, class Op>
 [[nodiscard]] Variable transform_subspan(const Dim dim, const scipp::index size,
                                          VariableConstProxy var1,
@@ -50,4 +70,4 @@ template <class Types, class Op>
 
 } // namespace scipp::core
 
-#endif // SCIPP_CORE_TRANSFORM_RESIZE_H
+#endif // SCIPP_CORE_TRANSFORM_SUBSPAN_H
