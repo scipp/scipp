b0cc6f32d709ac74a4fe493449420d077b4e5f28
Owen Arnold <arnow@tessella.co.uk>
2019-11-26 14:39:38 +0000

    Working commands for conda build and install
    Tested on win64 and then updated in conda configuration and new bat
    script. A number of small issues with cross platform path differences
    and exact tooling fixed.

---
+++ conda/bld.bat
@@ -0,0 +1,5 @@
+cmake -G"Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX% -DPYTHON_EXECUTABLE=%CONDA_PREFIX%\python -DWITH_CXX_TEST=OFF ..
+
+cmake --build . --target install --config Release
+
+move %CONDA_PREFIX%\scipp %CONDA_PREFIX%\lib\
