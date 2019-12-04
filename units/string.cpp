1eaff01116f5b6145bc3a3d7341abd62d20c99d4
Simon Heybrock <simon.heybrock@esss.se>
2019-12-04 12:42:57 +0100

    Add missing symbol visibility macro.

--- units/string.cpp
+++ units/string.cpp
@@ -19,7 +19,7 @@ template <class T> std::string to_string(const std::initializer_list<T> items) {
   return ss.str();
 }
 
-template std::string to_string<scipp::units::neutron::Unit>(
+template SCIPP_UNITS_EXPORT std::string to_string<scipp::units::neutron::Unit>(
     const std::initializer_list<scipp::units::neutron::Unit> items);
 
 } // namespace scipp::units
