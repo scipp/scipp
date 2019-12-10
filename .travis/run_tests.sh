13289d0e8e5f8587c3348a4dab7cfaf77a7c4cd1
Owen Arnold <owen.arnold@stfc.ac.uk>
2019-12-10 15:25:31 +0000

    Use travis.yml stage conditions instead

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -2,27 +2,21 @@
 
 set -xe
 
-# Build scipp and test ONLY if not deploying, i.e. not master and a pull_request event
-if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]   ; then
-   if [ "$TRAVIS_PULL_REQUEST_BRANCH" != "master" ]   ; then
-        mkdir -p build
-        mkdir -p install
-        cd build
-        cmake -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=OFF -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE} $@ -DCMAKE_INSTALL_PREFIX=../install ..
-        make -j2 install all-tests all-benchmarks
-        # C++ tests
-        ./common/test/scipp-common-test
-        ./units/test/scipp-units-test
-        ./core/test/scipp-core-test
-        ./neutron/test/scipp-neutron-test
-        
-        # Python tests
-        python3 -m pip install --user -r ../python/requirements.txt
-        export PYTHONPATH=$PYTHONPATH:../install
-        cd ../python
-        python3 -m pytest
-	exit 0
-    fi
-fi
-echo 'build and test step skipped on' $TRAVIS_PULL_REQUEST_BRANCH
-echo 'build type' $TRAVIS_EVENT_TYPE 
+# Build scipp
+mkdir -p build
+mkdir -p install
+cd build
+cmake -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=OFF -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE} $@ -DCMAKE_INSTALL_PREFIX=../install ..
+make -j2 install all-tests all-benchmarks
+
+# C++ tests
+./common/test/scipp-common-test
+./units/test/scipp-units-test
+./core/test/scipp-core-test
+./neutron/test/scipp-neutron-test
+
+# Python tests
+python3 -m pip install --user -r ../python/requirements.txt
+export PYTHONPATH=$PYTHONPATH:../install
+cd ../python
+python3 -m pytest
