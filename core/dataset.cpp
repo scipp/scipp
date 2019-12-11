41e9a9189cc97e75e5ab5e6d0c17ad9112761dea
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2019-12-11 17:07:35 +0000

    Inserting dense data after sparse now throws the correct error again
    - Moved extent == Dims Sparse check outside of `isUnknownEdgeState`, as `isUnknownEdgeState` now treats Dimensions::Sparse as "known"

--- core/dataset.cpp
+++ core/dataset.cpp
@@ -187,10 +187,11 @@ void setExtent(std::unordered_map<Dim, scipp::index> &dims, const Dim dim,
     dims[dim] = extents::makeUnknownEdgeState(extent);
   } else if (extent != Dimensions::Sparse) {
     auto &heldExtent = it->second;
+    if (heldExtent == Dimensions::Sparse) {
+      heldExtent = extents::makeUnknownEdgeState(extent);
+    }
     if (extents::isUnknownEdgeState(heldExtent)) {
-      if (heldExtent == Dimensions::Sparse) {
-        heldExtent = extents::makeUnknownEdgeState(extent);
-      } else if (extents::isSame(extent, heldExtent)) { // Do nothing
+      if (extents::isSame(extent, heldExtent)) { // Do nothing
       } else if (extents::oneLarger(extent, heldExtent) && isCoord) {
         heldExtent = extents::shrink(extent);
       } else if (extents::oneSmaller(extent, heldExtent) && !isCoord) {
