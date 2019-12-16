324d54ee35d26ecc9f65c00c559c79ff0c7c1d48
Simon Heybrock <simon.heybrock@esss.se>
2019-12-16 10:13:52 +0100

    Fix segfault from calling PYthon code without GIL.

---
+++ python/py_object.cpp
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2019 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#include "py_object.h"
+
+namespace py = pybind11;
+
+namespace scipp::python {
+
+PyObject::PyObject(const py::object &object) {
+  if (object) {
+    // It is essential to acquire the GIL here. For reasons not entirely clear,
+    // calling Python code otherwise causes a segfault if the GIL has been
+    // released previously. Since this copy operation is called by anything that
+    // copies variables, this includes almost every C++ function with Python
+    // bindings because we typically do release the GIL everywhere.
+    py::gil_scoped_acquire acquire;
+    py::module copy = py::module::import("copy");
+    py::object deepcopy = copy.attr("deepcopy");
+    m_object = deepcopy(object);
+  } else {
+    m_object = object;
+  }
+}
+
+bool PyObject::operator==(const PyObject &other) const {
+  // Similar to above, not releasing GIL here due to segfault in Python C API
+  // (PyObject_RichCompare).
+  py::gil_scoped_acquire acquire;
+  return to_pybind().equal(other.to_pybind());
+}
+
+} // namespace scipp::python
