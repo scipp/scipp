e93ec6facf21a44ee0ca2f3bfb2229cf5dd1448a
Owen Arnold <owen.arnold@stfc.ac.uk>
2020-01-06 12:37:36 +0000

    Specify working build path and add advanced

--- conda/bld.bat
+++ conda/bld.bat
@@ -7,7 +7,7 @@ echo building in %cd%
 cmake -G"Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX% -DPYTHON_EXECUTABLE=%CONDA_PREFIX%\python -DWITH_CXX_TEST=OFF -DCLCACHE_PATH=%MINICONDA%\Scripts ..
 
 :: Show cmake settings
-cmake -L
+cmake -B .. -LA
 :: The following is disabled owing to build time restrictions. Use clcache to improve build times.
 ::cmake --build . --target all-tests --config Release || echo ERROR && exit /b
 :::: C++ tests
