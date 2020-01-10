5f9ff2e8c9b512aa336eb218d4110503a6cac532
Simon Heybrock <simon.heybrock@esss.se>
2020-01-10 13:09:09 +0100

    Fix bad attrbite/label handling in convert.
    This prevented conversion from TOF to anything else for data that had
    previously been converted back to TOF.

--- neutron/convert.cpp
+++ neutron/convert.cpp
@@ -242,6 +242,10 @@ template <class T> T convert_impl(T d, const Dim from, const Dim to) {
 }
 
 namespace {
+template <class T> constexpr bool is_dataset(const T &d) {
+  return std::is_same_v<T, Dataset>;
+}
+
 template <class T>
 T swap_tof_related_labels_and_attrs(T &&x, const Dim from, const Dim to) {
   auto fields = {"position", "source_position", "sample_position"};
@@ -257,8 +261,9 @@ T swap_tof_related_labels_and_attrs(T &&x, const Dim from, const Dim to) {
         // a subsequent unit conversion of an item on its own would not be
         // possible. It needs to be determined if there is a better way to
         // handle attributes so this can be avoided.
-        for (const auto &item : iter(x))
-          item.second.attrs().set(field, x.labels()[field]);
+        if constexpr (is_dataset(x))
+          for (const auto &item : iter(x))
+            item.second.attrs().set(field, x.labels()[field]);
         x.attrs().set(field, x.labels()[field]);
         x.labels().erase(field);
       }
@@ -266,12 +271,14 @@ T swap_tof_related_labels_and_attrs(T &&x, const Dim from, const Dim to) {
   }
   if (to == Dim::Tof) {
     for (const auto &field : fields) {
-      if (x.labels().contains(field)) {
+      if (x.attrs().contains(field)) {
         x.labels().set(field, x.attrs()[field]);
         x.attrs().erase(field);
-        for (const auto &item : iter(x)) {
-          expect::equals(x.labels()[field], item.second.attrs()[field]);
-          item.second.attrs().erase(field);
+        if constexpr (is_dataset(x)) {
+          for (const auto &item : iter(x)) {
+            expect::equals(x.labels()[field], item.second.attrs()[field]);
+            item.second.attrs().erase(field);
+          }
         }
       }
     }
