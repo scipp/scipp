952a77984b87ee9eb5c79e364450b020b67d72d6
Simon Heybrock <simon.heybrock@esss.se>
2020-01-30 08:09:32 +0100

    Add test highlighting missing variance.

--- core/test/data_array_test.cpp
+++ core/test/data_array_test.cpp
@@ -54,6 +54,15 @@ auto make_histogram() {
   return DataArray(data, {{Dim::X, edges}});
 }
 
+auto make_histogram_no_variance() {
+  auto edges =
+      makeVariable<double>(Dims{Dim::Y, Dim::X}, Shape{2, 3},
+                           units::Unit(units::us), Values{0, 2, 4, 1, 3, 5});
+  auto data = makeVariable<double>(Dims{Dim::X}, Shape{2}, Values{2.0, 3.0});
+
+  return DataArray(data, {{Dim::X, edges}});
+}
+
 TEST(DataArraySparseArithmeticTest, fail_sparse_op_non_histogram) {
   const auto sparse = make_sparse();
   auto coord = makeVariable<double>(Dims{Dim::Y, Dim::X}, Shape{2, 2},
@@ -91,6 +100,34 @@ TEST(DataArraySparseArithmeticTest, sparse_times_histogram) {
                              Variances{1, 1, 1, 1}) *
         makeVariable<double>(Dims{Dim::X}, Shape{4}, Values{2.0, 2.0, 3.0, 0.0},
                              Variances{0.3, 0.3, 0.4, 0.0});
+    EXPECT_TRUE(equals(out_vals[1], expected.values<double>()));
+    EXPECT_TRUE(equals(out_vars[1], expected.variances<double>()));
+  }
+  EXPECT_EQ(copy(sparse) *= hist, sparse * hist);
+}
+
+TEST(DataArraySparseArithmeticTest, sparse_times_histogram_without_variances) {
+  const auto sparse = make_sparse();
+  auto hist = make_histogram_no_variance();
+
+  for (const auto result : {sparse * hist, hist * sparse}) {
+    EXPECT_EQ(result.coords()[Dim::X], sparse.coords()[Dim::X]);
+    EXPECT_TRUE(result.hasVariances());
+
+    const auto out_vals = result.data().sparseValues<double>();
+    const auto out_vars = result.data().sparseVariances<double>();
+
+    auto expected =
+        makeVariable<double>(Dims{Dim::X}, Shape{3}, Values{1, 1, 1},
+                             Variances{1, 1, 1}) *
+        makeVariable<double>(Dims{Dim::X}, Shape{3}, Values{2.0, 3.0, 3.0});
+    EXPECT_TRUE(equals(out_vals[0], expected.values<double>()));
+    EXPECT_TRUE(equals(out_vars[0], expected.variances<double>()));
+    // out of range of edges -> value set to 0, consistent with rebin behavior
+    expected = makeVariable<double>(Dims{Dim::X}, Shape{4}, Values{1, 1, 1, 1},
+                                    Variances{1, 1, 1, 1}) *
+               makeVariable<double>(Dims{Dim::X}, Shape{4},
+                                    Values{2.0, 2.0, 3.0, 0.0});
     EXPECT_TRUE(equals(out_vals[1], expected.values<double>()));
     EXPECT_TRUE(equals(out_vars[1], expected.variances<double>()));
   }
