6ddbec849d19903bb4f1d2f6afe534293084268f
Dan Nixon <dan@dan-nixon.com>
2020-02-17 19:38:53 +0000

    Run cmake-format

--- CMake/Eigen.cmake
+++ CMake/Eigen.cmake
@@ -1,10 +1,13 @@
 # Download and unpack Eigen at configure time
-configure_file(${CMAKE_SOURCE_DIR}/CMake/Eigen.in
-               ${CMAKE_BINARY_DIR}/Eigen-download/CMakeLists.txt)
+configure_file(
+  ${CMAKE_SOURCE_DIR}/CMake/Eigen.in
+  ${CMAKE_BINARY_DIR}/Eigen-download/CMakeLists.txt
+)
 
 execute_process(
   COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/Eigen-download RESULTS_VARIABLE result)
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/Eigen-download RESULTS_VARIABLE result
+)
 
 if(result)
   message(FATAL_ERROR "CMake step for Eigen failed: ${result}")
@@ -12,7 +15,8 @@ endif()
 
 execute_process(
   COMMAND ${CMAKE_COMMAND} --build .
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/Eigen-download RESULTS_VARIABLE result)
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/Eigen-download RESULTS_VARIABLE result
+)
 
 if(result)
   message(FATAL_ERROR "Build step for Eigen failed: ${result}")
