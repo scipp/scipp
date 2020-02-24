4f0b70aa2384a2c5d9a4060628fb5bb2ebbb1033
Dan Nixon <dan@dan-nixon.com>
2020-02-24 11:29:03 +0000

    Move Variable abs() to element wise operators
    Re #513

--- core/test/element_unary_operations_test.cpp
+++ core/test/element_unary_operations_test.cpp
@@ -10,6 +10,53 @@
 using namespace scipp;
 using namespace scipp::core;
 
+TEST(ElementAbsTest, unit) {
+  units::Unit m(units::m);
+  EXPECT_EQ(element::abs(m), units::abs(m));
+}
+
+TEST(ElementAbsTest, value) {
+  EXPECT_EQ(element::abs(-1.23), std::abs(-1.23));
+  EXPECT_EQ(element::abs(-1.23456789f), std::abs(-1.23456789f));
+}
+
+TEST(ElementAbsTest, value_and_variance) {
+  const ValueAndVariance x(-2.0, 1.0);
+  EXPECT_EQ(element::abs(x), abs(x));
+}
+
+TEST(ElementAbsOutArgTest, unit) {
+  units::Unit m(units::m);
+  units::Unit out(units::dimensionless);
+  element::abs_out_arg(out, m);
+  EXPECT_EQ(out, units::abs(m));
+}
+
+TEST(ElementAbsOutArgTest, value_double) {
+  double out;
+  element::abs_out_arg(out, -1.23);
+  EXPECT_EQ(out, std::abs(-1.23));
+}
+
+TEST(ElementAbsOutArgTest, value_float) {
+  float out;
+  element::abs_out_arg(out, -1.23456789f);
+  EXPECT_EQ(out, std::abs(-1.23456789f));
+}
+
+TEST(ElementAbsOutArgTest, value_and_variance) {
+  const ValueAndVariance x(-2.0, 1.0);
+  ValueAndVariance out(x);
+  element::abs_out_arg(out, x);
+  EXPECT_EQ(out, abs(x));
+}
+
+TEST(ElementAbsOutArgTest, supported_types) {
+  auto supported = decltype(element::abs_out_arg)::types{};
+  std::get<double>(supported);
+  std::get<float>(supported);
+}
+
 TEST(ElementSqrtTest, unit) {
   const units::Unit m2(units::m * units::m);
   EXPECT_EQ(element::sqrt(m2), units::sqrt(m2));
