5cf0729097c897df22d3d91126b42079e6c91aa2
Simon Heybrock <simon.heybrock@esss.se>
2020-03-05 13:31:38 +0100

    Undo bad change in test

--- core/test/dataset_comparison_test.cpp
+++ core/test/dataset_comparison_test.cpp
@@ -37,6 +37,7 @@ protected:
   Dataset_comparison_operators()
       : sparse_variable(makeVariable<event_list<double>>(Dims{Dim::Y, Dim::Z},
                                                          Shape{3, 2})) {
+    dataset.setCoord(Dim::X, makeVariable<double>(Dims{Dim::X}, Shape{4}));
     dataset.setCoord(Dim::Y, makeVariable<double>(Dims{Dim::Y}, Shape{3}));
 
     dataset.setCoord(Dim("labels"), makeVariable<int>(Dims{Dim::X}, Shape{4}));
