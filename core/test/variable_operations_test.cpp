59036c04cf14ba5f904e8887e17d5e1a63119b18
Owen Arnold <owen.arnold@stfc.ac.uk>
2020-03-12 13:37:09 +0000

    Remove unused includes

--- core/test/variable_operations_test.cpp
+++ core/test/variable_operations_test.cpp
@@ -6,9 +6,7 @@
 #include "test_macros.h"
 
 #include "fix_typed_test_suite_warnings.h"
-#include "scipp/common/constants.h"
 #include "scipp/core/dimensions.h"
-#include "scipp/core/event.h"
 #include "scipp/core/except.h"
 #include "scipp/core/variable.h"
 
