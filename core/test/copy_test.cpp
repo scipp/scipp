5ba9cd84461d459085516f7e0cf2c95802f3796b
Simon Heybrock <simon.heybrock@esss.se>
2020-03-17 13:00:37 +0100

    Tests for dataset copy

--- core/test/copy_test.cpp
+++ core/test/copy_test.cpp
@@ -22,6 +22,7 @@ protected:
 };
 
 TEST_F(CopyTest, data_array) { EXPECT_EQ(copy(array), array); }
+TEST_F(CopyTest, dataset) { EXPECT_EQ(copy(dataset), dataset); }
 
 TEST_F(CopyTest, data_array_drop_attrs) {
   auto copied = copy(array, AttrPolicy::Drop);
@@ -31,39 +32,79 @@ TEST_F(CopyTest, data_array_drop_attrs) {
   EXPECT_EQ(copied, array);
 }
 
+TEST_F(CopyTest, dataset_drop_attrs) {
+  // not implemented yet
+  EXPECT_ANY_THROW(copy(dataset, AttrPolicy::Drop));
+}
+
 struct CopyOutArgTest : public CopyTest {
-  CopyOutArgTest() : copied(copy(array)) {
-    copied.data() += 1.0;
-    copied.coords()[Dim::X] += 1.0;
-    copied.coords()[Dim::Y] += 1.0;
-    copied.masks()["masks_x"].assign(~copied.masks()["masks_x"]);
-    copied.attrs()["attr"] += 1.0;
-    EXPECT_NE(copied, array);
+  CopyOutArgTest() : dataset_copy(copy(dataset)), array_copy(copy(array)) {
+    array_copy.data() += 1.0;
+    array_copy.coords()[Dim::X] += 1.0;
+    array_copy.coords()[Dim::Y] += 1.0;
+    array_copy.masks()["masks_x"].assign(~array_copy.masks()["masks_x"]);
+    array_copy.attrs()["attr"] += 1.0;
+    EXPECT_NE(array_copy, array);
+    dataset_copy["data_xyz"].data() += 1.0;
+    dataset_copy["data_xyz"].attrs()["attr"] += 1.0;
+    dataset_copy.coords()[Dim::X] += 1.0;
+    dataset_copy.coords()[Dim::Y] += 1.0;
+    dataset_copy.masks()["masks_x"].assign(~array_copy.masks()["masks_x"]);
+    dataset_copy.attrs()["attr_x"] += 1.0;
+    EXPECT_NE(dataset_copy, dataset);
   }
 
 protected:
-  DataArray copied;
+  Dataset dataset_copy;
+  DataArray array_copy;
 };
 
 TEST_F(CopyOutArgTest, data_array_out_arg) {
   // copy with out arg also copies coords, masks, and attrs
-  EXPECT_EQ(copy(array, copied), array);
-  EXPECT_EQ(copied, array);
+  EXPECT_EQ(copy(array, array_copy), array);
+  EXPECT_EQ(array_copy, array);
+}
+
+TEST_F(CopyOutArgTest, dataset_out_arg) {
+  // copy with out arg also copies coords, masks, and attrs
+  EXPECT_EQ(copy(dataset, dataset_copy), dataset);
+  EXPECT_EQ(dataset_copy, dataset);
 }
 
 TEST_F(CopyOutArgTest, data_array_out_arg_drop_attrs) {
-  copied.attrs()["attr"].assign(array.attrs()["attr"]);
+  array_copy.attrs()["attr"].assign(array.attrs()["attr"]);
 
   // copy with out arg also copies coords, masks, and attrs
-  EXPECT_EQ(copy(array, copied, AttrPolicy::Drop), array);
-  EXPECT_EQ(copied, array);
+  EXPECT_EQ(copy(array, array_copy, AttrPolicy::Drop), array);
+  EXPECT_EQ(array_copy, array);
+}
+
+TEST_F(CopyOutArgTest, dataset_out_arg_drop_attrs) {
+  dataset_copy.attrs()["attr_x"].assign(dataset.attrs()["attr_x"]);
+  dataset_copy["data_xyz"].attrs()["attr"].assign(
+      dataset["data_xyz"].attrs()["attr"]);
+
+  // copy with out arg also copies coords, masks, and attrs
+  EXPECT_EQ(copy(dataset, dataset_copy, AttrPolicy::Drop), dataset);
+  EXPECT_EQ(dataset_copy, dataset);
 }
 
 TEST_F(CopyOutArgTest, data_array_out_arg_drop_attrs_untouched) {
   // copy with out arg leaves items in output that are not in the input
   // untouched. This also applies to dropped attributes.
-  EXPECT_NE(copy(array, copied, AttrPolicy::Drop), array);
-  EXPECT_NE(copied, array);
-  copied.attrs()["attr"].assign(array.attrs()["attr"]);
-  EXPECT_EQ(copied, array);
+  EXPECT_NE(copy(array, array_copy, AttrPolicy::Drop), array);
+  EXPECT_NE(array_copy, array);
+  array_copy.attrs()["attr"].assign(array.attrs()["attr"]);
+  EXPECT_EQ(array_copy, array);
+}
+
+TEST_F(CopyOutArgTest, dataset_out_arg_drop_attrs_untouched) {
+  // copy with out arg leaves items in output that are not in the input
+  // untouched. This also applies to dropped attributes.
+  EXPECT_NE(copy(dataset, dataset_copy, AttrPolicy::Drop), dataset);
+  EXPECT_NE(dataset_copy, dataset);
+  dataset_copy.attrs()["attr_x"].assign(dataset.attrs()["attr_x"]);
+  dataset_copy["data_xyz"].attrs()["attr"].assign(
+      dataset["data_xyz"].attrs()["attr"]);
+  EXPECT_EQ(dataset_copy, dataset);
 }
