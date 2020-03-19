d79fc15c6cbf6f698ba2e3ce5f9237f670fbcb3d
Simon Heybrock <simon.heybrock@esss.se>
2020-03-19 08:04:21 +0100

    Python tests

---
+++ python/tests/test_realign.py
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (c) 2020 Scipp contributors (https://github.com/scipp)
+# @author Simon Heybrock
+import numpy as np
+import pytest
+
+import scipp as sc
+
+from .common import assert_export
+
+
+def make_dataarray():
+    return sc.DataArray(data=sc.Variable(dims=['x'], values=np.ones(4)),
+                        coords={
+                            'x': sc.Variable(dims=['x'], values=np.ones(4)),
+                            'y': sc.Variable(dims=['x'], values=np.arange(4.0))
+                        })
+
+
+def test_realign():
+    base = make_dataarray()
+    y = sc.Variable(dims=['y'], values=[0.0, 4.0])
+    realigned = sc.realign(base, coords={'y': y})
+    assert realigned.data is None
+    assert realigned.unaligned == base
+    expected = sc.DataArray(data=sc.Variable(dims=['y'], values=[4.0]),
+                            coords={'y': y})
+    assert sc.histogram(realigned) == expected
