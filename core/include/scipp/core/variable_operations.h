6af3dcfa87386ffc70764d05e509d25347beeae1
Simon Heybrock <simon.heybrock@esss.se>
2020-04-06 15:09:53 +0200

    Add dedicated variable_trigonometry.h

--- core/include/scipp/core/variable_operations.h
+++ core/include/scipp/core/variable_operations.h
@@ -137,37 +137,6 @@ SCIPP_CORE_EXPORT VariableView sum(const VariableConstView &var, const Dim dim,
 
 SCIPP_CORE_EXPORT Variable copy(const VariableConstView &var);
 
-// Trigonometrics
-[[nodiscard]] SCIPP_CORE_EXPORT Variable sin(const VariableConstView &var);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable sin(Variable &&var);
-SCIPP_CORE_EXPORT VariableView sin(const VariableConstView &var,
-                                   const VariableView &out);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable cos(const VariableConstView &var);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable cos(Variable &&var);
-SCIPP_CORE_EXPORT VariableView cos(const VariableConstView &var,
-                                   const VariableView &out);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable tan(const VariableConstView &var);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable tan(Variable &&var);
-SCIPP_CORE_EXPORT VariableView tan(const VariableConstView &var,
-                                   const VariableView &out);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable asin(const VariableConstView &var);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable asin(Variable &&var);
-SCIPP_CORE_EXPORT VariableView asin(const VariableConstView &var,
-                                    const VariableView &out);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable acos(const VariableConstView &var);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable acos(Variable &&var);
-SCIPP_CORE_EXPORT VariableView acos(const VariableConstView &var,
-                                    const VariableView &out);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable atan(const VariableConstView &var);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable atan(Variable &&var);
-SCIPP_CORE_EXPORT VariableView atan(const VariableConstView &var,
-                                    const VariableView &out);
-[[nodiscard]] SCIPP_CORE_EXPORT Variable atan2(const Variable &y,
-                                               const Variable &x);
-SCIPP_CORE_EXPORT VariableView atan2(const VariableConstView &y,
-                                     const VariableConstView &x,
-                                     const VariableView &out);
-
 // Logical reductions
 [[nodiscard]] SCIPP_CORE_EXPORT Variable any(const VariableConstView &var);
 [[nodiscard]] SCIPP_CORE_EXPORT Variable any(const VariableConstView &var,
