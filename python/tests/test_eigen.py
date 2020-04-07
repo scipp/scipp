8763e064940882f95c45dccf85b224449b810bc2
Neil Vaytet <neil.vaytet@esss.se>
2020-04-07 11:19:27 +0200

    list constructor for Quat

--- python/tests/test_eigen.py
+++ python/tests/test_eigen.py
@@ -7,7 +7,16 @@ import numpy as np
 import scipp as sc
 
 
-def test_create_variable_0D_vector_3_float64():
+def test_create_variable_0D_vector_3_float64_from_list():
+    var = sc.Variable(value=[1, 2, 3],
+                      unit=sc.units.m,
+                      dtype=sc.dtype.vector_3_float64)
+    np.testing.assert_array_equal(var.value, [1, 2, 3])
+    assert var.dtype == sc.dtype.vector_3_float64
+    assert var.unit == sc.units.m
+
+
+def test_create_variable_0D_vector_3_float64_from_numpy():
     var = sc.Variable(value=np.array([1, 2, 3]),
                       unit=sc.units.m,
                       dtype=sc.dtype.vector_3_float64)
@@ -16,7 +25,7 @@ def test_create_variable_0D_vector_3_float64():
     assert var.unit == sc.units.m
 
 
-def test_create_variable_1D_vector_3_float64():
+def test_create_variable_1D_vector_3_float64_from_list():
     var = sc.Variable(dims=['x'],
                       values=[[1, 2, 3], [4, 5, 6]],
                       unit=sc.units.m,
@@ -29,7 +38,29 @@ def test_create_variable_1D_vector_3_float64():
     assert var.unit == sc.units.m
 
 
-def test_create_quaternion_float64():
+def test_create_variable_1D_vector_3_float64_from_numpy():
+    var = sc.Variable(dims=['x'],
+                      values=np.array([[1, 2, 3], [4, 5, 6]]),
+                      unit=sc.units.m,
+                      dtype=sc.dtype.vector_3_float64)
+    assert len(var.values) == 2
+    np.testing.assert_array_equal(var.values[0], [1, 2, 3])
+    np.testing.assert_array_equal(var.values[1], [4, 5, 6])
+    assert var.dims == ['x']
+    assert var.dtype == sc.dtype.vector_3_float64
+    assert var.unit == sc.units.m
+
+
+def test_create_quaternion_float64_from_list():
+    quat = sc.Quat([1, 2, 3, 4])
+    assert quat.x() == 1.0
+    assert quat.y() == 2.0
+    assert quat.z() == 3.0
+    assert quat.w() == 4.0
+    np.testing.assert_array_equal(quat.coeffs(), [1, 2, 3, 4])
+
+
+def test_create_quaternion_float64_from_numpy():
     quat = sc.Quat(np.arange(4.0))
     assert quat.x() == 0.0
     assert quat.y() == 1.0
