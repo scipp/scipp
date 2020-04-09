fe0cbb38e4d91c1edf3b2fcf3eed02a1ef743f84
Dan Nixon <dan@dan-nixon.com>
2020-04-09 09:47:59 +0100

    Allow building and running benchmarks in Conda build

--- conda/build.sh
+++ conda/build.sh
@@ -4,6 +4,9 @@ set -ex
 
 mkdir -p 'build' && cd 'build'
 
+# Should benchmarks be built and executed
+WITH_BENCHMARKS="${WITH_BENCHMARKS:-OFF}"
+
 # Perform CMake configuration
 cmake \
   -G Ninja \
@@ -12,6 +15,7 @@ cmake \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_VERSION \
   -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX$OSX_VERSION.sdk" \
   -DWITH_CTEST=OFF \
+  -DWITH_BENCHMARKS="$WITH_BENCHMARKS" \
   ..
 
 # Show cmake settings
@@ -27,6 +31,21 @@ ninja -v all-tests install
 ./variable/test/scipp-variable-test
 ./dataset/test/scipp-dataset-test
 ./neutron/test/scipp-neutron-test
+
+# Build and run benchmarks if they were requested
+if [ "$WITH_BENCHMARKS" != 'OFF' ]; then
+  ninja -v all-benchmarks
+  ./benchmark/dataset_benchmark
+  ./benchmark/dataset_operations_benchmark
+  ./benchmark/event_filter_benchmark
+  ./benchmark/groupby_benchmark
+  ./benchmark/histogram_benchmark
+  ./benchmark/neutron_convert_benchmark
+  ./benchmark/slice_benchmark
+  ./benchmark/sparse_histogram_op_benchmark
+  ./benchmark/transform_benchmark
+  ./benchmark/variable_benchmark
+fi
 
 # Move scipp Python library to site packages location
 mv "$CONDA_PREFIX/scipp" "$CONDA_PREFIX"/lib/python*/
