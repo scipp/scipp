62c925222217ec71e5ba86cb709772c1e5f614ce
Simon Heybrock <simon.heybrock@esss.se>
2020-04-15 15:07:49 +0200

    Fix dtype repr

--- python/variable_instantiate_py_object.cpp
+++ python/variable_instantiate_py_object.cpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2020 Scipp contributors (https://github.com/scipp)
 /// @file
 /// @author Simon Heybrock
+#include "scipp/variable/string.h"
 #include "scipp/variable/variable.tcc"
 
 #include "py_object.h"
@@ -12,3 +13,17 @@ namespace scipp::variable {
 INSTANTIATE_VARIABLE(PyObject, scipp::python::PyObject)
 
 } // namespace scipp::variable
+
+namespace scipp::python {
+// TODO We could actually return __repr__ here, but it may be too large?
+std::string to_string(const PyObject &) { return "<PyObject>"; }
+namespace {
+// Insert classes from scipp::python into formatting registry. The objects
+// themselves do nothing, but the constructor call with comma operator does the
+// insertion.
+auto register_python_types(
+    (variable::formatterRegistry().emplace(
+         dtype<PyObject>, std::make_unique<variable::Formatter<PyObject>>()),
+     0));
+} // namespace
+} // namespace scipp::python
