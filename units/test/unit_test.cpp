9de2448d24c519c783d57ef74c63fca107b75e78
Simon Heybrock <simon.heybrock@esss.se>
2020-04-28 12:02:56 +0200

    Fix test suite name

--- units/test/unit_test.cpp
+++ units/test/unit_test.cpp
@@ -10,7 +10,7 @@
 using namespace scipp;
 using scipp::units::Unit;
 
-TEST(UnitsTest, constants) {
+TEST(UnitTest, constants) {
   EXPECT_EQ(units::dimensionless, Unit(units::boost_units::dimensionless));
   EXPECT_EQ(units::one, Unit(units::boost_units::dimensionless));
   EXPECT_EQ(units::m, Unit(units::boost_units::m));
@@ -25,7 +25,7 @@ TEST(UnitsTest, constants) {
   EXPECT_EQ(units::c, Unit(units::boost_units::c));
 }
 
-TEST(units, c) {
+TEST(UnitTest, c) {
   auto c = 1.0 * units::boost_units::c;
   EXPECT_EQ(c.value(), 1.0);
 
@@ -33,62 +33,62 @@ TEST(units, c) {
   EXPECT_EQ(si_c.value(), 299792458.0);
 }
 
-TEST(Units, cancellation) {
+TEST(UnitTest, cancellation) {
   EXPECT_EQ(Unit(units::deg / units::deg), units::dimensionless);
   EXPECT_EQ(units::deg / units::deg, units::dimensionless);
   EXPECT_EQ(units::deg * Unit(units::rad / units::deg), units::rad);
 }
 
-TEST(Units, sin) {
+TEST(UnitTest, sin) {
   EXPECT_EQ(sin(units::rad), units::dimensionless);
   EXPECT_EQ(sin(units::deg), units::dimensionless);
   EXPECT_THROW(sin(units::m), except::UnitError);
   EXPECT_THROW(sin(units::dimensionless), except::UnitError);
 }
 
-TEST(Units, cos) {
+TEST(UnitTest, cos) {
   EXPECT_EQ(cos(units::rad), units::dimensionless);
   EXPECT_EQ(cos(units::deg), units::dimensionless);
   EXPECT_THROW(cos(units::m), except::UnitError);
   EXPECT_THROW(cos(units::dimensionless), except::UnitError);
 }
 
-TEST(Units, tan) {
+TEST(UnitTest, tan) {
   EXPECT_EQ(tan(units::rad), units::dimensionless);
   EXPECT_EQ(tan(units::deg), units::dimensionless);
   EXPECT_THROW(tan(units::m), except::UnitError);
   EXPECT_THROW(tan(units::dimensionless), except::UnitError);
 }
 
-TEST(Units, asin) {
+TEST(UnitTest, asin) {
   EXPECT_EQ(asin(units::dimensionless), units::rad);
   EXPECT_THROW(asin(units::m), except::UnitError);
   EXPECT_THROW(asin(units::rad), except::UnitError);
   EXPECT_THROW(asin(units::deg), except::UnitError);
 }
 
-TEST(Units, acos) {
+TEST(UnitTest, acos) {
   EXPECT_EQ(acos(units::dimensionless), units::rad);
   EXPECT_THROW(acos(units::m), except::UnitError);
   EXPECT_THROW(acos(units::rad), except::UnitError);
   EXPECT_THROW(acos(units::deg), except::UnitError);
 }
 
-TEST(Units, atan) {
+TEST(UnitTest, atan) {
   EXPECT_EQ(atan(units::dimensionless), units::rad);
   EXPECT_THROW(atan(units::m), except::UnitError);
   EXPECT_THROW(atan(units::rad), except::UnitError);
   EXPECT_THROW(atan(units::deg), except::UnitError);
 }
 
-TEST(Unit, construct) { ASSERT_NO_THROW(Unit u{units::dimensionless}); }
+TEST(UnitTest, construct) { ASSERT_NO_THROW(Unit u{units::dimensionless}); }
 
-TEST(Unit, construct_default) {
+TEST(UnitTest, construct_default) {
   Unit u;
   ASSERT_EQ(u, units::dimensionless);
 }
 
-TEST(Unit, compare) {
+TEST(UnitTest, compare) {
   Unit u1{units::dimensionless};
   Unit u2{units::m};
   ASSERT_TRUE(u1 == u1);
@@ -98,7 +98,7 @@ TEST(Unit, compare) {
   ASSERT_FALSE(u2 != u2);
 }
 
-TEST(Unit, add) {
+TEST(UnitTest, add) {
   Unit a{units::dimensionless};
   Unit b{units::m};
   Unit c{units::m * units::m};
@@ -113,7 +113,7 @@ TEST(Unit, add) {
   EXPECT_THROW(c + b, except::UnitError);
 }
 
-TEST(Unit, multiply) {
+TEST(UnitTest, multiply) {
   Unit a{units::dimensionless};
   Unit b{units::m};
   Unit c{units::m * units::m};
@@ -128,14 +128,14 @@ TEST(Unit, multiply) {
   EXPECT_THROW(c * c, except::UnitError);
 }
 
-TEST(Unit, multiply_counts) {
+TEST(UnitTest, multiply_counts) {
   Unit counts{units::counts};
   Unit none{units::dimensionless};
   EXPECT_EQ(counts * none, counts);
   EXPECT_EQ(none * counts, counts);
 }
 
-TEST(Unit, divide) {
+TEST(UnitTest, divide) {
   Unit one{units::dimensionless};
   Unit l{units::m};
   Unit t{units::s};
@@ -147,12 +147,12 @@ TEST(Unit, divide) {
   EXPECT_THROW(one / v, except::UnitError);
 }
 
-TEST(Unit, divide_counts) {
+TEST(UnitTest, divide_counts) {
   Unit counts{units::counts};
   EXPECT_EQ(counts / counts, units::dimensionless);
 }
 
-TEST(Unit, conversion_factors) {
+TEST(UnitTest, conversion_factors) {
   boost::units::quantity<units::detail::tof::wavelength> a(
       2.0 * units::boost_units::angstrom);
   boost::units::quantity<boost::units::si::length> b(
@@ -181,7 +181,7 @@ TEST(Unit, conversion_factors) {
   EXPECT_DOUBLE_EQ(h.value(), 9.0e6);
 }
 
-TEST(Unit, c) {
+TEST(UnitTest, c) {
   Unit c(units::c);
   EXPECT_EQ(c * units::m, Unit(units::c * units::m));
   EXPECT_EQ(c * units::m / units::m, units::c);
@@ -189,18 +189,18 @@ TEST(Unit, c) {
   EXPECT_EQ(units::meV / c / units::meV, Unit(units::dimensionless / units::c));
 }
 
-TEST(Unit, sqrt) {
+TEST(UnitTest, sqrt) {
   EXPECT_EQ(sqrt(units::m * units::m), units::m);
   EXPECT_EQ(sqrt(units::one), units::one);
 }
 
-TEST(Unit, sqrt_fail) {
+TEST(UnitTest, sqrt_fail) {
   Unit m{units::m};
   EXPECT_THROW_MSG(sqrt(m), except::UnitError,
                    "Unsupported unit as result of sqrt: sqrt(m).");
 }
 
-TEST(Unit, isCounts) {
+TEST(UnitTest, isCounts) {
   EXPECT_FALSE(units::dimensionless.isCounts());
   EXPECT_TRUE(units::counts.isCounts());
   EXPECT_FALSE(Unit(units::counts / units::us).isCounts());
@@ -208,7 +208,7 @@ TEST(Unit, isCounts) {
   EXPECT_FALSE(Unit(units::dimensionless / units::m).isCounts());
 }
 
-TEST(Unit, isCountDensity) {
+TEST(UnitTest, isCountDensity) {
   EXPECT_FALSE(units::dimensionless.isCountDensity());
   EXPECT_FALSE(units::counts.isCountDensity());
   EXPECT_TRUE(Unit(units::counts / units::us).isCountDensity());
