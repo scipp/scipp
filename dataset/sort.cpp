69540717befacb2dbd8bd55ebcd57fbdf50d9638
Simon Heybrock <simon.heybrock@esss.se>
2020-05-07 07:56:44 +0200

    Sort based on range-v3 zip view

--- dataset/sort.cpp
+++ dataset/sort.cpp
@@ -5,6 +5,7 @@
 #include <numeric>
 
 #include "scipp/core/element/sort.h"
+#include "scipp/core/parallel.h"
 #include "scipp/core/tag_util.h"
 #include "scipp/dataset/except.h"
 #include "scipp/dataset/sort.h"
@@ -64,22 +65,30 @@ Variable sort(const VariableConstView &var, const VariableConstView &key) {
 
 namespace {
 template <class T>
-auto sort(const T &var, const Dim dim, const VariableConstView &key) {
+auto permute(const T &var, const Dim dim, const VariableConstView &key) {
   if (dim != key.dims().inner())
     throw except::DimensionError("Sort key must depend on sort dimension");
-  return dataset::sort(var, key);
+  if constexpr (std::is_same_v<T, DataArrayConstView>)
+    return dataset::permute(var.data(), key);
+  else
+    return dataset::permute(var, key);
 }
 
-Dimensions sort(const Dimensions &dims, const Dim, const VariableConstView &) {
+Dimensions permute(const Dimensions &dims, const Dim,
+                   const VariableConstView &) {
   return dims;
 }
 } // namespace
 
+DataArray permute(const DataArrayConstView &array, const Dim dim,
+                  const VariableConstView &permutation) {
+  return apply_or_copy_dim(
+      array, [](auto &&... _) { return permute(_...); }, dim, permutation);
+}
+
 /// Return a DataArray sorted based on key.
 DataArray sort(const DataArrayConstView &array, const VariableConstView &key) {
-  return apply_or_copy_dim(
-      array, [](auto &&... _) { return sort(_...); }, key.dims().inner(),
-      makePermutation(key));
+  return permute(array, key.dims().inner(), makePermutation(key));
 }
 
 /// Return a DataArray sorted based on coordinate.
@@ -87,10 +96,37 @@ DataArray sort(const DataArrayConstView &array, const Dim &key) {
   return sort(array, array.coords()[key]);
 }
 
+void sort_v3(const VariableConstView &key, const VariableView &value) {
+  const Dim dim = key.dims().inner();
+  if (!value.dims().contains(dim))
+    return;
+  Variable key_(key);
+  variable::transform_in_place(variable::subspan_view(key_, dim),
+                               variable::subspan_view(value, dim),
+                               core::element::sort);
+}
+
 /// Return a Dataset sorted based on key.
 Dataset sort(const DatasetConstView &dataset, const VariableConstView &key) {
+  Dataset sorted(dataset);
+  std::vector<VariableView> to_sort;
+  for (const auto &item : sorted)
+    to_sort.emplace_back(item.data());
+  for (const auto &item : sorted.coords())
+    to_sort.emplace_back(item.second);
+  for (const auto &item : sorted.masks())
+    to_sort.emplace_back(item.second);
+  for (const auto &item : sorted.attrs())
+    to_sort.emplace_back(item.second);
+  core::parallel::parallel_for(
+      core::parallel::blocked_range(0, to_sort.size()), [&](const auto &range) {
+        for (scipp::index i = range.begin(); i != range.end(); ++i)
+          sort_v3(key, to_sort[i]);
+      });
+
+  return sorted;
   return apply_to_items(
-      dataset, [](auto &&... _) { return sort(_...); }, key.dims().inner(),
+      dataset, [](auto &&... _) { return permute(_...); }, key.dims().inner(),
       makePermutation(key));
 }
 
