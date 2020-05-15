3a130e7d5bec74905320a86077b2d87f466a1283
Simon Heybrock <simon.heybrock@esss.se>
2020-05-15 17:58:00 +0200

    Use lambdas instead of structs

--- core/include/scipp/core/element/comparison.h
+++ core/include/scipp/core/element/comparison.h
@@ -10,11 +10,9 @@
 #include "scipp/core/element/arg_list.h"
 #include "scipp/core/transform_common.h"
 
-namespace scipp::core {
-
 /// Operators to be used with transform and transform_in_place to implement
 /// operations for Variable.
-namespace element {
+namespace scipp::core::element {
 
 constexpr auto comparison =
     overloaded{arg_list<double, float, int64_t, int32_t>,
@@ -50,25 +48,20 @@ constexpr auto equal = overloaded{
 constexpr auto not_equal =
     overloaded{comparison, [](const auto &x, const auto &y) { return x != y; }};
 
-struct max_equals
-    : public transform_flags::expect_in_variance_if_out_variance_t {
-  template <class A, class B>
-  constexpr void operator()(A &&a, const B &b) const noexcept {
-    using std::max;
-    a = max(a, b);
-  }
-  using types = pair_self_t<double, float, int64_t, int32_t>;
-};
-struct min_equals
-    : public transform_flags::expect_in_variance_if_out_variance_t {
-  template <class A, class B>
-  constexpr void operator()(A &&a, const B &b) const noexcept {
-    using std::min;
-    a = min(a, b);
-  }
-  using types = pair_self_t<double, float, int64_t, int32_t>;
-};
+constexpr auto max_equals =
+    overloaded{arg_list<double, float, int64_t, int32_t>,
+               transform_flags::expect_in_variance_if_out_variance,
+               [](auto &&a, const auto &b) {
+                 using std::max;
+                 a = max(a, b);
+               }};
 
-} // namespace element
+constexpr auto min_equals =
+    overloaded{arg_list<double, float, int64_t, int32_t>,
+               transform_flags::expect_in_variance_if_out_variance,
+               [](auto &&a, const auto &b) {
+                 using std::min;
+                 a = min(a, b);
+               }};
 
-} // namespace scipp::core
+} // namespace scipp::core::element
--- core/include/scipp/core/transform_common.h
+++ core/include/scipp/core/transform_common.h
@@ -71,14 +71,12 @@ using expect_no_variance_arg_t = decltype(expect_no_variance_arg<N>);
 /// variances in the specified argument.
 template <int N> static constexpr auto expect_variance_arg = []() {};
 template <int N> using expect_variance_arg_t = decltype(expect_variance_arg<N>);
-// TODO Change to lambda when element/comparison.h is refactored to use lambdas.
-struct expect_in_variance_if_out_variance_t {
-  void operator()() {}
-};
+
 /// Add this to overloaded operator to indicate that the in-place operation
 /// requires inputs to have a variance if the output has a variance.
-static constexpr auto expect_in_variance_if_out_variance =
-    expect_in_variance_if_out_variance_t{};
+static constexpr auto expect_in_variance_if_out_variance = []() {};
+using expect_in_variance_if_out_variance_t =
+    decltype(expect_in_variance_if_out_variance);
 
 static constexpr auto expect_all_or_none_have_variance = []() {};
 using expect_all_or_none_have_variance_t =
