2f696da910f6178ceb2a3f0b31a748845809d9d5
Simon Heybrock <simon.heybrock@esss.se>
2020-05-28 16:21:52 +0200

    Minor cleanup in special value replacement kernels

--- core/include/scipp/core/element/unary_operations.h
+++ core/include/scipp/core/element/unary_operations.h
@@ -10,79 +10,64 @@
 #include "scipp/core/element/arg_list.h"
 #include "scipp/core/transform_common.h"
 
-namespace scipp::core {
+namespace scipp::core::element {
 
-/// Operators to be used with transform and transform_in_place to implement
-/// operations for Variable.
-namespace element {
+constexpr auto replace_special =
+    overloaded{transform_flags::expect_all_or_none_have_variance,
+               [](const units::Unit &x, const units::Unit &repl) {
+                 expect::equals(x, repl);
+                 return x;
+               }};
 
-auto unit_check_and_assign = [](units::Unit &a, const units::Unit &b,
-                                const units::Unit &repl) {
-  expect::equals(b, repl);
-  a = b;
-};
-
-auto unit_check_and_return = [](const units::Unit &x, const units::Unit &repl) {
-  expect::equals(x, repl);
-  return x;
-};
+constexpr auto replace_special_out_arg = overloaded{
+    transform_flags::expect_all_or_none_have_variance,
+    [](units::Unit &a, const units::Unit &b, const units::Unit &repl) {
+      expect::equals(b, repl);
+      a = b;
+    }};
 
 constexpr auto nan_to_num =
-    overloaded{transform_flags::expect_all_or_none_have_variance,
-               [](const auto x, const auto &repl) {
+    overloaded{replace_special, [](const auto x, const auto &repl) {
                  using std::isnan;
                  return isnan(x) ? repl : x;
-               },
-               unit_check_and_return};
+               }};
 
-constexpr auto nan_to_num_out_arg =
-    overloaded{transform_flags::expect_all_or_none_have_variance,
-               [](auto &x, const auto y, const auto &repl) {
-                 using std::isnan;
-                 x = isnan(y) ? repl : y;
-               },
-               unit_check_and_assign};
+constexpr auto nan_to_num_out_arg = overloaded{
+    replace_special_out_arg, [](auto &x, const auto y, const auto &repl) {
+      using std::isnan;
+      x = isnan(y) ? repl : y;
+    }};
 
 constexpr auto positive_inf_to_num =
-    overloaded{transform_flags::expect_all_or_none_have_variance,
-               [](const auto &x, const auto &repl) {
+    overloaded{replace_special, [](const auto &x, const auto &repl) {
                  if constexpr (is_ValueAndVariance_v<std::decay_t<decltype(x)>>)
                    return isinf(x) && x.value > 0 ? repl : x;
                  else
                    return std::isinf(x) && x > 0 ? repl : x;
-               },
-               unit_check_and_return};
+               }};
 
-constexpr auto positive_inf_to_num_out_arg =
-    overloaded{transform_flags::expect_all_or_none_have_variance,
-               [](auto &x, const auto &y, const auto &repl) {
-                 if constexpr (is_ValueAndVariance_v<std::decay_t<decltype(y)>>)
-                   x = isinf(y) && y.value > 0 ? repl : y;
-                 else
-                   x = std::isinf(y) && y > 0 ? repl : y;
-               },
-               unit_check_and_assign};
+constexpr auto positive_inf_to_num_out_arg = overloaded{
+    replace_special_out_arg, [](auto &x, const auto &y, const auto &repl) {
+      if constexpr (is_ValueAndVariance_v<std::decay_t<decltype(y)>>)
+        x = isinf(y) && y.value > 0 ? repl : y;
+      else
+        x = std::isinf(y) && y > 0 ? repl : y;
+    }};
 
 constexpr auto negative_inf_to_num =
-    overloaded{transform_flags::expect_all_or_none_have_variance,
-               [](const auto &x, const auto &repl) {
+    overloaded{replace_special, [](const auto &x, const auto &repl) {
                  if constexpr (is_ValueAndVariance_v<std::decay_t<decltype(x)>>)
                    return isinf(x) && x.value < 0 ? repl : x;
                  else
                    return std::isinf(x) && x < 0 ? repl : x;
-               },
-               unit_check_and_return};
+               }};
 
-constexpr auto negative_inf_to_num_out_arg =
-    overloaded{transform_flags::expect_all_or_none_have_variance,
-               [](auto &x, const auto &y, const auto &repl) {
-                 if constexpr (is_ValueAndVariance_v<std::decay_t<decltype(y)>>)
-                   x = isinf(y) && y.value < 0 ? repl : y;
-                 else
-                   x = std::isinf(y) && y < 0 ? repl : y;
-               },
-               unit_check_and_assign};
+constexpr auto negative_inf_to_num_out_arg = overloaded{
+    replace_special_out_arg, [](auto &x, const auto &y, const auto &repl) {
+      if constexpr (is_ValueAndVariance_v<std::decay_t<decltype(y)>>)
+        x = isinf(y) && y.value < 0 ? repl : y;
+      else
+        x = std::isinf(y) && y < 0 ? repl : y;
+    }};
 
-} // namespace element
-
-} // namespace scipp::core
+} // namespace scipp::core::element
