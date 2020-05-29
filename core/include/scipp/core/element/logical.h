1b52f29c9b043e62d83b0b29ca2966dac710f35e
Simon Heybrock <simon.heybrock@esss.se>
2020-05-29 07:23:50 +0200

    Do not use bitwise operators for bool

--- core/include/scipp/core/element/logical.h
+++ core/include/scipp/core/element/logical.h
@@ -12,27 +12,27 @@ namespace scipp::core::element {
 
 constexpr auto logical_and_equals =
     overloaded{arg_list<bool>, dimensionless_unit_check,
-               [](auto &&a, const auto &b) { a &= b; }};
+               [](auto &&a, const auto &b) { a = a && b; }};
 
 constexpr auto logical_or_equals =
     overloaded{arg_list<bool>, dimensionless_unit_check,
-               [](auto &&a, const auto &b) { a |= b; }};
+               [](auto &&a, const auto &b) { a = a || b; }};
 
 constexpr auto logical_xor_equals =
     overloaded{arg_list<bool>, dimensionless_unit_check,
-               [](auto &&a, const auto &b) { a ^= b; }};
+               [](auto &&a, const auto &b) { a = a != b; }};
 
 constexpr auto logical_and = overloaded{
     arg_list<bool>, dimensionless_unit_check_return,
-    [](const auto &var_, const auto &other_) { return var_ & other_; }};
+    [](const auto &var_, const auto &other_) { return var_ && other_; }};
 
 constexpr auto logical_or = overloaded{
     arg_list<bool>, dimensionless_unit_check_return,
-    [](const auto &var_, const auto &other_) { return var_ | other_; }};
+    [](const auto &var_, const auto &other_) { return var_ || other_; }};
 
 constexpr auto logical_xor = overloaded{
     arg_list<bool>, dimensionless_unit_check_return,
-    [](const auto &var_, const auto &other_) { return var_ ^ other_; }};
+    [](const auto &var_, const auto &other_) { return var_ != other_; }};
 
 constexpr auto logical_not =
     overloaded{arg_list<bool>, dimensionless_unit_check_return,
