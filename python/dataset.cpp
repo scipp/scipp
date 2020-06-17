3ab41eda5d32b55a175e5a6f15eaab1fc58acc5c
Simon Heybrock <simon.heybrock@esss.se>
2020-06-17 06:49:24 +0200

    clang-format

--- python/dataset.cpp
+++ python/dataset.cpp
@@ -331,15 +331,16 @@ void init_dataset(py::module &m) {
   py::class_<DataArray> dataArray(m, "DataArray", R"(
     Named variable with associated coords, masks, and attributes.)");
   dataArray.def(py::init<const DataArrayConstView &>());
-  dataArray.def(py::init([](VariableConstView data, std::map<Dim, VariableConstView> coords,
+  dataArray.def(py::init([](VariableConstView data,
+                            std::map<Dim, VariableConstView> coords,
                             std::map<std::string, VariableConstView> masks,
-                            std::map<std::string, VariableConstView> attrs){
-    return DataArray{Variable{data}, coords, masks, attrs};
-  }),
-  py::arg("data") = Variable{},
-  py::arg("coords") = std::map<Dim, VariableConstView>{},
-  py::arg("masks") = std::map<std::string, VariableConstView>{},
-  py::arg("attrs") = std::map<std::string, VariableConstView>{});
+                            std::map<std::string, VariableConstView> attrs) {
+                  return DataArray{Variable{data}, coords, masks, attrs};
+                }),
+                py::arg("data") = Variable{},
+                py::arg("coords") = std::map<Dim, VariableConstView>{},
+                py::arg("masks") = std::map<std::string, VariableConstView>{},
+                py::arg("attrs") = std::map<std::string, VariableConstView>{});
 
   py::class_<DataArrayConstView>(m, "DataArrayConstView")
       .def(py::init<const DataArray &>());
