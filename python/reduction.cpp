e730ad2f5fa486439d0b7a9d6fb83c87e0efe3f1
Owen Arnold <owen.arnold@stfc.ac.uk>
2020-06-25 15:30:59 +0100

    Expose and and all dimensionless
    These now are ovearloaded like max and min

--- python/reduction.cpp
+++ python/reduction.cpp
@@ -155,7 +155,7 @@ template <class T> void bind_max(py::module &m) {
           .c_str());
 }
 
-template <class T> Docstring docstring_bool(const std::string op) {
+template <class T> Docstring docstring_bool_dim(const std::string op) {
   return Docstring()
       .description("Element-wise " + op + " over the specified dimension.")
       .raises("If the dimension does not exist, or if the dtype is not bool.")
@@ -165,24 +165,42 @@ template <class T> Docstring docstring_bool(const std::string op) {
       .param("dim", "Dimension to reduce.", "Dim");
 }
 
+template <class T> Docstring docstring_bool(const std::string op) {
+  return Docstring()
+      .description("Element-wise " + op +
+                   " over all of the input's dimensions.")
+      .raises("If the dtype has no " + op + ", e.g., if it is a string.")
+      .returns("The " + op + " combination of the input values.")
+      .rtype<T>()
+      .template param<T>("x", "Data to reduce.");
+}
+
 template <class T> void bind_all(py::module &m) {
+  m.def(
+      "all", [](const typename T::const_view_type &x) { return all(x); },
+      py::arg("x"), py::call_guard<py::gil_scoped_release>(),
+      docstring_bool<T>("AND").c_str());
   m.def(
       "all",
       [](const typename T::const_view_type &x, const Dim dim) {
         return all(x, dim);
       },
-      py::arg("x"), py::arg("dim"), py::call_guard<py::gil_scoped_release>(),
-      docstring_bool<T>("AND").c_str());
+      py::arg("all"), py::arg("dim"), py::call_guard<py::gil_scoped_release>(),
+      docstring_bool_dim<T>("AND").c_str());
 }
 
 template <class T> void bind_any(py::module &m) {
+  m.def(
+      "any", [](const typename T::const_view_type &x) { return any(x); },
+      py::arg("x"), py::call_guard<py::gil_scoped_release>(),
+      docstring_bool<T>("OR").c_str());
   m.def(
       "any",
       [](const typename T::const_view_type &x, const Dim dim) {
         return any(x, dim);
       },
       py::arg("x"), py::arg("dim"), py::call_guard<py::gil_scoped_release>(),
-      docstring_bool<T>("OR").c_str());
+      docstring_bool_dim<T>("OR").c_str());
 }
 
 void init_reduction(py::module &m) {
