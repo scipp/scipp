d4a9485b66c506b02d901206156125ce6882d83c
Neil Vaytet <neil.vaytet@esss.se>
2020-07-04 16:07:34 +0200

    add unit test for realigned

--- python/tests/compat/test_dict.py
+++ python/tests/compat/test_dict.py
@@ -56,16 +56,54 @@ def test_data_array_to_dict():
         attrs={"attr1": sc.Variable(dims=["x"], values=np.random.random(10))},
         data=sc.Variable(dims=["y", "x"], values=np.random.random([5, 10])))
     da_dict = sc.to_dict(da)
-    assert da_dict["dims"] == da.dims
-    assert da_dict["shape"] == da.shape
-    assert np.array_equal(da_dict["values"], da.values)
-    assert np.array_equal(da_dict["variances"], da.variances)
-    assert da_dict["unit"] == da.unit
-    assert da_dict["dtype"] == da.dtype
+    assert da_dict["data"]["dims"] == da.dims
+    assert da_dict["data"]["shape"] == da.shape
+    assert np.array_equal(da_dict["data"]["values"], da.values)
+    assert np.array_equal(da_dict["data"]["variances"], da.variances)
+    assert da_dict["data"]["unit"] == da.unit
+    assert da_dict["data"]["dtype"] == da.dtype
     assert sc.from_dict(da_dict["coords"]["x"]) == da.coords["x"]
     assert sc.from_dict(da_dict["coords"]["y"]) == da.coords["y"]
     assert sc.from_dict(da_dict["masks"]["amask"]) == da.masks["amask"]
     assert sc.from_dict(da_dict["attrs"]["attr1"]) == da.attrs["attr1"]
+
+
+def test_data_array_unaligned_to_dict():
+    N = 50
+    values = 10 * np.random.rand(N)
+    da = sc.DataArray(data=sc.Variable(dims=['position'],
+                                       unit=sc.units.counts,
+                                       values=values,
+                                       variances=values),
+                      coords={
+                          'position':
+                          sc.Variable(
+                              dims=['position'],
+                              values=['site-{}'.format(i) for i in range(N)]),
+                          'x':
+                          sc.Variable(dims=['position'],
+                                      unit=sc.units.m,
+                                      values=np.random.rand(N)),
+                          'y':
+                          sc.Variable(dims=['position'],
+                                      unit=sc.units.m,
+                                      values=np.random.rand(N))
+                      })
+    xbins = sc.Variable(dims=['x'], unit=sc.units.m, values=[0.1, 0.5, 0.9])
+    ybins = sc.Variable(dims=['y'],
+                        unit=sc.units.m,
+                        values=[0.1, 0.3, 0.5, 0.7, 0.9])
+    realigned = sc.realign(da, {'y': ybins, 'x': xbins})
+
+    da_dict = sc.to_dict(realigned)
+    assert "unaligned" in da_dict
+    assert "data" not in da_dict
+    assert sc.from_dict(da_dict["coords"]["x"]) == xbins
+    assert sc.from_dict(da_dict["coords"]["y"]) == ybins
+    assert sc.from_dict(
+        da_dict["unaligned"]["coords"]["x"]) == realigned.unaligned.coords["x"]
+    assert sc.from_dict(
+        da_dict["unaligned"]["coords"]["y"]) == realigned.unaligned.coords["y"]
 
 
 def test_data_array_from_dict():
