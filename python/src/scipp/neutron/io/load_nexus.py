ea9633517152f092d6c7b389ef89bfe419afd1fa
Neil Vaytet <neil.vaytet@esss.se>
2020-07-13 11:14:57 +0200

    cleanup

--- python/src/scipp/neutron/io/load_nexus.py
+++ python/src/scipp/neutron/io/load_nexus.py
@@ -2,19 +2,7 @@ from ..._scipp import core as sc
 
 import h5py
 import numpy as np
-
-# class NxsData:
-#     """
-#     Class to hold data in Nexus file
-#     """
-
-#     def __init__(self, fields=None):
-
-#         for key in fields.keys():
-#             setattr(self, key, fields[key]["data"])
-
-#         return
-
+from os.path import join
 from timeit import default_timer as timer
 
 
@@ -25,61 +13,30 @@ def load_nexus(filename, entry="/", verbose=False, convert_ids=False):
     i.e. if more than one is present in the file, the data will be read
     from the first one found.
     """
+    total_time = timer()
+
     fields = {}
-
-    # fields = {"x": {"pattern": ["/x_pixel_offset"],
-    #                 "entry": None,
-    #                 "data": None,
-    #                 "dtype": np.float64},
-    #           "y": {"pattern": ["/y_pixel_offset"],
-    #                 "entry": None,
-    #                 "data": None,
-    #                 "dtype": np.float64},
-    #           "title":{"pattern": ["/title"],
-    #                    "entry": None,
-    #                    "data": None,
-    #                    "dtype": str}
-    #           }
-
     fields["event_id"] = {
         "pattern": ["event_id"],
-        "entry": None,
-        "data": None,
         "dtype": np.int32
     }
-    # if tofs:
     fields["event_time_offset"] = {
         "pattern": ["event_time_offset"],
-        "entry": None,
-        "data": None,
         "dtype": np.float64
     }
-
     fields["event_time_zero"] = {
         "pattern": ["event_time_zero"],
-        "entry": None,
-        "data": None,
         "dtype": np.float64
     }
-
     fields["event_index"] = {
         "pattern": ["event_index"],
-        "entry": None,
-        "data": None,
         "dtype": np.float64
     }
+    entries = { key: {} for key in fields}
 
-    entries = {
-        "event_id": {},
-        "event_time_offset": {},
-        "event_time_zero": {},
-        "event_index": {}
-    }
-
-    spec_min = np.Inf
-    spec_max = 0
-
-    spec_num = []
+    spec_min = np.Inf # Min spectrum number
+    spec_max = 0 # Max spectrum number
+    spec_num = [] # List of unique spectral numbers
 
     start = timer()
     with h5py.File(filename, "r", libver='latest', swmr=True) as f:
@@ -87,21 +44,7 @@ def load_nexus(filename, entry="/", verbose=False, convert_ids=False):
         contents = []
         f[entry].visit(contents.append)
 
-        # for item in contents:
-        #     print(item)
-        #     for key in fields.keys():
-        #         if fields[key]["entry"] is None:
-        #             for p in fields[key]["pattern"]:
-        #                 if item.endswith(p):
-        #                     fields[key]["entry"] = item
-
-        # for key in fields.keys():
-        #     fields[key]["data"] = np.array(f[fields[key]["entry"]][...],
-        #                                    dtype=fields[key]["dtype"],
-        #                                    copy=True)
-
         for item in contents:
-            # print(item)
             for key in fields.keys():
                 for p in fields[key]["pattern"]:
                     if item.endswith(p):
@@ -116,13 +59,13 @@ def load_nexus(filename, entry="/", verbose=False, convert_ids=False):
                             spec_num.append(np.unique(entries[key][root]))
     print("Reading file:", timer() - start)
 
+    # Combine all banks into single arrays into a new dict
     data = {}
     for key, item in entries.items():
         data[key] = np.concatenate(list(item.values()))
 
-    for key, item in data.items():
-        print(key, item.shape)
-
+    # Determine number of spectra and create map from spectrum number to
+    # detector id
     start = timer()
     spec_num = np.unique(np.concatenate(spec_num).ravel())
     spec_map = {val: key for (key, val) in enumerate(spec_num)}
@@ -130,73 +73,54 @@ def load_nexus(filename, entry="/", verbose=False, convert_ids=False):
     data['spectrum'] = np.zeros_like(data['event_id'])
     for i in range(nspec):
         data['spectrum'][i] = spec_map[data['event_id'][i]]
-    print(data)
-    print("Spec map:", timer() - start)
+    print("Building spectrum number map:", timer() - start)
 
-
-    # ids = sc.Variable(['x'], values=data['event_id'])
-    # temp = sc.Dataset()
-    # temp['tof'] = sc.Variable(['x'], values=data['event_time_offset'])
-    # temp['ids'] = ids
-
-
+    # Sort the event ids so we can fill in event lists in chunks instead of one
+    # event at a time.
+    # TODO: sort using scipp.sort()
     start = timer()
-    # sort = sc.sort(temp, ids)
-    # print(temp)
     indices = np.argsort(data["event_id"])
     print("ArgSort:", timer() - start)
     start = timer()
     data['event_id'] = data['event_id'][indices]
     data['event_time_offset'] = data['event_time_offset'][indices]
     print("Sort indexing:", timer() - start)
+
+    # Now find the locations where the event ids change/jump. This will tell
+    # us the number of events that are in each pixel.
+    # TODO: Using scipp Variables and indexing to get diff1d:
+    # var[dim, 1:] - var[dim, :-1]
     diff = np.ediff1d(data['event_id'])
-    print(diff)
     locs = np.where(diff > 0)[0]
-    print(locs)
-    print(np.ediff1d(locs))
-    n_events_per_id = np.concatenate([locs[0:1], np.ediff1d(locs)])
+    # Need to add leading zero and the total size of the array at the end
     locs = np.concatenate([[0], locs, [len(data['event_id'])]])
 
-
-
+    # Create event list
     var = sc.Variable(dims=['spectrum'],
                       shape=[nspec],
                       dtype=sc.dtype.event_list_float64)
-
-
+    # Weights are set to one by default
     weights = sc.Variable(
         dims=['spectrum'],
         shape=[nspec],
         unit=sc.units.counts,
         dtype=sc.dtype.event_list_float64,
         variances=True)
-    # for i in range(nspec):
-    #     ones = np.ones(len(var['spectrum', i].values))
-    #     weights['spectrum', i].values = ones
-    #     weights['spectrum', i].variances = ones
 
-
-
+    # Populate event list chunk by chunk
     start = timer()
-    # for key, item in entries["event_id"].items():
-    #     print(key)
-    print(len(locs))
-    print(nspec)
-    print(locs[-1])
     for n in range(nspec):
-        # ispec = spec_map[it]
-        # print(n)
-        # print(locs[n])
         var['spectrum', n].values.extend(data["event_time_offset"][locs[n]:locs[n+1]])
         ones = np.ones_like(var['spectrum', n].values)
         weights['spectrum', n].values = ones
         weights['spectrum', n].variances = ones
-
-        # break
     print("Filling events:", timer() - start)
-    print(var)
+
+    # arange variable for spectra indices
     specs = sc.Variable(dims=['spectrum'], values=np.arange(nspec), dtype=sc.dtype.int32)
+    # Also store the indices in the output? Are they every needed?
     ids = sc.Variable(dims=['spectrum'], values=spec_num, dtype=sc.dtype.int32)
+    # Put everything together in a DataArray
     da = sc.DataArray(data=weights,
                       coords={
                           'spectrum': specs,
@@ -205,223 +129,50 @@ def load_nexus(filename, entry="/", verbose=False, convert_ids=False):
                       })
     da.coords['tof'].unit = sc.units.us
 
-
-
-
+    print("Total time:", timer() - total_time)
     return da
 
 
+def load_positions(filename, entry='/', dim='position'):
+    """
+    Usage:
+      d = sc.Dataset()
+      d.coords['position'] = sc.neutron.load_positions('LOKI_Tube_Definition.hdf5')
+    """
 
+    # TODO: We need to think about how to link the correct position to the
+    # correct pixel/spectrum, as currently the order is just the order in
+    # which the banks are written to the file.
 
-
-
-
-    specs = sc.Variable(dims=['spectrum'], values=np.arange(nspec), dtype=sc.dtype.int32)
-
-
-    # var = sc.Variable(dims=['x'],
-    #                   shape=[1],
-    #                   dtype=sc.dtype.event_list_float64,
-    #                   unit=sc.units.us)
-    # var['x', 0].values.append(data['event_time_offset'])
-
-    da = sc.DataArray(data=sc.Variable(dims=['x'], values=np.ones_like(data['event_time_offset'])),
-          coords={'x': sc.Variable(dims=['x'], values=data['event_time_offset']),
-          'spectrum': sc.Variable(dims=['x'], values=data['spectrum'])})
-
-    grouped = sc.groupby(da, 'spectrum', bins=specs).flatten('x')
-    print(grouped)
-    return
-
-
-    var = sc.Variable(dims=['spectrum'],
-                      shape=[nspec],
-                      dtype=sc.dtype.event_list_float64)
-
-    start = timer()
-    for key, item in entries["event_id"].items():
-        print(key)
-        for n, it in enumerate(item):
-            ispec = spec_map[it]
-            var['spectrum',
-                ispec].values.append(entries["event_time_offset"][key][n])
-        # break
-    print("Filling events:", timer() - start)
-    print(var)
-
-    weights = sc.Variable(
-        dims=['spectrum'],
-        shape=[nspec],
-        unit=sc.units.counts,
-        dtype=sc.dtype.event_list_float64,
-        variances=True)
-    for i in range(nspec):
-        ones = np.ones(len(var['spectrum', i].values))
-        weights['spectrum', i].values = ones
-        weights['spectrum', i].variances = ones
-
-    da = sc.DataArray(data=weights,
-                      coords={
-                          'spectrum': specs,
-                          'ids': ids,
-                          'tof': var
-                      })
-    da.coords['tof'].unit = sc.units.us
-
-    return da
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #         if fields[key]["entry"] is None:
-    #             for p in fields[key]["pattern"]:
-    #                 if item.endswith(p):
-    #                     fields[key]["entry"] = item
-
-    # for key in fields.keys():
-    #     fields[key]["data"] = np.array(f[fields[key]["entry"]][...],
-    #                                    dtype=fields[key]["dtype"],
-    #                                    copy=True)
-
-    # print(data)
-
-    # # # Store the detector size in pixels
-    # # nx, ny = np.shape(fields["x"]["data"])
-    # # fields["nx"] = {"entry": None, "data": nx}
-    # # fields["ny"] = {"entry": None, "data": ny}
-
-    # # if ids and convert_ids and (np.amax(fields["ids"]["data"]) >= nx*ny):
-    # #     print("Warning: maximum id exceeds total number of pixels. "
-    # #           "Attempting to fix ids...")
-    # #     fields["ids"]["data"] = __convert_id(fields["ids"]["data"], nx)
-
-    # if verbose:
-    #     for key in fields.keys():
-    #         print("Loaded {} from: {}".format(key, fields[key]["entry"]))
-    #         print("  - Data size: {} : Min={} , "
-    #               "Max={}".format(np.shape(fields[key]["data"]),
-    #                               np.amin(fields[key]["data"]),
-    #                               np.amax(fields[key]["data"])))
-
-
-
-
-
-
-
-    # print(spec_min, spec_max)
-
-    # print(len(spec_num))
-    # spec_num = np.unique(np.concatenate(spec_num).ravel())
-    # print(spec_num.shape)
-
-    # nspec = len(spec_num)
-
-    # ids_bins = sc.Variable(['ids'], values=np.arange(spec_min, spec_max + 1))
-
-    
-
-    # spec_map = {val: key for (key, val) in enumerate(spec_num)}
-
-    # ids = sc.Variable(dims=['spectrum'], values=spec_num, dtype=sc.dtype.int32)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def __convert_id(ids, nx, id_offset=0):
-#     """
-#     Attempt to convert the event ids if their range exceeds the maximum index
-#     allowed by the pixel geometry.
-#     """
-#     x = np.bitwise_and(ids, 0xFFFF)
-#     y = np.right_shift(ids, 16)
-#     return id_offset + x + (nx * y)
-
-
-def load_positions(filename, entry='/'):
-
-    instrument = {}
     pattern = 'transformations/location'
 
-    lx = []
-    ly = []
-    lz = []
-
-    # nmax = 8
-    # i = 0
+    xyz = "xyz"
+    positions = {x: [] for x in xyz}
 
     with h5py.File(filename, "r", libver='latest', swmr=True) as f:
 
         contents = []
         f[entry].visit(contents.append)
 
-        # for item in contents:
-        #     print(item)
-        #     for key in fields.keys():
-        #         if fields[key]["entry"] is None:
-        #             for p in fields[key]["pattern"]:
-        #                 if item.endswith(p):
-        #                     fields[key]["entry"] = item
-
-        # for key in fields.keys():
-        #     fields[key]["data"] = np.array(f[fields[key]["entry"]][...],
-        #                                    dtype=fields[key]["dtype"],
-        #                                    copy=True)
-
         for item in contents:
 
-            print(item)
             if item.endswith(pattern) and (item.count('detector') > 0):
-                # i += 1
                 root = item.replace(pattern, '')
                 pos = f[item][()] * np.array(f[item].attrs['vector'])
-                
-                xoffset = f[root + 'x_pixel_offset'][()].astype(np.float64) + pos[0]
-                yoffset = f[root + 'y_pixel_offset'][()].astype(np.float64) + pos[1]
-                zoffset = f[root + 'z_pixel_offset'][()].astype(np.float64) + pos[2]
+                offsets = {x: None for x in xyz}
+                size = None
+                for i, x in enumerate(xyz):
+                    entry = join(root, '{}_pixel_offset'.format(x))
+                    if entry in f:
+                        offsets[x] = f[entry][()].astype(np.float64) + pos[i]
+                        size = len(offsets[x])
+                for i, x in enumerate(xyz):
+                    if offsets[x] is not None:
+                        positions[x].append(offsets[x])
+                    else:
+                        positions[x].append(np.zeros(size))
 
-                # positions = np.array([xoffset, yoffset, zoffset]).T
-                # print(positions)
-                # break
-                # return xoffset, yoffset, zoffset
-                lx.append(xoffset)
-                ly.append(yoffset)
-                lz.append(zoffset)
-
-                # if i > nmax:
-                #     return np.concatenate(lx), np.concatenate(ly), np.concatenate(lz)
-
-            # for key, value in f[item].attrs.items():
-            #     print(key, value)
-
-            # # print(item)
-            # for key in fields.keys():
-            #     for p in fields[key]["pattern"]:
-            #         if item.endswith(p):
-            #             root = item.replace(key, '')
-            #             entries[key][root] = np.array(f[item][...],
-            #                                dtype=fields[key]["dtype"],
-            #                                copy=True)
-            #             if key == "event_id":
-            #                 spec_min = min(spec_min, entries[key][root].min())
-            #                 spec_max = max(spec_max, entries[key][root].max())
-            #                 spec_num.append(np.unique(entries[key][root]))
-    return np.array([np.concatenate(lx), np.concatenate(ly), np.concatenate(lz)]).T
+    array = np.array([np.concatenate(positions['x']),
+                      np.concatenate(positions['y']),
+                      np.concatenate(positions['z'])]).T
+    return sc.Variable([dim], values=array, dtype=sc.dtype.vector_3_float64)
