cdb4b0a642366ac8538bf099fb8bbcb7f2e10db8
Simon Heybrock <simon.heybrock@esss.se>
2020-08-03 09:28:41 +0200

    Add some tests for resize

---
+++ dataset/test/shape_test.cpp
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Scipp contributors (https://github.com/scipp)
+#include <gtest/gtest.h>
+
+#include "scipp/dataset/shape.h"
+
+using namespace scipp;
+using namespace scipp::dataset;
+
+TEST(ResizeTest, data_array_1d) {
+  const auto var = makeVariable<double>(Dims{Dim::X}, Shape{2}, Values{1, 2});
+  DataArray a(var);
+  a.coords().set(Dim::X, var);
+  a.unaligned_coords().set(Dim::Y, var);
+  a.masks().set("mask", var);
+  DataArray expected(makeVariable<double>(Dims{Dim::X}, Shape{3}));
+  EXPECT_EQ(resize(a, Dim::X, 3), expected);
+}
+
+TEST(ResizeTest, data_array_2d) {
+  const auto var = makeVariable<double>(Dims{Dim::Y, Dim::X}, Shape{3, 2},
+                                        Values{1, 2, 3, 4, 5, 6});
+  auto x = var.slice({Dim::Y, 0});
+  auto y = var.slice({Dim::X, 0});
+  DataArray a(var);
+  a.coords().set(Dim::X, x);
+  a.coords().set(Dim::Y, y);
+  a.unaligned_coords().set(Dim("unaligned-x"), x);
+  a.unaligned_coords().set(Dim("unaligned-y"), y);
+  a.masks().set("mask-x", x);
+  a.masks().set("mask-y", y);
+
+  DataArray expected(makeVariable<double>(Dims{Dim::Y, Dim::X}, Shape{1, 2}));
+  expected.coords().set(Dim::X, x);
+  expected.unaligned_coords().set(Dim("unaligned-x"), x);
+  expected.masks().set("mask-x", x);
+
+  EXPECT_EQ(resize(a, Dim::Y, 1), expected);
+}
