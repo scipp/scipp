ab8fe71fec5fc870aecb7da398c21e4e509dc2f1
Owen Arnold <owen.arnold@stfc.ac.uk>
2020-08-03 12:37:57 +0100

    Revert "Remove is_approx binding"
    This reverts commit 0563698d1d246982e3751e985abd573b6c29f316.

--- python/tests/test_variable_comparision.py
+++ python/tests/test_variable_comparision.py
@@ -22,6 +22,41 @@ def test_comparison_op_exports_for_variable():
     _check_comparison_ops_on(var['x', :])
 
 
+def test_is_approx_int():
+    # non-trivial bindings
+    a = sc.Variable(['x'], values=np.array([1, 2, 3]))
+    b = sc.Variable(['x'], values=np.array([2, 2, 3]))
+    assert sc.is_approx(a, a, 0)
+    assert sc.is_approx(a, b, 1)
+    assert sc.is_approx(a['x', :], b, 1)
+    assert sc.is_approx(a, b['x', :], 1)
+    assert sc.is_approx(a['x', :], b['x', :], 1)
+
+
+def test_is_approx_float():
+    # non-trivial bindings
+    a = sc.Variable(['x'], values=np.array([1, 2, 3]), dtype=sc.dtype.float64)
+    b = sc.Variable(['x'], values=np.array([2, 2, 3]), dtype=sc.dtype.float64)
+    assert sc.is_approx(a, a, 0.0)
+    assert sc.is_approx(a, b, 1.0)
+    assert sc.is_approx(a['x', :], b, 1.0)
+    assert sc.is_approx(a, b['x', :], 1.0)
+    assert sc.is_approx(a['x', :], b['x', :], 1.0)
+
+
+def test_tol_type_mismatch():
+    a = sc.Variable(['x'], values=np.array([1, 2, 3]), dtype=sc.dtype.int64)
+    with pytest.raises(RuntimeError):
+        # tol is float
+        sc.is_approx(a, a, 1.0)
+
+
+def test_is_approx_unsupported_type():
+    a = sc.Variable(value=sc.Dataset())
+    with pytest.raises(RuntimeError):
+        sc.is_approx(a, a, 1)
+
+
 def test_is_equal():
     var = sc.Variable(['x'], values=np.array([1]))
     assert_export(sc.is_equal, var, var)
