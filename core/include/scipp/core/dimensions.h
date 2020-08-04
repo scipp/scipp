e7aff567cbc5966f486315ad1260253355335e3b
Simon Heybrock <simon.heybrock@esss.se>
2020-08-04 07:18:21 +0200

    Spelling

--- core/include/scipp/core/dimensions.h
+++ core/include/scipp/core/dimensions.h
@@ -89,8 +89,8 @@ public:
 
   /// Returns the number of occurences of `dim` in *this.
   ///
-  /// This exists for convenience so generic code support std::unordered_map can
-  /// be written.
+  /// This exists for convenience so generic code supporting std::unordered_map
+  /// can be written.
   constexpr scipp::index count(const Dim dim) const noexcept {
     return contains(dim) ? 1 : 0;
   }
