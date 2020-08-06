b7e2c17f5aac43b25ea4d19009aabfdcceb2468d
Simon Heybrock <simon.heybrock@esss.se>
2020-08-06 06:58:22 +0200

    Update outdated comment

--- python/dataset.cpp
+++ python/dataset.cpp
@@ -66,7 +66,7 @@ void bind_mutable_view(py::module &m, const std::string &name) {
                self.set(key, var);
            })
       // This additional setitem allows us to do things like
-      // d.attrs["a"] = scipp.detail.move(scipp.Variable())
+      // d.coords["a"] = scipp.detail.move(scipp.Variable())
       .def("__setitem__",
            [](T &self, const typename T::key_type key, MoveableVariable &mvar) {
              self.set(key, std::move(mvar.var));
