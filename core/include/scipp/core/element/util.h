0bb871edb5302d0c8552db192454e418fa9f0ccb
Simon Heybrock <simon.heybrock@esss.se>
2020-08-07 07:49:06 +0200

    Support integers in is_sorted

--- core/include/scipp/core/element/util.h
+++ core/include/scipp/core/element/util.h
@@ -63,8 +63,9 @@ constexpr auto variances = overloaded{
     [](const units::Unit &u) { return u * u; }};
 
 constexpr auto is_sorted_common = overloaded{
-    core::element::arg_list<std::tuple<bool, double, double>,
-                            std::tuple<bool, float, float>>,
+    core::element::arg_list<
+        std::tuple<bool, double, double>, std::tuple<bool, float, float>,
+        std::tuple<bool, int64_t, int64_t>, std::tuple<bool, int32_t, int32_t>>,
     transform_flags::expect_no_variance_arg<1>,
     [](units::Unit &out, const units::Unit &left, const units::Unit &right) {
       core::expect::equals(left, right);
