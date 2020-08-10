68cc96a3890d48d5c33c8e0ad7d3659468732313
Owen Arnold <owen.arnold@stfc.ac.uk>
2020-08-10 09:11:08 +0100

    Use inverse in docs rather than prefix a or arc

--- python/src/scipp/_trigonometry.py
+++ python/src/scipp/_trigonometry.py
@@ -42,44 +42,44 @@ def tan(x, out=None):
 
 
 def asin(x, out=None):
-    """Element-wise asin.
+    """Element-wise inverse sin.
 
     :param x: Input data.
     :param out: Optional output buffer.
     :raises: If the unit is not dimensionless.
-    :return: The asin values of the input.
+    :return: The inverse sin values of the input.
     """
     return _call_cpp_func(_cpp.asin, x, out=out)
 
 
 def acos(x, out=None):
-    """Element-wise acos.
+    """Element-wise inverse cos.
 
     :param x: Input data.
     :param out: Optional output buffer.
     :raises: If the unit is not dimensionless.
-    :return: The acos values of the input.
+    :return: The inverse cos values of the input.
     """
     return _call_cpp_func(_cpp.acos, x, out=out)
 
 
 def atan(x, out=None):
-    """Element-wise atan.
+    """Element-wise inverse tan.
 
     :param x: Input data.
     :param out: Optional output buffer.
     :raises: If the unit is not dimensionless.
-    :return: The atan values of the input.
+    :return: The inverse tan values of the input.
     """
     return _call_cpp_func(_cpp.atan, x, out=out)
 
 
 def atan2(y, x, out=None):
-    """Element-wise atan2.
+    """Element-wise inverse tan providing signed angle.
 
     :param y: Input y values.
     :param x: Input x values.
     :param out: Optional output buffer.
-    :return: The atan2 values of the inputs.
+    :return: The signed inverse tan values of the inputs.
     """
     return _call_cpp_func(_cpp.atan, x, out=out)
