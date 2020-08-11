Fix bounds calculation in case of negative/decreasing limits

python/src/scipp/plot/plot_2d.py
@@ -372,26 +372,17 @@ class Slicer2d(Slicer):
         """
         self.xlim_updated = False
         self.ylim_updated = False
-        xylims = {
-            "x": np.array(self.ax.get_xlim()),
-            "y": np.array(self.ax.get_ylim())
-        }
-
+        xylims = {}
         # Make sure we don't overrun the original array bounds
-        xylims["x"][0] = max(
-            xylims["x"][0],
-            self.slider_xlims[self.name][self.button_dims[1]][0])
-        xylims["x"][1] = min(
-            xylims["x"][1],
-            self.slider_xlims[self.name][self.button_dims[1]][1])
-        xylims["y"][0] = max(
-            xylims["y"][0],
-            self.slider_xlims[self.name][self.button_dims[0]][0])
-        xylims["y"][1] = min(
-            xylims["y"][1],
-            self.slider_xlims[self.name][self.button_dims[0]][1])
-        dx = self.current_lims["x"][1] - self.current_lims["x"][0]
-        dy = self.current_lims["y"][1] - self.current_lims["y"][0]
+        xylims["x"] = np.clip(
+            self.ax.get_xlim(),
+            *sorted(self.slider_xlims[self.name][self.button_dims[1]]))
+        xylims["y"] = np.clip(
+            self.ax.get_ylim(),
+            *sorted(self.slider_xlims[self.name][self.button_dims[0]]))
+
+        dx = np.abs(self.current_lims["x"][1] - self.current_lims["x"][0])
+        dy = np.abs(self.current_lims["y"][1] - self.current_lims["y"][0])
         diffx = np.abs(self.current_lims["x"] - xylims["x"]) / dx
         diffy = np.abs(self.current_lims["y"] - xylims["y"]) / dy
         diff = diffx.sum() + diffy.sum()