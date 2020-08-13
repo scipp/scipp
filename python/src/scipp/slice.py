adc8c1f6f7e245dd6d31e2a270de8a64b2dc8e07
Owen Arnold <owen.arnold@stfc.ac.uk>
2020-08-13 21:53:06 +0100

    Extend testing focusing on range based slicing
    Working on edge cases

--- python/src/scipp/slice.py
+++ python/src/scipp/slice.py
@@ -7,9 +7,8 @@ def slice(array, dim, start, end):
     # scipp treats bins as closed on left and open on right: [left, right)
     first = sc.sum(sc.less_equal(coord, start), dim).value - 1
     last = bins - sc.sum(sc.greater_equal(coord, end), dim).value
-    assert first >= 0
-    assert last < bins
-    if first == last:
-        return array[dim, first]
-    else:
-        return array[dim, first:last]
+    if first < 0:
+        first = 0
+    if last > bins:
+        last = bins - 1
+    return array[dim, first:last]
