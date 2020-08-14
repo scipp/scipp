82bfbf95e919d8720822b34fe02aabb174fe8f71
Owen Arnold <owen.arnold@stfc.ac.uk>
2020-08-14 10:39:22 +0100

    2d coord handling
    By which i mean not handling for value based slicing.

--- python/src/scipp/slice.py
+++ python/src/scipp/slice.py
@@ -1,9 +1,13 @@
 import scipp as sc
 
 
-def slice(array, dim, start, end):
-    coord = array.coords[dim]
-    bins = coord.shape[0]  # TODO check dimensionality
+def slice(array, coord_name, start=None, end=None):
+    coord = array.coords[coord_name]
+    if len(coord.shape) > 1:
+        raise RuntimeError(
+            "multi-dimensional coordinates not supported in slice")
+    dim = coord.dims[0]
+    bins = coord.shape[0]
     # scipp treats bins as closed on left and open on right: [left, right)
     first = sc.sum(sc.less_equal(coord, start), dim).value - 1
     last = bins - sc.sum(sc.greater_equal(coord, end), dim).value
--- python/tests/test_slice.py
+++ python/tests/test_slice.py
@@ -1,6 +1,7 @@
 import scipp as sc
 from scipp.slice import slice
 import numpy as np
+import pytest
 
 working_unit = sc.units.dimensionless
 
@@ -22,6 +23,9 @@ def test_slice_range_on_point_coords_1D():
                              end=13.0,
                              dim_name='x',
                              bin_edges=False)
+    # test no-effect slicing
+    out = slice(da, 'x', start=3.0 * working_unit, end=13.0 * working_unit)
+    assert sc.is_equal(da, out)
     # Test start on left boundary (closed on left), so includes boundary
     out = slice(da, 'x', start=3.0 * working_unit, end=4.0 * working_unit)
     assert sc.is_equal(out.coords['x'], da['x', 0:1].coords['x'])
@@ -41,7 +45,9 @@ def test_slice_range_on_edge_coords_1D():
     #    Data Values            [0.0] ...       [9.0]
     #    Coord Values (edges) [3.0][4.0] ... [11.0][12.0]
     da = _make_1d_data_array(begin=3.0, end=13.0, dim_name='x', bin_edges=True)
-    print(da)
+    # test no-effect slicing
+    out = slice(da, 'x', start=3.0 * working_unit, end=13.0 * working_unit)
+    assert sc.is_equal(da, out)
     # Test start on left boundary (closed on left), so includes boundary
     out = slice(da, 'x', start=3.0 * working_unit, end=4.0 * working_unit)
     assert sc.is_equal(out.coords['x'], da['x', 0:1].coords['x'])
@@ -61,6 +67,28 @@ def test_slice_range_on_point_coords_2D():
     data = sc.Variable(['y', 'x'], values=np.arange(100).reshape(5, 20))
     x = sc.Variable(['x'], values=np.arange(-10.0, 10.0))
     y = sc.Variable(['y'], values=np.arange(-2.5, 2.5))
+    assert data.shape[0] == y.shape[0]  # Ensure working with points
+    assert data.shape[1] == x.shape[0]  # Ensure working with points
     da = sc.DataArray(data=data, coords={'x': x, 'y': y})
-    out = slice(da, 'x', start=-10.0 * working_unit, end=-9.0 * working_unit)
-    #assert sc.is_equal
+
+    out = slice(da, 'x', start=-10.0 * working_unit, end=10.0 * working_unit)
+    # assert no-effect slicing
+    assert sc.is_equal(da, out)
+    # Test slice x range by value
+    out = slice(da, 'x', start=-10.0 * working_unit, end=0.0 * working_unit)
+    assert sc.is_equal(out.coords['y'],
+                       da.coords['y'])  # unaffected by x-value slicing
+    assert sc.is_equal(out.coords['x'], da.coords['x']['x', 0:10])
+    # Test slice y range by value
+    out = slice(da, 'y', start=0.0 * working_unit, end=2.5 * working_unit)
+    assert sc.is_equal(out.coords['x'],
+                       da.coords['x'])  # unaffected by y-value slicing
+    assert sc.is_equal(out.coords['y'], da.coords['y']['y', 2:5])
+
+
+def test_2d_coord_unsupported():
+    coord2d = sc.Variable(['y', 'x'], values=np.arange(10).reshape(5, 2))
+    data = coord2d.copy()
+    da = sc.DataArray(data=data, coords={'p': coord2d})
+    with pytest.raises(RuntimeError):
+        slice(da, coord_name='p')
