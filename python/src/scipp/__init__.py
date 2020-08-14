d3edc8d794d09a34bc48a9012d57c556cc5beb02
Owen Arnold <owen.arnold@stfc.ac.uk>
2020-08-14 17:03:40 +0100

    consistent naming of file

--- python/src/scipp/__init__.py
+++ python/src/scipp/__init__.py
@@ -29,7 +29,7 @@ from ._comparison import *
 from ._math import *
 from ._reduction import *
 from ._trigonometry import *
-from .slice import *
+from ._slice import *
 
 setattr(Variable, '_repr_html_', make_html)
 setattr(VariableConstView, '_repr_html_', make_html)
