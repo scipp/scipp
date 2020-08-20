4608b468e92688f78c13e51e95c8bf1204160243
Simon Heybrock <simon.heybrock@esss.se>
2020-08-20 15:55:34 +0200

    clang-format

--- variable/include/scipp/variable/bucket_model.h
+++ variable/include/scipp/variable/bucket_model.h
@@ -6,8 +6,8 @@
 #include "scipp/core/bucket_array_view.h"
 #include "scipp/core/dimensions.h"
 #include "scipp/core/except.h"
-#include "scipp/variable/except.h"
 #include "scipp/variable/data_model.h"
+#include "scipp/variable/except.h"
 
 namespace scipp::variable {
 
