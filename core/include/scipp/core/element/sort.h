82fb0fb76f2f723ad88f90521010267a67d08ac3
Simon Heybrock <simon.heybrock@esss.se>
2020-08-25 06:51:47 +0200

    Simplify element::sort* lambdas

--- core/include/scipp/core/element/sort.h
+++ core/include/scipp/core/element/sort.h
@@ -7,49 +7,41 @@
 #include "scipp/common/overloaded.h"
 #include "scipp/common/span.h"
 #include "scipp/core/element/arg_list.h"
+#include "scipp/core/element/comparison.h"
 #include "scipp/core/transform_common.h"
 #include "scipp/core/value_and_variance.h"
 #include "scipp/units/unit.h"
 
-#include <stddef.h>
-
 namespace scipp::core::element {
 
-constexpr auto sort_common = overloaded{
-    core::element::arg_list<span<int64_t>, span<int32_t>, span<double>,
-                            span<float>, span<std::string>>,
-    [](units::Unit &) {}};
-
-template <typename F1, typename F2>
-constexpr auto sort_to_specify(F1 for_zip, F2 for_no_var) {
+namespace {
+template <class Compare> constexpr auto make_sort(Compare compare) {
   return overloaded{
-      sort_common, [&for_zip, &for_no_var](auto &range) {
+      core::element::arg_list<span<int64_t>, span<int32_t>, span<double>,
+                              span<float>, span<std::string>>,
+      [](units::Unit &) {},
+      [&compare](auto &range) {
         using T = std::decay_t<decltype(range)>;
         constexpr bool vars = is_ValueAndVariance_v<T>;
         if constexpr (vars) {
-          using B = typename T::value_type;
-          using A = typename B::value_type;
-          std::vector<std::pair<A, A>> zipped;
+          std::vector<ValueAndVariance<typename T::value_type::value_type>>
+              zipped;
           for (scipp::index i = 0; i < scipp::size(range.value); i++) {
-            zipped.push_back(std::make_pair(range.value[i], range.variance[i]));
+            zipped.emplace_back(range.value[i], range.variance[i]);
           }
-          std::sort(std::begin(zipped), std::end(zipped), for_zip);
+          std::sort(std::begin(zipped), std::end(zipped), compare);
           for (scipp::index i = 0; i < scipp::size(range.value); i++) {
-            range.value[i] = zipped[i].first;
-            range.variance[i] = zipped[i].second;
+            range.value[i] = zipped[i].value;
+            range.variance[i] = zipped[i].variance;
           }
         } else {
-          std::sort(range.begin(), range.end(), for_no_var);
+          std::sort(range.begin(), range.end(), compare);
         }
       }};
 }
+} // namespace
 
-auto sort_nonascending = sort_to_specify(
-    [](const auto &a, const auto &b) { return a.first > b.first; },
-    [](const auto &a, const auto &b) { return a > b; });
-
-auto sort_nondescending = sort_to_specify(
-    [](const auto &a, const auto &b) { return a.first < b.first; },
-    [](const auto &a, const auto &b) { return a < b; });
+auto sort_nonascending = make_sort(greater);
+auto sort_nondescending = make_sort(less);
 
 } // namespace scipp::core::element
