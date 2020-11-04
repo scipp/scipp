dd32648270b0f2849a29848fc24f2e321bbe40b0
Simon Heybrock <simon.heybrock@esss.se>
2020-11-04 10:12:10 +0100

    Make bucket scale work with 2d histograms

--- python/src/scipp/compat/mantid.py
+++ python/src/scipp/compat/mantid.py
@@ -630,7 +630,9 @@ def convert_EventWorkspace_to_data_array(ws,
     contains_weighted_events = ((evtp == EventType.WEIGHTED)
                                 or (evtp == EventType.WEIGHTED_NOTIME))
 
-    begins = sc.Variable([spec_dim], shape=[nHist], dtype=sc.dtype.int64)
+    begins = sc.Variable([spec_dim, dim],
+                         shape=[nHist, 1],
+                         dtype=sc.dtype.int64)
     ends = begins.copy()
     current = 0
     for i in range(nHist):
@@ -655,6 +657,15 @@ def convert_EventWorkspace_to_data_array(ws,
 
     coords_labs_data = _convert_MatrixWorkspace_info(
         ws, advanced_geometry=advanced_geometry, load_run_logs=load_run_logs)
+    # For now we ignore potential finer bin edges to avoid creating too many
+    # buckets. Use just a single bucket along dim and use extents given by
+    # workspace edges.
+    # TODO If there are events outside edges this might create buckets with
+    # events that are not within bucket bounds. Consider using bucketby
+    # instead?
+    edges = coords_labs_data['coords'][dim]
+    coords_labs_data['coords'][dim] = sc.concatenate(edges[dim, 0],
+                                                     edges[dim, -1], dim)
 
     coords_labs_data["data"] = sc.to_buckets(begin=begins,
                                              end=ends,
