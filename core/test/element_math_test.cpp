28435fd6ed53d46714ee0670b307a01a947b3fd3
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-11-20 12:02:31 +0100

    Remove tests of element sqrt_out_arg

--- core/test/element_math_test.cpp
+++ core/test/element_math_test.cpp
@@ -62,34 +62,9 @@ TEST(ElementSqrtTest, value_and_variance) {
   EXPECT_EQ(element::sqrt(x), sqrt(x));
 }
 
-TEST(ElementSqrtOutArgTest, unit) {
-  const units::Unit m2(units::m * units::m);
-  units::Unit out(units::one);
-  element::sqrt_out_arg(out, m2);
-  EXPECT_EQ(out, units::sqrt(m2));
-}
-
-TEST(ElementSqrtOutArgTest, value_double) {
-  double out;
-  element::sqrt_out_arg(out, 1.23);
-  EXPECT_EQ(out, std::sqrt(1.23));
-}
-
-TEST(ElementSqrtOutArgTest, value_float) {
-  float out;
-  element::sqrt_out_arg(out, 1.23456789f);
-  EXPECT_EQ(out, std::sqrt(1.23456789f));
-}
-
-TEST(ElementSqrtOutArgTest, value_and_variance) {
-  const ValueAndVariance x(2.0, 1.0);
-  ValueAndVariance out(x);
-  element::sqrt_out_arg(out, x);
-  EXPECT_EQ(out, sqrt(x));
-}
-
-TEST(ElementSqrtOutArgTest, supported_types) {
-  auto supported = decltype(element::sqrt_out_arg)::types{};
+TEST(ElementSqrtTest, supported_types) {
+  auto supported = decltype(element::sqrt)::types{};
+  static_assert(std::tuple_size_v<decltype(supported)> == 2);
   std::get<double>(supported);
   std::get<float>(supported);
 }
