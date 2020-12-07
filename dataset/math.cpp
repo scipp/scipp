fff7fa23184bc8e1d0df4b62b0e15a9eacf57677
Simon Heybrock <simon.heybrock@esss.se>
2020-12-07 07:47:56 +0100

    Remove empty source

--- dataset/math.cpp
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (c) 2020 Scipp contributors (https://github.com/scipp)
-/// @file
-/// @author Simon Heybrock
-
-namespace scipp::dataset {
-
-} // namespace scipp::dataset
