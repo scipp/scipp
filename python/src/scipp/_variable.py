8f9db5021fc0744ef3645bd2bd1db105ec974215
Matthew-Andrew <matthew.andrew@tessella.com>
2020-12-09 17:00:33 +0000

    Updated to take types from type hints

--- python/src/scipp/_variable.py
+++ python/src/scipp/_variable.py
@@ -1,18 +1,16 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (c) 2020 Scipp contributors (https://github.com/scipp)
 # @author Matthew Andrew
-from ._scipp import core as _cpp
-from . import units, Unit, dtype
-from typing import Any, Sequence
-
-
-dtype_overload = (bool, int, float, list)
+from ._scipp.core import _Dtype
+from . import units, Unit, dtype, Variable
+from typing import Any, Sequence, Union
+import numpy
 
 
 def scalar(value: Any,
            variance: Any = None,
            unit: Unit = units.dimensionless,
-           dtype: dtype = None):
+           dtype: _Dtype = None) -> Variable:
     """Constructs a zero dimensional :class:`Variable` with a unit and optional
     variance.
 
@@ -23,18 +21,15 @@ def scalar(value: Any,
       in which case type is inferred from value input.
       Cannot be specified for value types of
       str, Dataset or DataArray.
-    :type value: Any
-    :type variance: Same type as value
-    :type unit: Unit
     """
     if dtype is None:
-        return _cpp.Variable(value=value, variance=variance, unit=unit)
+        return Variable(value=value, variance=variance, unit=unit)
     else:
         try:
-            return _cpp.Variable(value=value,
-                                 variance=variance,
-                                 unit=unit,
-                                 dtype=dtype)
+            return Variable(value=value,
+                            variance=variance,
+                            unit=unit,
+                            dtype=dtype)
         except TypeError:
             # Raise a more comprehensible error message in the case
             # where a dtype cannot be specified.
@@ -46,8 +41,8 @@ def zeros(*,
           dims: Sequence[str],
           shape: Sequence[int],
           unit: Unit = units.dimensionless,
-          dtype: dtype = dtype.float64,
-          variances: bool = False):
+          dtype: _Dtype = dtype.float64,
+          variances: bool = False) -> Variable:
     """Constructs a :class:`Variable` with default initialised values with
     given dimension labels and shape.
     Optionally can add default initialised variances.
@@ -57,29 +52,24 @@ def zeros(*,
     :param shape: Dimension sizes.
     :param unit: Optional, unit of contents. Default=dimensionless
     :param dtype: Optional, type of underlying data. Default=float64
-    :param variance: Optional, boolean flag, if True includes variances
-      initialised to the default value for dtype. 
+    :param variances: Optional, boolean flag, if True includes variances
+      initialised to the default value for dtype.
       For example for a float type values and variances would all be
       initialised to 0.0. Default=False
-    :type dims: list[str]
-    :type shape: list[int]
-    :type unit: Unit
-    :type variance: bool
-    :type dtype: dtype
     """
-    return _cpp.Variable(dims=dims,
-                         shape=shape,
-                         unit=unit,
-                         dtype=dtype,
-                         variances=variances)
+    return Variable(dims=dims,
+                    shape=shape,
+                    unit=unit,
+                    dtype=dtype,
+                    variances=variances)
 
 
 def array(*,
           dims: Sequence[str],
-          values,
-          variances=None,
+          values: Union[numpy.ndarray, list],
+          variances: Union[numpy.ndarray, list] = None,
           unit: Unit = units.dimensionless,
-          dtype: dtype = None):
+          dtype: _Dtype = None) -> Variable:
     """Constructs a :class:`Variable` with given dimensions, containing given
     values and optional variances. Dimension and value shape must match.
     Only keyword arguments accepted.
@@ -91,14 +81,9 @@ def array(*,
     :param unit: Optional, data unit. Default=dimensionless
     :param dtype: Optional, type of underlying data. Default=None,
       in which case type is inferred from value input.
-    :type dims: list[str]
-    :type values: numpy.ndarray, list
-    :type variances: numpy.ndarray, list
-    :type unit: Unit
-    :type dtype: dtype
     """
-    return _cpp.Variable(dims=dims,
-                         values=values,
-                         variances=variances,
-                         unit=unit,
-                         dtype=dtype)
+    return Variable(dims=dims,
+                    values=values,
+                    variances=variances,
+                    unit=unit,
+                    dtype=dtype)
