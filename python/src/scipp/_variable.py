109d04e75f6c6f6716343c83fc44be1379a7809b
Matthew-Andrew <matthew.andrew@tessella.com>
2020-12-10 10:24:41 +0000

    Added simple wrapper tests

--- python/src/scipp/_variable.py
+++ python/src/scipp/_variable.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (c) 2020 Scipp contributors (https://github.com/scipp)
 # @author Matthew Andrew
-from ._scipp.core import _Dtype
 from . import units, Unit, dtype, Variable
 from typing import Any, Sequence, Union
 import numpy
@@ -10,7 +9,7 @@ import numpy
 def scalar(value: Any,
            variance: Any = None,
            unit: Unit = units.dimensionless,
-           dtype: _Dtype = None) -> Variable:
+           dtype: type(dtype.float64) = None) -> Variable:
     """Constructs a zero dimensional :class:`Variable` with a unit and optional
     variance.
 
@@ -21,6 +20,7 @@ def scalar(value: Any,
       in which case type is inferred from value input.
       Cannot be specified for value types of
       str, Dataset or DataArray.
+    :raises: Add this
     """
     if dtype is None:
         return Variable(value=value, variance=variance, unit=unit)
@@ -33,15 +33,15 @@ def scalar(value: Any,
         except TypeError:
             # Raise a more comprehensible error message in the case
             # where a dtype cannot be specified.
-            raise RuntimeError(f"dtype cannot be specified for value"
-                               f"type of {type(value)}")
+            raise TypeError(f"dtype cannot be specified for value"
+                            f" type of {type(value)}")
 
 
 def zeros(*,
           dims: Sequence[str],
           shape: Sequence[int],
           unit: Unit = units.dimensionless,
-          dtype: _Dtype = dtype.float64,
+          dtype: type(dtype.float64) = dtype.float64,
           variances: bool = False) -> Variable:
     """Constructs a :class:`Variable` with default initialised values with
     given dimension labels and shape.
@@ -69,7 +69,7 @@ def array(*,
           values: Union[numpy.ndarray, list],
           variances: Union[numpy.ndarray, list] = None,
           unit: Unit = units.dimensionless,
-          dtype: _Dtype = None) -> Variable:
+          dtype: type(dtype.float64) = None) -> Variable:
     """Constructs a :class:`Variable` with given dimensions, containing given
     values and optional variances. Dimension and value shape must match.
     Only keyword arguments accepted.
