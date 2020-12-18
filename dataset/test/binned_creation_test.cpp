501a34ab149e992989e8f238d6ae22a4d1c9d92e
Simon Heybrock <simon.heybrock@esss.se>
2020-12-18 14:36:54 +0100

    Improve empty_like tests

--- dataset/test/binned_creation_test.cpp
+++ dataset/test/binned_creation_test.cpp
@@ -12,16 +12,25 @@ class BinnedCreationTest : public ::testing::Test {
 protected:
   Variable m_indices = makeVariable<scipp::index_pair>(
       Dims{Dim::X}, Shape{2}, Values{std::pair{0, 2}, std::pair{2, 5}});
-  Variable m_data =
-      makeVariable<double>(Dims{Dim::Event}, Shape{5}, Values{1, 2, 3, 4, 5});
+  Variable m_data = makeVariable<double>(Dims{Dim::Event}, Shape{5}, units::m,
+                                         Values{1, 2, 3, 4, 5});
   DataArray m_buffer = DataArray(m_data, {{Dim::X, m_data}}, {{"mask", m_data}},
                                  {{Dim("attr"), 1.2 * units::m}});
   Variable m_var = make_bins(m_indices, Dim::Event, m_buffer);
+
+  void check(const VariableConstView &var) const {
+    const auto [indices, dim, buf] = var.constituents<core::bin<DataArray>>();
+    EXPECT_EQ(buf.unit(), units::m);
+    EXPECT_EQ(buf.attrs(), m_buffer.attrs()); // scalar, so copied, not resized
+    EXPECT_TRUE(buf.masks().contains("mask"));
+    EXPECT_TRUE(buf.coords().contains(Dim::X));
+  }
 };
 
 TEST_F(BinnedCreationTest, empty_like_default_shape) {
   const auto empty = empty_like(m_var);
   EXPECT_EQ(empty.dims(), m_var.dims());
+  check(empty);
   const auto [indices, dim, buf] = empty.constituents<core::bin<DataArray>>();
   EXPECT_EQ(indices, m_indices);
 }
@@ -29,6 +38,7 @@ TEST_F(BinnedCreationTest, empty_like_default_shape) {
 TEST_F(BinnedCreationTest, empty_like_slice_default_shape) {
   const auto empty = empty_like(m_var.slice({Dim::X, 1}));
   EXPECT_EQ(empty.dims(), m_var.slice({Dim::X, 1}).dims());
+  check(empty);
   const auto [indices, dim, buf] = empty.constituents<core::bin<DataArray>>();
   EXPECT_EQ(indices, makeVariable<scipp::index_pair>(Values{std::pair{0, 3}}));
 }
@@ -40,9 +50,7 @@ TEST_F(BinnedCreationTest, empty_like) {
   EXPECT_EQ(empty.dims(), shape.dims());
   const auto [indices, dim, buf] = empty.constituents<core::bin<DataArray>>();
   EXPECT_EQ(buf.dims(), Dimensions(Dim::Event, 21));
-  EXPECT_EQ(buf.attrs(), m_buffer.attrs()); // scalar, so copied, not resized
-  EXPECT_TRUE(buf.masks().contains("mask"));
-  EXPECT_TRUE(buf.coords().contains(Dim::X));
+  check(empty);
   scipp::index i = 0;
   for (const auto n : {1, 2, 5, 6, 3, 4}) {
     EXPECT_EQ(empty.values<core::bin<DataArray>>()[i++].dims()[Dim::Event], n);
