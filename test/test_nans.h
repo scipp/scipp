f6cbc9e8ffa9ab867de6bea1ef5a417b805cbf1d
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2020-12-31 12:33:39 +0100

    Check is_iec559 to enable NaN testing

--- test/test_nans.h
+++ test/test_nans.h
@@ -11,8 +11,8 @@ template <typename T> struct MeanTest : public ::testing::Test {
    * For any integer input type we expect the return type to be double
    */
   using RetType = std::conditional_t<std::is_integral_v<T>, double, T>;
-  /// Held type supports nan testing. All FP types -> true
-  constexpr static bool TestNans = !std::numeric_limits<T>::is_integer;
+  /// Held type supports nan testing.
+  constexpr static bool TestNans = std::numeric_limits<T>::is_iec559;
   /// Held type supports variances testing. All FP types -> true
   constexpr static bool TestVariances = !std::numeric_limits<T>::is_integer;
 };
