d2a11f27c66906de4b3719dc950b8e8dbdeed3bf
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2020-12-31 12:32:35 +0100

    Replace numeric_limits::is_integer with  is_integral

--- test/test_nans.h
+++ test/test_nans.h
@@ -10,8 +10,7 @@ template <typename T> struct MeanTest : public ::testing::Test {
    * mean and nanmean will preserve input type for floating point types.
    * For any integer input type we expect the return type to be double
    */
-  using RetType =
-      std::conditional_t<std::numeric_limits<T>::is_integer, double, T>;
+  using RetType = std::conditional_t<std::is_integral_v<T>, double, T>;
   /// Held type supports nan testing. All FP types -> true
   constexpr static bool TestNans = !std::numeric_limits<T>::is_integer;
   /// Held type supports variances testing. All FP types -> true
