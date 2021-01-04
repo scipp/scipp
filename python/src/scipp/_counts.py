ff5f0c92b4a88da8cac7fc45f7579f1dd5fe41ed
Neil Vaytet <neil.vaytet@esss.se>
2021-01-04 20:31:34 +0100

    2020 -> 2021

--- python/src/scipp/_counts.py
+++ python/src/scipp/_counts.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
-# Copyright (c) 2020 Scipp contributors (https://github.com/scipp)
+# Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 # @author Matthew Andrew
 from ._scipp import core as _cpp
 from ._cpp_wrapper_util import call_func as _call_cpp_func
--- variable/special_values.cpp
+++ variable/special_values.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (c) 2020 Scipp contributors (https://github.com/scipp)
+// Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 #include "scipp/core/element/special_values.h"
 #include "scipp/variable/misc_operations.h"
 #include "scipp/variable/transform.h"
