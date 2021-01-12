91bde83b695e6b856ab5ab3dfb23219af10d5129
Simon Heybrock <simon.heybrock@esss.se>
2021-01-12 07:06:27 +0100

    Add benchmark showing bad accumulate performance for outer dim

--- benchmark/transform_benchmark.cpp
+++ benchmark/transform_benchmark.cpp
@@ -136,6 +136,38 @@ BENCHMARK(BM_transform_in_place_transposed)
     ->RangeMultiplier(2)
     ->Ranges({{1, 2ul << 18ul}, {false, true}});
 
+static void BM_accumulate_in_place(benchmark::State &state) {
+  const auto nx = 1000;
+  const auto ny = state.range(0);
+  const auto n = nx * ny;
+  const bool use_variances = state.range(1);
+  const bool outer = state.range(2);
+  auto a = makeBenchmarkVariable(Dimensions{{Dim::X, nx}}, use_variances);
+  auto b = makeBenchmarkVariable(outer ? Dimensions{{Dim::Y, ny}, {Dim::X, nx}}
+                                       : Dimensions{{Dim::X, nx}, {Dim::Y, ny}},
+                                 use_variances);
+  static constexpr auto op{[](auto &a_, const auto &b_) { a_ += b_; }};
+
+  for ([[maybe_unused]] auto _ : state) {
+    accumulate_in_place<Types>(a, b, op);
+  }
+
+  const scipp::index variance_factor = use_variances ? 2 : 1;
+  state.SetItemsProcessed(state.iterations() * n * variance_factor);
+  state.SetBytesProcessed(state.iterations() * n * variance_factor *
+                          sizeof(double));
+  state.counters["n"] = n;
+  state.counters["variances"] = use_variances;
+  state.counters["accumulate-outer"] = outer;
+  state.counters["size"] = benchmark::Counter(
+      static_cast<double>(n * variance_factor * sizeof(double)),
+      benchmark::Counter::kDefaults, benchmark::Counter::OneK::kIs1024);
+}
+
+BENCHMARK(BM_accumulate_in_place)
+    ->RangeMultiplier(2)
+    ->Ranges({{1, 2ul << 18ul}, {false, true}, {false, true}});
+
 static void BM_transform(benchmark::State &state) {
   run<false>(
       state,
