cb078898f137c8556487d062e5834bd1ddfd15ac
Simon Heybrock <simon.heybrock@esss.se>
2021-01-18 09:24:16 +0100

    Test bin helpers

--- variable/test/bin_util_test.cpp
+++ variable/test/bin_util_test.cpp
@@ -7,4 +7,15 @@
 using namespace scipp;
 using namespace scipp::variable;
 
-TEST(BinUtilTest, xxx) {}
+TEST(BinUtilTest, left_edge_right_edge) {
+  const auto edges =
+      makeVariable<double>(Dims{Dim::X}, Shape{4}, Values{1, 2, 3, 4});
+  EXPECT_EQ(left_edge(edges),
+            makeVariable<double>(Dims{Dim::X}, Shape{3}, Values{1, 2, 3}));
+  EXPECT_EQ(right_edge(edges),
+            makeVariable<double>(Dims{Dim::X}, Shape{3}, Values{2, 3, 4}));
+  EXPECT_THROW(left_edge(edges.slice({Dim::X, 0, 0})), except::BinEdgeError);
+  EXPECT_THROW(left_edge(edges.slice({Dim::X, 0, 1})), except::BinEdgeError);
+  EXPECT_THROW(right_edge(edges.slice({Dim::X, 0, 0})), except::BinEdgeError);
+  EXPECT_THROW(right_edge(edges.slice({Dim::X, 0, 1})), except::BinEdgeError);
+}
