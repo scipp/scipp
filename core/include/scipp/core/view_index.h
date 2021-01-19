5aee9f8102d6d01f83f016bf431b517ba4b2593b
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-01-19 09:27:01 +0100

    Remove ViewIndex::has_stride_zero

--- core/include/scipp/core/view_index.h
+++ core/include/scipp/core/view_index.h
@@ -62,10 +62,6 @@ public:
     return m_fullIndex != other.m_fullIndex;
   }
 
-  [[nodiscard]] constexpr bool has_stride_zero() const noexcept {
-    return m_dims > m_subdims;
-  }
-
 private:
   // NOTE:
   // We investigated different containers for the m_delta, m_coord & m_extent
