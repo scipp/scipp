e7781f439450ec4ba18dd45e1410d1e63f61312c
Simon Heybrock <simon.heybrock@esss.se>
2021-01-29 09:09:41 +0100

    Add constnts and conversions tests

--- neutron/test/constants_test.cpp
+++ neutron/test/constants_test.cpp
@@ -11,7 +11,10 @@ using namespace scipp::neutron;
 
 namespace mock {
 
-struct Dummy {};
+struct Dummy {
+  Variable ei;
+  Variable ef;
+};
 
 Variable source_position(const Dummy &) {
   return makeVariable<Eigen::Vector3d>(Values{Eigen::Vector3d{1.0, 2.0, 3.0}},
@@ -35,13 +38,21 @@ Variable scattering_angle(const Dummy &) {
   return 0.123 * units::rad;
 }
 
-Variable flight_path_length(const Dummy &dummy) {
-  return norm(sample_position(dummy) - source_position(dummy)) +
-         norm((position(dummy) - sample_position(dummy)));
+Variable l1(const Dummy &dummy) {
+  return norm(sample_position(dummy) - source_position(dummy));
 }
 
-Variable incident_energy(const Dummy &) { return Variable{}; }
-Variable final_energy(const Dummy &) { return Variable{}; }
+Variable l2(const Dummy &dummy) {
+  return norm(position(dummy) - sample_position(dummy));
+}
+
+Variable flight_path_length(const Dummy &dummy) {
+  return norm(sample_position(dummy) - source_position(dummy)) +
+         norm(position(dummy) - sample_position(dummy));
+}
+
+Variable incident_energy(const Dummy &dummy) { return dummy.ei; }
+Variable final_energy(const Dummy &dummy) { return dummy.ef; }
 
 } // namespace mock
 
@@ -49,6 +60,8 @@ class ConstantsTest : public ::testing::Test {
 protected:
   mock::Dummy dummy;
   const Variable theta = mock::scattering_angle(dummy);
+  const Variable L1 = mock::l1(dummy);
+  const Variable L2 = mock::l2(dummy);
   const Variable L = mock::flight_path_length(dummy);
   const Variable source = mock::source_position(dummy);
   const Variable sample = mock::sample_position(dummy);
@@ -84,6 +97,35 @@ TEST_F(ConstantsTest, tof_to_wavelength) {
 TEST_F(ConstantsTest, tof_to_energy) {
   EXPECT_EQ(constants::tof_to_energy(dummy),
             L * L * Variable(constants::tof_to_energy_physical_constants));
+}
+
+TEST_F(ConstantsTest, tof_to_energy_transfer_fails) {
+  EXPECT_THROW(constants::tof_to_energy_transfer(dummy), std::runtime_error);
+  dummy.ei = 3.0 * units::meV;
+  dummy.ef = 3.0 * units::meV;
+  EXPECT_THROW(constants::tof_to_energy_transfer(dummy), std::runtime_error);
+}
+
+TEST_F(ConstantsTest, tof_to_energy_transfer_direct) {
+  const auto Ei = 3.0 * units::meV;
+  dummy.ei = Ei;
+  const auto [scale, tof_shift, energy_shift] =
+      constants::tof_to_energy_transfer(dummy);
+  const auto C = Variable(constants::tof_to_energy_physical_constants);
+  EXPECT_EQ(scale, -L2 * L2 * C);
+  EXPECT_EQ(tof_shift, sqrt(L1 * L1 * C / Ei));
+  EXPECT_EQ(energy_shift, -Ei);
+}
+
+TEST_F(ConstantsTest, tof_to_energy_transfer_indirect) {
+  const auto Ef = 3.0 * units::meV;
+  dummy.ef = Ef;
+  const auto [scale, tof_shift, energy_shift] =
+      constants::tof_to_energy_transfer(dummy);
+  const auto C = Variable(constants::tof_to_energy_physical_constants);
+  EXPECT_EQ(scale, L1 * L1 * C);
+  EXPECT_EQ(tof_shift, sqrt(L2 * L2 * C / Ef));
+  EXPECT_EQ(energy_shift, Ef);
 }
 
 TEST_F(ConstantsTest, wavelength_to_q) {
