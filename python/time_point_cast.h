a2d6b90bab6d692a1ca6ef6fda7ba76287e52a6d
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-02-01 17:56:14 +0100

    Change author

--- python/time_point_cast.h
+++ python/time_point_cast.h
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 /// @file
-/// @author Piotr Rozyczko
+/// @author Jan-Lukas Wynen
 #pragma once
 
 #include <iomanip>
