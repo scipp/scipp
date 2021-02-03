053cb293bf318fd7f2666c2cad4a409dbcd1eda6
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-02-03 21:33:44 +0100

    Add parse_datetime_dtype

--- python/dtype.cpp
+++ python/dtype.cpp
@@ -56,3 +56,31 @@ scipp::core::DType scipp_dtype(const py::object &type) {
     return scipp_dtype(py::dtype::from_args(type));
   }
 }
+
+// Awful handwritten parser. But std::regex does not support string_view and
+// you have to go through c-strings in order to extract the unit scale.
+[[nodiscard]] scipp::units::Unit
+parse_datetime_dtype(const std::string_view dtype_name) {
+  if (const auto length = std::size(dtype_name); length == 13 || length == 14) {
+    if (dtype_name.substr(0, 11) == "datetime64[" &&
+        dtype_name[length - 2] == 's' && dtype_name.back() == ']') {
+      if (length == 13) {
+        // no scale given
+        return scipp::units::s;
+      }
+      switch (dtype_name[11]) {
+      case 'n':
+        return scipp::units::ns;
+      case 'u':
+        return scipp::units::us;
+      default:
+        throw std::invalid_argument(
+            std::string("Unsupported unit in datetime: ") + dtype_name[11] +
+            "s");
+      }
+    }
+  }
+
+  throw std::invalid_argument(
+      std::string("Invalid dtype, expected datetime64, got ").append(dtype_name));
+}
--- python/dtype.h
+++ python/dtype.h
@@ -3,11 +3,17 @@
 /// @file
 #pragma once
 
+#include <string_view>
+
 #include "pybind11.h"
 #include <scipp/core/dtype.h>
+#include <scipp/units/unit.h>
 
 namespace pybind11 {
 class dtype;
 }
 
 scipp::core::DType scipp_dtype(const pybind11::object &type);
+
+[[nodiscard]] scipp::units::Unit
+parse_datetime_dtype(std::string_view dtype_name);
