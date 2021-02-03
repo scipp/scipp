62cd927ced6b473f9b7328af79b1c6350d5f5524
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-02-03 15:30:39 +0100

    Make time_point.duration public

--- core/include/scipp/core/time_point.h
+++ core/include/scipp/core/time_point.h
@@ -11,51 +11,50 @@ namespace scipp::core {
 /// Time-point similar to std::chrono::time point but without compile-time unit.
 class time_point {
 public:
-  constexpr time_point() noexcept = default;
-  constexpr time_point(const int64_t &d) noexcept : m_duration(d){};
-  constexpr time_point(const int32_t &d) noexcept
-      : m_duration(static_cast<int64_t>(d)){};
+  int64_t duration{0};
 
-  int64_t time_since_epoch() const noexcept { return m_duration; };
+  constexpr time_point() noexcept = default;
+  constexpr time_point(const int64_t &d) noexcept : duration(d){};
+  constexpr time_point(const int32_t &d) noexcept
+      : duration(static_cast<int64_t>(d)){};
+
+  int64_t time_since_epoch() const noexcept { return duration; };
 
   time_point &operator+=(const int64_t duration) {
-    m_duration += duration;
+    duration += duration;
     return *this;
   };
   time_point &operator-=(const int64_t duration) {
-    m_duration -= duration;
+    duration -= duration;
     return *this;
   };
   time_point operator+(const int64_t &duration) {
-    return time_point(m_duration + duration);
+    return time_point(duration + duration);
   }
   time_point operator-(const int64_t &duration) {
-    return time_point(m_duration - duration);
+    return time_point(duration - duration);
   }
   int64_t operator-(const time_point &time) const noexcept {
-    return m_duration - time.time_since_epoch();
+    return duration - time.time_since_epoch();
   }
   bool operator==(const time_point &time) const noexcept {
-    return m_duration == time.time_since_epoch();
+    return duration == time.time_since_epoch();
   }
   bool operator!=(const time_point &time) const noexcept {
-    return m_duration != time.time_since_epoch();
+    return duration != time.time_since_epoch();
   }
   bool operator<(const time_point &time) const noexcept {
-    return m_duration < time.time_since_epoch();
+    return duration < time.time_since_epoch();
   }
   bool operator>(const time_point &time) const noexcept {
-    return m_duration > time.time_since_epoch();
+    return duration > time.time_since_epoch();
   }
   bool operator<=(const time_point &time) const noexcept {
-    return m_duration <= time.time_since_epoch();
+    return duration <= time.time_since_epoch();
   }
   bool operator>=(const time_point &time) const noexcept {
-    return m_duration >= time.time_since_epoch();
+    return duration >= time.time_since_epoch();
   }
-
-private:
-  int64_t m_duration{0};
 };
 
 } // namespace scipp::core
