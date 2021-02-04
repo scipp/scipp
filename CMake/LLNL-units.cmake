12af1a5f86ee73776b0e041de0f1dd0beee94ff6
Simon Heybrock <simon.heybrock@esss.se>
2021-02-04 06:47:52 +0100

    Download LLNL/units at configure time

---
+++ CMake/LLNL-units.cmake
@@ -0,0 +1,32 @@
+# Prevent overriding the parent project's compiler/linker settings on Windows
+set(llnl_units_force_shared_crt
+    ON
+    CACHE BOOL "" FORCE
+)
+
+# Download and unpack llnl-units at configure time
+configure_file(
+  ${CMAKE_SOURCE_DIR}/CMake/LLNL-units.in
+  ${CMAKE_BINARY_DIR}/llnl-units-download/CMakeLists.txt
+)
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/llnl-units-download
+)
+if(result)
+  message(FATAL_ERROR "CMake step for llnl-units failed: ${result}")
+endif()
+execute_process(
+  COMMAND ${CMAKE_COMMAND} --build .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/llnl-units-download
+)
+if(result)
+  message(FATAL_ERROR "Build step for llnl-units failed: ${result}")
+endif()
+
+add_subdirectory(
+  ${CMAKE_BINARY_DIR}/llnl-units-src ${CMAKE_BINARY_DIR}/llnl-units-build
+  EXCLUDE_FROM_ALL
+)
