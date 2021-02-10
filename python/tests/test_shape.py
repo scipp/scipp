78299aa003031b77d97b5a50e774bcce08496d5b
Neil Vaytet <neil.vaytet@esss.se>
2021-02-10 21:31:27 +0100

    add python tests

---
+++ python/tests/test_shape.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
+# @file
+
+import scipp as sc
+from .common import assert_export
+
+
+def test_reshape_variable():
+    assert_export(sc.reshape, sc.Variable())
+
+
+def test_reshape_data_array():
+    assert_export(sc.reshape, sc.DataArray(data=sc.scalar(1.0)))
+
+
+def test_reshape_dataset():
+    assert_export(sc.reshape, sc.Dataset())
