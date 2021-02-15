e41fe059f7b1b7aa1658a6ff0ce1a53f9e1ccf27
Simon Heybrock <simon.heybrock@esss.se>
2021-02-15 09:37:51 +0100

    Add RuntimeError as exception base

--- python/except.cpp
+++ python/except.cpp
@@ -11,9 +11,13 @@ namespace py = pybind11;
 
 void init_exceptions(py::module &m) {
   using namespace scipp;
-  py::register_exception<except::UnitError>(m, "UnitError");
-  py::register_exception<except::TypeError>(m, "DTypeError");
-  py::register_exception<except::DimensionError>(m, "DimensionError");
-  py::register_exception<except::BinnedDataError>(m, "BinnedDataError");
-  py::register_exception<except::CoordMismatchError>(m, "CoordError");
+  py::register_exception<except::UnitError>(m, "UnitError", PyExc_RuntimeError);
+  py::register_exception<except::TypeError>(m, "DTypeError",
+                                            PyExc_RuntimeError);
+  py::register_exception<except::DimensionError>(m, "DimensionError",
+                                                 PyExc_RuntimeError);
+  py::register_exception<except::BinnedDataError>(m, "BinnedDataError",
+                                                  PyExc_RuntimeError);
+  py::register_exception<except::CoordMismatchError>(m, "CoordError",
+                                                     PyExc_RuntimeError);
 }
