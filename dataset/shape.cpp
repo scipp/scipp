e59157ae1af823fba11b1b40f21058ee30a73a6c
Neil Vaytet <neil.vaytet@esss.se>
2021-02-23 21:42:11 +0100

    now catch concatenation error and do not add coord if there is a bin-edge error during concatenation

--- dataset/shape.cpp
+++ dataset/shape.cpp
@@ -9,7 +9,6 @@
 
 #include "../variable/operations_common.h"
 #include "dataset_operations_common.h"
-#include <iostream>
 
 namespace scipp::dataset {
 
@@ -215,20 +214,18 @@ Variable maybe_broadcast(const VariableConstView &var,
   const auto &var_dims = var.dims();
   // const auto new_dims = unstack_dims(var_dims, from_dims, to_dim);
 
-  bool contains_one_dim = false;
-  bool contains_all_dims = true;
+  bool vardim_found_in_fromdims = false;
   for (const auto dim : var_dims.labels())
     if (from_dims.contains(dim))
-      contains_one_dim = true;
-    else
-      contains_all_dims = false;
-  std::cout << "maybe_broadcast 1 " << contains_one_dim << " "
-            << contains_one_dim << std::endl;
-  std::cout << var_dims << " " << from_dims << std::endl;
+      vardim_found_in_fromdims = true;
+
+  bool vardims_contains_all_fromdims = true;
+  for (const auto dim : from_dims.labels())
+    if (!var_dims.contains(dim))
+      vardims_contains_all_fromdims = false;
 
   // if (need_broadcast && !var_dims.contains(from_dims)) {
-  if (contains_one_dim && !contains_all_dims) {
-    std::cout << "maybe_broadcast 2 " << std::endl;
+  if (vardim_found_in_fromdims && !vardims_contains_all_fromdims) {
     Dimensions broadcast_dims;
     for (const auto dim : var_dims.labels())
       if (!from_dims.contains(dim))
@@ -241,7 +238,6 @@ Variable maybe_broadcast(const VariableConstView &var,
             broadcast_dims.addInner(lab, var_dims[lab]);
           else
             broadcast_dims.addInner(lab, from_dims[lab]);
-    std::cout << "broadcasting to: " << broadcast_dims << std::endl;
     return broadcast(var, broadcast_dims);
   } else {
     return Variable(std::move(var)); // reshape(v, new_dims);
@@ -283,7 +279,6 @@ Variable slice_and_stack(const VariableConstView &var, const Dim from_dim,
 
 Variable slice_and_unstack(const VariableConstView &var,
                            const Dimensions &from_dims, const Dim to_dim) {
-  std::cout << "slice_and_unstack 1" << std::endl;
   const auto labels = from_dims.labels();
   // const auto to_lab = labels[labels.size() - 1];
   // scipp::index step;
@@ -293,8 +288,6 @@ Variable slice_and_unstack(const VariableConstView &var,
     if (buf.dims().contains(labels[l])) {
       // step = buf.dims()[from_dim] / to_dims.shape()[l];
       reshaped = Variable(buf.slice({labels[l], 0}));
-      std::cout << "slicing along " << labels[l] << std::endl;
-      std::cout << reshaped << std::endl;
       for (int32_t i = 1; i < from_dims.shape()[l]; ++i)
         reshaped = join_edges(VariableView(reshaped), buf.slice({labels[l], i}),
                               labels.back());
@@ -305,14 +298,11 @@ Variable slice_and_unstack(const VariableConstView &var,
   }
 
   buf.rename(labels.back(), to_dim);
-  std::cout << "slice_and_unstack 2" << std::endl;
-  std::cout << buf << std::endl;
   return buf;
 }
 
 const bool has_bin_edges(const VariableConstView &var,
                          const Dimensions &array_dims) {
-  // bool has_edges = false;
   for (const auto dim : var.dims().labels())
     if (is_bin_edges(var, array_dims, dim))
       return true;
@@ -370,52 +360,45 @@ DataArray reshape(const DataArrayConstView &a,
       throw except::DimensionError(
           "Reshape: dimension to be reshaped not found in DataArray.");
 
-  std::cout << "reshape 1" << std::endl;
   Dimensions stacked_dims;
   for (const auto dim : from_dims)
     stacked_dims.addInner(dim, old_dims[dim]);
-  std::cout << "reshape 2" << std::endl;
 
   auto reshaped = DataArray(
       reshape(a.data(), unstack_dims(old_dims, stacked_dims, to_dim)));
-  std::cout << "reshape 3" << std::endl;
 
   for (auto &&[name, coord] : a.coords()) {
-    std::cout << "reshape 4" << std::endl;
-    // bool has_bin_edges = false;
-    // for (const auto dim : coord.dims().labels()) {
-    //   if (is_bin_edges(coord, old_dims, dim)) {
-    //     has_bin_edges = true;
-    //     break;
-    //   }
-    // }
-    // std::cout << "has_bin_edges: " << has_bin_edges << std::endl;
     if (has_bin_edges(coord, old_dims)) {
-      std::cout << "reshape 5" << std::endl;
-      // const auto var = maybe_broadcast(coord, stacked_dims, to_dim);
-      // try {
-      reshaped.coords().set(
+      try {
+        reshaped.coords().set(
           name, slice_and_unstack(maybe_broadcast(coord, stacked_dims, to_dim),
                                   stacked_dims, to_dim));
-      std::cout << "reshape 6" << std::endl;
-      // } catch (const std::exception&) { /* */ }
+      } catch (except::VariableMismatchError &) { }
     } else {
-      std::cout << "reshape 7" << std::endl;
       reshaped.coords().set(
           name, reshape(maybe_broadcast(coord, stacked_dims, to_dim),
                         unstack_dims(coord.dims(), stacked_dims, to_dim)));
-      std::cout << "reshape 8" << std::endl;
     }
   }
-  std::cout << "reshape 9" << std::endl;
 
-  // for (auto &&[name, attr] : a.attrs())
-  //   reshaped.attrs().set(
-  //       name, maybe_broadcast_and_reshape(attr, stacked_dims, to_dim));
+  for (auto &&[name, attr] : a.attrs()) {
+    if (has_bin_edges(attr, old_dims)) {
+      try {
+        reshaped.attrs().set(
+          name, slice_and_unstack(maybe_broadcast(attr, stacked_dims, to_dim),
+                                  stacked_dims, to_dim));
+      } catch (except::VariableMismatchError &) { }
+    } else {
+      reshaped.attrs().set(
+          name, reshape(maybe_broadcast(attr, stacked_dims, to_dim),
+                        unstack_dims(attr.dims(), stacked_dims, to_dim)));
+    }
+  }
 
-  // for (auto &&[name, mask] : a.masks())
-  //   reshaped.masks().set(
-  //       name, maybe_broadcast_and_reshape(mask, stacked_dims, to_dim));
+  for (auto &&[name, mask] : a.masks())
+    reshaped.masks().set(
+        name, reshape(maybe_broadcast(mask, stacked_dims, to_dim),
+        	unstack_dims(mask.dims(), stacked_dims, to_dim)));
 
   return reshaped;
 }
