dc2abc3e7770f2dfd3ac60b32fafe09401c6ae09
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-03-02 10:29:09 +0100

    Add RGN for different dtypes

---
+++ test/test_random.cpp
@@ -0,0 +1,44 @@
+#include "test_random.h"
+
+[[nodiscard]] scipp::variable::Variable
+Random::make_variable(const scipp::Dimensions &dims, const scipp::core::DType dtype,
+              const scipp::units::Unit unit,
+              const bool with_variances) {
+  using namespace scipp;
+  if (dtype == core::dtype<double>) {
+    if (with_variances) {
+      return makeVariable<double>(
+          dims, Values(generate<double>(dims.volume())),
+          Variances(generate<double>(dims.volume()),
+                    [](double x) { return std::abs(x); }),
+          unit);
+    }
+    return makeVariable<double>(dims, Values(generate<double>(dims.volume())),
+                                unit);
+  }
+  assert(!with_variances); // the other dtypes do not support variances
+  if (dtype == core::dtype<int64_t>) {
+    return makeVariable<int64_t>(
+        dims, Values(generate<int64_t>(dims.volume())), unit);
+  }
+  if (dtype == core::dtype<int32_t>) {
+    return makeVariable<int32_t>(
+        dims, Values(generate<int32_t>(dims.volume())), unit);
+  }
+  if (dtype == core::dtype<core::time_point>) {
+    return makeVariable<core::time_point>(
+        dims, Values(generate<core::time_point>(dims.volume())), unit);
+  }
+  throw std::invalid_argument(
+      "Random variable generation is not implemented for dtype " +
+      to_string(dtype));
+}
+
+
+scipp::Variable makeRandom(const scipp::Dimensions &dims,
+                           const double min,
+                           const double max) {
+using namespace scipp;
+Random rand(min, max);
+return makeVariable<double>(Dimensions{dims}, Values(rand(dims.volume())));
+}
--- test/test_random.h
+++ test/test_random.h
@@ -8,20 +8,64 @@
 #include <random>
 #include <vector>
 
+#include "scipp/common/traits.h"
 #include "scipp/variable/variable.h"
 
+namespace detail {
+template <class T> struct identity {
+  constexpr auto operator()(const T &x) const { return x; }
+};
+} // namespace detail
+
 class Random {
-  std::mt19937 mt{std::random_device()()};
-  std::uniform_real_distribution<double> dist;
+  std::mt19937 m_mt{std::random_device()()};
+  double m_min;
+  double m_max;
 
 public:
-  Random(double min = -2.0, double max = 2.0) : dist{min, max} {}
-  std::vector<double> operator()(const int64_t size) {
-    std::vector<double> data(size);
-    std::generate(data.begin(), data.end(), [this]() { return dist(mt); });
+  explicit Random(double min = -10.0, double max = 10.0)
+      : m_min{min}, m_max{max} {}
+
+  void seed(const uint32_t value) { m_mt.seed(value); }
+
+  template <class T, class F = detail::identity<T>>
+  auto generate(const scipp::index size,
+                const F &&transform = detail::identity<T>{}) {
+    std::vector<T> data(size);
+    if constexpr (std::is_floating_point_v<T>) {
+      std::uniform_real_distribution<T> dist{m_min, m_max};
+      std::generate(
+          data.begin(), data.end(),
+          [this, dist, &transform]() mutable { return transform(dist(m_mt)); });
+    } else if constexpr (std::is_integral_v<T>) {
+      std::uniform_int_distribution<T> dist{static_cast<T>(m_min),
+                                            static_cast<T>(m_max)};
+      std::generate(
+          data.begin(), data.end(),
+          [this, dist, &transform]() mutable { return transform(dist(m_mt)); });
+    } else if constexpr (std::is_same_v<T, scipp::core::time_point>) {
+      std::uniform_int_distribution<int64_t> dist{static_cast<int64_t>(m_min),
+                                                  static_cast<int64_t>(m_max)};
+      std::generate(data.begin(), data.end(),
+                    [this, dist, &transform]() mutable {
+                      return transform(scipp::core::time_point{dist(m_mt)});
+                    });
+    } else {
+      static_assert(
+          scipp::common::always_false<T>,
+          "Random data generation is not implemented for the given type.");
+    }
     return data;
   }
-  void seed(const uint32_t value) { mt.seed(value); }
+
+  std::vector<double> operator()(const int64_t size) {
+    return generate<double>(size);
+  }
+
+  [[nodiscard]] scipp::variable::Variable
+  make_variable(const scipp::Dimensions &dims, scipp::core::DType dtype,
+                scipp::units::Unit unit = scipp::units::one,
+                bool with_variances = false);
 };
 
 class RandomBool {
@@ -38,10 +82,5 @@ public:
   void seed(const uint32_t value) { mt.seed(value); }
 };
 
-inline scipp::Variable makeRandom(const scipp::Dimensions &dims,
-                                  const double min = -2.0,
-                                  const double max = 2.0) {
-  using namespace scipp;
-  Random rand(min, max);
-  return makeVariable<double>(Dimensions{dims}, Values(rand(dims.volume())));
-}
+scipp::Variable makeRandom(const scipp::Dimensions &dims, double min = -2.0,
+                           double max = 2.0);
