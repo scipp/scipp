07917b6fdf620be30df41e9dc0fc0b6a3f43c7eb
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-03-04 15:57:21 +0000

    Add new branching for equal nan comparisions

--- variable/comparison.cpp
+++ variable/comparison.cpp
@@ -4,6 +4,7 @@
 /// @author Piotr Rozyczko
 #include "scipp/core/element/comparison.h"
 #include "scipp/variable/comparison.h"
+#include "scipp/variable/special_values.h"
 #include "scipp/variable/transform.h"
 #include "scipp/variable/variable.h"
 
@@ -12,8 +13,13 @@ using namespace scipp::core;
 namespace scipp::variable {
 
 Variable is_approx(const VariableConstView &a, const VariableConstView &b,
-                   const VariableConstView &t) {
-  return transform(a, b, t, element::is_approx);
+                   const VariableConstView rtol, const VariableConstView atol,
+                   const NanComparisons equal_nans) {
+  // TODO atol, rtol - > tol
+  if (equal_nans == NanComparisons::Equal)
+    return variable::transform(a, b, rtol, element::is_approx_equal_nan);
+  else
+    return variable::transform(a, b, rtol, element::is_approx);
 }
 
 } // namespace scipp::variable
