f9c817681827cc9420fde9c4c8740809b64cc78b
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-03-05 10:34:44 +0100

    Add is_discrete

--- python/src/scipp/_utils/is_type.py
+++ python/src/scipp/_utils/is_type.py
@@ -3,6 +3,8 @@
 # @file
 # @author Neil Vaytet
 
+import numpy as np
+
 from .._scipp import core as sc
 
 
@@ -32,3 +34,14 @@ def is_dataset_or_array(obj):
     Return True if the input object is either a Dataset or DataArray.
     """
     return is_dataset(obj) or is_data_array(obj)
+
+
+def is_discrete(obj):
+    """
+    Return True if the input object is a type that has a discrete domain.
+    """
+    return isinstance(obj, (int, np.datetime64)) or (
+        isinstance(obj, np.ndarray)
+        and obj.dtype in (np.int32, np.int64, np.dtype(np.datetime64))) or (
+            is_variable(obj) and obj.dtype
+            in (sc.dtype.int32, sc.dtype.int64, sc.dtype.datetime64))
