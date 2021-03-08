e8da8c0e9561186dcb02477d91188d53f98cec2f
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-03-08 14:31:45 +0000

    Add is_near to utilities and test

---
+++ python/src/scipp/_utils/comparison.py
@@ -0,0 +1,60 @@
+import scipp as sc
+
+
+def is_near(a,
+            b,
+            rtol=None,
+            atol=None,
+            include_attrs=True,
+            include_data=True,
+            equal_nan=True):
+    """
+    Similar to scipp is_close, but intended to compare whole DataArrays.
+    Coordinates compared element by element with abs(a - b) <= atol + rtol * b
+
+    :param a: lhs input
+    :param b: rhs input
+    :param rtol: relative tolerance (to b)
+    :param atol: absolute tolerance
+    :param include_data: Compare that data is within tolerance
+    :param include_attrs: Compare all meta (coords and attrs) between a and b,
+           otherwise only compare coordinates from meta
+    :param equal_nan: If True, consider Nans or Infs to be equal
+                       providing that they match in location and, for infs,
+                       have the same sign
+    :type a: DataArray
+    :type b: DataArray
+    :type tol :
+    :raises: If a, b are not going to be logically comparable
+             for reasons relating to shape, item naming or non-finite elements.
+    :return True if near:
+    """
+    same_data = sc.all(
+        sc.is_close(a.data,
+                    b.data,
+                    rtol=float(rtol),
+                    atol=float(atol),
+                    equal_nan=equal_nan)).value if include_data else True
+    same_len = len(a.meta) == len(b.meta) if include_attrs else len(
+        a.coords) == len(b.coords)
+    if not same_len:
+        raise RuntimeError('Different number of items'
+                           f'in meta {len(a.meta)} {len(b.meta)}')
+    for key, val in a.meta.items() if include_attrs else a.coords.items():
+        x = a.meta[key] if include_attrs else a.coords[key]
+        y = b.meta[key] if include_attrs else b.coords[key]
+        if x.shape != y.shape:
+            raise RuntimeError(f'For meta {key} have different'
+                               f' shapes {x.shape}, {y.shape}')
+        if val.dtype in [sc.dtype.float64, sc.dtype.float32]:
+            if sc.sum(~sc.isfinite(x)).value > 0 or sc.sum(
+                    ~sc.isfinite(y)).value > 0:
+                raise RuntimeError(f'For meta {key} have non-finite entries')
+            if not sc.all(
+                    sc.is_close(x,
+                                y,
+                                rtol=float(rtol),
+                                atol=float(atol),
+                                equal_nan=equal_nan)).value:
+                return False
+    return same_data
---
+++ python/tests/_utils/test_comparision.py
@@ -0,0 +1,82 @@
+import numpy as np
+import scipp as sc
+import scipp._utils as su
+import pytest
+
+
+def test_wont_match_when_meta_size_unequal():
+    point = sc.scalar(value=1.0)
+    a = sc.DataArray(data=point, attrs={'x': point})
+    b = sc.DataArray(data=point)
+    with pytest.raises(RuntimeError):
+        su.is_near(a, b, rtol=0, atol=1.0)
+    # Raise nothing if we are ignoring differing parts
+    su.is_near(a, b, rtol=0, atol=1.0, include_attrs=False)
+
+
+def test_wont_match_when_meta_keys_unequal():
+    point = sc.scalar(value=1.0)
+    a = sc.DataArray(data=point, attrs={'x': point})
+    b = sc.DataArray(data=point, attrs={'y': point})
+    with pytest.raises(RuntimeError):
+        su.is_near(a, b, rtol=0, atol=1.0)
+    # Raise nothing if we are ignoring differing parts
+    su.is_near(a, b, rtol=0, atol=1.0, include_attrs=False)
+
+
+def test_wont_match_when_meta_sizes_unequal():
+    point = sc.scalar(value=1.0)
+    a = sc.DataArray(data=point, attrs={'x': point})
+    b = sc.DataArray(data=point,
+                     attrs={'x': sc.array(dims=['x'], values=np.arange(2))})
+    with pytest.raises(RuntimeError):
+        su.is_near(a, b, rtol=0, atol=1.0)
+    # Raise nothing if we are ignoring differing parts
+    su.is_near(a, b, rtol=0, atol=1.0, include_attrs=False)
+
+
+def test_data_scalar_no_coords():
+    a = sc.DataArray(data=sc.scalar(value=1.0))
+    assert su.is_near(a, a, rtol=0, atol=1e-14)
+    b = a + 1.0 * sc.units.one
+    assert su.is_near(a, b, rtol=0.0, atol=1.0)
+    assert su.is_near(a, b, rtol=1.0, atol=0.0)
+    assert not su.is_near(a, b, rtol=0, atol=0.9999)
+
+
+def test_data_scalar_no_coords_no_data():
+    a = sc.DataArray(data=sc.scalar(value=1))
+    b = a.copy()
+    assert su.is_near(a, b, rtol=0, atol=1e-14,
+                      include_data=False)  # Should compare equal
+
+
+def test_scalar_with_coord():
+    point = sc.scalar(value=1.0)
+    a = sc.DataArray(data=point, coords={'x': point})
+    assert su.is_near(a, a, rtol=0.0, atol=1e-14)
+    b = sc.DataArray(data=point, coords={'x': point + point})
+    assert su.is_near(a, b, rtol=0.0, atol=1.0)
+    assert not su.is_near(a, b, rtol=0.0, atol=0.9999)
+
+
+def test_with_many_coords():
+    x = sc.array(dims=['x'], values=np.arange(10.0))
+    xx = sc.array(dims=['x'], values=np.arange(1, 11.0))
+    a = sc.DataArray(data=x, coords={'a': x, 'b': x})
+    b = sc.DataArray(data=x, coords={'a': x, 'b': xx})
+    assert su.is_near(a, a, rtol=0.0, atol=1e-14)
+    assert su.is_near(a, b, rtol=0.0, atol=1.0)
+    assert not su.is_near(a, b, rtol=0.0, atol=0.9999)
+
+
+def test_with_many_coords_and_attrs():
+    x = sc.array(dims=['x'], values=np.arange(10.0))
+    xx = sc.array(dims=['x'], values=np.arange(1, 11.0))
+    a = sc.DataArray(data=x, coords={'a': x, 'b': x}, attrs={'c': x, 'd': x})
+    b = sc.DataArray(data=x, coords={'a': x, 'b': x}, attrs={'c': x, 'd': xx})
+    assert su.is_near(a, a, rtol=0.0, atol=1e-14)
+    assert su.is_near(a, b, rtol=0.0, atol=1.0)
+    assert not su.is_near(a, b, rtol=0.0, atol=0.9999)
+    # Now disable attrs matching (should be near)
+    assert su.is_near(a, b, rtol=0.0, atol=0.9999, include_attrs=False)
