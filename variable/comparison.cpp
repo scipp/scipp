64bf596f068b239309f6c136ff8dae7642fa27c2
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-03-09 20:19:32 +0000

    Call self solution for values and variances

--- variable/comparison.cpp
+++ variable/comparison.cpp
@@ -22,20 +22,9 @@ Variable isclose(const VariableConstView &a, const VariableConstView &b,
                  const NanComparisons equal_nans) {
   auto tol = atol + rtol * abs(b);
   if (a.hasVariances() && b.hasVariances()) {
-    auto error_tol = atol + rtol * abs(variances(b));
-    if (equal_nans == NanComparisons::Equal)
-      return variable::transform(a, b, _values(std::move(error_tol)),
-                                 element::isclose_equal_nan) &
-             variable::transform(sqrt(variances(a)), sqrt(variances(b)),
-                                 _values(std::move(error_tol)),
-                                 element::isclose_equal_nan);
-    else
-      return variable::transform(
-                 a, b, error_tol.hasVariances() ? values(error_tol) : error_tol,
-                 element::isclose) &
-             variable::transform(sqrt(variances(a)), sqrt(variances(b)),
-                                 _values(std::move(error_tol)),
-                                 element::isclose);
+    return isclose(values(a), values(b), rtol, atol, equal_nans) &
+           isclose(sqrt(variances(a)), sqrt(variances(b)), rtol, atol,
+                   equal_nans);
   } else {
     if (equal_nans == NanComparisons::Equal)
       return variable::transform(a, b, _values(std::move(tol)),
