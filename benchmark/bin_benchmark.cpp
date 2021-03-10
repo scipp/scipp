3fcdcc652c3ce534aa3fc23bd4a7c3155d71fb35
Simon Heybrock <simon.heybrock@esss.se>
2021-03-10 07:39:54 +0100

    Change licence from GPLv3 to BSD-3

--- benchmark/bin_benchmark.cpp
+++ benchmark/bin_benchmark.cpp
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 /// @file
 #include <benchmark/benchmark.h>
