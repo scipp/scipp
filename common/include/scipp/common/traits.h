650ca7b46e8a14e727a587e97fc666604688260e
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-03-16 13:48:24 +0100

    Add traits to detect const views

--- common/include/scipp/common/traits.h
+++ common/include/scipp/common/traits.h
@@ -20,4 +20,25 @@ struct index_in_tuple<T, std::tuple<U, Types...>> {
       1 + index_in_tuple<T, std::tuple<Types...>>::value;
 };
 
+template <class, class = void> struct has_const_view_type : std::false_type {};
+
+template <class T>
+struct has_const_view_type<T, std::void_t<typename T::const_view_type>>
+    : std::true_type {};
+
+/// true iff T has a const view, e.g. Variable or ConstVariableView.
+template <class T>
+constexpr inline bool has_const_view_type_v = has_const_view_type<T>::value;
+
+template <class, class = void> struct is_const_view_type : std::false_type {};
+
+// All const views T have a const_view_type member which is equal to T itself.
+template <class T>
+struct is_const_view_type<T, std::enable_if_t<has_const_view_type_v<T>>>
+    : std::is_same<typename T::const_view_type, std::decay_t<T>> {};
+
+/// true iff T is a const view, e.g. ConstVariableView.
+template <class T>
+constexpr inline bool is_const_view_type_v = is_const_view_type<T>::value;
+
 } // namespace scipp::common
