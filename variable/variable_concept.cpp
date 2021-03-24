92f0a33479458390ec461366a25967c31f963845
Simon Heybrock <simon.heybrock@esss.se>
2021-03-24 09:29:08 +0100

    Remove deep_ptr

--- variable/variable_concept.cpp
+++ variable/variable_concept.cpp
@@ -3,28 +3,6 @@
 
 namespace scipp::variable {
 
-/*
-VariableConceptHandle::VariableConceptHandle(const VariableConceptHandle &other)
-    : VariableConceptHandle(other ? other->clone() : VariableConceptHandle()) {}
-
-VariableConceptHandle &
-VariableConceptHandle::operator=(const VariableConceptHandle &other) {
-  if (*this && other) {
-    // Avoid allocation of new element_array if output is of correct shape.
-    // This yields a 5x speedup in assignment operations of variables.
-    auto &varconcept = **this;
-    auto &otherConcept = *other;
-    if (varconcept.dtype() == otherConcept.dtype() &&
-        varconcept.size() == otherConcept.size() &&
-        varconcept.hasVariances() == otherConcept.hasVariances()) {
-      varconcept.assign(otherConcept);
-      return *this;
-    }
-  }
-  return *this = other ? other->clone() : VariableConceptHandle();
-}
-*/
-
 VariableConcept::VariableConcept(const units::Unit &unit) : m_unit(unit) {}
 
 } // namespace scipp::variable
