77fb562c1c15948fde7704861e83291525f1630e
Simon Heybrock <simon.heybrock@esss.se>
2021-03-25 14:05:29 +0100

    Begin refactoring metadata dicts

---
+++ core/sizes.cpp
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#include "scipp/core/except.h"
+#include "scipp/core/sizes.h"
+
+namespace scipp::core {
+
+Sizes::Sizes(const Dimensions &dims) {
+  for (const auto dim : dims.labels())
+    m_sizes[dim] = dims[dim];
+}
+
+scipp::index Sizes::operator[](const Dim dim) const {
+  if (!contains(dim))
+    throw except::DimensionError("dim not found");
+  return m_sizes.at(dim);
+}
+
+bool Sizes::contains(const Dimensions &dims) {
+  for (const auto &dim : dims.labels())
+    if (m_sizes.count(dim) == 0 || m_sizes.at(dim) != dims[dim])
+      return false;
+  return true;
+}
+
+Sizes Sizes::slice(const Slice &params) const {
+  auto sizes = m_sizes;
+  if (params.isRange())
+    sizes.at(params.dim()) = params.end() - params.begin();
+  else
+    sizes.erase(params.dim());
+  return {sizes};
+}
+
+} // namespace scipp::core
