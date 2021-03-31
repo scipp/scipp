70699aba257fe672f3e78d77c48d03ab0b495c49
Neil Vaytet <neil.vaytet@esss.se>
2021-03-31 12:09:57 +0200

    try to set install target path and copy all libraries over

--- conda/build.sh
+++ conda/build.sh
@@ -2,10 +2,14 @@
 
 set -ex
 
-if test -z "${ALREADY_BUILT}"
+if test -z "${INSTALL_TARGET}"
 then
   ./tools/make_and_install.sh
+  INSTALL_TARGET=$CONDA_PREFIX
 fi
 
 # Move scipp Python library to site packages location
-mv "$CONDA_PREFIX/scipp" "$CONDA_PREFIX"/lib/python*/
+mv "$INSTALL_TARGET"/scipp "$CONDA_PREFIX"/lib/python*/
+mv "$INSTALL_TARGET"/lib/*.so "$CONDA_PREFIX"/lib/
+mv "$INSTALL_TARGET"/lib/cmake/scipp "$CONDA_PREFIX"/lib/cmake/
+mv "$INSTALL_TARGET"/include/* "$CONDA_PREFIX"/include/
