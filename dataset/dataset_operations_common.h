0138c7d1eb5be420c718f905dc6176788d60b7c8
Simon Heybrock <simon.heybrock@esss.se>
2021-04-06 14:57:11 +0200

    Simplify and improve coord->attr mapping code

--- dataset/dataset_operations_common.h
+++ dataset/dataset_operations_common.h
@@ -18,14 +18,7 @@ constexpr auto unaligned_by_dim_slice = [](const auto &item,
     return false;
   const Dim dim = params.dim();
   const auto &[key, var] = item;
-  if constexpr (std::is_same_v<std::decay_t<decltype(item.first)>, Dim>) {
-    // TODO why duplicate?
-    const bool is_dimension_coord = var.dims().contains(key);
-    return var.dims().contains(dim) &&
-           (is_dimension_coord ? key == dim : var.dims().inner() == dim);
-  } else {
-    return false;
-  }
+  return var.dims().contains(dim) && dim_of_coord(var, key) == dim;
 };
 
 template <class T1, class T2> auto union_(const T1 &a, const T2 &b) {
