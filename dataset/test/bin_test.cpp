dc5938b1bcd5a22187ca8bcac6a1a56dda8f7f4c
Simon Heybrock <simon.heybrock@esss.se>
2021-04-06 11:24:12 +0200

    Avoid copying meta dict before erase

--- dataset/test/bin_test.cpp
+++ dataset/test/bin_test.cpp
@@ -8,6 +8,7 @@
 #include "scipp/dataset/bins.h"
 #include "scipp/dataset/bins_view.h"
 #include "scipp/dataset/histogram.h"
+#include "scipp/dataset/string.h"
 #include "scipp/variable/arithmetic.h"
 #include "scipp/variable/comparison.h"
 #include "scipp/variable/misc_operations.h"
