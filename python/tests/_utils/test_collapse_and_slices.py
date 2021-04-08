512c55794b0d6586d72eb7e562af823cc8a54e8a
Simon Heybrock <simon.heybrock@esss.se>
2021-04-08 12:07:41 +0200

    Remove outdated test file

--- python/tests/_utils/test_collapse_and_slices.py
+++ python/tests/_utils/test_collapse_and_slices.py
@@ -17,6 +17,7 @@ def make_dataset():
     a[1, 1, :] = np.abs(10.0 * np.cos((x - b) * 2.0 / b))
     v = 0.1 * np.random.random([L, M, N])
     d = sc.Dataset()
+    d['sample'] = sc.Variable(['y', 'x', 'tof'], values=a, variances=v)
     d.coords['tof'] = sc.Variable(['tof'], values=x, unit=sc.units.us)
     d.coords['x'] = sc.Variable(['x'],
                                 values=np.arange(M).astype(np.float64),
@@ -24,7 +25,6 @@ def make_dataset():
     d.coords['y'] = sc.Variable(['y'],
                                 values=np.arange(L).astype(np.float64),
                                 unit=sc.units.m)
-    d['sample'] = sc.Variable(['y', 'x', 'tof'], values=a, variances=v)
     return d
 
 
