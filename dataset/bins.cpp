cfba2a38cb49bdb3feaeff79b1f32bcdb954e421
Simon Heybrock <simon.heybrock@esss.se>
2021-04-08 07:11:04 +0200

    Make Dimensions(Sizes) constructor explicit

--- dataset/bins.cpp
+++ dataset/bins.cpp
@@ -169,7 +169,8 @@ Variable make_bins_no_validate(Variable indices, const Dim dim,
 /// Each bin is represented by a Variable slice. `indices` defines the array of
 /// bins as slices of `buffer` along `dim`.
 Variable make_bins(Variable indices, const Dim dim, Dataset buffer) {
-  expect_valid_bin_indices(indices.data_handle(), dim, buffer.dims());
+  expect_valid_bin_indices(indices.data_handle(), dim,
+                           Dimensions(buffer.sizes()));
   return make_bins_no_validate(std::move(indices), dim, std::move(buffer));
 }
 
