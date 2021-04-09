14d1bcf17f17daf3edf0e690c6480a8e8ba3683d
Simon Heybrock <simon.heybrock@esss.se>
2021-04-09 12:16:43 +0200

    Use concrete int size to fix Windows Python tests

--- python/tests/test_setitem.py
+++ python/tests/test_setitem.py
@@ -33,9 +33,11 @@ def test_setitem_coords_required_for_inplace_ops():
     da = sc.DataArray(data=var)
     da.coords['x'] = var
     da['x', 2:].coords['x'] += 1
-    assert sc.identical(da.coords['x'],
-                        sc.array(dims=['x'], values=[0, 0, 1, 1]))
+    assert sc.identical(
+        da.coords['x'],
+        sc.array(dims=['x'], dtype=sc.dtype.int64, values=[0, 0, 1, 1]))
     ds = sc.Dataset({'a': da})
     ds['x', 2:].coords['x'] += 1
-    assert sc.identical(ds.coords['x'],
-                        sc.array(dims=['x'], values=[0, 0, 2, 2]))
+    assert sc.identical(
+        ds.coords['x'],
+        sc.array(dims=['x'], dtype=sc.dtype.int64, values=[0, 0, 2, 2]))
