16cbb8cd2966a0fe602ebe4fc3e65ce5cd947d86
Neil Vaytet <neil.vaytet@esss.se>
2021-04-13 11:29:14 +0200

    move the rest of the files from the install step into the correct places for the conda build

--- conda/bld.bat
+++ conda/bld.bat
@@ -4,3 +4,11 @@ if "%INSTALL_TARGET%" == "" call tools\make_and_install.bat & set INSTALL_TARGET
 dir /s/b %INSTALL_TARGET%
 
 move %INSTALL_TARGET%\scipp %CONDA_PREFIX%\lib\
+move %INSTALL_TARGET%\bin\scipp-*.dll %CONDA_PREFIX%\bin\
+move %INSTALL_TARGET%\bin\units-shared.dll %CONDA_PREFIX%\bin\
+move %INSTALL_TARGET%\Lib\scipp-*.lib %CONDA_PREFIX%\Lib\
+move %INSTALL_TARGET%\Lib\units-shared.lib %CONDA_PREFIX%\Lib\
+move %INSTALL_TARGET%\Lib\cmake\scipp %CONDA_PREFIX%\Lib\cmake\
+move %INSTALL_TARGET%\include\scipp* %CONDA_PREFIX%\include\
+move %INSTALL_TARGET%\include\Eigen %CONDA_PREFIX%\include\
+move %INSTALL_TARGET%\include\units %CONDA_PREFIX%\include\
