4f8dde5d1ae2521edfe3350e2f04e601e8bef5df
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-04-16 10:52:34 +0200

    Add copy test for DataArray

--- python/tests/test_ownership_dataset.py
+++ python/tests/test_ownership_dataset.py
@@ -3,17 +3,21 @@
 # @author Jan-Lukas Wynen
 from copy import copy, deepcopy
 
-import numpy as np
 import scipp as sc
 
 
-def test_own_darr_set():
-    # Data and metadata are shared
+def make_data_array():
     v = sc.array(dims=['x'], values=[10, 20], unit='m')
     c = sc.array(dims=['x'], values=[1, 2], unit='s')
     a = sc.array(dims=['x'], values=[100, 200])
     m = sc.array(dims=['x'], values=[True, False])
     da = sc.DataArray(v, coords={'x': c}, attrs={'a': a}, masks={'m': m})
+    return da, v, c, a, m
+
+
+def test_own_darr_set():
+    # Data and metadata are shared
+    da, v, c, a, m = make_data_array()
     da['x', 0] = -10
     da.data['x', 1] = -20
     da.coords['x']['x', 0] = -1
@@ -24,10 +28,13 @@ def test_own_darr_set():
     m['x', 1] = True
     da.unit = 'kg'
     da.coords['x'].unit = 'J'
-    assert sc.identical(da, sc.DataArray(sc.array(dims=['x'], values=[-10, -20], unit='kg'),
-                                         coords={'x': sc.array(dims=['x'], values=[-1, -2], unit='J')},
-                                         attrs={'a': sc.array(dims=['x'], values=[-100, -200])},
-                                         masks={'m': sc.array(dims=['x'], values=[False, True])}))
+    assert sc.identical(
+        da,
+        sc.DataArray(
+            sc.array(dims=['x'], values=[-10, -20], unit='kg'),
+            coords={'x': sc.array(dims=['x'], values=[-1, -2], unit='J')},
+            attrs={'a': sc.array(dims=['x'], values=[-100, -200])},
+            masks={'m': sc.array(dims=['x'], values=[False, True])}))
     assert sc.identical(v, sc.array(dims=['x'], values=[-10, -20], unit='kg'))
     assert sc.identical(c, sc.array(dims=['x'], values=[-1, -2], unit='J'))
     assert sc.identical(a, sc.array(dims=['x'], values=[-100, -200]))
@@ -39,10 +46,13 @@ def test_own_darr_set():
     da.coords['x'] = sc.array(dims=['x'], values=[3, 4], unit='s')
     da.attrs['a'] = sc.array(dims=['x'], values=[300, 400])
     da.masks['m'] = sc.array(dims=['x'], values=[True, True])
-    assert sc.identical(da, sc.DataArray(sc.array(dims=['x'], values=[11, 22], unit='m'),
-                                         coords={'x': sc.array(dims=['x'], values=[3, 4], unit='s')},
-                                         attrs={'a': sc.array(dims=['x'], values=[300, 400])},
-                                         masks={'m': sc.array(dims=['x'], values=[True, True])}))
+    assert sc.identical(
+        da,
+        sc.DataArray(
+            sc.array(dims=['x'], values=[11, 22], unit='m'),
+            coords={'x': sc.array(dims=['x'], values=[3, 4], unit='s')},
+            attrs={'a': sc.array(dims=['x'], values=[300, 400])},
+            masks={'m': sc.array(dims=['x'], values=[True, True])}))
     assert sc.identical(v, sc.array(dims=['x'], values=[11, 22], unit='m'))
     assert sc.identical(c, sc.array(dims=['x'], values=[-1, -2], unit='J'))
     assert sc.identical(a, sc.array(dims=['x'], values=[-100, -200]))
@@ -51,10 +61,7 @@ def test_own_darr_set():
 
 def test_own_darr_get():
     # Data and metadata are shared.
-    da = sc.DataArray(sc.array(dims=['x'], values=[10, 20], unit='m'),
-                      coords={'x': sc.array(dims=['x'], values=[1, 2], unit='s')},
-                      attrs={'a': sc.array(dims=['x'], values=[100, 200])},
-                      masks={'m': sc.array(dims=['x'], values=[True, False])})
+    da = make_data_array()[0]
     v = da.data
     c = da.coords['x']
     a = da.attrs['a']
@@ -69,10 +76,13 @@ def test_own_darr_get():
     m['x', 1] = True
     da.unit = 'kg'
     da.coords['x'].unit = 'J'
-    assert sc.identical(da, sc.DataArray(sc.array(dims=['x'], values=[-10, -20], unit='kg'),
-                                         coords={'x': sc.array(dims=['x'], values=[-1, -2], unit='J')},
-                                         attrs={'a': sc.array(dims=['x'], values=[-100, -200])},
-                                         masks={'m': sc.array(dims=['x'], values=[False, True])}))
+    assert sc.identical(
+        da,
+        sc.DataArray(
+            sc.array(dims=['x'], values=[-10, -20], unit='kg'),
+            coords={'x': sc.array(dims=['x'], values=[-1, -2], unit='J')},
+            attrs={'a': sc.array(dims=['x'], values=[-100, -200])},
+            masks={'m': sc.array(dims=['x'], values=[False, True])}))
     assert sc.identical(v, sc.array(dims=['x'], values=[-10, -20], unit='kg'))
     assert sc.identical(c, sc.array(dims=['x'], values=[-1, -2], unit='J'))
     assert sc.identical(a, sc.array(dims=['x'], values=[-100, -200]))
@@ -84,10 +94,13 @@ def test_own_darr_get():
     da.coords['x'] = sc.array(dims=['x'], values=[3, 4], unit='s')
     da.attrs['a'] = sc.array(dims=['x'], values=[300, 400])
     da.masks['m'] = sc.array(dims=['x'], values=[True, True])
-    assert sc.identical(da, sc.DataArray(sc.array(dims=['x'], values=[11, 22], unit='m'),
-                                         coords={'x': sc.array(dims=['x'], values=[3, 4], unit='s')},
-                                         attrs={'a': sc.array(dims=['x'], values=[300, 400])},
-                                         masks={'m': sc.array(dims=['x'], values=[True, True])}))
+    assert sc.identical(
+        da,
+        sc.DataArray(
+            sc.array(dims=['x'], values=[11, 22], unit='m'),
+            coords={'x': sc.array(dims=['x'], values=[3, 4], unit='s')},
+            attrs={'a': sc.array(dims=['x'], values=[300, 400])},
+            masks={'m': sc.array(dims=['x'], values=[True, True])}))
     assert sc.identical(v, sc.array(dims=['x'], values=[11, 22], unit='m'))
     assert sc.identical(c, sc.array(dims=['x'], values=[-1, -2], unit='J'))
     assert sc.identical(a, sc.array(dims=['x'], values=[-100, -200]))
@@ -96,9 +109,8 @@ def test_own_darr_get():
 
 def test_own_darr_get_meta():
     # Data and metadata are shared.
-    da = sc.DataArray(sc.array(dims=['x'], values=[10, 20], unit='m'),
-                      coords={'x': sc.array(dims=['x'], values=[1, 2], unit='s')},
-                      attrs={'a': sc.array(dims=['x'], values=[100, 200])})
+    da = make_data_array()[0]
+    del da.masks['m']  # not accessible through .meta and tested elsewhere
     v = da.data
     c = da.meta['x']
     a = da.meta['a']
@@ -110,9 +122,12 @@ def test_own_darr_get_meta():
     a['x', 1] = -200
     da.unit = 'kg'
     da.coords['x'].unit = 'J'
-    assert sc.identical(da, sc.DataArray(sc.array(dims=['x'], values=[-10, -20], unit='kg'),
-                                         coords={'x': sc.array(dims=['x'], values=[-1, -2], unit='J')},
-                                         attrs={'a': sc.array(dims=['x'], values=[-100, -200])}))
+    assert sc.identical(
+        da,
+        sc.DataArray(
+            sc.array(dims=['x'], values=[-10, -20], unit='kg'),
+            coords={'x': sc.array(dims=['x'], values=[-1, -2], unit='J')},
+            attrs={'a': sc.array(dims=['x'], values=[-100, -200])}))
     assert sc.identical(v, sc.array(dims=['x'], values=[-10, -20], unit='kg'))
     assert sc.identical(c, sc.array(dims=['x'], values=[-1, -2], unit='J'))
     assert sc.identical(a, sc.array(dims=['x'], values=[-100, -200]))
@@ -122,9 +137,54 @@ def test_own_darr_get_meta():
     da.data = sc.array(dims=['x'], values=[11, 22], unit='m')
     da.coords['x'] = sc.array(dims=['x'], values=[3, 4], unit='s')
     da.attrs['a'] = sc.array(dims=['x'], values=[300, 400])
-    assert sc.identical(da, sc.DataArray(sc.array(dims=['x'], values=[11, 22], unit='m'),
-                                         coords={'x': sc.array(dims=['x'], values=[3, 4], unit='s')},
-                                         attrs={'a': sc.array(dims=['x'], values=[300, 400])}))
+    assert sc.identical(
+        da,
+        sc.DataArray(
+            sc.array(dims=['x'], values=[11, 22], unit='m'),
+            coords={'x': sc.array(dims=['x'], values=[3, 4], unit='s')},
+            attrs={'a': sc.array(dims=['x'], values=[300, 400])}))
     assert sc.identical(v, sc.array(dims=['x'], values=[11, 22], unit='m'))
     assert sc.identical(c, sc.array(dims=['x'], values=[-1, -2], unit='J'))
     assert sc.identical(a, sc.array(dims=['x'], values=[-100, -200]))
+
+
+def test_own_darr_copy():
+    # TODO intentional?
+    # Copy function and method copy only data and masks,
+    # deepcopy copies all members.
+    da, _, c, a, m = make_data_array()
+    da_copy = copy(da)
+    da_deepcopy = deepcopy(da)
+    da_methcopy = da.copy()
+    da['x', 0] = -10
+    da.data['x', 1] = -20
+    da.coords['x']['x', 0] = -1
+    da.attrs['a']['x', 0] = -100
+    da.masks['m']['x', 0] = False
+    c['x', 1] = -2
+    a['x', 1] = -200
+    m['x', 1] = True
+    da.unit = 'kg'
+    da.coords['x'].unit = 'J'
+    assert sc.identical(
+        da,
+        sc.DataArray(
+            sc.array(dims=['x'], values=[-10, -20], unit='kg'),
+            coords={'x': sc.array(dims=['x'], values=[-1, -2], unit='J')},
+            attrs={'a': sc.array(dims=['x'], values=[-100, -200])},
+            masks={'m': sc.array(dims=['x'], values=[False, True])}))
+    assert sc.identical(
+        da_copy,
+        sc.DataArray(
+            sc.array(dims=['x'], values=[10, 20], unit='m'),
+            coords={'x': sc.array(dims=['x'], values=[-1, -2], unit='J')},
+            attrs={'a': sc.array(dims=['x'], values=[-100, -200])},
+            masks={'m': sc.array(dims=['x'], values=[True, False])}))
+    assert sc.identical(da_deepcopy, make_data_array()[0])
+    assert sc.identical(
+        da_methcopy,
+        sc.DataArray(
+            sc.array(dims=['x'], values=[10, 20], unit='m'),
+            coords={'x': sc.array(dims=['x'], values=[-1, -2], unit='J')},
+            attrs={'a': sc.array(dims=['x'], values=[-100, -200])},
+            masks={'m': sc.array(dims=['x'], values=[True, False])}))
