e070d18c205c53d89e1e16dc571a1171b0184ac9
Neil Vaytet <neil.vaytet@esss.se>
2021-04-20 15:41:54 +0200

    try with xcopy and try to use multiple cores

--- conda/bld.bat
+++ conda/bld.bat
@@ -3,15 +3,15 @@ if "%INSTALL_PREFIX%" == "" set INSTALL_PREFIX=%cd% & call tools\make_and_instal
 
 dir /s/b %INSTALL_PREFIX%
 
-robocopy %INSTALL_PREFIX%\scipp %CONDA_PREFIX%\lib\ /e
-robocopy %INSTALL_PREFIX%\bin\scipp-*.dll %CONDA_PREFIX%\bin\ /e
-robocopy %INSTALL_PREFIX%\bin\units-shared.dll %CONDA_PREFIX%\bin\ /e
-robocopy %INSTALL_PREFIX%\Lib\scipp-*.lib %CONDA_PREFIX%\Lib\ /e
-robocopy %INSTALL_PREFIX%\Lib\units-shared.lib %CONDA_PREFIX%\Lib\ /e
-robocopy %INSTALL_PREFIX%\Lib\cmake\scipp %CONDA_PREFIX%\Lib\cmake\ /e
-robocopy %INSTALL_PREFIX%\include\scipp*.h %CONDA_PREFIX%\include\ /e
-robocopy %INSTALL_PREFIX%\include\scipp %CONDA_PREFIX%\include\ /e
-robocopy %INSTALL_PREFIX%\include\Eigen %CONDA_PREFIX%\include\ /e
-robocopy %INSTALL_PREFIX%\include\units %CONDA_PREFIX%\include\ /e
+xcopy %INSTALL_PREFIX%\scipp %CONDA_PREFIX%\lib\ /s /e
+xcopy %INSTALL_PREFIX%\bin\scipp-*.dll %CONDA_PREFIX%\bin\ /s /e
+xcopy %INSTALL_PREFIX%\bin\units-shared.dll %CONDA_PREFIX%\bin\ /s /e
+xcopy %INSTALL_PREFIX%\Lib\scipp-*.lib %CONDA_PREFIX%\Lib\ /s /e
+xcopy %INSTALL_PREFIX%\Lib\units-shared.lib %CONDA_PREFIX%\Lib\ /s /e
+xcopy %INSTALL_PREFIX%\Lib\cmake\scipp %CONDA_PREFIX%\Lib\cmake\ /s /e
+xcopy %INSTALL_PREFIX%\include\scipp*.h %CONDA_PREFIX%\include\ /s /e
+xcopy %INSTALL_PREFIX%\include\scipp %CONDA_PREFIX%\include\ /s /e
+xcopy %INSTALL_PREFIX%\include\Eigen %CONDA_PREFIX%\include\ /s /e
+xcopy %INSTALL_PREFIX%\include\units %CONDA_PREFIX%\include\ /s /e
 
 dir /s/b %CONDA_PREFIX%
