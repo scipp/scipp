1701f3dd57f23f9f5f9de7286c8b218713a046e3
Neil Vaytet <neil.vaytet@esss.se>
2021-04-20 19:03:55 +0200

    try again with robocopy

--- conda/bld.bat
+++ conda/bld.bat
@@ -3,15 +3,15 @@ if "%INSTALL_PREFIX%" == "" set INSTALL_PREFIX=%cd% & call tools\make_and_instal
 
 dir /s/b %INSTALL_PREFIX%
 
-xcopy %INSTALL_PREFIX%\scipp %CONDA_PREFIX%\lib\ /s /e
-xcopy %INSTALL_PREFIX%\bin\scipp-*.dll %CONDA_PREFIX%\bin\ /s /e
-xcopy %INSTALL_PREFIX%\bin\units-shared.dll %CONDA_PREFIX%\bin\ /s /e
-xcopy %INSTALL_PREFIX%\Lib\scipp-*.lib %CONDA_PREFIX%\Lib\ /s /e
-xcopy %INSTALL_PREFIX%\Lib\units-shared.lib %CONDA_PREFIX%\Lib\ /s /e
-xcopy %INSTALL_PREFIX%\Lib\cmake\scipp %CONDA_PREFIX%\Lib\cmake\ /s /e
-xcopy %INSTALL_PREFIX%\include\scipp*.h %CONDA_PREFIX%\include\ /s /e
-xcopy %INSTALL_PREFIX%\include\scipp %CONDA_PREFIX%\include\ /s /e
-xcopy %INSTALL_PREFIX%\include\Eigen %CONDA_PREFIX%\include\ /s /e
-xcopy %INSTALL_PREFIX%\include\units %CONDA_PREFIX%\include\ /s /e
+robocopy %INSTALL_PREFIX%\scipp %CONDA_PREFIX%\lib\scipp /e /move
+robocopy %INSTALL_PREFIX%\bin\scipp-*.dll %CONDA_PREFIX%\bin\ /e /move
+robocopy %INSTALL_PREFIX%\bin\units-shared.dll %CONDA_PREFIX%\bin\ /e /move
+robocopy %INSTALL_PREFIX%\Lib\scipp-*.lib %CONDA_PREFIX%\Lib\ /e /move
+robocopy %INSTALL_PREFIX%\Lib\units-shared.lib %CONDA_PREFIX%\Lib\ /e /move
+robocopy %INSTALL_PREFIX%\Lib\cmake\scipp %CONDA_PREFIX%\Lib\cmake\scipp /e /move
+robocopy %INSTALL_PREFIX%\include\scipp*.h %CONDA_PREFIX%\include\ /e /move
+robocopy %INSTALL_PREFIX%\include\scipp %CONDA_PREFIX%\include\scipp /e /move
+robocopy %INSTALL_PREFIX%\include\Eigen %CONDA_PREFIX%\include\Eigen /e /move
+robocopy %INSTALL_PREFIX%\include\units %CONDA_PREFIX%\include\units /e /move
 
 dir /s/b %CONDA_PREFIX%
