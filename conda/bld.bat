735c60f0f5c4aad67841a9cfba252bac11506764
Neil Vaytet <neil.vaytet@esss.se>
2021-04-20 20:43:19 +0200

    try to create directories also with robocopy

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,13 +1,18 @@
 
 if "%INSTALL_PREFIX%" == "" set INSTALL_PREFIX=%cd% & call tools\make_and_install.bat
 
-robocopy %INSTALL_PREFIX%\scipp %CONDA_PREFIX%\lib\scipp /e /move
+if not exist "%CONDA_PREFIX%\lib\scipp" mkdir %CONDA_PREFIX%\lib\scipp
+robocopy %INSTALL_PREFIX%\scipp %CONDA_PREFIX%\lib\scipp\ /e /move
 robocopy %INSTALL_PREFIX%\bin %CONDA_PREFIX%\bin\ scipp-*.dll /e /move
 robocopy %INSTALL_PREFIX%\bin %CONDA_PREFIX%\bin\ units-shared.dll /e /move
 robocopy %INSTALL_PREFIX%\Lib %CONDA_PREFIX%\Lib\ scipp-*.lib /e /move
 robocopy %INSTALL_PREFIX%\Lib %CONDA_PREFIX%\Lib\ units-shared.lib /e /move
-robocopy %INSTALL_PREFIX%\Lib\cmake\scipp %CONDA_PREFIX%\Lib\cmake\scipp /e /move
+if not exist "%CONDA_PREFIX%\Lib\cmake\scipp" mkdir %CONDA_PREFIX%\Lib\cmake\scipp
+robocopy %INSTALL_PREFIX%\Lib\cmake\scipp %CONDA_PREFIX%\Lib\cmake\scipp\ /e /move
 robocopy %INSTALL_PREFIX%\include %CONDA_PREFIX%\include\ scipp*.h /e /move
-robocopy %INSTALL_PREFIX%\include\scipp %CONDA_PREFIX%\include\scipp /e /move
-robocopy %INSTALL_PREFIX%\include\Eigen %CONDA_PREFIX%\include\Eigen /e /move
-robocopy %INSTALL_PREFIX%\include\units %CONDA_PREFIX%\include\units /e /move
+if not exist "%CONDA_PREFIX%\include\scipp" mkdir %CONDA_PREFIX%\include\scipp
+robocopy %INSTALL_PREFIX%\include\scipp %CONDA_PREFIX%\include\scipp\ /e /move
+if not exist "%CONDA_PREFIX%\include\Eigen" mkdir %CONDA_PREFIX%\include\Eigen
+robocopy %INSTALL_PREFIX%\include\Eigen %CONDA_PREFIX%\include\Eigen\ /e /move
+if not exist "%CONDA_PREFIX%\include\units" mkdir %CONDA_PREFIX%\include\units
+robocopy %INSTALL_PREFIX%\include\units %CONDA_PREFIX%\include\units\ /e /move
