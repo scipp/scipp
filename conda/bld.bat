0e3021c06ee81fcd68dacf7a553958d40486bfc6
Neil Vaytet <neil.vaytet@esss.se>
2021-04-20 20:37:52 +0200

    need a different syntax with robocopy when moving files

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,17 +1,13 @@
 
 if "%INSTALL_PREFIX%" == "" set INSTALL_PREFIX=%cd% & call tools\make_and_install.bat
 
-dir /s/b %INSTALL_PREFIX%
-
 robocopy %INSTALL_PREFIX%\scipp %CONDA_PREFIX%\lib\scipp /e /move
-robocopy %INSTALL_PREFIX%\bin\scipp-*.dll %CONDA_PREFIX%\bin\ /e /move
-robocopy %INSTALL_PREFIX%\bin\units-shared.dll %CONDA_PREFIX%\bin\ /e /move
-robocopy %INSTALL_PREFIX%\Lib\scipp-*.lib %CONDA_PREFIX%\Lib\ /e /move
-robocopy %INSTALL_PREFIX%\Lib\units-shared.lib %CONDA_PREFIX%\Lib\ /e /move
+robocopy %INSTALL_PREFIX%\bin %CONDA_PREFIX%\bin\ scipp-*.dll /e /move
+robocopy %INSTALL_PREFIX%\bin %CONDA_PREFIX%\bin\ units-shared.dll /e /move
+robocopy %INSTALL_PREFIX%\Lib %CONDA_PREFIX%\Lib\ scipp-*.lib /e /move
+robocopy %INSTALL_PREFIX%\Lib %CONDA_PREFIX%\Lib\ units-shared.lib /e /move
 robocopy %INSTALL_PREFIX%\Lib\cmake\scipp %CONDA_PREFIX%\Lib\cmake\scipp /e /move
-robocopy %INSTALL_PREFIX%\include\scipp*.h %CONDA_PREFIX%\include\ /e /move
+robocopy %INSTALL_PREFIX%\include %CONDA_PREFIX%\include\ scipp*.h /e /move
 robocopy %INSTALL_PREFIX%\include\scipp %CONDA_PREFIX%\include\scipp /e /move
 robocopy %INSTALL_PREFIX%\include\Eigen %CONDA_PREFIX%\include\Eigen /e /move
 robocopy %INSTALL_PREFIX%\include\units %CONDA_PREFIX%\include\units /e /move
-
-dir /s/b %CONDA_PREFIX%
