93c99b48602ed4e7d1c3008c761a9c3c3c820dd0
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-04-23 10:58:37 +0100

    slice data items, and avoid unused vars

--- dataset/dataset.cpp
+++ dataset/dataset.cpp
@@ -165,11 +165,11 @@ Dataset &Dataset::setSlice(const Slice s, const Dataset &data) {
 Dataset &Dataset::setSlice(const Slice s, const DataArray &data) {
   // Validate slice on all items as a dry-run
   expect::coordsAreSuperset(slice(s), data);
-  for (const auto &[name, item] : m_data)
-    item.validateSlice(s, data);
+  for (const auto &item : m_data)
+    item.second.validateSlice(s, data);
   // Only if all items checked for dry-run does modification go-ahead
-  for (auto &&item : *this)
-    item.setSlice(s, data);
+  for (auto &item : m_data)
+    item.second.setSlice(s, data);
   return *this;
 }
 
