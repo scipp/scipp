3e7650679a1439a813c6fed791ba10287fa0824d
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-04-26 14:11:44 +0200

    Update ElementArrayView tests to use Strides

--- core/test/element_array_view_test.cpp
+++ core/test/element_array_view_test.cpp
@@ -17,7 +17,7 @@ TEST(ElementArrayView, full_volume) {
   Dimensions dims({{Dim::Y, 4}, {Dim::X, 2}});
   std::vector<double> variable(dims.volume());
   std::iota(variable.begin(), variable.end(), 0);
-  ElementArrayView<double> view(variable.data(), 0, dims, dims);
+  ElementArrayView<double> view(variable.data(), 0, dims, Strides{dims});
   auto it = view.begin();
   ASSERT_EQ(std::distance(it, view.end()), 8);
   EXPECT_EQ(*it++, 0.0);
@@ -36,11 +36,9 @@ TEST(ElementArrayView, subvolume) {
   std::iota(variable.begin(), variable.end(), 0);
 
   Dimensions variableDims({{Dim::Y, 3}, {Dim::X, 1}});
-  std::cout << "aaaaaa" << std::endl;
-  ElementArrayView<double> view(variable.data(), 0, variableDims, dims);
-  std::cout << "bbbbbb" << std::endl;
+  ElementArrayView<double> view(variable.data(), 0, variableDims,
+                                Strides{dims});
   auto it = view.begin();
-  std::cout << "ccccc" << std::endl;
   ASSERT_EQ(std::distance(it, view.end()), 3);
   EXPECT_EQ(*it++, 0.0);
   EXPECT_EQ(*it++, 2.0);
@@ -53,7 +51,7 @@ TEST(ElementArrayView, edges_first) {
   std::vector<double> variable(edgeDims.volume());
   std::iota(variable.begin(), variable.end(), 0);
 
-  ElementArrayView<double> view(variable.data(), 0, dims, edgeDims);
+  ElementArrayView<double> view(variable.data(), 0, dims, Strides{3, 1});
   auto it = view.begin();
   ASSERT_EQ(std::distance(it, view.end()), 8);
   EXPECT_EQ(*it++, 0.0);
@@ -72,7 +70,7 @@ TEST(ElementArrayView, edges_second) {
   std::vector<double> variable(edgeDims.volume());
   std::iota(variable.begin(), variable.end(), 0);
 
-  ElementArrayView<double> view(variable.data(), 0, dims, edgeDims);
+  ElementArrayView<double> view(variable.data(), 0, dims, Strides{2, 1});
   auto it = view.begin();
   ASSERT_EQ(std::distance(it, view.end()), 8);
   EXPECT_EQ(*it++, 0.0);
@@ -91,7 +89,7 @@ TEST(ElementArrayView, subview) {
   std::iota(variable.begin(), variable.end(), 0);
 
   Dimensions variableDims({{Dim::Y, 3}});
-  ElementArrayView<double> view(variable.data(), 0, variableDims, dims);
+  ElementArrayView<double> view(variable.data(), 0, variableDims, Strides{2});
   auto it = view.begin();
   ASSERT_EQ(std::distance(it, view.end()), 3);
   EXPECT_EQ(*it++, 0.0);
@@ -105,200 +103,236 @@ auto range(const scipp::index end) {
   return data;
 }
 
+Strides transposed_strides(Dimensions &from, const span<const Dim> order) {
+  return core::transpose(Strides{from}, from, order);
+}
+
 TEST(ElementArrayViewTest, transpose_2d) {
   Dimensions dims{{Dim::X, Dim::Y}, {2, 3}};
   Dimensions target({Dim::Y, Dim::X}, {3, 2});
-  EXPECT_TRUE(equals(ElementArrayView(range(6).data(), 0, target, dims),
-                     {0, 3, 1, 4, 2, 5}));
+  EXPECT_TRUE(
+      equals(ElementArrayView(range(6).data(), 0, target,
+                              transposed_strides(dims, target.labels())),
+             {0, 3, 1, 4, 2, 5}));
 }
 
 TEST(ElementArrayViewTest, transpose_3d_yx) {
   Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
   Dimensions target({Dim::Y, Dim::X, Dim::Z}, {3, 2, 4});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
-                     {0,  1,  2,  3,  12, 13, 14, 15, 4,  5,  6,  7,
-                      16, 17, 18, 19, 8,  9,  10, 11, 20, 21, 22, 23}));
+  EXPECT_TRUE(
+      equals(ElementArrayView(range(24).data(), 0, target,
+                              transposed_strides(dims, target.labels())),
+             {0,  1,  2,  3,  12, 13, 14, 15, 4,  5,  6,  7,
+              16, 17, 18, 19, 8,  9,  10, 11, 20, 21, 22, 23}));
 }
 
 TEST(ElementArrayViewTest, transpose_3d_zy) {
   Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
   Dimensions target({Dim::X, Dim::Z, Dim::Y}, {2, 4, 3});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
-                     {0,  4,  8,  1,  5,  9,  2,  6,  10, 3,  7,  11,
-                      12, 16, 20, 13, 17, 21, 14, 18, 22, 15, 19, 23}));
+  EXPECT_TRUE(
+      equals(ElementArrayView(range(24).data(), 0, target,
+                              transposed_strides(dims, target.labels())),
+             {0,  4,  8,  1,  5,  9,  2,  6,  10, 3,  7,  11,
+              12, 16, 20, 13, 17, 21, 14, 18, 22, 15, 19, 23}));
 }
 
 TEST(ElementArrayViewTest, transpose_3d_zx) {
   Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
   Dimensions target({Dim::Z, Dim::Y, Dim::X}, {4, 3, 2});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
-                     {0, 12, 4, 16, 8,  20, 1, 13, 5, 17, 9,  21,
-                      2, 14, 6, 18, 10, 22, 3, 15, 7, 19, 11, 23}));
+  EXPECT_TRUE(
+      equals(ElementArrayView(range(24).data(), 0, target,
+                              transposed_strides(dims, target.labels())),
+             {0, 12, 4, 16, 8,  20, 1, 13, 5, 17, 9,  21,
+              2, 14, 6, 18, 10, 22, 3, 15, 7, 19, 11, 23}));
 }
 
 TEST(ElementArrayViewTest, transpose_3d_zxy) {
   Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
   Dimensions target({Dim::Z, Dim::X, Dim::Y}, {4, 2, 3});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
-                     {0, 4, 8,  12, 16, 20, 1, 5, 9,  13, 17, 21,
-                      2, 6, 10, 14, 18, 22, 3, 7, 11, 15, 19, 23}));
+  EXPECT_TRUE(
+      equals(ElementArrayView(range(24).data(), 0, target,
+                              transposed_strides(dims, target.labels())),
+             {0, 4, 8,  12, 16, 20, 1, 5, 9,  13, 17, 21,
+              2, 6, 10, 14, 18, 22, 3, 7, 11, 15, 19, 23}));
 }
 
 TEST(ElementArrayViewTest, collapse_inner) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y}, {2, 3});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{3 * 4, 4};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 4, 8, 12, 16, 20}));
   // This is a typical use for the offset parameter.
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 3, target, dims),
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 3, target, strides),
                      {3, 7, 11, 15, 19, 23}));
 }
 
 TEST(ElementArrayViewTest, collapse_interior) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Z}, {2, 4});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{4 * 3, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 2, 3, 12, 13, 14, 15}));
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 4, target, dims),
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 4, target, strides),
                      {4, 5, 6, 7, 16, 17, 18, 19}));
 }
 
 TEST(ElementArrayViewTest, collapse_outer) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::Y, Dim::Z}, {3, 4});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{4, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}));
 }
 
 TEST(ElementArrayViewTest, collapse_inner_and_outer) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target(Dim::Y, 3);
-  EXPECT_TRUE(
-      equals(ElementArrayView(range(24).data(), 0, target, dims), {0, 4, 8}));
+  Strides strides{4};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
+                     {0, 4, 8}));
 }
 
 TEST(ElementArrayViewTest, collapse_inner_two) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target(Dim::X, 2);
+  Strides strides{3 * 4};
   EXPECT_TRUE(
-      equals(ElementArrayView(range(24).data(), 0, target, dims), {0, 12}));
+      equals(ElementArrayView(range(24).data(), 0, target, strides), {0, 12}));
 }
 
 TEST(ElementArrayViewTest, collapse_outer_two) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target(Dim::Z, 4);
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 2, 3}));
 }
 
 TEST(ElementArrayViewTest, collapse_all) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
   Dimensions target;
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims), {0}));
+  Strides strides{};
+  EXPECT_TRUE(
+      equals(ElementArrayView(range(24).data(), 0, target, strides), {0}));
 }
 
 // Note the result of slicing with extent 1 is equivalent to that of collapsing.
 TEST(ElementArrayViewTest, slice_inner) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y, Dim::Z}, {2, 3, 1});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{3 * 4, 4, 4};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 4, 8, 12, 16, 20}));
   // This is a typical use for the offset parameter.
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 3, target, dims),
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 3, target, strides),
                      {3, 7, 11, 15, 19, 23}));
 }
 
 TEST(ElementArrayViewTest, slice_interior) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y, Dim::Z}, {2, 1, 4});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{3 * 4, 4, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 2, 3, 12, 13, 14, 15}));
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 4, target, dims),
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 4, target, strides),
                      {4, 5, 6, 7, 16, 17, 18, 19}));
 }
 
 TEST(ElementArrayViewTest, slice_outer) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y, Dim::Z}, {1, 3, 4});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{3 * 4, 4, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}));
 }
 
 TEST(ElementArrayViewTest, slice_inner_and_outer) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y, Dim::Z}, {1, 3, 1});
-  EXPECT_TRUE(
-      equals(ElementArrayView(range(24).data(), 0, target, dims), {0, 4, 8}));
+  Strides strides{3 * 4, 4, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
+                     {0, 4, 8}));
 }
 
 TEST(ElementArrayViewTest, slice_inner_two) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y, Dim::Z}, {2, 1, 1});
+  Strides strides{3 * 4, 4, 1};
   EXPECT_TRUE(
-      equals(ElementArrayView(range(24).data(), 0, target, dims), {0, 12}));
+      equals(ElementArrayView(range(24).data(), 0, target, strides), {0, 12}));
 }
 
 TEST(ElementArrayViewTest, slice_outer_two) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y, Dim::Z}, {1, 1, 4});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{3 * 4, 4, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 2, 3}));
 }
 
 TEST(ElementArrayViewTest, slice_all) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target{{Dim::X, Dim::Y, Dim::Z}, {1, 1, 1}};
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims), {0}));
+  Strides strides{3 * 4, 4, 1};
+  EXPECT_TRUE(
+      equals(ElementArrayView(range(24).data(), 0, target, strides), {0}));
 }
 
 TEST(ElementArrayViewTest, slice_range_inner) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y, Dim::Z}, {2, 3, 2});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{3 * 4, 4, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 4, 5, 8, 9, 12, 13, 16, 17, 20, 21}));
 }
 
 TEST(ElementArrayViewTest, slice_range_interior) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y, Dim::Z}, {2, 2, 4});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{3 * 4, 4, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 2, 3, 4, 5, 6, 7, 12, 13, 14, 15, 16, 17, 18, 19}));
 }
 
 TEST(ElementArrayViewTest, slice_range_inner_and_outer) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y, Dim::Z}, {2, 3, 2});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{3 * 4, 4, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 4, 5, 8, 9, 12, 13, 16, 17, 20, 21}));
 }
 
 TEST(ElementArrayViewTest, slice_range_inner_two) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y, Dim::Z}, {2, 2, 2});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{3 * 4, 4, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 4, 5, 12, 13, 16, 17}));
 }
 
 TEST(ElementArrayViewTest, slice_range_outer_two) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target({Dim::X, Dim::Y, Dim::Z}, {1, 2, 4});
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{3 * 4, 4, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 2, 3, 4, 5, 6, 7}));
 }
 
 TEST(ElementArrayViewTest, slice_range_all) {
-  Dimensions dims{{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}};
+  // base dims = {{Dim::X, Dim::Y, Dim::Z}, {2, 3, 4}}
   Dimensions target{{Dim::X, Dim::Y, Dim::Z}, {1, 2, 2}};
-  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, dims),
+  Strides strides{3 * 4, 4, 1};
+  EXPECT_TRUE(equals(ElementArrayView(range(24).data(), 0, target, strides),
                      {0, 1, 4, 5}));
 }
 
 TEST(ElementArrayViewTest, view_of_view_bad_broadcast) {
-  Dimensions dims{{Dim::X, Dim::Y}, {2, 3}};
-  Dimensions target{{Dim::X, Dim::Y}, {2, 2}};
+  Dimensions dims1{{Dim::X, Dim::Y}, {2, 2}};
+  Dimensions dims2{{Dim::X, Dim::Y}, {2, 3}};
+  Strides strides{2, 1};
   const auto data = range(6);
   // Base view with sliced Y
-  ElementArrayView base(data.data(), 0, target, dims);
-  EXPECT_THROW(ElementArrayView<const int32_t>(base, dims),
+  ElementArrayView base(data.data(), 0, dims1, strides);
+  EXPECT_THROW(ElementArrayView<const int32_t>(base, dims2),
                except::DimensionError);
 }
