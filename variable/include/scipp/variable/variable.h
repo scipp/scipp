cbac0ae82ea9800459b0396538ef867539d8dde8
Simon Heybrock <simon.heybrock@esss.se>
2021-04-27 13:50:31 +0200

    Remove unused include

--- variable/include/scipp/variable/variable.h
+++ variable/include/scipp/variable/variable.h
@@ -23,7 +23,6 @@
 #include "scipp/core/except.h"
 #include "scipp/core/slice.h"
 #include "scipp/core/strides.h"
-#include "scipp/core/tag_util.h"
 
 #include "scipp/variable/variable_keyword_arg_constructor.h"
 
