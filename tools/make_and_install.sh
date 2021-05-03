73095f1d224fa4d73bcbbaf86b86e020c419a102
Neil Vaytet <neil.vaytet@esss.se>
2021-05-03 12:04:29 +0200

    try to get remote make script

--- tools/make_and_install.sh
+++ tools/make_and_install.sh
@@ -2,32 +2,8 @@
 
 set -ex
 
-mkdir -p 'build' && cd 'build'
-
-if test -z "${OSX_VERSION}"
-then
-  IPO="ON"
-else
-  # Disable IPO for OSX due to unexplained linker failure.
-  IPO="OFF"
-fi
-
-# Perform CMake configuration
-cmake \
-  -G Ninja \
-  -DPYTHON_EXECUTABLE="${CONDA_PREFIX}/bin/python" \
-  -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_VERSION \
-  -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${OSX_VERSION}.sdk" \
-  -DWITH_CTEST=OFF \
-  -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=$IPO \
-  ..
-
-# Show cmake settings
-cmake -B . -S .. -LA
-
-# Build benchmarks, C++ tests and install Python package
-ninja -v all-benchmarks all-tests install
+wget https://raw.githubusercontent.com/scipp/pipelines/main/make.sh
+bash make.sh
 
 # Run C++ tests
 ./bin/scipp-common-test
