02c60357d3cc13eae03331ad9d028dc48272dcfe
Simon Heybrock <simon.heybrock@esss.se>
2021-05-12 06:21:39 +0200

    Improve convolve performance

--- variable/convolution.cpp
+++ variable/convolution.cpp
@@ -21,7 +21,21 @@ Variable convolve(const Variable &var, const Variable &kernel) {
     var_ = var_.slice({dim, 0, var.dims()[dim] - kernel.dims()[dim] + 1});
   }
   auto convoluted = special_like(var_, FillValue::ZeroNotBool);
-  var_ = broadcast(var_, merge(var_.dims(), kernel_.dims()));
+  // accumulate_in_place is not multi-threaded at this point and performance is
+  // thus currently limited by iterator performance. MultiIndex appears to
+  // perform poorly with short inner dimensions, as in typical kernels. We
+  // therefore keep the data's inner dimension as inner, and insert the kernel
+  // dims to the left of that. Compared to
+  //    auto dims_ = merge(var_.dims(), kernel_.dims())
+  // this provides a 4x speedup, even though the naive merge should be more
+  // cache friendly:
+  auto dims_ = var_.dims();
+  const auto inner = var_.dims().inner();
+  const auto inner_size = var_.dims()[inner];
+  dims_.erase(inner);
+  dims_ = merge(dims_, kernel_.dims());
+  dims_.addInner(inner, inner_size);
+  var_ = broadcast(var_, dims_);
   // After broadcast strides along kernel_.dims() are all 0, change to var's
   // strides to setup a sliding window
   for (auto &dim : kernel.dims().labels()) {
