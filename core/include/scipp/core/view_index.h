d525de4bb1061fa8ff11df82b30fa926213fe5c3
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-05-17 15:55:15 +0200

    Use for loop

--- core/include/scipp/core/view_index.h
+++ core/include/scipp/core/view_index.h
@@ -22,12 +22,11 @@ public:
   ViewIndex(const Dimensions &target_dimensions, const Strides &strides);
 
   constexpr void increment_outer() noexcept {
-    scipp::index d = 0;
-    while ((m_coord[d] == m_shape[d]) && (d < NDIM_MAX - 1)) {
+    for (scipp::index d = 0; (m_coord[d] == m_shape[d]) && (d < NDIM_MAX - 1);
+         ++d) {
       m_memory_index += m_delta[d + 1];
       ++m_coord[d + 1];
       m_coord[d] = 0;
-      ++d;
     }
   }
   constexpr void increment() noexcept {
@@ -71,47 +70,6 @@ public:
   }
 
 private:
-  // NOTE:
-  // We investigated different containers for the m_delta, m_coord & m_extent
-  // arrays, and their impact on performance when iterating over a variable
-  // view.
-  // Using std::array or C-style arrays give good performance (7.5 Gb/s) as long
-  // as a range based loop is used:
-  //
-  //   for ( const auto x : view ) {
-  //
-  // If a loop which explicitly accesses the begin() and end() of the container
-  // is used, e.g.
-  //
-  //   for ( auto it = view.begin(); it != view.end(); ++it ) {
-  //
-  // then the results differ widely.
-  // - using std::array is 80x slower than above, at ~90 Mb/s
-  // - using C-style arrays is 20x slower than above, at ~330 Mb/s
-  //
-  // We can recover the maximum performance by storing the view.end() in a
-  // variable, e.g.
-  //
-  //   auto iend = view.end();
-  //   for ( auto it = view.begin(); it != iend; ++it ) {
-  //
-  // for both std::array and C-style arrays.
-  //
-  // Finally, when using C-style arrays, we get a compilation warning from L37
-  //
-  //   m_delta[d] -= m_delta[d2];
-  //
-  // with the GCC compiler:
-  //
-  //  warning: array subscript is above array bounds [-Warray-bounds]
-  //
-  // which disappears when switching to std::array. This warning is not given
-  // by the CLANG compiler, and is not fully understood as d2 is always less
-  // than d and should never overflow the array bounds.
-  // We decided to go with std::array as our final choice to avoid the warning,
-  // as the performance is identical to C-style arrays, as long as range based
-  // loops are used.
-
   /// Index into memory.
   scipp::index m_memory_index{0};
   /// Steps to advance one element.
