e71b55223e9fac1260197f97829ef5d2a02f50e0
Neil Vaytet <neil.vaytet@esss.se>
2021-05-21 17:26:46 +0200

    put /m option back in

--- tools/make_and_install.bat
+++ tools/make_and_install.bat
@@ -8,10 +8,10 @@ cmake -G"Visual Studio 16 2019" -A x64 -DCMAKE_CXX_STANDARD=20 -DWITH_CTEST=Off 
 cmake -B . -S .. -LA
 
 :: Benchmarks
-cmake --build . --target all-benchmarks --config Release || echo ERROR && exit /b 1
+cmake --build . --target all-benchmarks --config Release -- /m:%NUMBER_OF_PROCESSORS% || echo ERROR && exit /b 1
 
 :: C++ tests
-cmake --build . --target all-tests --config Release|| echo ERROR && exit /b 1
+cmake --build . --target all-tests --config Release -- /m:%NUMBER_OF_PROCESSORS% || echo ERROR && exit /b 1
 bin\Release\scipp-common-test.exe || echo ERROR && exit /b 1
 bin\Release\scipp-core-testss.exe || echo ERROR && exit /b 1
 bin\Release\scipp-variable-test.exe || echo ERROR && exit /b 1
@@ -19,4 +19,4 @@ bin\Release\scipp-dataset-test.exe || echo ERROR && exit /b 1
 bin\Release\scipp-units-test.exe || echo ERROR && exit /b 1
 
 :: Build and install Python library
-cmake --build . --target install --config Release|| echo ERROR && exit /b 1
+cmake --build . --target install --config Release -- /m:%NUMBER_OF_PROCESSORS% || echo ERROR && exit /b 1
