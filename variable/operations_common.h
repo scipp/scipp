7809de1a306c9e3deb7ae0c0b0d06a7c7903fdeb
Simon Heybrock <simon.heybrock@esss.se>
2021-05-21 13:51:24 +0200

    Remove bad EXPORT macro

--- variable/operations_common.h
+++ variable/operations_common.h
@@ -25,9 +25,7 @@ SCIPP_VARIABLE_EXPORT Variable &nanmean_impl(const Variable &var, const Dim dim,
                                              const Variable &masks_sum,
                                              Variable &out);
 
-template <class T>
-SCIPP_VARIABLE_EXPORT T normalize_impl(const T &nominator,
-                                       const T &denominator) {
+template <class T> T normalize_impl(const T &nominator, const T &denominator) {
   // Nominator may be and int or a Eigen::Vector3d => use double
   // This approach would be wrong if we supported vectors of float
   const auto type =
