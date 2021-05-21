b72b929f3a8391616917f867ea7b31c73123568f
Neil Vaytet <neil.vaytet@esss.se>
2021-05-21 17:25:34 +0200

    use exit 1

--- tools/make_and_install.bat
+++ tools/make_and_install.bat
@@ -2,21 +2,21 @@ mkdir build
 cd build
 
 echo building in %cd%
-cmake -G"Visual Studio 16 2019" -A x64 -DCMAKE_CXX_STANDARD=20 -DWITH_CTEST=Off -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -DPYTHON_EXECUTABLE=%CONDA_PREFIX%\python ..
+cmake -G"Visual Studio 16 2019" -A x64 -DCMAKE_CXX_STANDARD=20 -DWITH_CTEST=Off -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -DPYTHON_EXECUTABLE=%CONDA_PREFIX%\python .. || echo ERROR && exit /b 1
 
 :: Show cmake settings
 cmake -B . -S .. -LA
 
 :: Benchmarks
-cmake --build . --target all-benchmarks --config Release || echo ERROR && exit /b
+cmake --build . --target all-benchmarks --config Release || echo ERROR && exit /b 1
 
 :: C++ tests
-cmake --build . --target all-tests --config Release|| echo ERROR && exit /b
-bin\Release\scipp-common-test.exe || echo ERROR && exit /b
-bin\Release\scipp-core-testss.exe || echo ERROR && exit /b
-bin\Release\scipp-variable-test.exe || echo ERROR && exit /b
-bin\Release\scipp-dataset-test.exe || echo ERROR && exit /b
-bin\Release\scipp-units-test.exe || echo ERROR && exit /b
+cmake --build . --target all-tests --config Release|| echo ERROR && exit /b 1
+bin\Release\scipp-common-test.exe || echo ERROR && exit /b 1
+bin\Release\scipp-core-testss.exe || echo ERROR && exit /b 1
+bin\Release\scipp-variable-test.exe || echo ERROR && exit /b 1
+bin\Release\scipp-dataset-test.exe || echo ERROR && exit /b 1
+bin\Release\scipp-units-test.exe || echo ERROR && exit /b 1
 
 :: Build and install Python library
-cmake --build . --target install --config Release|| echo ERROR && exit /b
+cmake --build . --target install --config Release|| echo ERROR && exit /b 1
