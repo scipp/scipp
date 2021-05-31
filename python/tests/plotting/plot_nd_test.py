4eb46858ea5e2960fc548c7f254f78ddd1abf16f
Neil Vaytet <neil.vaytet@esss.se>
2021-05-31 17:14:57 +0200

    use new factory for nd plots tests

--- python/tests/plotting/plot_nd_test.py
+++ python/tests/plotting/plot_nd_test.py
@@ -6,7 +6,8 @@
 import numpy as np
 import pytest
 import scipp as sc
-from plot_helper import make_dense_dataset, plot
+from ..factory import make_dense_data_array, make_dense_dataset
+from .plot_helper import plot
 
 # TODO: For now we are just checking that the plot does not throw any errors.
 # In the future it would be nice to check the output by either comparing
@@ -14,35 +15,39 @@ from plot_helper import make_dense_dataset, plot
 
 
 def test_plot_sliceviewer_2d():
+    plot(make_dense_data_array(ndim=3))
+
+
+def test_plot_sliceviewer_2d_dataset():
     plot(make_dense_dataset(ndim=3))
 
 
 def test_plot_sliceviewer_2d_with_two_sliders():
-    plot(make_dense_dataset(ndim=4))
+    plot(make_dense_data_array(ndim=4))
 
 
 def test_plot_sliceviewer_2d_with_axes():
-    plot(make_dense_dataset(ndim=3), axes={'y': 'tof'})
+    plot(make_dense_data_array(ndim=3), axes={'y': 'tof'})
 
 
 def test_plot_sliceviewer_2d_with_axes_redundant():
-    plot(make_dense_dataset(ndim=3), axes={'y': 'tof', 'x': 'x'})
+    plot(make_dense_data_array(ndim=3), axes={'y': 'tof', 'x': 'x'})
 
 
 def test_plot_sliceviewer_2d_with_two_axes():
-    plot(make_dense_dataset(ndim=3), axes={'x': 'y', 'y': 'tof'})
+    plot(make_dense_data_array(ndim=3), axes={'x': 'y', 'y': 'tof'})
 
 
 def test_plot_sliceviewer_2d_with_labels():
-    plot(make_dense_dataset(ndim=3, labels=True), axes={'x': "somelabels"})
+    plot(make_dense_data_array(ndim=3, labels=True), axes={'x': "somelabels"})
 
 
 def test_plot_sliceviewer_2d_with_attrs():
-    plot(make_dense_dataset(ndim=3, attrs=True), axes={'x': "attr"})
+    plot(make_dense_data_array(ndim=3, attrs=True), axes={'x': "attr"})
 
 
 def test_plot_sliceviewer_2d_with_binedges():
-    plot(make_dense_dataset(ndim=3, binedges=True))
+    plot(make_dense_data_array(ndim=3, binedges=True))
 
 
 def test_plot_variable_3d():
@@ -54,37 +59,35 @@ def test_plot_variable_3d():
 
 
 def test_plot_4d_with_masks_no_coords():
-    data = sc.DataArray(data=sc.Variable(
-        dims=['pack', 'tube', 'straw', 'pixel'],
-        values=np.random.rand(2, 8, 7, 256)),
-                        coords={})
+    da = sc.DataArray(data=sc.Variable(dims=['pack', 'tube', 'straw', 'pixel'],
+                                       values=np.random.rand(2, 8, 7, 256)),
+                      coords={})
     a = np.sin(np.linspace(0, 3.14, num=256))
-    data += sc.Variable(dims=['pixel'], values=a)
-    data.masks['tube_ends'] = sc.Variable(dims=['pixel'],
-                                          values=np.where(
-                                              a > 0.5, True, False))
-    plot(data)
-    plot(data, axes={'y': 'tube'})
+    da += sc.Variable(dims=['pixel'], values=a)
+    da.masks['tube_ends'] = sc.Variable(dims=['pixel'],
+                                        values=np.where(a > 0.5, True, False))
+    plot(da)
+    plot(da, axes={'y': 'tube'})
 
 
 def test_plot_3d_data_ragged():
     """
     This test has caught MANY bugs and should not be disabled.
     """
-    d = make_dense_dataset(ndim=3, ragged=True)
-    plot(d)
+    da = make_dense_data_array(ndim=3, ragged=True)
+    plot(da)
     # Also check that it raises an error if we try to have ragged coord along
     # slider dim
     with pytest.raises(RuntimeError) as e:
-        plot(d, axes={'x': 'tof', 'y': 'x'})
+        plot(da, axes={'x': 'tof', 'y': 'x'})
     assert str(e.value) == ("A ragged coordinate cannot lie along "
                             "a slider dimension, it must be one of "
                             "the displayed dimensions.")
 
 
 def test_plot_3d_data_ragged_with_edges():
-    plot(make_dense_dataset(ndim=3, ragged=True, binedges=True))
+    plot(make_dense_data_array(ndim=3, ragged=True, binedges=True))
 
 
 def test_plot_3d_data_ragged_with_masks():
-    plot(make_dense_dataset(ndim=3, ragged=True, masks=True))
+    plot(make_dense_data_array(ndim=3, ragged=True, masks=True))
