37178bcb04a9cf618a54cb3c367325142cc0ee38
Gregory Tucker <gregory.tucker@ess.eu>
2021-06-02 12:16:34 +0200

    Simplify  following Simon Heybrock's suggestion

--- python/src/scipp/utils/get.py
+++ python/src/scipp/utils/get.py
@@ -3,32 +3,20 @@
 # @author Gregory Tucker
 
 
-def hascallableattr(obj, named):
-    return hasattr(obj, named) and callable(getattr(obj, named))
-
-
 def get(x, key, default=None):
     """
     Get the value associated with the provided key from a `dict`-like object
 
     :param x: dictlike
-        Any object with an `items()` *or* both `keys()` and `values()` methods
+        Any object with `__contains__()` or `__iter__()` and `__getitem__()`
+        methods which accept `key`
     :param key:
         The key to extract from the provided `dict`-like object`
     :param default:
         A default value to return if the provided key is not present
     :return:
         Either the value associated with the key or the default if the key
-        is missing or the object does not implement the required methods.
+        is missing. If he object does not implement the required methods an
+        error will be thrown.
     """
-
-    return_value = default
-    if hascallableattr(x, 'items'):
-        for k, v in x.items():
-            if k == key:
-                return_value = v
-    elif hascallableattr(x, 'keys') and hascallableattr(x, 'values'):
-        for k, v in zip(x.keys(), x.values()):
-            if k == key:
-                return_value = v
-    return return_value
+    return x[key] if key in x else default
