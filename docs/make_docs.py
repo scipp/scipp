b419729b1e9cc78385fc8ae64c2c0903b1500f01
Neil Vaytet <neil.vaytet@esss.se>
2021-06-07 15:23:54 +0200

    use absolute paths in make_docs

--- docs/make_docs.py
+++ docs/make_docs.py
@@ -3,6 +3,8 @@
 # @author Neil Vaytet
 
 import argparse
+import os
+import pathlib
 import subprocess
 import sys
 
@@ -10,12 +12,24 @@ parser = argparse.ArgumentParser(description='Build doc pages with sphinx')
 parser.add_argument('--prefix', default='build')
 parser.add_argument('--work_dir', default='.doctrees')
 
+
+def get_abs_path(path, root):
+    if os.path.isabs(path):
+        return path
+    else:
+        return os.path.join(root, path)
+
+
 if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    docs_dir = pathlib.Path(__file__).parent.absolute()
+    work_dir = get_abs_path(path=args.work_dir, root=docs_dir)
+    prefix = get_abs_path(path=args.prefix, root=docs_dir)
+
     # Build the docs with sphinx-build
     status = subprocess.check_call(
-        ['sphinx-build', '-d', args.work_dir, '.', args.prefix],
+        ['sphinx-build', '-d', work_dir, docs_dir, prefix],
         stderr=subprocess.STDOUT,
         shell=sys.platform == "win32")
