8b78460f0f0f214fd91160566aca7f381ca9b69e
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-06-08 13:37:28 +0200

    Create new function for bound method

--- python/src/scipp/_binding.py
+++ python/src/scipp/_binding.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 # @author Jan-Lukas Wynen
 
+import types
+
 from ._scipp import core
 from .utils import get
 
@@ -13,7 +15,8 @@ def bind_get():
 
 def bind_functions_as_methods(cls, namespace, func_names):
     for func_name, func in map(lambda n: (n, namespace[n]), func_names):
-        method = func.__get__(None, cls)
+        method = types.FunctionType(func.__code__, func.__globals__, func_name,
+                                    func.__defaults__, func.__closure__)
         # Extract the summary from the docstring.
         # This relies on check W293 in flake8 to avoid implementing a more
         # sophisticate / expensive parser that running during import of scipp.
