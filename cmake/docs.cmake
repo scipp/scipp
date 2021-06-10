8ed93d9e92d3fd08fdf11637af0200c03691355e
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-06-10 11:00:51 +0200

    cmake-format

--- cmake/docs.cmake
+++ cmake/docs.cmake
@@ -3,16 +3,16 @@
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 # ~~~
 function(add_docs_target name)
-    set(oneValueArgs BUILDER)
-    set(multiValueArgs DEPENDS)
-    cmake_parse_arguments(PARSE_ARGV 0 ADD_DOCS_TARGET "" "${oneValueArgs}" "${multiValueArgs}")
-    add_custom_target(
-        ${name}
-        COMMAND
-         ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/docs/build_docs.py
-         --builder=${ADD_DOCS_TARGET_BUILDER} --prefix=${CMAKE_CURRENT_BINARY_DIR}/docs
-         --work_dir=${CMAKE_CURRENT_BINARY_DIR}/docs/doctrees
-        DEPENDS
-        ${ADD_DOCS_TARGET_DEPENDS}
-   )
+  set(oneValueArgs BUILDER)
+  set(multiValueArgs DEPENDS)
+  cmake_parse_arguments(PARSE_ARGV 0 ADD_DOCS_TARGET "" "${oneValueArgs}" "${multiValueArgs}")
+  add_custom_target(
+    ${name}
+    COMMAND
+      ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/docs/build_docs.py
+      --builder=${ADD_DOCS_TARGET_BUILDER}
+      --prefix=${CMAKE_CURRENT_BINARY_DIR}/docs
+      --work_dir=${CMAKE_CURRENT_BINARY_DIR}/docs/doctrees
+    DEPENDS ${ADD_DOCS_TARGET_DEPENDS}
+  )
 endfunction()
