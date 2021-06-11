585f0aa48d2c7a8ba48a6f5c4ed7c0d44151f23e
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-06-11 15:54:01 +0200

    Check for overlap of data and key

--- dataset/sort.cpp
+++ dataset/sort.cpp
@@ -6,6 +6,25 @@
 #include "scipp/dataset/groupby.h"
 
 namespace scipp::dataset {
+namespace {
+template <class T> void ensure_distinct_buffers(const T &to_sort, const Variable &key) {
+  if constexpr (std::is_same_v<std::decay_t<T>, DataArray>) {
+    if (&to_sort.data().data() == &key.data()) {
+      throw std::invalid_argument("The groupby key is equal to the data. "
+                                  "Consider copying the key variable first.");
+    }
+  } else {
+    for (auto it = to_sort.items_begin(); it != to_sort.items_end(); ++it) {
+      const auto &[name, item] = *it;
+      if (&item.data().data() == &key.data()) {
+        throw std::invalid_argument(
+            "The groupby key is equal to dataset entry '" + name +
+            "'. Consider copying the key variable first.");
+      }
+    }
+  }
+}
+} // namespace
 
 /// Return a Variable sorted based on key.
 Variable sort(const Variable &var, const Variable &key, const SortOrder order) {
@@ -15,6 +34,7 @@ Variable sort(const Variable &var, const Variable &key, const SortOrder order) {
 /// Return a DataArray sorted based on key.
 DataArray sort(const DataArray &array, const Variable &key,
                const SortOrder order) {
+  ensure_distinct_buffers(array, key);
   auto helper = array;
   const Dim dummy = Dim::InternalSort;
   helper.coords().set(dummy, key);
@@ -31,6 +51,7 @@ DataArray sort(const DataArray &array, const Dim &key, const SortOrder order) {
 /// Return a Dataset sorted based on key.
 Dataset sort(const Dataset &dataset, const Variable &key,
              const SortOrder order) {
+  ensure_distinct_buffers(dataset, key);
   auto helper = dataset;
   const Dim dummy = Dim::InternalSort;
   helper.coords().set(dummy, key);
