b2c04549eee8bd321193e8d0fdc013763f733341
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-06-18 16:29:53 +0200

    Switch to with_variance args

--- python/creation.cpp
+++ python/creation.cpp
@@ -16,23 +16,24 @@ void init_creation(py::module &m) {
       "empty",
       [](const std::vector<Dim> &dims, const std::vector<scipp::index> &shape,
          const units::Unit &unit, const py::object &dtype,
-         const bool variances) {
+         const bool with_variance) {
         const auto dtype_ = scipp_dtype(dtype);
         py::gil_scoped_release release;
         return variable::empty(Dimensions(dims, shape), unit, dtype_,
-                               variances);
+                               with_variance);
       },
       py::arg("dims"), py::arg("shape"), py::arg("unit") = units::one,
-      py::arg("dtype") = py::none(), py::arg("variances") = std::nullopt);
+      py::arg("dtype") = py::none(), py::arg("with_variance") = std::nullopt);
   m.def(
       "ones",
       [](const std::vector<Dim> &dims, const std::vector<scipp::index> &shape,
          const units::Unit &unit, const py::object &dtype,
-         const bool variances) {
+         const bool with_variance) {
         const auto dtype_ = scipp_dtype(dtype);
         py::gil_scoped_release release;
-        return variable::ones(Dimensions(dims, shape), unit, dtype_, variances);
+        return variable::ones(Dimensions(dims, shape), unit, dtype_,
+                              with_variance);
       },
       py::arg("dims"), py::arg("shape"), py::arg("unit") = units::one,
-      py::arg("dtype") = py::none(), py::arg("variances") = std::nullopt);
+      py::arg("dtype") = py::none(), py::arg("with_variance") = std::nullopt);
 }
