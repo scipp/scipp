04240dadc69ba8b2ed7a3d0d7553b9f369bfa1e8
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-06-21 13:47:50 +0200

    Add converting_cast

--- python/dtype.h
+++ python/dtype.h
@@ -20,6 +20,22 @@ scipp::core::DType cast_dtype(const pybind11::object &dtype);
 void ensure_conversion_possible(scipp::core::DType from, scipp::core::DType to,
                                 const std::string &data_name);
 
+template <class T> decltype(auto) converting_cast(const pybind11::object &obj) {
+  return obj.cast<T>();
+}
+
+template <>
+decltype(auto) converting_cast<int64_t>(const pybind11::object &obj) {
+  if (dtype_of(obj) == scipp::dtype<double>) {
+    // This conversion is not implemented in pybind11 v2.6.2
+    return obj.cast<pybind11::int_>().cast<int64_t>();
+  } else {
+    // All other conversions are either supported by pybind11 or not
+    // desired anyway.
+    return obj.cast<int64_t>();
+  }
+}
+
 scipp::core::DType common_dtype(const pybind11::object &values,
                                 const pybind11::object &variances,
                                 scipp::core::DType dtype, bool plural);
