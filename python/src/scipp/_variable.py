728103d5e429ae09c8668d81a6b4670e9d34212f
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-06-23 16:11:05 +0200

    Fix typos

--- python/src/scipp/_variable.py
+++ python/src/scipp/_variable.py
@@ -149,10 +149,10 @@ def ones(*,
 
     :param dims: Optional (if sizes is specified), dimension labels.
     :param shape: Optional (if sizes is specified), dimension sizes.
-    :param sizes: Optional, imension label to size map.
+    :param sizes: Optional, dimension label to size map.
     :param unit: Optional, unit of contents. Default=dimensionless
     :param dtype: Optional, type of underlying data. Default=float64
-    :param wiht_variance: Optional, boolean flag, if True includes variances
+    :param with_variance: Optional, boolean flag, if True includes variances
                       initialized to 1. Default=False
     """
     return _cpp.ones(**_parse_dims_shape_sizes(dims, shape, sizes),
