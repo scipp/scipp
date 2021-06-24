2a70a85164085fb84dda23d6d0e1a74d118d744a
Simon Heybrock <simon.heybrock@esss.se>
2021-06-24 09:47:03 +0200

    Avoid subclassing Plot

--- python/src/scipp/plotting/plot1d.py
+++ python/src/scipp/plotting/plot1d.py
@@ -8,7 +8,25 @@ from .view1d import PlotView1d
 from .figure1d import PlotFigure1d
 
 
-def plot1d(*args, filename=None, **kwargs):
+def plot1d(scipp_obj_dict,
+           filename=None,
+           labels=None,
+           errorbars=None,
+           masks=None,
+           ax=None,
+           pax=None,
+           figsize=None,
+           mpl_line_params=None,
+           norm=None,
+           vmin=None,
+           vmax=None,
+           resolution=None,
+           scale=None,
+           grid=False,
+           title=None,
+           xlabel=None,
+           ylabel=None,
+           legend=None):
     """
     Plot one or more Scipp data objects as a 1 dimensional line plot.
 
@@ -16,82 +34,54 @@ def plot1d(*args, filename=None, **kwargs):
     made.
     If the data contains more than one dimensions, sliders are added to
     navigate to extra dimensions.
-    """
-    sp = Plot1d(*args, **kwargs)
-    if filename is not None:
-        sp.savefig(filename)
-    else:
-        return sp
-
-
-class Plot1d(Plot):
-    """
-    Class for 1 dimensional plots.
 
     If the input data has more than 1 dimensions, a `PlotPanel` with additional
     buttons is displayed. This allow the duplication of the currently
     displayed line, a functionality inspired by the Superplot in the Lamp
     software.
     """
-    def __init__(self,
-                 scipp_obj_dict=None,
-                 labels=None,
-                 errorbars=None,
-                 masks=None,
-                 ax=None,
-                 pax=None,
-                 figsize=None,
-                 mpl_line_params=None,
-                 norm=None,
-                 vmin=None,
-                 vmax=None,
-                 resolution=None,
-                 scale=None,
-                 grid=False,
-                 title=None,
-                 xlabel=None,
-                 ylabel=None,
-                 legend=None):
+    if masks is None:
+        masks = {"color": "k"}
 
-        if masks is None:
-            masks = {"color": "k"}
+    sp = Plot(scipp_obj_dict=scipp_obj_dict,
+              labels=labels,
+              norm=norm,
+              scale=scale,
+              vmin=vmin,
+              vmax=vmax,
+              errorbars=errorbars,
+              masks=masks,
+              view_ndims=1)
+    # The view which will display the 1d plot and send pick events back to
+    # the controller
+    sp.view = PlotView1d(figure=PlotFigure1d(
+        ax=ax,
+        figsize=figsize,
+        errorbars=sp.errorbars,
+        norm=norm,
+        title=title,
+        mask_color=sp.params['masks']['color'],
+        mpl_line_params=mpl_line_params,
+        picker=True,
+        grid=grid,
+        xlabel=xlabel,
+        ylabel=ylabel,
+        legend=legend),
+                         formatters=sp._formatters)
 
-        super().__init__(scipp_obj_dict=scipp_obj_dict,
-                         labels=labels,
-                         norm=norm,
-                         scale=scale,
-                         vmin=vmin,
-                         vmax=vmax,
-                         errorbars=errorbars,
-                         masks=masks,
-                         view_ndims=1)
+    # Profile view which displays an additional dimension as a 1d plot
+    if len(sp.dims) > 1:
+        sp.profile = sp._make_profile(ax=pax)
+        # An additional panel view with widgets to save/remove lines
+        sp.panel = PlotPanel1d(data_names=list(scipp_obj_dict.keys()))
 
-        # The view which will display the 1d plot and send pick events back to
-        # the controller
-        self.view = PlotView1d(figure=PlotFigure1d(
-            ax=ax,
-            figsize=figsize,
-            errorbars=self.errorbars,
-            norm=norm,
-            title=title,
-            mask_color=self.params['masks']['color'],
-            mpl_line_params=mpl_line_params,
-            picker=True,
-            grid=grid,
-            xlabel=xlabel,
-            ylabel=ylabel,
-            legend=legend),
-                               formatters=self._formatters)
+    sp.controller = sp._make_controller(norm=norm,
+                                        scale=scale,
+                                        resolution=resolution)
 
-        # Profile view which displays an additional dimension as a 1d plot
-        if len(self.dims) > 1:
-            self.profile = self._make_profile(ax=pax)
-            # An additional panel view with widgets to save/remove lines
-            self.panel = PlotPanel1d(data_names=list(scipp_obj_dict.keys()))
-
-        self.controller = self._make_controller(norm=norm,
-                                                scale=scale,
-                                                resolution=resolution)
-
-        # Render the figure once all components have been created.
-        self.render()
+    # Render the figure once all components have been created.
+    sp.render()
+    if filename is not None:
+        sp.savefig(filename)
+    else:
+        return sp
--- python/src/scipp/plotting/plot2d.py
+++ python/src/scipp/plotting/plot2d.py
@@ -7,82 +7,72 @@ from .view2d import PlotView2d
 from .figure2d import PlotFigure2d
 
 
-def plot2d(*args, filename=None, **kwargs):
+def plot2d(scipp_obj_dict,
+           filename=None,
+           labels=None,
+           errorbars=None,
+           masks=None,
+           ax=None,
+           cax=None,
+           pax=None,
+           figsize=None,
+           aspect=None,
+           cmap=None,
+           norm=None,
+           scale=None,
+           vmin=None,
+           vmax=None,
+           resolution=None,
+           title=None,
+           xlabel=None,
+           ylabel=None):
     """
     Plot a 2d slice through a N dimensional dataset.
     For every dimension above 2, a slider is created to adjust the position
     of the slice in that particular dimension.
-    """
-    sp = Plot2d(*args, **kwargs)
-    if filename is not None:
-        sp.savefig(filename)
-    else:
-        return sp
-
-
-class Plot2d(Plot):
-    """
-    Class for 2 dimensional plots.
 
     It uses Matplotlib's `imshow` to view 2d arrays are images, and implements
     a dynamic image resampling for better performance with large images.
     """
-    def __init__(self,
-                 scipp_obj_dict=None,
-                 labels=None,
-                 errorbars=None,
-                 masks=None,
-                 ax=None,
-                 cax=None,
-                 pax=None,
-                 figsize=None,
-                 aspect=None,
-                 cmap=None,
-                 norm=None,
-                 scale=None,
-                 vmin=None,
-                 vmax=None,
-                 resolution=None,
-                 title=None,
-                 xlabel=None,
-                 ylabel=None):
+    sp = Plot(scipp_obj_dict=scipp_obj_dict,
+              labels=labels,
+              cmap=cmap,
+              norm=norm,
+              scale=scale,
+              vmin=vmin,
+              vmax=vmax,
+              errorbars=errorbars,
+              masks=masks,
+              view_ndims=2)
+    # The view which will display the 2d image and send pick events back to
+    # the controller
+    sp.view = PlotView2d(figure=PlotFigure2d(
+        ax=ax,
+        cax=cax,
+        figsize=figsize,
+        aspect=aspect,
+        cmap=sp.params["values"]["cmap"],
+        norm=sp.params["values"]["norm"],
+        name=sp.name,
+        cbar=sp.params["values"]["cbar"],
+        mask_cmap=sp.params['masks']['cmap'],
+        extend=sp.extend_cmap,
+        title=title,
+        xlabel=xlabel,
+        ylabel=ylabel),
+                         formatters=sp._formatters)
 
-        super().__init__(scipp_obj_dict=scipp_obj_dict,
-                         labels=labels,
-                         cmap=cmap,
-                         norm=norm,
-                         scale=scale,
-                         vmin=vmin,
-                         vmax=vmax,
-                         errorbars=errorbars,
-                         masks=masks,
-                         view_ndims=2)
+    # Profile view which displays an additional dimension as a 1d plot
+    if len(sp.dims) > 2:
+        sp.profile = sp._make_profile(ax=pax)
 
-        # The view which will display the 2d image and send pick events back to
-        # the controller
-        self.view = PlotView2d(figure=PlotFigure2d(
-            ax=ax,
-            cax=cax,
-            figsize=figsize,
-            aspect=aspect,
-            cmap=self.params["values"]["cmap"],
-            norm=self.params["values"]["norm"],
-            name=self.name,
-            cbar=self.params["values"]["cbar"],
-            mask_cmap=self.params['masks']['cmap'],
-            extend=self.extend_cmap,
-            title=title,
-            xlabel=xlabel,
-            ylabel=ylabel),
-                               formatters=self._formatters)
+    sp.controller = sp._make_controller(norm=norm,
+                                        scale=scale,
+                                        resolution=resolution)
 
-        # Profile view which displays an additional dimension as a 1d plot
-        if len(self.dims) > 2:
-            self.profile = self._make_profile(ax=pax)
-
-        self.controller = self._make_controller(norm=norm,
-                                                scale=scale,
-                                                resolution=resolution)
-
-        # Render the figure once all components have been created.
-        self.render()
+    # Render the figure once all components have been created.
+    sp.render()
+    if filename is not None:
+        sp.savefig(filename)
+    else:
+        return sp
