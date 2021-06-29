d6924d9daff45afd627537922b8b3b1cecdc42d5
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-06-29 10:29:20 +0200

    Make zeros value-init values and variances

--- python/variable_creation.cpp
+++ python/variable_creation.cpp
@@ -2,14 +2,30 @@
 // Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 /// @file
 /// @author Simon Heybrock
+#include "scipp/core/eigen.h"
+#include "scipp/core/tag_util.h"
+#include "scipp/dataset/dataset.h"
 #include "scipp/variable/creation.h"
 
 #include "dtype.h"
+#include "py_object.h"
 #include "pybind11.h"
 
 using namespace scipp;
 
 namespace py = pybind11;
+
+template <class T> struct MakeZeros {
+  static variable::Variable apply(const std::vector<Dim> &dims,
+                                  const std::vector<scipp::index> &shape,
+                                  const units::Unit &unit,
+                                  const bool with_variances) {
+    return with_variances
+               ? makeVariable<T>(Dimensions{dims, shape}, unit, Values{},
+                                 Variances{})
+               : makeVariable<T>(Dimensions{dims, shape}, unit, Values{});
+  }
+};
 
 void init_creation(py::module &m) {
   m.def(
@@ -31,8 +47,12 @@ void init_creation(py::module &m) {
          const bool with_variances) {
         const auto dtype_ = scipp_dtype(dtype);
         py::gil_scoped_release release;
-        return variable::zeros(Dimensions(dims, shape), unit, dtype_,
-                               with_variances);
+        return core::CallDType<
+            double, float, int64_t, int32_t, bool, scipp::core::time_point,
+            std::string, Variable, dataset::DataArray, dataset::Dataset,
+            Eigen::Vector3d, Eigen::Matrix3d,
+            python::PyObject>::apply<MakeZeros>(dtype_, dims, shape, unit,
+                                                with_variances);
       },
       py::arg("dims"), py::arg("shape"), py::arg("unit") = units::one,
       py::arg("dtype") = py::none(), py::arg("with_variances") = std::nullopt);
