1c24d34e669ee3e251a5905cc8e58aa946047a9c
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-07-02 09:15:55 +0200

    Copy expliictly
    Co-authored-by: Simon Heybrock <simon.heybrock@esss.se>

--- variable/to_unit.cpp
+++ variable/to_unit.cpp
@@ -18,8 +18,8 @@ constexpr double days_multiplier = llnl::units::precise::day.multiplier();
 
 Variable to_unit(const Variable &var, const units::Unit &unit,
                  const CopyPolicy copy) {
-  if (unit == var.unit() && copy == CopyPolicy::TryAvoid)
-    return var; // no copy
+  if (unit == var.unit())
+    return copy == CopyPolicy::Always ? copy(var) : var;
   const auto scale = llnl::units::quick_convert(
       variableFactory().elem_unit(var).underlying(), unit.underlying());
   if (std::isnan(scale))
