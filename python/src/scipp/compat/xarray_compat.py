7267ad18dd46393a0e44b75e9d5d836074a5ae90
Tom Willemsen <tomwillemsen1995@gmail.com>
2021-07-14 10:49:18 +0100

    Add initial converter for xarray.Dataset

--- python/src/scipp/compat/xarray_compat.py
+++ python/src/scipp/compat/xarray_compat.py
@@ -1,5 +1,17 @@
+from typing import Union
+
 import xarray
 import scipp
+
+
+def from_xarray(xarray_obj: Union[xarray.DataArray, xarray.Dataset]):
+    if isinstance(xarray_obj, xarray.DataArray):
+        return from_xarray_dataarray(xarray_obj)
+    elif isinstance(xarray_obj, xarray.Dataset):
+        return from_xarray_dataset(xarray_obj)
+    else:
+        raise ValueError(
+            f"from_xarray: cannot convert type '{type(xarray_obj)}'")
 
 
 def from_xarray_dataarray(
@@ -14,8 +26,7 @@ def from_xarray_dataarray(
         )
 
     for attrib in xarray_dataarray.attrs:
-        sc_attribs[attrib] = scipp.Variable(
-            dims=[], values=xarray_dataarray.attrs[attrib])
+        sc_attribs[attrib] = scipp.scalar(xarray_dataarray.attrs[attrib])
 
     return scipp.DataArray(data=scipp.Variable(values=xarray_dataarray.values,
                                                dims=xarray_dataarray.dims),
@@ -24,8 +35,34 @@ def from_xarray_dataarray(
                            name=xarray_dataarray.name or "")
 
 
-def from_xarray_dataset(xarray_dataset: xarray.Dataset) -> scipp.Dataset:
+def from_xarray_dataset(xarray_dataset: xarray.Dataset,
+                        *,
+                        attrib_prefix="attrib_") -> scipp.Dataset:
+    sc_data = {}
+    sc_coords = {}
+
+    for data_key in xarray_dataset.data_vars:
+        sc_data[data_key] = from_xarray_dataarray(
+            xarray_dataset.data_vars[data_key])
+
+    for coord in xarray_dataset.coords:
+        sc_coords[coord] = scipp.Variable(
+            dims=[coord],
+            values=xarray_dataset.coords[coord].values,
+        )
+
+    for attrib in xarray_dataset.attrs:
+        attrib_name = "{}{}".format(attrib_prefix, attrib)
+        if attrib_name in sc_data:
+            raise ValueError(
+                f"Attribute {attrib} would collide with an existing "
+                f"data object '{attrib_name}' (using attrib_prefix "
+                f"'{attrib_prefix}'). Specify a different attrib_prefix "
+                f"in the call to from_xarray_dataset")
+
+        sc_data[attrib_name] = scipp.scalar(xarray_dataset.attrs[attrib])
+
     return scipp.Dataset(
-        coords=xarray_dataset.coords,
-        attrs=xarray_dataset.attrs,
+        data=sc_data,
+        coords=sc_coords,
     )
--- python/tests/compat/xarray_compat_test.py
+++ python/tests/compat/xarray_compat_test.py
@@ -1,7 +1,9 @@
+import pytest
+
 import numpy
 import xarray
 import scipp
-from scipp.compat.xarray_compat import from_xarray_dataarray
+from scipp.compat.xarray_compat import from_xarray
 
 
 def test_empty_attrs_dataarray():
@@ -9,7 +11,7 @@ def test_empty_attrs_dataarray():
                                    dims={"x"},
                                    attrs={})
 
-    sc_dataarray = from_xarray_dataarray(x_dataarray)
+    sc_dataarray = from_xarray(x_dataarray)
 
     assert len(sc_dataarray.attrs) == 0
 
@@ -28,7 +30,7 @@ def test_attrs_dataarray():
                                        "attrib_str": "test-string",
                                    })
 
-    sc_dataarray = from_xarray_dataarray(x_dataarray)
+    sc_dataarray = from_xarray(x_dataarray)
 
     assert sc_dataarray.attrs["attrib_int"].values == 5
     assert sc_dataarray.attrs["attrib_float"].values == 6.54321
@@ -40,7 +42,7 @@ def test_named_dataarray():
                                    dims={"x"},
                                    name="my-test-dataarray")
 
-    sc_dataarray = from_xarray_dataarray(x_dataarray)
+    sc_dataarray = from_xarray(x_dataarray)
 
     assert sc_dataarray.name == "my-test-dataarray"
 
@@ -50,7 +52,7 @@ def test_1d_1_element_dataarray():
                                    dims={"x"},
                                    attrs={})
 
-    sc_dataarray = from_xarray_dataarray(x_dataarray)
+    sc_dataarray = from_xarray(x_dataarray)
 
     assert (sc_dataarray.values == scipp.zeros(dims=["x"],
                                                shape=(1, )).values).all()
@@ -61,7 +63,7 @@ def test_1d_100_element_dataarray():
                                    dims={"x"},
                                    attrs={})
 
-    sc_dataarray = from_xarray_dataarray(x_dataarray)
+    sc_dataarray = from_xarray(x_dataarray)
 
     assert (sc_dataarray.values == scipp.zeros(dims=["x"],
                                                shape=(100, )).values).all()
@@ -72,7 +74,56 @@ def test_2d_100x100_element_dataarray():
                                    dims={"x", "y"},
                                    attrs={})
 
-    sc_dataarray = from_xarray_dataarray(x_dataarray)
+    sc_dataarray = from_xarray(x_dataarray)
 
     assert (sc_dataarray.values == scipp.zeros(dims=["x", "y"],
                                                shape=(100, 100)).values).all()
+
+
+def test_empty_dataset():
+    x_dataset = xarray.Dataset(data_vars={})
+
+    sc_dataset = from_xarray(x_dataset)
+
+    assert len(sc_dataset) == 0
+
+
+def test_dataset_with_attrs():
+    x_dataset = xarray.Dataset(data_vars={},
+                               attrs={
+                                   "int": 5,
+                                   "float": 6.54321,
+                                   "str": "test-string",
+                               })
+
+    sc_dataset = from_xarray(x_dataset)
+
+    assert sc_dataset["attrib_int"].values == 5
+    assert sc_dataset["attrib_float"].values == 6.54321
+    assert sc_dataset["attrib_str"].values == "test-string"
+
+
+def test_dataset_with_colliding_data_and_attrs_throws_error():
+    x_dataset = xarray.Dataset(
+        data_vars={"attrib_test1": xarray.Variable(["x"], numpy.zeros((1, )))},
+        attrs={"test1": 5})
+
+    with pytest.raises(ValueError):
+        from_xarray(x_dataset)
+
+
+def test_dataset_with_data():
+    x_dataset = xarray.Dataset(
+        data_vars={
+            "array1":
+            xarray.DataArray(data=numpy.zeros((100, )), dims={"x"}, attrs={}),
+            "array2":
+            xarray.DataArray(data=numpy.zeros((50, )), dims={"y"}, attrs={}),
+        })
+
+    sc_dataset = from_xarray(x_dataset)
+
+    assert (sc_dataset["array1"].values == scipp.zeros(
+        dims=["x"], shape=(100, )).values).all()
+    assert (sc_dataset["array2"].values == scipp.zeros(
+        dims=["y"], shape=(50, )).values).all()
