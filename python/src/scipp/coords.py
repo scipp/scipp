f168aa4875af97a15bdcb6c54afe2402700d1a04
Simon Heybrock <simon.heybrock@esss.se>
2021-07-15 14:21:43 +0200

    Cleanup and add methods

--- python/src/scipp/coords.py
+++ python/src/scipp/coords.py
@@ -7,7 +7,7 @@ from typing import Union
 from . import Variable, DataArray, Dataset
 
 
-def _add_coords(*, name, obj, tree):
+def _add_coord(*, name, obj, tree):
     if isinstance(tree[name], str):
         out = _get_coord(tree[name], obj, tree)
     else:
@@ -28,16 +28,16 @@ def _get_coord(name, obj, tree):
             obj.attrs[name] = obj.coords[name]
             del obj.coords[name]
         return obj.attrs[name]
-    elif isinstance(tree[name], str):
-        return _get_coord(tree[name], obj, tree)
     else:
-        _add_coords(name=name, obj=obj, tree=tree)
+        _add_coord(name=name, obj=obj, tree=tree)
         return _get_coord(name, obj, tree)
 
 
 def transform_coords(obj: Union[DataArray, Dataset], coords,
                      tree: dict) -> Union[DataArray, Dataset]:
-    # Keys in tree may be lists or tuple to define multiple outputs
+    """
+    """
+    # Keys in tree may be tuple to define multiple outputs
     simple_tree = {}
     for key in tree:
         if isinstance(key, str):
@@ -46,6 +46,6 @@ def transform_coords(obj: Union[DataArray, Dataset], coords,
             for k in key:
                 simple_tree[k] = tree[key]
     obj = obj.copy(deep=False)
-    for name in coords:
-        _add_coords(name=name, obj=obj, tree=simple_tree)
+    for name in [coords] if isinstance(coords, str) else coords:
+        _add_coord(name=name, obj=obj, tree=simple_tree)
     return obj
