7c28d35324e81e8c9fc88eb9dfdd54a9a130b3a9
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-08-10 15:44:14 +0200

    Remove duplicate prints

--- tools/debug/debug_multi_index_fragment.h
+++ tools/debug/debug_multi_index_fragment.h
@@ -7,25 +7,6 @@ friend std::ostream &operator<<(std::ostream &os, const MultiIndex<N> &index) {
   os << "  coord = " << index.m_coord << '\n';
   os << "  shape = " << index.m_shape << '\n';
   os << "  ndim = " << index.m_ndim << '\n';
-  os << "  stride = [";
-  for (scipp::index dim = 0; dim < std::max(index.m_ndim, scipp::index{2});
-       ++dim) {
-    os << '[';
-    for (scipp::index data = 0; data < N; ++data)
-      os << index.m_stride[dim][data] << ' ';
-    os << "] ";
-  }
-  os << "]\n";
-  os << "  coord = [";
-  for (scipp::index dim = 0; dim < std::max(index.m_ndim, scipp::index{2});
-       ++dim)
-    os << index.m_coord[dim] << ' ';
-  os << "]\n";
-  os << "  shape = [";
-  for (scipp::index dim = 0; dim < std::max(index.m_ndim, scipp::index{2});
-       ++dim)
-    os << index.m_shape[dim] << ' ';
-  os << "]\n";
   os << "  inner_ndim = " << index.m_inner_ndim << '\n';
   os << "  bin_stride = " << index.m_bin_stride << '\n';
   os << "  nested_dim_index = " << index.m_nested_dim_index << '\n';
