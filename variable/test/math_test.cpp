802b6017bb9b3988a94b574d48fa6ee324205405
Samuel Jones <samuel.jones@stfc.ac.uk>
2021-09-02 14:35:59 +0100

    Update and improve some round tests

--- variable/test/math_test.cpp
+++ variable/test/math_test.cpp
@@ -434,7 +434,7 @@ TEST(Variable, log10_bad_unit) {
 TEST(Variable, rint_round) {
   auto preRoundedVar = makeVariable<double>(Values{1.2, 2.9, 1.5, 2.5});
   auto roundedVar = makeVariable<double>(Values{1, 3, 2, 2});
-  EXPECT_EQ(round(preRoundedVar), roundedVar);
+  EXPECT_EQ(rint(preRoundedVar), roundedVar);
 }
 
 TEST(Variable, ceil_round) {
