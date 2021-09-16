4ba2988586879fc418bc011f0237dfa74884f0f4
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-09-16 15:12:31 +0200

    Add Shape{3, 1} to transform tests

--- variable/test/transform_test.cpp
+++ variable/test/transform_test.cpp
@@ -60,9 +60,9 @@ auto make_bin_indices(const scipp::index size, const scipp::index n_bins) {
 
 const std::vector<Shape> shapes{Shape{1},       Shape{2},       Shape{3},
                                 Shape{5},       Shape{16},      Shape{1, 1},
-                                Shape{1, 2},    Shape{2, 8},    Shape{5, 7},
-                                Shape{1, 1, 1}, Shape{1, 1, 4}, Shape{1, 5, 1},
-                                Shape{7, 1, 1}, Shape{2, 8, 4}};
+                                Shape{1, 2},    Shape{3, 1},    Shape{2, 8},
+                                Shape{5, 7},    Shape{1, 1, 1}, Shape{1, 1, 4},
+                                Shape{1, 5, 1}, Shape{7, 1, 1}, Shape{2, 8, 4}};
 
 auto make_slices(const Shape &shape) {
   std::vector<Slice> res{Slice{Dim::X, 0, shape.data.at(0) - 1},
