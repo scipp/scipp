827c7d1cfe567a8506a3e591fdd3da378aa11c3f
Simon Heybrock <simon.heybrock@esss.se>
2021-09-24 08:21:40 +0200

    flake8

--- .conan-recipes/llnl-units/conanfile.py
+++ .conan-recipes/llnl-units/conanfile.py
@@ -1,7 +1,6 @@
 from conans import ConanFile, CMake, tools
 import os
 
-
 CMAKE_PROJECT_STR = """project(
     UNITS
     LANGUAGES CXX
@@ -22,14 +21,18 @@ class UnitsConan(ConanFile):
     topics = ("units", "dimensions", "quantities", "physical-units",
               "dimensional-analysis", "run-time")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False],
-               "fPIC": [True, False],
-               "base_type": ["uint32_t", "uint64_t"],
-               "namespace": "ANY"}
-    default_options = {"shared": False,
-                       "fPIC": True,
-                       "base_type": "uint32_t",
-                       "namespace": None}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "base_type": ["uint32_t", "uint64_t"],
+        "namespace": "ANY"
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "base_type": "uint32_t",
+        "namespace": None
+    }
     generators = "cmake"
 
     def source(self):
@@ -37,12 +40,10 @@ class UnitsConan(ConanFile):
         git.clone("https://github.com/SimonHeybrock/units.git")
 
         cmake_project_str = (CMAKE_PROJECT_STR.replace("\n", os.linesep)
-                             if self.settings.os == "Windows" else
-                             CMAKE_PROJECT_STR)
+                             if self.settings.os == "Windows" else CMAKE_PROJECT_STR)
 
-        tools.replace_in_file("units/CMakeLists.txt",
-                              cmake_project_str,
-                              cmake_project_str + """
+        tools.replace_in_file(
+            "units/CMakeLists.txt", cmake_project_str, cmake_project_str + """
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()""")
 
@@ -73,8 +74,6 @@ conan_basic_setup()""")
     def package_info(self):
         self.cpp_info.libs = ["units"]
         units_namespace = self.options.get_safe("namespace")
-        self.cpp_info.defines = [
-            f"UNITS_BASE_TYPE={self.options.base_type}"
-        ]
+        self.cpp_info.defines = [f"UNITS_BASE_TYPE={self.options.base_type}"]
         if units_namespace:
             self.cpp_info.defines.append(f"UNITS_NAMESPACE={units_namespace}")
