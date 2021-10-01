ee0e03d3817e2b3f5c686e3ed8c9ee9e47a2a15a
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-10-01 14:32:36 +0200

    Allow printing scipp types in debug functions

--- tools/debug/debugout.h
+++ tools/debug/debugout.h
@@ -5,16 +5,21 @@
 #include <sstream>
 #include <vector>
 
+#include "scipp/core/string.h"
+
 // Use namespace std to make the operators discoverable through ADL.
 namespace std { // NOLINT
 template <typename T, size_t N>
 std::ostream &operator<<(std::ostream &os, const std::array<T, N> &a) {
+  using scipp::core::to_string;
+  using std::to_string;
+
   os << '[';
   for (size_t i = 0; i + 1 < std::size(a); ++i) {
-    os << a[i] << ", ";
+    os << to_string(a[i]) << ", ";
   }
   if (std::size(a) > 0) {
-    os << a.back();
+    os << to_string(a.back());
   }
   os << ']';
   return os;
@@ -22,12 +27,15 @@ std::ostream &operator<<(std::ostream &os, const std::array<T, N> &a) {
 
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+  using scipp::core::to_string;
+  using std::to_string;
+
   os << '[';
   for (size_t i = 0; i + 1 < std::size(v); ++i) {
-    os << v[i] << ", ";
+    os << to_string(v[i]) << ", ";
   }
   if (std::size(v) > 0) {
-    os << v.back();
+    os << to_string(v.back());
   }
   os << ']';
   return os;
