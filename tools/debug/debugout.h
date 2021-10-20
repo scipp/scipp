d7e7a11832dc11783682f7089ddfc9a2f3decbd3
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-10-20 14:40:52 +0200

    Do not use to_string in debug output

--- tools/debug/debugout.h
+++ tools/debug/debugout.h
@@ -7,37 +7,31 @@
 
 #include "scipp/core/string.h"
 
+namespace debug::detail {
+template <class T, class C>
+void print_std_container(std::ostream &os, const C &c) {
+  os << '[';
+  for (size_t i = 0; i + 1 < std::size(c); ++i) {
+    os << c[i] << ", ";
+  }
+  if (std::size(c) > 0) {
+    os << c.back();
+  }
+  os << ']';
+}
+} // namespace debug::detail
+
 // Use namespace std to make the operators discoverable through ADL.
 namespace std { // NOLINT
 template <typename T, size_t N>
 std::ostream &operator<<(std::ostream &os, const std::array<T, N> &a) {
-  using scipp::core::to_string;
-  using std::to_string;
-
-  os << '[';
-  for (size_t i = 0; i + 1 < std::size(a); ++i) {
-    os << to_string(a[i]) << ", ";
-  }
-  if (std::size(a) > 0) {
-    os << to_string(a.back());
-  }
-  os << ']';
+  debug::detail::print_std_container<T>(os, a);
   return os;
 }
 
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
-  using scipp::core::to_string;
-  using std::to_string;
-
-  os << '[';
-  for (size_t i = 0; i + 1 < std::size(v); ++i) {
-    os << to_string(v[i]) << ", ";
-  }
-  if (std::size(v) > 0) {
-    os << to_string(v.back());
-  }
-  os << ']';
+  debug::detail::print_std_container<T>(os, v);
   return os;
 }
 } // namespace std
