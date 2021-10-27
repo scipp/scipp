e5739dd2aef621e3e27586ab54b6192546c77c9e
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-10-27 13:22:45 +0200

    More tests for Variable::dim

--- variable/test/variable_scalar_accessors_test.cpp
+++ variable/test/variable_scalar_accessors_test.cpp
@@ -78,3 +78,16 @@ TYPED_TEST(Variable_scalar_accessors, dim_dim_1) {
   auto &&v = TestFixture::access(v_);
   ASSERT_EQ(v.dim(), Dim::X);
 }
+
+TYPED_TEST(Variable_scalar_accessors, dim_dim_0) {
+  auto v_ = variable::makeVariable<double>(Dims{}, Shape{}, Values{1.1});
+  auto &&v = TestFixture::access(v_);
+  ASSERT_THROW_DISCARD(v.dim(), except::DimensionError);
+}
+
+TYPED_TEST(Variable_scalar_accessors, dim_dim_2) {
+  auto v_ = variable::makeVariable<double>(Dims{Dim::X, Dim::Y}, Shape{1, 1},
+                                           Values{1.1});
+  auto &&v = TestFixture::access(v_);
+  ASSERT_THROW_DISCARD(v.dim(), except::DimensionError);
+}
