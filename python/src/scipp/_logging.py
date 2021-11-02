43cf477ffb2d63f93de01abf73fb02595dc3ce06
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-11-02 10:24:32 +0100

    Factor out LogWidget

--- python/src/scipp/_logging.py
+++ python/src/scipp/_logging.py
@@ -2,9 +2,12 @@
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 # @author Jan-Lukas Wynen
 
+import html
 import logging
+import time
 
 from .utils import running_in_jupyter
+from ._styling import inject_style
 
 
 def get_logger() -> logging.Logger:
@@ -30,40 +33,38 @@ def make_stream_handler() -> logging.StreamHandler:
     return handler
 
 
-class WidgetHandler(logging.Handler):
-    def __init__(self, level, widget):
-        super().__init__(level)
-        self.widget = widget
+if running_in_jupyter():
+    from ipywidgets import HTML
 
-    def emit(self, record: logging.LogRecord) -> None:
-        formatted_record = self.format(record)
-        new_output = {
-            'name': 'stdout',
-            'output_type': 'stream',
-            'text': formatted_record + '\n'
-        }
-        self.widget.outputs = (new_output, ) + self.widget.outputs
+    class LogWidget(HTML):
+        def __init__(self, **kwargs):
+            super().__init__(**kwargs)
+            self._rows_str = ''
 
-    def clear_logs(self):
-        """ Clear the current logs """
-        self.widget.clear_output()
+        def add_row(self, time_stamp: str, level: str, message: str) -> None:
+            self._rows_str += self._format_row(time_stamp, level, message)
+            self._update()
+
+        @staticmethod
+        def _format_row(time_stamp: str, level: str, message: str) -> str:
+            return (f'<tr><td class="sc-log-time-stamp">{html.escape(time_stamp)}</td> '
+                    f'<td class="sc-log-level">{level}</td> '
+                    f'<td class="sc-log-message">{html.escape(message)}</td></tr>')
+
+        def _update(self) -> None:
+            self.value = f'<div class="sc-log"><table>{self._rows_str}</table></div>'
+
+        def clear(self) -> None:
+            self._rows_str = ''
+            self._update()
 
 
-def _make_output_widget():
+def make_log_widget():
     if not running_in_jupyter():
         raise RuntimeError('Cannot construct a logging widget because '
                            'Python is not running in a Jupyter notebook.')
     else:
-        from ipywidgets import Output
-        layout = {'width': '100%', 'height': '160px', 'border': '1px solid black'}
-        return Output(layout=layout)
-
-
-def make_widget_handler() -> WidgetHandler:
-    handler = WidgetHandler(level=logging.INFO, widget=_make_output_widget())
-    handler.setFormatter(
-        logging.Formatter('%(asctime)s  - [%(levelname)s] %(message)s'))
-    return handler
+        return LogWidget()
 
 
 def get_log_widget():
@@ -78,8 +79,25 @@ def get_log_widget():
 
 def display_logs() -> None:
     from IPython.display import display
+    inject_style()
     display(get_log_widget())
 
 
 def clear_log_widget() -> None:
-    get_log_widget().clear_output()
+    get_log_widget().clear()
+
+
+class WidgetHandler(logging.Handler):
+    def __init__(self, level: int, widget):
+        super().__init__(level)
+        self.widget = widget
+        self._rows = []
+
+    def emit(self, record: logging.LogRecord) -> None:
+        time_stamp = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(record.created))
+        self.widget.add_row(time_stamp, record.levelname, self.format(record))
+
+
+def make_widget_handler() -> WidgetHandler:
+    handler = WidgetHandler(level=logging.INFO, widget=make_log_widget())
+    return handler
