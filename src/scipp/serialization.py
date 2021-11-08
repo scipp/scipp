e8a313e9eab0350b9011b2f67e9e738fdccc647f
Simon Heybrock <simon.heybrock@esss.se>
2021-11-08 09:35:36 +0100

    Support datetime serialization

--- src/scipp/serialization.py
+++ src/scipp/serialization.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 # @author Simon Heybrock
 from . import Variable, DataArray
+from .core import bins, dtype
 
 
 def _encode_dict(meta):
@@ -11,14 +12,21 @@ def _encode_dict(meta):
 def encode(obj):
     import msgpack_numpy as m
     if isinstance(obj, Variable):
-        enc = {
-            '__scipp.Variable__': True,
-            'dims': obj.dims,
-            'unit': str(obj.unit),
-            'values': m.encode(obj.values)
-        }
-        if obj.variances is not None:
-            enc['variances'] = m.encode(obj.variances)
+        enc = {'__scipp.Variable__': True}
+        if obj.bins is None:
+            enc.update({
+                'dims': obj.dims,
+                'unit': str(obj.unit),
+                'dtype': str(obj.dtype)
+            })
+            if obj.dtype == dtype.datetime64:
+                enc['values'] = m.encode(obj.values.view('int64'))
+            else:
+                enc['values'] = m.encode(obj.values)
+            if obj.variances is not None:
+                enc['variances'] = m.encode(obj.variances)
+        else:
+            enc['bins'] = _encode_dict(obj.bins.constituents)
         return enc
     elif isinstance(obj, DataArray):
         return {
@@ -34,10 +42,16 @@ def encode(obj):
 def decode(obj):
     import msgpack_numpy as m
     if '__scipp.Variable__' in obj:
+        if 'bins' in obj:
+            return bins(**obj['bins'])
+        if obj['dtype'] == str(dtype.datetime64):
+            values = m.decode(obj['values']).view(f"datetime64[{obj['unit']}]")
+        else:
+            values = m.decode(obj['values'])
         variances = m.decode(obj['variances']) if 'variances' in obj else None
         return Variable(dims=obj['dims'],
                         unit=obj['unit'],
-                        values=m.decode(obj['values']),
+                        values=values,
                         variances=variances)
     if '__scipp.DataArray__' in obj:
         return DataArray(data=obj['data'],
