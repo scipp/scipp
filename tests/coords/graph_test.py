9e6fa44139624f504476cfcbdf7ee151af2fdb12
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-12-02 11:21:16 +0100

    Add test_graph_for

--- tests/coords/graph_test.py
+++ tests/coords/graph_test.py
@@ -3,9 +3,18 @@
 # @author Jan-Lukas Wynen
 
 import scipp.coords.graph as scgraph
+from scipp.coords.rule import ComputeRule, FetchRule, RenameRule
+import scipp as sc
 
 
 def graph_0():
+    r"""
+    a b c
+    |╳| |
+    f e |
+    |  \|
+    h   g
+    """
     def fd(a, b, c):
         pass
 
@@ -18,7 +27,16 @@ def graph_0():
     def fg(c, e):
         pass
 
-    return scgraph.Graph({'d': fd, 'e': fe, 'f': ff, 'g': fg})
+    return scgraph.Graph({'d': fd, 'e': fe, 'f': ff, 'g': fg, 'h': 'f'})
+
+
+def make_data(coords, dims=('x', )):
+    return sc.DataArray(sc.ones(dims=dims, shape=[1] * len(dims)),
+                        coords={
+                            name: sc.ones(dims=[name] if name in dims else dims[:1],
+                                          shape=[1])
+                            for name in coords
+                        })
 
 
 def test_children_of():
@@ -28,8 +46,9 @@ def test_children_of():
     assert set(graph.children_of('c')) == {'d', 'g'}
     assert set(graph.children_of('d')) == set()
     assert set(graph.children_of('e')) == {'g'}
-    assert set(graph.children_of('f')) == set()
+    assert set(graph.children_of('f')) == {'h'}
     assert set(graph.children_of('g')) == set()
+    assert set(graph.children_of('h')) == set()
 
 
 def test_parents_of():
@@ -41,10 +60,41 @@ def test_parents_of():
     assert set(graph.parents_of('e')) == {'a', 'b'}
     assert set(graph.parents_of('f')) == {'a', 'b'}
     assert set(graph.parents_of('g')) == {'c', 'e'}
+    assert set(graph.parents_of('h')) == {'f'}
 
 
 def test_neighbors_of():
     graph = graph_0()
-    for node in ('a', 'b', 'c', 'd', 'e', 'f', 'g'):
+    for node in ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'):
         assert set(graph.neighbors_of(node)) == (set(graph.children_of(node))
                                                  | set(graph.parents_of(node)))
+
+
+def test_graph_for():
+    base_graph = graph_0()
+    da = make_data(('a', 'b', 'c'))
+
+    graph = base_graph.graph_for(da, {'f'})
+    assert isinstance(graph['a'], FetchRule)
+    assert isinstance(graph['b'], FetchRule)
+    assert isinstance(graph['f'], ComputeRule)
+    assert set(graph['f'].dependencies) == {'a', 'b'}
+
+    graph = base_graph.graph_for(da, {'g'})
+    assert isinstance(graph['a'], FetchRule)
+    assert isinstance(graph['b'], FetchRule)
+    assert isinstance(graph['c'], FetchRule)
+    assert isinstance(graph['e'], ComputeRule)
+    assert set(graph['e'].dependencies) == {'a', 'b'}
+    assert isinstance(graph['g'], ComputeRule)
+    assert set(graph['g'].dependencies) == {'c', 'e'}
+
+    graph = base_graph.graph_for(da, {'h', 'e'})
+    assert isinstance(graph['a'], FetchRule)
+    assert isinstance(graph['b'], FetchRule)
+    assert isinstance(graph['f'], ComputeRule)
+    assert set(graph['f'].dependencies) == {'a', 'b'}
+    assert isinstance(graph['e'], ComputeRule)
+    assert set(graph['e'].dependencies) == {'a', 'b'}
+    assert isinstance(graph['h'], RenameRule)
+    assert set(graph['h'].dependencies) == {'f'}
