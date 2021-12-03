b0231c7fe46c60402dac4ddb3c34c4af82a3ac6d
Simon Heybrock <simon.heybrock@esss.se>
2021-12-03 14:41:23 +0100

    Automatic version handling

--- setup.py
+++ setup.py
@@ -4,8 +4,15 @@
 from skbuild import setup
 from setuptools import find_packages
 
+
+def get_version():
+    import subprocess
+    return subprocess.run(['git', 'describe', '--tags', '--abbrev=0'],
+                          stdout=subprocess.PIPE).stdout.decode('utf8').strip()
+
+
 setup(name='scipp',
-      version='0.9.0',
+      version=get_version(),
       description='Multi-dimensional data arrays with labeled dimensions',
       author='Scipp contributors (https://github.com/scipp)',
       url='https://scipp.github.io',
