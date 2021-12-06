1209519b3bdfde3e53f0097c5cd8d6ab6ed47df8
Simon Heybrock <simon.heybrock@esss.se>
2021-12-06 06:36:50 +0100

    Try Development.Module

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 # @author Simon Heybrock
-# flake8: noqa
 from skbuild import setup
 from setuptools import find_packages
 
@@ -12,24 +11,19 @@ def get_version():
                           stdout=subprocess.PIPE).stdout.decode('utf8').strip()
 
 
-setup(
-    name='scipp',
-    version=get_version(),
-    description='Multi-dimensional data arrays with labeled dimensions',
-    author='Scipp contributors (https://github.com/scipp)',
-    url='https://scipp.github.io',
-    license='BSD-3-Clause',
-    packages=find_packages(where="src"),
-    package_dir={'': 'src'},
-    cmake_args=[
-        '-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=OFF',
-        '-DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")',
-        '-DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var(\'LIBDIR\'))")'
-    ],
-    cmake_install_dir='src/scipp',
-    include_package_data=True,
-    python_requires='>=3.7',
-    install_requires=[
-        'numpy',
-        'python-configuration',
-    ])
+setup(name='scipp',
+      version=get_version(),
+      description='Multi-dimensional data arrays with labeled dimensions',
+      author='Scipp contributors (https://github.com/scipp)',
+      url='https://scipp.github.io',
+      license='BSD-3-Clause',
+      packages=find_packages(where="src"),
+      package_dir={'': 'src'},
+      cmake_args=['-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=OFF'],
+      cmake_install_dir='src/scipp',
+      include_package_data=True,
+      python_requires='>=3.7',
+      install_requires=[
+          'numpy',
+          'python-configuration',
+      ])
