1dc3a80fbb44a36f50bb05c2164b7d1ca24ea35b
Simon Heybrock <simon.heybrock@esss.se>
2021-12-06 06:45:43 +0100

    Add span.h

---
+++ lib/common/include/scipp/common/span.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+
+#define TCP_SPAN_NAMESPACE_NAME std
+#include "span.hpp"
