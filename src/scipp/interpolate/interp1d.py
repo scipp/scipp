abc29610f46ba8da4edf87f7cf8acd5335f17e92
Simon Heybrock <simon.heybrock@esss.se>
2021-12-10 15:39:49 +0100

    Add example

--- src/scipp/interpolate/interp1d.py
+++ src/scipp/interpolate/interp1d.py
@@ -49,6 +49,31 @@ def interp1d(da: DataArray, dim, **kwargs) -> Callable:
                  rounds up and 'nearest’ rounds down. Default is 'linear’.
     :param fill_value: Set to 'extrapolate' to allow for extrapolation of points
                        outside the range.
+
+    Examples:
+
+      >>> # Create discrete data approximately defining function
+      >>> x = sc.geomspace(dim='x', start=0.1, stop=0.4, num=4, unit='rad')
+      >>> da = sc.DataArray(sc.sin(x), coords={'x': x})
+      >>> # Create interpolation function
+      >>> from scipp.interpolate import interp1d
+      >>> f = interp1d(da, 'x')
+      >>> # Apply interpolation function
+      >>> xnew = sc.linspace(dim='x', start=0.1, stop=0.4, num=5, unit='rad')
+      >>> f(xnew)
+      <scipp.DataArray>
+      Dimensions: Sizes[x:5, ]
+      Coordinates:
+        x                         float64            [rad]  (x)  [0.100000, 0.175000, 0.250000, 0.325000, 0.400000]
+      Data:
+                                  float64  [dimensionless]  (x)  [0.099833, 0.173987, 0.247384, 0.318433, 0.389418]
+      >>> f(xnew, midpoints=True)
+      <scipp.DataArray>
+      Dimensions: Sizes[x:4, ]
+      Coordinates:
+        x                         float64            [rad]  (x [bin-edge])  [0.100000, 0.175000, 0.250000, 0.325000, 0.400000]
+      Data:
+                                  float64  [dimensionless]  (x)  [0.137015, 0.210685, 0.282941, 0.353926]
     """  # noqa #501
     import scipy.interpolate as inter
     if 'axis' in kwargs:
