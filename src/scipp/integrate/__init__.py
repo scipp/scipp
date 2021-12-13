7983bd5f6f8c4d6fd2663f4ee929a1be3bec75e4
Simon Heybrock <simon.heybrock@esss.se>
2021-12-13 09:17:06 +0100

    Add scipp.integrate

---
+++ src/scipp/integrate/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
+# @author Simon Heybrock
+# flake8: noqa
+"""Sub-package for integration."""
+
+from ._integrate import simpson, trapezoid
+
+__all__ = ['simpson', 'trapezoid']
