2c4798d8f5b5eea3d25936eb3345329dfe359d11
Simon Heybrock <simon.heybrock@ess.eu>
2022-01-13 13:58:25 +0100

    Add script for migrating hdf5 files for scipp-0.11

---
+++ tools/migration/scipp-0.11-hdf5-files.py
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+import h5py
+import sys
+from shutil import copyfile
+
+
+def migrate_object(name, obj):
+    if 'dtype' in obj.attrs:
+        mapping = {
+            'vector_3_float64': 'vector3',
+            'matrix_3_float64': 'linear_transform3'
+        }
+        for old, new in mapping.items():
+            if obj.attrs['dtype'] == old:
+                obj.attrs['dtype'] = new
+                print(f'Changing dtype attr of {name} from {old} to {new}')
+
+
+def migrate_file(filename):
+    with h5py.File(filename, 'r+') as f:
+        f.visititems(migrate_object)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print('Usage: python scipp-0.11-hdf5-files.py filename1 [filename2...]')
+    for filename in sys.argv[1:]:
+        backup = f'{filename}.pre-0.11'
+        print(f'Backing up original file to {backup}')
+        copyfile(filename, backup)
+        migrate_file(filename)
