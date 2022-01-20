3b289e228e6c91870cffda8a6ea0f07f7a27fabf
Simon Heybrock <simon.heybrock@ess.eu>
2022-01-20 06:51:46 +0100

    Revert "Add squeeze for DataArray and Dataset"
    This reverts commit 6e91f3d4f253caa9aed72df4b97743f1026bad38.

--- lib/dataset/include/scipp/dataset/shape.h
+++ lib/dataset/include/scipp/dataset/shape.h
@@ -40,9 +40,4 @@ transpose(const DataArray &a, scipp::span<const Dim> dims = {});
 [[nodiscard]] SCIPP_DATASET_EXPORT Dataset
 transpose(const Dataset &d, scipp::span<const Dim> dims = {});
 
-[[nodiscard]] SCIPP_DATASET_EXPORT DataArray
-squeeze(const DataArray &a, std::optional<scipp::span<const Dim>> dims);
-[[nodiscard]] SCIPP_DATASET_EXPORT Dataset
-squeeze(const Dataset &d, std::optional<scipp::span<const Dim>> dims);
-
 } // namespace scipp::dataset
