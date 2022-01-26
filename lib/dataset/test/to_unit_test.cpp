248531874d83d96c12f744ccb6d41317f3d9e122
Simon Heybrock <simon.heybrock@ess.eu>
2022-01-26 09:35:29 +0100

    Remove original test code

--- lib/dataset/test/to_unit_test.cpp
+++ lib/dataset/test/to_unit_test.cpp
@@ -8,41 +8,6 @@
 
 using namespace scipp;
 
-class ToUnitDataArrayTest : public ::testing::Test {
-protected:
-  const DataArray data_array =
-      DataArray(makeVariable<double>(Dims{Dim::X}, Shape{3},
-                                     Values{1.0, 2.0, 3.0}, units::m),
-                {{Dim::X, makeVariable<int>(Dims{Dim::X}, Shape{3},
-                                            Values{4, 5, 6}, units::s)}},
-                {{"m", makeVariable<bool>(Dims{Dim::X}, Shape{3},
-                                          Values{true, false, true})}});
-
-  static void do_check(const DataArray &original, const units::Unit target_unit,
-                       const CopyPolicy copy_policy, const bool expect_copy) {
-    const auto converted = to_unit(original, target_unit, copy_policy);
-
-    EXPECT_EQ(converted.data(), to_unit(original.data(), target_unit));
-    EXPECT_EQ(converted.coords()[Dim::X].unit(), units::s);
-    EXPECT_EQ(converted.masks(), original.masks());
-
-    EXPECT_TRUE(converted.coords()[Dim::X].is_same(original.coords()[Dim::X]));
-    EXPECT_EQ(converted.data().is_same(original.data()), !expect_copy);
-    EXPECT_EQ(converted.masks()["m"].is_same(original.masks()["m"]),
-              !expect_copy);
-  }
-};
-
-TEST_F(ToUnitDataArrayTest, different_unit) {
-  do_check(data_array, units::mm, CopyPolicy::TryAvoid, true);
-  do_check(data_array, units::mm, CopyPolicy::Always, true);
-}
-
-TEST_F(ToUnitDataArrayTest, same_unit) {
-  do_check(data_array, units::m, CopyPolicy::TryAvoid, false);
-  do_check(data_array, units::m, CopyPolicy::Always, true);
-}
-
 auto make_array() {
   return DataArray(makeVariable<double>(Values{2.0}, units::m),
                    {{Dim("coord"), makeVariable<int>(Values{4}, units::s)}},
