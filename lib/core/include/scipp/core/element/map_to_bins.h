1a19a650805dba36df7d6140af492ab33e124e0e
Simon Heybrock <simon.heybrock@ess.eu>
2022-02-10 07:23:09 +0100

    Readability and assert power of 2

--- lib/core/include/scipp/core/element/map_to_bins.h
+++ lib/core/include/scipp/core/element/map_to_bins.h
@@ -32,9 +32,13 @@ auto map_to_bins_direct = [](const auto &binned, auto &bins, const auto &data,
   }
 };
 
+constexpr bool is_powerof2(int v) { return v && ((v & (v - 1)) == 0); }
+
 template <int chunksize>
 auto map_to_bins_chunkwise = [](const auto &binned, auto &bins,
                                 const auto &data, const auto &bin_indices) {
+  // compiler is smart for div or mod 2**N, otherwise this would be too slow
+  static_assert(is_powerof2(chunksize));
   const auto size = scipp::size(bin_indices);
   using T = std::decay_t<decltype(data)>;
 
@@ -56,7 +60,7 @@ auto map_to_bins_chunkwise = [](const auto &binned, auto &bins,
       const auto i_bin = bin_indices[i];
       if (i_bin < 0)
         continue;
-      const uint8_t j = i_bin % chunksize; // compiler is smart for mod 2**N
+      const uint8_t j = i_bin % chunksize;
       const auto i_chunk = i_bin / chunksize;
       auto &[vals, ind] = chunks[i_chunk];
       if constexpr (is_ValueAndVariance_v<T>) {
@@ -116,11 +120,11 @@ static constexpr auto bin = overloaded{
       // chunks into bins. For example, instead of mapping directly to 65536
       // bins, we may map to 256 chunks, and each chunk to 256 bins.
       if (bins.size() > 512 && size > 128 * 1024) { // avoid overhead
-        if (128 * 128 >= bins.size())
+        if (bins.size() <= 128 * 128)
           map_to_bins_chunkwise<128>(binned, bins, data, bin_indices);
-        else if (256 * 256 >= bins.size())
+        else if (bins.size() <= 256 * 256)
           map_to_bins_chunkwise<256>(binned, bins, data, bin_indices);
-        else if (512 * 512 >= bins.size())
+        else if (bins.size() <= 512 * 512)
           map_to_bins_chunkwise<512>(binned, bins, data, bin_indices);
         else
           map_to_bins_chunkwise<1024>(binned, bins, data, bin_indices);
