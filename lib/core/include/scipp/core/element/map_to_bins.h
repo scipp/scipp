94e6cd99715e7c61e27777ca91b353a1407c88e4
Simon Heybrock <simon.heybrock@ess.eu>
2022-02-10 06:51:53 +0100

    Draft of faster map_to_bins

--- lib/core/include/scipp/core/element/map_to_bins.h
+++ lib/core/include/scipp/core/element/map_to_bins.h
@@ -39,15 +39,80 @@ static constexpr auto bin = overloaded{
       auto bins(offsets.sizes());
       const auto size = scipp::size(bin_indices);
       using T = std::decay_t<decltype(data)>;
-      for (scipp::index i = 0; i < size; ++i) {
-        const auto i_bin = bin_indices[i];
-        if (i_bin < 0)
-          continue;
-        if constexpr (is_ValueAndVariance_v<T>) {
-          binned.variance[bins[i_bin]] = data.variance[i];
-          binned.value[bins[i_bin]++] = data.value[i];
-        } else {
-          binned[bins[i_bin]++] = data[i];
+      // If there are many bins, we have two performance issues:
+      // 1. `bins` is large and will not fit into L1, L2, or L3 cache.
+      // 2. Writes to output are very random, implying a cache miss for every
+      //    event.
+      // We can avoid some of this issue by first sorting into chunks, then
+      // chunks into bins. For example, instead of mapping directly to 65536
+      // bins, we may map to 256 chunks, and each chunk to 256 bins.
+      // TODO Ideally we should recurse this approach, with optimal
+      // intermediate chunk sizes. These will in principle depend on cache
+      // sizes.
+      if (bins.size() > 512 && size > 128 * 1024) { // avoid overhead
+        using Val = std::conditional_t<is_ValueAndVariance_v<T>,
+                                       typename T::value_type, T>;
+        // TODO Ideally this buffer would be reused (on a per-thread basis)
+        // for every application of the kernel.
+        // std::unordered_map<scipp::index,
+        //                   std::tuple<std::vector<typename Val::value_type>,
+        //                              std::vector<uint8_t>>>
+        std::vector<std::tuple<std::vector<typename Val::value_type>,
+                               std::vector<uint8_t>>>
+            chunks;
+        chunks.resize(bins.size() / 256);
+        for (scipp::index i = 0; i < size;) {
+          // We operate in blocks so the size of the map of buffers, i.e.,
+          // additional memory use of the algorithm, is bounded. This also
+          // avoids costly allocations from resize operations.
+          scipp::index max = std::min(size, i + scipp::size(bins) * 64);
+          // 1. Map to chunks
+          for (; i < max; ++i) {
+            const auto i_bin = bin_indices[i];
+            if (i_bin < 0)
+              continue;
+            // TODO use sqrt rounded to power of 2
+            const uint8_t j = i_bin % 256; // compiler is smart for mod 2**N
+            // const auto i_chunk = i_bin - j;
+            const auto i_chunk = i_bin / 256;
+            auto &[vals, ind] = chunks[i_chunk];
+            if constexpr (is_ValueAndVariance_v<T>) {
+              vals.emplace_back(data.value[i]);
+              vals.emplace_back(data.variance[i]);
+            } else {
+              vals.emplace_back(data[i]);
+            }
+            ind.emplace_back(j);
+          }
+          // 2. Map chunks to bins
+          // for (auto &[i_chunk, chunk] : chunks)
+          for (scipp::index i_chunk = 0; i_chunk < scipp::size(chunks);
+               ++i_chunk) {
+            auto &[vals, ind] = chunks[i_chunk];
+            for (scipp::index j = 0; j < scipp::size(ind); ++j) {
+              const auto i_bin = 256 * i_chunk + ind[j];
+              if constexpr (is_ValueAndVariance_v<T>) {
+                binned.variance[bins[i_bin]] = vals[2 * j + 1];
+                binned.value[bins[i_bin]++] = vals[2 * j];
+              } else {
+                binned[bins[i_bin]++] = vals[j];
+              }
+            }
+            vals.clear();
+            ind.clear();
+          }
+        }
+      } else {
+        for (scipp::index i = 0; i < size; ++i) {
+          const auto i_bin = bin_indices[i];
+          if (i_bin < 0)
+            continue;
+          if constexpr (is_ValueAndVariance_v<T>) {
+            binned.variance[bins[i_bin]] = data.variance[i];
+            binned.value[bins[i_bin]++] = data.value[i];
+          } else {
+            binned[bins[i_bin]++] = data[i];
+          }
         }
       }
     }};
