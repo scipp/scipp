8085f2a099efdc4f8762ac621d2d0ce37a17fac2
Simon Heybrock <simon.heybrock@ess.eu>
2022-02-15 13:06:55 +0100

    Maybe bool

--- src/scipp/plotting/resampling_model.py
+++ src/scipp/plotting/resampling_model.py
@@ -214,7 +214,10 @@ class ResamplingModel():
                                 out.meta,
                                 sanitize=isinstance(self, ResamplingBinnedModel))
                 # rebin changes to float-valued mask, but need bool
-                self._resampled.masks[name] = m.data > scalar(0.0, unit=None)
+                if m.dtype != DType.bool:
+                    self._resampled.masks[name] = m.data > scalar(0.0, unit=None)
+                else:
+                    self._resampled.masks[name] = m.data
             for dim in params:
                 size_one = self._resampled.sizes.get(dim, None) == 1
                 no_resolution = self.resolution.get(dim, None) is None
