2fc38657a1a9a34efef782ff1831ca3191b67bd7
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-02-16 14:04:23 +0100

    Print steps: None if there are no steps

--- src/scipp/coords/transform_coords.py
+++ src/scipp/coords/transform_coords.py
@@ -139,6 +139,7 @@ def _log_transform(rules: List[Rule], targets: Set[str],
                      | set(rule_output_names(rules, ComputeRule))) - targets
         if coords.total_usages(name) < 0
     }
+    steps = [rule for rule in rules if not isinstance(rule, FetchRule)]
 
     message = f'Transformed coords ({", ".join(sorted(inputs))}) ' \
               f'-> ({", ".join(sorted(targets))})'
@@ -148,8 +149,8 @@ def _log_transform(rules: List[Rule], targets: Set[str],
         dim_rename_steps = '\n'.join(f'    {t} <- {f}'
                                      for f, t in dim_name_changes.items())
         message += '\n  Renamed dimensions:\n' + dim_rename_steps
-    message += '\n  Steps:\n' + '\n'.join(
-        f'    {rule}' for rule in rules if not isinstance(rule, FetchRule))
+    message += '\n  Steps:\n' + ('\n'.join(f'    {rule}'
+                                           for rule in steps) if steps else '    None')
 
     get_logger().info(message)
 
