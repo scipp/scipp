8b37e163748d82912e45da031eb9788d324121b8
Neil Vaytet <neil.vaytet@esss.se>
2022-02-22 15:33:49 +0100

    first draft using the new version of metatoenv script based on conda debug

--- tools/metatoenv.py
+++ tools/metatoenv.py
@@ -9,17 +9,14 @@
 
 import os
 import argparse
-from functools import reduce
-import platform as _platform
+import sys
+import subprocess
 
 parser = argparse.ArgumentParser(
     description='Generate a conda environment file from a conda recipe meta.yaml file')
 parser.add_argument('--dir',
                     default='.',
                     help='the directory where the conda meta.yaml file is located')
-parser.add_argument('--meta-file',
-                    default='meta.yaml',
-                    help='the name of the conda meta yaml file')
 parser.add_argument('--env-file',
                     default='environment.yml',
                     help='name of the output environment file')
@@ -34,189 +31,64 @@ parser.add_argument('--channels',
                     help='the conda channels to be added at the top of the '
                     'environment file (to specify multiple channels, separate them '
                     'with commas: --channels=conda-forge,scipp)')
-parser.add_argument('--platform',
-                    default=None,
-                    help='the platform (linux, osx, or win) on which the environment '
-                    'is destined to be created (if no platform is specified, the '
-                    'platform on which this script is running will be used)')
-parser.add_argument('--extra',
-                    default='',
-                    help='additional packages to be included in the environment '
-                    '(use commas to separate multiple entries, e.g. '
-                    '--extra=numpy,matplotlib,scipy)')
-parser.add_argument('--merge-with',
-                    default='',
-                    help='a second environment file that is to be merged with the '
-                    'one that would be created by the conda meta.yaml file alone')
+
+# def _run(command, shell, executable=None, return_output=False):
+#     print(command)
+#     if return_output:
+#         return subprocess.check_output(command,
+#                                        stderr=subprocess.STDOUT,
+#                                        shell=shell,
+#                                        executable=executable).decode()
+#     else:
+#         subprocess.check_call(command,
+#                               stderr=subprocess.STDOUT,
+#                               shell=shell,
+#                               executable=executable)
 
 
-def _indentation_level(string):
-    """
-    Count the number of leading spaces in a string.
-    """
-    return len(string) - len(string.lstrip(' '))
+def main(condadir, envfile, envname, channels):
 
-
-def _parse_yaml(text):
-    """
-    Parse yaml text and store into a dict.
-    Description of algorithm:
-     - if a line ends with `:`, we make a new nested dict. Current handle is then this
-       new nested dict.
-     - if a line does not end with `:`, but contains `:`, we set the value at the
-       current handle in the dict
-     - if a line does not end with `:`, and starts with `-`, we add the entry as a key
-       in the dict and set the value to `None`
-     - if the indentation level of the current line is less than the previous line, it
-       implies we a closing a block. We iterate backwards in the file until we find a
-       line with the same indent level and use the parent block of that line as our new
-       handle
-    """
-    out = {}
-    nspaces = 0
-    handle = out
-    path = []
-
-    # Remove all comments and empty lines in text
-    clean_text = []
-    for line in text:
-        line = line.rstrip(' \n')
-        if len(line) > 0:
-            line = line.split("#")[0]
-            if len(line.strip()) > 0:
-                clean_text.append(line.rstrip(' \n'))
-
-    for i, line in enumerate(clean_text):
-
-        if i > 0:
-            line_indent = _indentation_level(line)
-            if line_indent < _indentation_level(clean_text[i - 1]):
-                current_indent = line_indent
-                for p in path[::-1]:
-                    if current_indent == p[1]:
-                        ind = path.index(p)
-                        handle = out
-                        for j in range(ind):
-                            handle = handle[path[j][0]]
-                        path = path[:ind]
-                        break
-
-        if line.endswith(':'):
-            key = line.strip(' -\n')
-            handle[key] = {}
-            handle = handle[key]
-            nspaces = _indentation_level(line)
-            path.append((key, nspaces))
-        else:
-            stripped = line.lstrip(' ')
-            if stripped.startswith('-'):
-                handle[stripped.strip(' -')] = None
-            elif ':' in stripped:
-                ind = stripped.find(':')
-                handle[stripped[:ind]] = stripped[ind + 1:]
-
-    return out
-
-
-def _jinja_filter(dependencies, platform):
-    """
-    Filter out deps for requested platform via jinja syntax.
-    """
-    out = {}
-    for key, value in dependencies.items():
-        if isinstance(value, dict):
-            out[key] = _jinja_filter(value, platform)
-        else:
-            ok = True
-            if key.count('[') > 0:
-                left = key.find('[')
-                right = key.find(']')
-                if key.count(']') != key.count('[') or right < left:
-                    raise RuntimeError("Bad preprocessing selector: "
-                                       "unmatched square brackets or closing bracket "
-                                       "found before opening bracket: {}".format(key))
-                selector = key[left:right + 1]
-                if selector.startswith('[not'):
-                    if (platform in selector) or (selector.replace(
-                            '[not', '')[:-1].strip() in platform):
-                        ok = False
-                else:
-                    if (platform not in selector) and (selector[1:-1] not in platform):
-                        ok = False
-                if ok:
-                    key = key.replace(selector, '').strip(' \n')
-            if ok:
-                out[key] = value
-    return out
-
-
-def _merge_dicts(a, b, path=None):
-    """
-    Merges b into a.
-    From: https://stackoverflow.com/a/7205107/13086629
-    """
-    if path is None:
-        path = []
-    for key in b:
-        if key in a:
-            if isinstance(a[key], dict) and isinstance(b[key], dict):
-                _merge_dicts(a[key], b[key], path + [str(key)])
-            elif a[key] == b[key]:
-                pass  # same leaf value
-            else:
-                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
-        else:
-            a[key] = b[key]
-    return a
-
-
-def _write_dict(d, file_handle, indent):
-    for key, value in d.items():
-        file_handle.write((" " * indent) + "- {}\n".format(key))
-        if isinstance(value, dict):
-            _write_dict(value, file_handle=file_handle, indent=indent + 2)
-
-
-def main(metafile, envfile, envname, channels, platform, extra, mergewith):
-
-    # Find current platform
-    if platform is None:
-        platform_mapping = {"Linux": "linux", "Darwin": "osx", "Windows": "win"}
-        platform = platform_mapping[_platform.system()]
-
-    # Read and parse metafile
-    with open(metafile, "r") as f:
-        metacontent = f.readlines()
-    meta = _parse_yaml(metacontent)
-
-    # Merge three dicts into one
-    meta_dependencies = meta["requirements:"]["build:"].copy()
-    reduce(
-        _merge_dicts,
-        [meta_dependencies, meta["requirements:"]["run:"], meta["test:"]["requires:"]])
-
-    # Read file with additional dependencies
-    if len(mergewith) > 0:
-        with open(mergewith, "r") as f:
-            mergecontent = f.readlines()
-        additional = _parse_yaml(mergecontent)
-        additional_dependencies = additional["dependencies:"]
-        _merge_dicts(meta_dependencies, additional_dependencies)
-        channels = set(channels + list(additional["channels:"].keys()))
-
-    # Add dependencies added via the command line
-    for e in extra:
-        if len(e) > 0:
-            meta_dependencies[e] = None
+    shell = sys.platform == 'win32'
 
     # Generate envname from output file name if name is not defined
     if len(envname) == 0:
         envname = os.path.splitext(envfile)[0]
 
-    # Apply Jinja syntax filtering depending on platform
-    meta_dependencies = _jinja_filter(meta_dependencies, platform=platform)
+    out = subprocess.check_output(
+        ['conda', 'debug', '--channel', 'conda-forge', condadir],
+        stderr=subprocess.STDOUT,
+        shell=shell).decode()
+    print(out)
 
-    # Write to output env file
+    lines = out.split('\n')
+    env_setup_file = None
+    for line in reversed(lines):
+        if all(x in line for x in ['cd', 'conda-bld', '&&']):
+            print(line)
+            env_setup_file = line.split()[-1]
+            break
+
+    if env_setup_file is None:
+        raise RuntimeError("No setup file found in output from conda debug.")
+    print(env_setup_file)
+
+    with open(env_setup_file, 'r') as f:
+        setup_file_contents = f.readlines()
+
+    history_file = None
+    for line in reversed(setup_file_contents):
+        if line.startswith('conda activate'):
+            print(line)
+            history_file = os.path.join(line.split()[-1].replace('"', ''), 'conda-meta',
+                                        'history')
+            break
+
+    if history_file is None:
+        raise RuntimeError("No history file found in the conda debug folder.")
+
+    with open(history_file, 'r') as f:
+        history = f.readlines()
+
     with open(envfile, "w") as out:
         out.write("##############################################################\n")
         out.write("# WARNING! This environment file was generated from the\n")
@@ -229,7 +101,12 @@ def main(metafile, envfile, envname, channels, platform, extra, mergewith):
         for channel in channels:
             out.write("  - {}\n".format(channel))
         out.write("dependencies:\n")
-        _write_dict(meta_dependencies, file_handle=out, indent=2)
+
+        for line in history:
+            if line.startswith('+'):
+                # Replace the first two '-' from the right with '='
+                package = '='.join(line.split('::')[-1].rsplit('-', 2))
+                out.write("  - {}".format(package))
 
 
 if __name__ == '__main__':
@@ -238,14 +115,11 @@ if __name__ == '__main__':
     channels = [args.channels]
     if "," in args.channels:
         channels = args.channels.split(",")
-    extra = [args.extra]
-    if "," in args.extra:
-        extra = args.extra.split(",")
+    # extra = [args.extra]
+    # if "," in args.extra:
+    #     extra = args.extra.split(",")
 
-    main(metafile=os.path.join(args.dir, args.meta_file),
+    main(condadir=args.dir,
          envfile=args.env_file,
          envname=args.env_name,
-         channels=channels,
-         platform=args.platform,
-         extra=extra,
-         mergewith=args.merge_with)
+         channels=channels)
