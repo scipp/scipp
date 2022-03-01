310f427e05b388c8abc2b20719b81676f2427e25
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-03-01 16:14:27 +0100

    Move variable binary op tests to arithmetic_test.py

--- tests/core/arithmetic_test.py
+++ tests/core/arithmetic_test.py
@@ -6,6 +6,238 @@ import numpy as np
 import scipp as sc
 
 
+def make_variables():
+    data = np.arange(1, 4, dtype=float)
+    a = sc.Variable(dims=['x'], values=data)
+    b = sc.Variable(dims=['x'], values=data)
+    a_slice = a['x', :]
+    b_slice = b['x', :]
+    return a, b, a_slice, b_slice, data
+
+
+# This check is important: It can happen that an implementation of,
+# e.g., __iadd__ does an in-place modification, updating `b`, but then the
+# return value is assigned to `a`, which could break the connection unless
+# the correct Python object is returned.
+def test_iadd_returns_original_object():
+    a = sc.scalar(1.2)
+    b = a
+    a += 1.0
+    assert sc.identical(a, b)
+
+
+def test_isub_returns_original_object():
+    a = sc.scalar(1.2)
+    b = a
+    a -= 1.0
+    assert sc.identical(a, b)
+
+
+def test_imul_returns_original_object():
+    a = sc.scalar(1.2)
+    b = a
+    a *= 1.0
+    assert sc.identical(a, b)
+
+
+def test_itruediv_returns_original_object():
+    a = sc.scalar(1.2)
+    b = a
+    a /= 1.0
+    assert sc.identical(a, b)
+
+
+def test_add_variable():
+    a, b, a_slice, b_slice, data = make_variables()
+    c = a + b
+    assert np.array_equal(c.values, data + data)
+    c = a + 2.0
+    assert np.array_equal(c.values, data + 2.0)
+    c = a + b_slice
+    assert np.array_equal(c.values, data + data)
+    c += b
+    assert np.array_equal(c.values, data + data + data)
+    c += b_slice
+    assert np.array_equal(c.values, data + data + data + data)
+    c = 3.5 + c
+    assert np.array_equal(c.values, data + data + data + data + 3.5)
+
+
+def test_sub_variable():
+    a, b, a_slice, b_slice, data = make_variables()
+    c = a - b
+    assert np.array_equal(c.values, data - data)
+    c = a - 2.0
+    assert np.array_equal(c.values, data - 2.0)
+    c = a - b_slice
+    assert np.array_equal(c.values, data - data)
+    c -= b
+    assert np.array_equal(c.values, data - data - data)
+    c -= b_slice
+    assert np.array_equal(c.values, data - data - data - data)
+    c = 3.5 - c
+    assert np.array_equal(c.values, 3.5 - data + data + data + data)
+
+
+def test_mul_variable():
+    a, b, a_slice, b_slice, data = make_variables()
+    c = a * b
+    assert np.array_equal(c.values, data * data)
+    c = a * 2.0
+    assert np.array_equal(c.values, data * 2.0)
+    c = a * b_slice
+    assert np.array_equal(c.values, data * data)
+    c *= b
+    assert np.array_equal(c.values, data * data * data)
+    c *= b_slice
+    assert np.array_equal(c.values, data * data * data * data)
+    c = 3.5 * c
+    assert np.array_equal(c.values, data * data * data * data * 3.5)
+
+
+def test_truediv_variable():
+    a, b, a_slice, b_slice, data = make_variables()
+    c = a / b
+    assert np.array_equal(c.values, data / data)
+    c = a / 2.0
+    assert np.array_equal(c.values, data / 2.0)
+    c = a / b_slice
+    assert np.array_equal(c.values, data / data)
+    c /= b
+    assert np.array_equal(c.values, data / data / data)
+    c /= b_slice
+    assert np.array_equal(c.values, data / data / data / data)
+    c = 2.0 / a
+    assert np.array_equal(c.values, 2.0 / data)
+
+
+def test_pow_variable():
+    a, b, a_slice, b_slice, data = make_variables()
+    c = a**b
+    assert np.array_equal(c.values, data**data)
+    c **= b
+    assert np.array_equal(c.values, (data**data)**data)
+    c = a**3
+    assert np.array_equal(c.values, data**3)
+    c **= 3
+    assert np.array_equal(c.values, (data**3)**3)
+    c = a**3.0
+    assert np.array_equal(c.values, data**3.0)
+    c **= 3.0
+    assert np.array_equal(c.values, (data**3.0)**3.0)
+    c = a**b_slice
+    assert np.array_equal(c.values, data**data)
+    c **= b_slice
+    assert np.array_equal(c.values, (data**data)**data)
+    c = 2**b
+    assert np.array_equal(c.values, 2**data)
+    c = 2.0**b
+    assert np.array_equal(c.values, 2.0**data)
+
+
+def test_bitwise_ior_variable_with_variable():
+    a = sc.scalar(False)
+    b = sc.scalar(True)
+    a |= b
+    assert sc.identical(a, sc.scalar(True))
+
+    a = sc.Variable(dims=['x'], values=np.array([False, True, False, True]))
+    b = sc.Variable(dims=['x'], values=np.array([False, False, True, True]))
+    a |= b
+    assert sc.identical(
+        a, sc.Variable(dims=['x'], values=np.array([False, True, True, True])))
+
+
+def test_bitwise_or_variable_with_variable():
+    a = sc.scalar(False)
+    b = sc.scalar(True)
+    assert sc.identical((a | b), sc.scalar(True))
+
+    a = sc.Variable(dims=['x'], values=np.array([False, True, False, True]))
+    b = sc.Variable(dims=['x'], values=np.array([False, False, True, True]))
+    assert sc.identical((a | b),
+                        sc.Variable(dims=['x'],
+                                    values=np.array([False, True, True, True])))
+
+
+def test_bitwise_iand_variable_with_variable():
+    a = sc.scalar(False)
+    b = sc.scalar(True)
+    a &= b
+    assert sc.identical(a, sc.scalar(False))
+
+    a = sc.Variable(dims=['x'], values=np.array([False, True, False, True]))
+    b = sc.Variable(dims=['x'], values=np.array([False, False, True, True]))
+    a &= b
+    assert sc.identical(
+        a, sc.Variable(dims=['x'], values=np.array([False, False, False, True])))
+
+
+def test_bitwise_and_variable_with_variable():
+    a = sc.scalar(False)
+    b = sc.scalar(True)
+    assert sc.identical((a & b), sc.scalar(False))
+
+    a = sc.Variable(dims=['x'], values=np.array([False, True, False, True]))
+    b = sc.Variable(dims=['x'], values=np.array([False, False, True, True]))
+    assert sc.identical((a & b),
+                        sc.Variable(dims=['x'],
+                                    values=np.array([False, False, False, True])))
+
+
+def test_ixor_variable_with_variable():
+    a = sc.scalar(False)
+    b = sc.scalar(True)
+    a ^= b
+    assert sc.identical(a, sc.scalar(True))
+
+    a = sc.Variable(dims=['x'], values=np.array([False, True, False, True]))
+    b = sc.Variable(dims=['x'], values=np.array([False, False, True, True]))
+    a ^= b
+    assert sc.identical(
+        a, sc.Variable(dims=['x'], values=np.array([False, True, True, False])))
+
+
+def test_xor_variable_with_variable():
+    a = sc.scalar(False)
+    b = sc.scalar(True)
+    assert sc.identical((a ^ b), sc.scalar(True))
+
+    a = sc.Variable(dims=['x'], values=np.array([False, True, False, True]))
+    b = sc.Variable(dims=['x'], values=np.array([False, False, True, True]))
+    assert sc.identical((a ^ b),
+                        sc.Variable(dims=['x'],
+                                    values=np.array([False, True, True, False])))
+
+
+def test_iadd_variable_with_scalar():
+    v = sc.Variable(dims=['x'], values=[10.0])
+    expected = sc.Variable(dims=['x'], values=[12.0])
+    v += 2
+    assert sc.identical(v, expected)
+
+
+def test_isub_variable_with_scalar():
+    v = sc.Variable(dims=['x'], values=[10.0])
+    expected = sc.Variable(dims=['x'], values=[9.0])
+    v -= 1
+    assert sc.identical(v, expected)
+
+
+def test_imul_variable_with_scalar():
+    v = sc.Variable(dims=['x'], values=[10.0])
+    expected = sc.Variable(dims=['x'], values=[30.0])
+    v *= 3
+    assert sc.identical(v, expected)
+
+
+def test_itruediv_variable_with_scalar():
+    v = sc.Variable(dims=['x'], values=[10.0])
+    expected = sc.Variable(dims=['x'], values=[5.0])
+    v /= 2
+    assert sc.identical(v, expected)
+
+
 def test_iadd_dataset_with_dataarray():
     da = sc.DataArray(
         data=sc.Variable(dims=['x'], values=np.arange(1.0, 10.0)),
