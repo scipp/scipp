3acc1751382bb67feee16f0e2b1bbd039cb4d087
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-03-04 15:24:46 +0100

    Add stub files for C++ classes

---
+++ src/scipp/core/cpp_classes.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+
+from .._scipp.core import Coords, DataArray, Dataset, DType, GroupByDataArray, \
+    GroupByDataset, Masks, Unit, Variable
+
+from .._scipp.core import BinEdgeError, BinnedDataError, CoordError, \
+    DataArrayError, DatasetError, DimensionError, \
+    DTypeError, SizeError, SliceError, \
+    UnitError, VariableError, VariancesError
