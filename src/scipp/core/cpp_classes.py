4a63654e4a947db8c5c76f5460fcdc8518bd635b
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-03-04 15:35:38 +0100

    Suppress flake8 'imported but unused'

--- src/scipp/core/cpp_classes.py
+++ src/scipp/core/cpp_classes.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
 
+# flake8: noqa: F401
+
 from .._scipp.core import Coords, DataArray, Dataset, DType, GroupByDataArray, \
     GroupByDataset, Masks, Unit, Variable
 
