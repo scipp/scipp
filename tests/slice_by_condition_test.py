235deeaa0854f85cc3eaae1de3f372c5c62f660e
Simon Heybrock <simon.heybrock@ess.eu>
2022-03-23 09:20:49 +0100

    Revert "Fix fixtures"
    This reverts commit 30ecbee1eb8bd32d9a67d6c105124f1498882c6d.

--- tests/slice_by_condition_test.py
+++ tests/slice_by_condition_test.py
@@ -1,60 +1,86 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+# @file
 # @author Simon Heybrock
 import pytest
 import scipp as sc
 
 
-def test_all_false_gives_empty_slice(sliceable):
+def make_var() -> sc.Variable:
+    v = sc.arange('dummy', 12, dtype='int64')
+    return v.fold(dim='dummy', sizes={'xx': 4, 'yy': 3})
+
+
+def make_array() -> sc.DataArray:
+    da = sc.DataArray(make_var())
+    da.coords['xx'] = sc.arange('xx', 4, dtype='int64')
+    da.coords['yy'] = sc.arange('yy', 3, dtype='int64')
+    return da
+
+
+def make_dataset() -> sc.Dataset:
+    ds = sc.Dataset()
+    ds['xy'] = make_array()
+    ds['x'] = ds.coords['xx']
+    return ds
+
+
+@pytest.mark.parametrize("obj", [make_var(), make_array(), make_dataset()])
+def test_all_false_gives_empty_slice(obj):
     condition = sc.array(dims=['xx'], values=[False, False, False, False])
-    assert sc.identical(sliceable[condition], sliceable['xx', 0:0])
+    assert sc.identical(obj[condition], obj['xx', 0:0])
 
 
-def test_all_true_gives_copy(sliceable):
+@pytest.mark.parametrize("obj", [make_var(), make_array(), make_dataset()])
+def test_all_true_gives_copy(obj):
     condition = sc.array(dims=['xx'], values=[True, True, True, True])
-    assert sc.identical(sliceable[condition], sliceable)
+    assert sc.identical(obj[condition], obj)
 
 
-def test_true_and_false_concats_slices(sliceable):
+@pytest.mark.parametrize("obj", [make_var(), make_array(), make_dataset()])
+def test_true_and_false_concats_slices(obj):
     condition = sc.array(dims=['xx'], values=[True, False, True, True])
-    assert sc.identical(sliceable[condition],
-                        sc.concat([sliceable['xx', 0], sliceable['xx', 2:]], 'xx'))
+    assert sc.identical(obj[condition], sc.concat([obj['xx', 0], obj['xx', 2:]], 'xx'))
 
 
-def test_non_dimension_coords_are_preserved(data_array_xx4_yy3):
-    da = data_array_xx4_yy3.copy()
+def test_non_dimension_coords_are_preserved():
+    da = make_array()
     da.coords['xx2'] = da.coords['xx']
     condition = sc.array(dims=['xx'], values=[True, False, True, True])
     assert sc.identical(da[condition].coords['xx2'],
                         sc.array(dims=['xx'], dtype='int64', values=[0, 2, 3]))
 
 
-def test_bin_edges_are_dropped(data_array_xx4_yy3):
-    da = data_array_xx4_yy3.copy()
+def test_bin_edges_are_dropped():
+    da = make_array()
     base = da.copy()
     da.coords['edges'] = sc.concat([da.coords['xx'], da.coords['xx'][-1] + 1], 'xx')
     condition = sc.array(dims=['xx'], values=[True, False, True, True])
     assert sc.identical(da[condition], sc.concat([base['xx', 0], base['xx', 2:]], 'xx'))
 
 
-def test_dataset_item_independent_of_condition_dim_preserved_unchanged(dataset_xx4_yy3):
+def test_dataset_item_independent_of_condition_dim_preserved_unchanged():
     condition = sc.array(dims=['yy'], values=[True, False, True])
-    assert sc.identical(dataset_xx4_yy3[condition]['x'], dataset_xx4_yy3['x'])
+    ds = make_dataset()
+    assert sc.identical(ds[condition]['x'], ds['x'])
 
 
-def test_non_boolean_condition_raises_DTypeError(variable_xx4_yy3):
+def test_non_boolean_condition_raises_DTypeError():
+    var = make_var()
     with pytest.raises(sc.DTypeError):
-        condition = (variable_xx4_yy3 < 3).to(dtype='int32')
-        variable_xx4_yy3[condition]
+        condition = (var < 3).to(dtype='int32')
+        var[condition]
 
 
-def test_2d_condition_raises_DimensionError(variable_xx4_yy3):
+def test_2d_condition_raises_DimensionError():
+    var = make_var()
     with pytest.raises(sc.DimensionError):
-        condition = variable_xx4_yy3 < 3
-        variable_xx4_yy3[condition]
+        condition = var < 3
+        var[condition]
 
 
-def test_0d_condition_raises_DimensionError(variable_xx4_yy3):
+def test_0d_condition_raises_DimensionError():
+    var = make_var()
     with pytest.raises(sc.DimensionError):
         condition = sc.scalar(False)
-        variable_xx4_yy3[condition]
+        var[condition]
