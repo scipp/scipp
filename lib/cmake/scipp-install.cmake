9da64e7d511fb1cd2226c3231a460bf20557527c
SimonHeybrock <SimonHeybrock@users.noreply.github.com>
2022-03-28 09:38:26 +0000

    Apply automatic formatting

--- lib/cmake/scipp-install.cmake
+++ lib/cmake/scipp-install.cmake
@@ -14,10 +14,14 @@ function(scipp_install_component)
       TARGETS ${SCIPP_INSTALL_COMPONENT_TARGET}
       EXPORT ${EXPORT_NAME}
       RUNTIME_DEPENDENCIES
-      PRE_INCLUDE_REGEXES "tbb"
-      PRE_EXCLUDE_REGEXES ".*"
+      PRE_INCLUDE_REGEXES
+      "tbb"
+      PRE_EXCLUDE_REGEXES
+      ".*"
       # Required for Windows. Other platforms search rpath
-      DIRECTORIES ${CMAKE_BINARY_DIR}/lib/onetbb/lib ${CMAKE_BINARY_DIR}/lib/onetbb/bin
+      DIRECTORIES
+      ${CMAKE_BINARY_DIR}/lib/onetbb/lib
+      ${CMAKE_BINARY_DIR}/lib/onetbb/bin
       RUNTIME DESTINATION ${PYTHONDIR}
       ARCHIVE DESTINATION ${ARCHIVEDIR}
       FRAMEWORK DESTINATION ${ARCHIVEDIR}
