bff1709cdb00e1b5d0702bb23051da299bfdf733
Gregory Tucker <gregory.tucker@ess.eu>
2022-04-29 10:39:13 +0200

    [Fix] format to work in more cases
    - Initial testing did not cover the default case where no format
      specification is provided, which raises an error before this commit.
      Also not checked were cases where floating-point representation causes
      a rounded number to have following zeros with a very small deviation
      from the true rounded value.
    - A check for 'None' specification or zero-length specification, and a
      switch to `__repr__` as fallback formatter allows the default case to
      work.
      Explicitly building a format string for each rounded value solves the
      problem caused by floating point representation.

--- src/scipp/format/formatter.py
+++ src/scipp/format/formatter.py
@@ -1,7 +1,23 @@
 from __future__ import annotations
+from typing import Union
 
 from ..logging import get_logger
 from ..typing import VariableLike
+
+def _dtype_lut():
+    from .._scipp.core import DType as d
+    # For types understood by numpy we do not actually need this special
+    # handling, but will do as we add support for other types such as
+    # variable-length strings.
+    dtypes = [
+        d.float64, d.float32, d.int64, d.int32, d.bool, d.datetime64, d.string,
+        d.Variable, d.DataArray, d.Dataset, d.VariableView, d.DataArrayView,
+        d.DatasetView, d.vector3, d.linear_transform3, d.affine_transform3,
+        d.translation3, d.rotation3
+    ]
+    names = [str(dtype) for dtype in dtypes]
+    return dict(zip(names, dtypes))
+
 
 class DatasetFormatter:
     @staticmethod
@@ -21,8 +37,8 @@ class VariableFormatter:
     @staticmethod
     def format(data, spec):
         dtype = str(data.dtype)
-        if not any([x in dtype for x in ('float', 'int')]):
-            return f"{data}"
+        if not any([x in dtype for x in ('float', 'int')]) or spec is None or len(spec) < 1:
+            return data.__repr__()
         compact = spec[-1] == 'c'
         is_scalar = False if data.shape else True
         val = data.value if is_scalar else data.values
@@ -41,14 +57,16 @@ class VariableFormatter:
             if p > -1:
                 es *= pp
                 vs = vs.astype('int')
-            return f"{', '.join([f'{v}({e})' for v,e in zip(vs, es)])} {data.unit}"
+            specs = ['d' if x > -1 else f'0.{int(-x):d}f' for x in p]
+            fvs = ["{v:{x}}".format(v=v, x=spec) for v, spec in zip(vs, specs)]
+            return f"{', '.join([f'{v}({e})' for v,e in zip(fvs, es)])} {data.unit}"
         # elif is_scalar:
         #     if compact:
         #         spec[-1] = 'f' if '.' in spec else 'd'
         #     # how can we pass the format specification into the C++ object?
 
         # punt (for now)
-        return f"{data}"
+        return data.__repr__()
 
 
 class Formatter:
