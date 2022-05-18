8e8f6375e075c2de0913aef56e12278a7db1e863
Simon Heybrock <simon.heybrock@ess.eu>
2022-05-18 13:41:35 +0200

    Update versions

--- tools/release/prepare-release.py
+++ tools/release/prepare-release.py
@@ -20,13 +20,8 @@ def make_version_select(root):
     with open(f'{root}/tools/release/templates/launchbuttons.html', 'r') as f:
         contents = f.read()
 
-    versions = [
-        '0.13', '0.12', '0.11', '0.10', '0.9', '0.8', '0.7', '0.6', '0.5', '0.4', '0.3'
-    ]
-    targets = [
-        '0.13.0', '0.12.1', '0.11.1', '0.10.1', '0.9.0', '0.8.4', '0.7.1', '0.6.1',
-        '0.5.0', '0.4.0', '0.3.0'
-    ]
+    versions = ['0.14', '0.13', '0.12', '0.11', '0.10', '0.9', '0.8']
+    targets = ['0.14.0', '0.13.1', '0.12.1', '0.11.1', '0.10.1', '0.9.0', '0.8.4']
 
     entries = []
     for version, target in zip(versions, targets):
