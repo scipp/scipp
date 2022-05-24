9a066c7e85de10fe2f9841088e69d02fe59c5271
Neil Vaytet <neil.vaytet@esss.se>
2022-05-24 16:55:48 +0200

    cleanup

--- src/scipp/html/table.py
+++ src/scipp/html/table.py
@@ -16,7 +16,7 @@ def _string_in_cell(v):
 def _make_groups(obj, attrs):
     out = [obj.coords, obj]
     if attrs:
-        out.append(attrs)
+        out.append(obj[list(obj.keys())[0]].attrs)
     return out
 
 
@@ -26,23 +26,10 @@ def _make_row(obj, attrs, ind=0):
     for group in _make_groups(obj, attrs=attrs):
         for var in group.values():
             out += _string_in_cell(var[0])
-            # v = var[0]
-            # out += f'<td>{value_to_string(v.value)}'
-            # if v.variance is not None:
-            #     out += f'&plusmn;{value_to_string(v.variance)}'
-            # out += '</td>'
     return out
 
 
 def _empty_strings_or_values(group):
-    # out = {}
-    # for key, var in group.items():
-    #     if group.is_edges(key):
-    #         need_extra_row = True
-    #         out[key] = var[-1:]
-    #     else:
-    #         out[key] = sc.array(dims=var.dims, values=[""])
-    # return out
     return [
         _string_in_cell(var[-1]) if group.is_edges(key) else '<td></td>'
         for key, var in group.items()
@@ -87,75 +74,16 @@ def table(obj, max_rows=20):
         else:
             out += f'<tr>{_make_row(obj=obj[i:i+1], attrs=attrs)}</tr>'
 
-    # need_extra_row = False
-    # bin_edge_row = ['<td></td>'] * ncols
+    # Maybe add an extra row if there are bin edges in the coords or attrs
     empty_cell = '<td></td>'
-
     bin_edge_coords = _empty_strings_or_values(obj.coords)
     bin_edge_attrs = _empty_strings_or_values(attrs)
-
-    # [
-    #     _string_in_cell(var[-1]) if obj.coords.is_edges(key) else '<td></td>'
-    #     for key, var in obj.coords.items()
-    # ]
-
-    # bin_edge_attrs = [
-    #     _string_in_cell(var[-1]) if attrs.is_edges(key) else '<td></td>'
-    #     for key, var in attrs.items()
-    # ]
-
     extra_row = "".join(bin_edge_coords + ([empty_cell] * len(obj.keys())) +
                         bin_edge_attrs)
-
     if len(extra_row) > len(empty_cell) * ncols:
         out += f'<tr>{extra_row}</tr>'
 
-    print(extra_row)
-
-    #     if obj.coords.is_edges(key):
-    #         bin_edge_row[ind] = _string_in_cell(var[-1])
-    #     ind += 1
-
-    # ind = 0
-    # for key, var in obj.coords.items():
-    #     if obj.coords.is_edges(key):
-    #         bin_edge_row[ind] = _string_in_cell(var[-1])
-    #     ind += 1
-    # for key, var in obj.coords.items():
-    #     if obj.coords.is_edges(key):
-    #         bin_edge_row[ind] = _string_in_cell(var[-1])
-    #     ind += 1
-
-    # need_extra_row = False
-    # for group in (obj.coords, attrs):
-    #     for key in group:
-    #         if group.is_edges(key):
-    #             need_extra_row = True
-    # if need_extra_row:
-    #     bin_edge = Dataset(data={key: var.data for key, var in last_row.items()})
-    #     out += f'<tr>{_make_row(obj=obj[-1:], attrs=attrs, ind=1)}</tr>'
-
-    # #     bin_edge.coords[key] =
-    # # , **attrs}.items():
-
-    # # bin_edge_row = False
-
-    # last_row = obj[-1:]
-    # bin_edge = Dataset(data={key: var.data for key, var in last_row.items()},
-    #     coords=_empty_strings_or_values(last_row.coords))
-
-    # for key, var in last_row.coords.items():
-    #     if last_row.coords.is_edges(key):
-    #         need_extra_row = True
-    #         bin_edge.coords[key] = var[1:]
-    #     else:
-    #         bin_edge.coords[key] = sc.array(dims=var.dims, values=[""])
-
-    #     bin_edge.coords[key] =
-    # , **attrs}.items():
-
     out += '</table>'
 
-    # return out
     from IPython.display import display, HTML
     display(HTML(out))
