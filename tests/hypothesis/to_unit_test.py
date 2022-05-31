015332a6925f71b90813f15cf55491b3a9fc5d02
Simon Heybrock <simon.heybrock@ess.eu>
2022-05-31 14:26:06 +0200

    pytest.approx has hidden default of `abs=1e-12`

--- tests/hypothesis/to_unit_test.py
+++ tests/hypothesis/to_unit_test.py
@@ -14,26 +14,26 @@ import scipp as sc
 
 @given(st.floats(min_value=1e-300, max_value=1e300))
 def test_to_unit_value_1(x):
-    unit = sc.Unit(f'{x:.12E}')
+    unit = sc.Unit(f'{x:.14E}')
     var = sc.scalar(x, unit='')
     result = sc.to_unit(var, unit)
     assert result.unit == unit
-    assert pytest.approx(result.value, 1e-14) == 1.0
+    assert pytest.approx(result.value, abs=0.0, rel=1e-14) == 1.0
 
 
 @given(st.floats(min_value=1e-300, max_value=1e300))
 def test_to_unit_small_value(x):
-    unit = sc.Unit(f'{x:.12E}')
+    unit = sc.Unit(f'{x:.14E}')
     var = sc.scalar(1.2345e-6 * x, unit='')
     result = sc.to_unit(var, unit)
     assert result.unit == unit
-    assert pytest.approx(result.value, 1e-20) == 1.2345e-6
+    assert pytest.approx(result.value, abs=0.0, rel=1e-14) == 1.2345e-6
 
 
 @given(st.floats(min_value=1e-300, max_value=1e300))
 def test_to_unit_large_value(x):
-    unit = sc.Unit(f'{x:.12E}')
+    unit = sc.Unit(f'{x:.14E}')
     var = sc.scalar(1.2345e6 * x, unit='')
     result = sc.to_unit(var, unit)
     assert result.unit == unit
-    assert pytest.approx(result.value, 1e-8) == 1.2345e6
+    assert pytest.approx(result.value, abs=0.0, rel=1e-14) == 1.2345e6
