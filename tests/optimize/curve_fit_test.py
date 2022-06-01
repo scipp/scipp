37f793dcf040054ee7fba5e4e08d918ed4689f1c
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-06-01 14:25:51 +0200

    Raise ValueError if variance is 0 in curve_fit

--- tests/optimize/curve_fit_test.py
+++ tests/optimize/curve_fit_test.py
@@ -64,6 +64,14 @@ def test_should_raise_TypeError_when_sigma_given_as_param():
         curve_fit(func, array1d(), sigma=np.arange(4))
 
 
+def test_should_raise_ValueError_when_sigma_contains_zeros():
+    da = array1d(size=50)
+    da.variances = np.random.default_rng().normal(0.0, 0.1, size=50)
+    da['xx', 21].variance = 0.0
+    with pytest.raises(ValueError):
+        curve_fit(func, da)
+
+
 def test_should_raise_KeyError_when_data_array_has_no_coord():
     da = array1d()
     del da.coords[da.dim]
