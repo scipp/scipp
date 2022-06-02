32fb0c46aa32f763770473cb8f64b33f80ee24c6
Gregory Tucker <gregory.tucker@ess.eu>
2022-06-02 18:18:45 +0200

    [Refactor] Move digit handling out of format function
    - The digit handling to determine precision was too opaque and could
      benefit from being held in a separate function with commenting.
    - Two new local-scope functions handle rouding to a precision specified
      by a provided error and formatting a value and error to that
      precision.
    - The addition of comments in the broken-out code hopefully improves the
      ability to follow the logic but may be too verbose. Other developers
      may prefer less comments in the future.

--- src/scipp/format/formatter.py
+++ src/scipp/format/formatter.py
@@ -1,9 +1,13 @@
+from numpy import ndarray
+
+
 def format_variable(data, spec):
     """
     String formats the Variable according to the provided specification.
+
     Parameters
     ----------
-    obj
+    data
         A scalar or array-like scipp Variable object
     spec
         Format specification; only 'c' for Compact error-reporting supported at present
@@ -12,37 +16,64 @@ def format_variable(data, spec):
     -------
     The formatted string
     """
-    from scipp import Unit
+    from ..core.cpp_classes import Unit
+    from numpy import array
     dtype = str(data.dtype)
     if not any([x in dtype for x in ('float', 'int')]) or spec is None or len(spec) < 1:
         return data.__repr__()
     compact = spec[-1] == 'c'
-    is_scalar = False if data.shape else True
-    val = data.value if is_scalar else data.values
-    var = data.variance if is_scalar else data.variances
+
+    val = data.values if data.shape else array((data.value, ))
+    var = data.variances if data.shape else array((data.variance, ))
     unt = "" if data.unit == Unit('dimensionless') else f" {data.unit}"
-    if compact and var is not None:
-        from numpy import floor, log10, round, array, sqrt, power
-        if is_scalar:
-            val = array((val, ))
-            var = array((var, ))
-        err = sqrt(var)
-        p = floor(log10(err))
-        p[round(err * power(10., -p)).astype('int') == 1] -= 1
-        np, pp = power(10., -p), power(10., p)
-        es = round(err * np).astype('int')
-        vs = round(val * np) * pp
-        if p > -1:
-            es *= pp.astype('int')
-            vs = vs.astype('int')
-        specs = ['d' if x > -1 else f'0.{int(-x):d}f' for x in p]
-        fvs = ["{v:{x}}".format(v=v, x=spec) for v, spec in zip(vs, specs)]
-        return f"{', '.join([f'{v}({e})' for v,e in zip(fvs, es)])}{unt}"
+
+    if compact and not (var is None or any([x is None for x in var])):
+        from numpy import sqrt
+        return f"{_format_to(*_round_to(val, sqrt(var)))}{unt}"
     elif compact:
-        if is_scalar:
-            from numpy import array
-            val = array((val, ))
         return f"{', '.join([f'{v}' for v in val])}{unt}"
 
     # punt (for now)
     return data.__repr__()
+
+
+def _round_to(values: ndarray, errors: ndarray):
+    from numpy import floor, log10, round, power, any
+    # Determine how many digits before (+) or after (-) the decimal place
+    # the error(s) allow for one-digit uncertainty of precision
+    precision = floor(log10(errors))
+
+    # By convention, if the first digit of the error rounds to 1
+    # add an extra digit of precision, so there are two-digits of uncertainty
+    precision[round(errors * power(10., -precision)) == 1] -= 1
+
+    # Build powers of ten to enable rounding to the specified precision
+    negative_power = power(10., -precision)
+    positive_power = power(10., precision)
+
+    # Round the error(s), keeping the shifted value(s) for the compact string
+    errors = round(errors * negative_power).astype('int')
+    # Round the value(s), shifting back after rounding
+    values = round(values * negative_power) * positive_power
+
+    # If the precision is greater than that of 0.1
+    greater = precision > -1
+    if any(greater):
+        # pad the error(s) to have the right number of trailing zeros
+        errors[greater] *= positive_power[greater].astype('int')
+
+    return values, errors, precision
+
+
+def _format_to(values: ndarray, errors: ndarray, precision: ndarray):
+    # positive precisions should have no decimals, accomplished by '0.0f'
+    formats = [f'0.{max(0, int(-p)):d}f' for p in precision]
+
+    # format the values to the required precision
+    formatted = ["{v:{s}}".format(v=v, s=s) for v, s in zip(values, formats)]
+
+    # and add on the formatted uncertainties
+    formatted = [f'{v}({e})' for v, e in zip(formatted, errors)]
+
+    # concatenate multiple values
+    return ', '.join(formatted)
--- tests/format/format_test.py
+++ tests/format/format_test.py
@@ -1,32 +1,48 @@
 import scipp as sc
 import numpy as np
 
-sets = {
-  (100, None, 's', '100 s'),
-  (100., 1., 'm', '100.0(10) m'),
-  (100., 2., '1', '100(2)'),
-  (100., 10., 'counts', '100(10) counts'),
-  (100., 100., 'us', '100(100) µs'),
-  (0.01, 0.001, 'angstrom', '0.0100(10) Å'),
-  (0.01, 0.002, 'cm', '0.010(2) cm'),
-  (np.pi, 0.00003, 'rad', '3.14159(3) rad'),
-  # default rounding rules for half:
-  (234.567, 1.25, 'km', '234.6(12) km'),  # even + 0.5 -> even
-  (234.567, 1.35, 'km', '234.6(14) km'),  # odd + 0.5 -> even (odd + 1)
-}
 
-
-def measure(value, error, unit):
+def scalar_string(value, error, unit):
     if error is None:
-        return sc.scalar(value=value, unit=unit)
-    return sc.scalar(value=value, variance=error**2, unit=unit)
+        scalar = sc.scalar(value=value, unit=unit)
+    else:
+        scalar = sc.scalar(value=value, variance=error**2, unit=unit)
+    return f"{scalar:c}"
 
 
-def check_string(value, error, unit, expected):
-    got = f"{measure(value, error, unit):c}"
-    assert got in expected and expected in got
+def vector_string(value, error, unit):
+    if error is None:
+        vector = sc.array(values=value, unit=unit, dims=['x'])
+    else:
+        vector = sc.array(values=value, variances=error**2, unit=unit, dims=['x'])
+    return f"{vector:c}"
 
 
-def test_scalar_sets():
-    for s in sets:
-        check_string(*s)
+def test_scalar_variables():
+    scalar_variables = [
+      (100, None, 's', '100 s'),
+      (100., 1., 'm', '100.0(10) m'),
+      (100., 2., '1', '100(2)'),
+      (100., 10., 'counts', '100(10) counts'),
+      (100., 100., 'us', '100(100) µs'),
+      (0.01, 0.001, 'angstrom', '0.0100(10) Å'),
+      (0.01, 0.002, 'cm', '0.010(2) cm'),
+      (np.pi, 0.00003, 'rad', '3.14159(3) rad'),
+      # default rounding rules for half:
+      (234.567, 1.25, 'km', '234.6(12) km'),  # even + 0.5 -> even
+      (234.567, 1.35, 'km', '234.6(14) km'),  # odd + 0.5 -> even (odd + 1)
+    ]
+    for value, error, unit, expected in scalar_variables:
+        assert scalar_string(value, error, unit) == expected
+
+
+def test_vector_variables():
+    from numpy import array
+    vector_variables = [
+      (array([100, 20, 3]), None, 's', '100, 20, 3 s'),
+      (array([100., 20.]), array([1., 2.]), 'm', '100.0(10), 20(2) m'),
+      (array([9000., 800., 70., 6.]), array([100., 20., 3., 0.4]), '1',
+       '9000(100), 800(20), 70(3), 6.0(4)')
+    ]
+    for value, error, unit, expected in vector_variables:
+        assert vector_string(value, error, unit) == expected
