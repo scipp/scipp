b3b3d0c0422ce6e7a2a695c332c7ecfcfa8e0a8b
Simon Heybrock <simon.heybrock@ess.eu>
2022-06-10 07:29:57 +0200

    Fix test flakyness and search critical param regions better

--- tests/hypothesis/to_unit_test.py
+++ tests/hypothesis/to_unit_test.py
@@ -14,32 +14,53 @@ import scipp as sc
 _max_examples = 1000
 
 
-@given(st.floats(min_value=1e-300, max_value=1e300))
 @settings(max_examples=_max_examples)
-def test_to_unit_value_1(x):
+def _to_unit_value_one(x):
     unit = sc.Unit(f'{x:.14E}')
     var = sc.scalar(x, unit='')
     result = sc.to_unit(var, unit)
     assert result.unit == unit
     # to_unit is rounding to 1e-12 so generally we cannot expect more precision
-    assert pytest.approx(result.value, abs=0.0, rel=1e-12) == 1.0
+    assert pytest.approx(result.value, abs=0.0, rel=1.1e-12) == 1.0
 
 
-@given(st.floats(min_value=1e-300, max_value=1e300))
+test_to_unit_value_one_1 = given(st.floats(min_value=1e-19,
+                                           max_value=1e-17))(_to_unit_value_one)
+test_to_unit_value_one_2 = given(st.floats(min_value=1e-30,
+                                           max_value=1e30))(_to_unit_value_one)
+test_to_unit_value_one_3 = given(st.floats(min_value=1e-300,
+                                           max_value=1e300))(_to_unit_value_one)
+
+
 @settings(max_examples=_max_examples)
-def test_to_unit_small_value(x):
+def _to_unit_small_value(x):
     unit = sc.Unit(f'{x:.14E}')
     var = sc.scalar(1.2345e-6 * x, unit='')
     result = sc.to_unit(var, unit)
     assert result.unit == unit
-    assert pytest.approx(result.value, abs=0.0, rel=1e-12) == 1.2345e-6
+    assert pytest.approx(result.value, abs=0.0, rel=1.1e-12) == 1.2345e-6
 
 
-@given(st.floats(min_value=1e-300, max_value=1e300))
+test_to_unit_small_value_1 = given(st.floats(min_value=1e-19,
+                                             max_value=1e-17))(_to_unit_small_value)
+test_to_unit_small_value_2 = given(st.floats(min_value=1e-30,
+                                             max_value=1e30))(_to_unit_small_value)
+test_to_unit_small_value_3 = given(st.floats(min_value=1e-300,
+                                             max_value=1e300))(_to_unit_small_value)
+
+
 @settings(max_examples=_max_examples)
-def test_to_unit_large_value(x):
+def _to_unit_large_value(x):
     unit = sc.Unit(f'{x:.14E}')
     var = sc.scalar(1.2345e6 * x, unit='')
     result = sc.to_unit(var, unit)
     assert result.unit == unit
-    assert pytest.approx(result.value, abs=0.0, rel=1e-12) == 1.2345e6
+    assert pytest.approx(result.value, abs=0.0, rel=1.1e-12) == 1.2345e6
+
+
+test_to_unit_large_value_1 = given(st.floats(min_value=1e-19,
+                                             max_value=1e-17))(_to_unit_large_value)
+test_to_unit_large_value_2 = given(st.floats(min_value=1e-30,
+                                             max_value=1e30))(_to_unit_large_value)
+test_to_unit_large_value_3 = given(st.floats(min_value=1e-300,
+                                             max_value=1e300))(_to_unit_large_value)
