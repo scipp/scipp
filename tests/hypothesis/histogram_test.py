91e7b8a8ad4288e4497b66faba2a4e3f31aeabad
Simon Heybrock <simon.heybrock@ess.eu>
2022-06-13 13:28:58 +0200

    Add histogram and bin tests with random bounds

---
+++ tests/hypothesis/histogram_test.py
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+# @author Simon Heybrock
+from hypothesis import given
+from hypothesis import strategies as st
+
+import scipp as sc
+import numpy as np
+
+float_args = dict(min_value=-1e300,
+                  max_value=1e300,
+                  allow_nan=False,
+                  allow_infinity=False,
+                  allow_subnormal=False)
+
+
+@given(st.floats(**float_args), st.floats(**float_args))
+def test_histogram_linspace_bounds(a, b):
+    x = sc.array(dims=['row'], values=[a, b])
+    table = sc.DataArray(sc.ones(dims=['row'], shape=[2]))
+    table.coords['x'] = x
+    upper = x.max()
+    upper.value = np.nextafter(upper.value, np.inf)
+    edges = sc.linspace('x', x.min(), upper, num=3)
+    hist = sc.histogram(table, bins=edges)
+    assert hist.sum().value == 2
