2026df7c45b5168f55f262791129532de569e93f
Simon Heybrock <simon.heybrock@ess.eu>
2022-06-14 13:52:04 +0200

    Add C++ test for linspace binning

--- lib/dataset/test/histogram_test.cpp
+++ lib/dataset/test/histogram_test.cpp
@@ -379,14 +379,14 @@ TEST_F(Histogram2DTest, noncontiguous_slice) {
 TEST(HistogramLinspaceTest, event_mapped_to_correct_bin) {
   const auto val10 = makeVariable<double>(Dims{Dim::X}, Shape{2}, Values{1, 0});
   const auto val01 = makeVariable<double>(Dims{Dim::X}, Shape{2}, Values{0, 1});
+  const Dimensions dims(Dim::Row, 1);
+  const auto data = makeVariable<double>(dims, Values{1.0});
   for (auto step = 1.23546e-6; step < 1e4; step *= 1.004354345) {
     const auto edges = makeVariable<double>(
         Dims{Dim::X}, Shape{3}, Values{1.0 * step, 2.0 * step, 3.0 * step});
     const auto mid = edges.values<double>()[1];
     for (const auto pos :
          {mid, std::nextafter(mid, 0.0), std::nextafter(mid, 1e30)}) {
-      const Dimensions dims(Dim::Row, 1);
-      const auto data = makeVariable<double>(dims, Values{1.0});
       const auto x = makeVariable<double>(dims, Values{pos});
       const DataArray da(data, {{Dim::X, x}});
       const auto hist = histogram(da, edges);
@@ -402,14 +402,14 @@ TEST(HistogramLinspaceTest, event_mapped_to_correct_bin) {
 TEST(HistogramLinspaceTest, event_mapped_to_correct_bin_at_begin) {
   const auto val00 = makeVariable<double>(Dims{Dim::X}, Shape{2}, Values{0, 0});
   const auto val10 = makeVariable<double>(Dims{Dim::X}, Shape{2}, Values{1, 0});
+  const Dimensions dims(Dim::Row, 1);
+  const auto data = makeVariable<double>(dims, Values{1.0});
   for (auto step = 1.23546e-6; step < 1e4; step *= 1.004354345) {
     const auto edges = makeVariable<double>(
         Dims{Dim::X}, Shape{3}, Values{1.0 * step, 2.0 * step, 3.0 * step});
     const auto begin = edges.values<double>()[0];
     for (const auto pos :
          {begin, std::nextafter(begin, 0.0), std::nextafter(begin, 1e30)}) {
-      const Dimensions dims(Dim::Row, 1);
-      const auto data = makeVariable<double>(dims, Values{1.0});
       const auto x = makeVariable<double>(dims, Values{pos});
       const DataArray da(data, {{Dim::X, x}});
       const auto hist = histogram(da, edges);
@@ -425,14 +425,14 @@ TEST(HistogramLinspaceTest, event_mapped_to_correct_bin_at_begin) {
 TEST(HistogramLinspaceTest, event_mapped_to_correct_bin_at_end) {
   const auto val00 = makeVariable<double>(Dims{Dim::X}, Shape{2}, Values{0, 0});
   const auto val01 = makeVariable<double>(Dims{Dim::X}, Shape{2}, Values{0, 1});
+  const Dimensions dims(Dim::Row, 1);
+  const auto data = makeVariable<double>(dims, Values{1.0});
   for (auto step = 1.23546e-6; step < 1e4; step *= 1.004354345) {
     const auto edges = makeVariable<double>(
         Dims{Dim::X}, Shape{3}, Values{1.0 * step, 2.0 * step, 3.0 * step});
     const auto end = edges.values<double>()[2];
     for (const auto pos :
          {end, std::nextafter(end, 0.0), std::nextafter(end, 1e30)}) {
-      const Dimensions dims(Dim::Row, 1);
-      const auto data = makeVariable<double>(dims, Values{1.0});
       const auto x = makeVariable<double>(dims, Values{pos});
       const DataArray da(data, {{Dim::X, x}});
       const auto hist = histogram(da, edges);
