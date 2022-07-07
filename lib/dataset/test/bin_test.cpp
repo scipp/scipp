5003cd86276bcc9c49c22d33781c6986b6037535
Simon Heybrock <simon.heybrock@ess.eu>
2022-07-07 10:43:20 +0200

    Improve test

--- lib/dataset/test/bin_test.cpp
+++ lib/dataset/test/bin_test.cpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 
 #include "dataset_test_common.h"
+#include "random.h"
 
 #include "scipp/dataset/bin.h"
 #include "scipp/dataset/bins.h"
@@ -11,6 +12,7 @@
 #include "scipp/dataset/string.h"
 #include "scipp/variable/arithmetic.h"
 #include "scipp/variable/comparison.h"
+#include "scipp/variable/creation.h"
 #include "scipp/variable/reduction.h"
 #include "scipp/variable/util.h"
 
@@ -673,8 +675,14 @@ TEST(BinLinspaceTest, event_mapped_to_correct_bin_at_end) {
 }
 
 TEST(BinLinspaceTest, many_events_many_bins) {
-  auto da = make_table(9000000);
-  da.setSlice(Slice{}, makeVariable<double>(Values{1.0}, Variances{1.0}));
-  const auto x = linspace(0.0 * units::one, 1.0 * units::one, Dim::X, 70000);
-  EXPECT_EQ(sum(bin(da, {x}).data()), sum(da.data()));
+  // 9M, 70k
+  Random rand(0.0, 1.0);
+  rand.seed(0);
+  const Dimensions dims(Dim::Row, 9000000);
+  const auto data = variable::ones(dims, units::one, dtype<double>);
+  const auto x = makeVariable<double>(dims, Values(rand(dims.volume())));
+  auto da = DataArray(data, {{Dim::X, x}});
+  const auto edges =
+      linspace(0.0 * units::one, 1.0 * units::one, Dim::X, 70000);
+  EXPECT_EQ(sum(bin(da, {edges}).data()), sum(da.data()));
 }
