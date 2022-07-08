a8d58013ed58054a36c4d635b9e63d404f734a50
Simon Heybrock <simon.heybrock@ess.eu>
2022-07-08 11:21:26 +0200

    More tests

--- tests/lookup_test.py
+++ tests/lookup_test.py
@@ -5,8 +5,44 @@ import pytest
 import scipp as sc
 
 
+@pytest.mark.parametrize("dtype", ['bool', 'int32', 'int64', 'float32', 'float64'])
+def test_getitem(dtype):
+    x_lin = sc.linspace(dim='xx', start=0, stop=1, num=4)
+    x = x_lin.copy()
+    x.values[0] -= 0.01
+    data = sc.array(dims=['xx'], values=[0, 1, 0], dtype=dtype)
+    hist_lin = sc.DataArray(data=data, coords={'xx': x_lin})
+    hist = sc.DataArray(data=data, coords={'xx': x})
+    var = sc.array(dims=['event'], values=[0.1, 0.4, 0.1, 0.6, 0.9, 0.2])
+    lut = sc.lookup(hist, 'xx')
+    expected = sc.array(dims=['event'], values=[0, 1, 0, 1, 0, 0], dtype=dtype)
+    assert sc.identical(lut[var], expected)
+    lut = sc.lookup(hist_lin, 'xx')
+    assert sc.identical(lut[var], expected)
+
+
 @pytest.mark.parametrize('mode', ['nearest', 'previous'])
 def test_raises_with_histogram_if_mode_set(mode):
     da = sc.DataArray(sc.arange('x', 4), coords={'x': sc.arange('x', 5)})
     with pytest.raises(ValueError):
         sc.lookup(da, mode=mode)
+
+
+@pytest.mark.parametrize("dtype", ['bool', 'int32', 'int64', 'float32', 'float64'])
+def test_previous(dtype):
+    x = sc.linspace(dim='xx', start=0, stop=1, num=4)
+    data = sc.array(dims=['xx'], values=[0, 1, 0, 2], dtype=dtype)
+    da = sc.DataArray(data=data, coords={'xx': x})
+    var = sc.array(dims=['event'], values=[0.1, 0.4, 0.1, 0.6, 0.9, 1.1, 0.2])
+    expected = sc.array(dims=['event'], values=[0, 1, 0, 1, 0, 2, 0], dtype=dtype)
+    assert sc.identical(sc.lookup(da, mode='previous')(var), expected)
+
+
+@pytest.mark.parametrize("dtype", ['bool', 'int32', 'int64', 'float32', 'float64'])
+def test_nearest(dtype):
+    x = sc.linspace(dim='xx', start=0, stop=1, num=5)
+    data = sc.array(dims=['xx'], values=[0, 1, 0, 2, 2], dtype=dtype)
+    da = sc.DataArray(data=data, coords={'xx': x})
+    var = sc.array(dims=['event'], values=[0.1, 0.4, 0.1, 0.6, 0.9, 1.1, 0.2])
+    expected = sc.array(dims=['event'], values=[0, 0, 0, 0, 2, 2, 1], dtype=dtype)
+    assert sc.identical(sc.lookup(da, mode='nearest')(var), expected)
