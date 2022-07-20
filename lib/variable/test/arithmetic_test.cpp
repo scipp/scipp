f876dc56705220bf011001de54b53cf2cce88ee0
Simon Heybrock <simon.heybrock@ess.eu>
2022-07-20 12:34:44 +0200

    Test with units

--- lib/variable/test/arithmetic_test.cpp
+++ lib/variable/test/arithmetic_test.cpp
@@ -45,13 +45,13 @@ TEST(ArithmeticTest, x_minus_equals_x_with_variances_equals_0_x) {
 }
 
 TEST(ArithmeticTest, x_times_x_with_variances_equals_x_squared) {
-  const auto x = makeVariable<double>(Values{2.0}, Variances{4.0});
+  const auto x = makeVariable<double>(Values{2.0}, Variances{4.0}, units::m);
   const auto two = makeVariable<double>(Values{2.0});
   EXPECT_EQ(x * x, pow(x, two));
 }
 
 TEST(ArithmeticTest, x_times_equals_x_with_variances_equals_x_squared) {
-  auto x = makeVariable<double>(Values{2.0}, Variances{4.0});
+  auto x = makeVariable<double>(Values{2.0}, Variances{4.0}, units::m);
   const auto two = makeVariable<double>(Values{2.0});
   const auto expected = pow(x, two);
   EXPECT_EQ(x *= x, expected);
@@ -59,14 +59,14 @@ TEST(ArithmeticTest, x_times_equals_x_with_variances_equals_x_squared) {
 }
 
 TEST(ArithmeticTest, x_divide_x_with_variances_equals_x_to_the_power_of_zero) {
-  const auto x = makeVariable<double>(Values{2.0}, Variances{4.0});
+  const auto x = makeVariable<double>(Values{2.0}, Variances{4.0}, units::m);
   const auto zero = makeVariable<double>(Values{0.0});
   EXPECT_EQ(x / x, pow(x, zero));
 }
 
 TEST(ArithmeticTest,
      x_divide_equals_x_with_variances_equals_x_to_the_power_of_zero) {
-  auto x = makeVariable<double>(Values{2.0}, Variances{4.0});
+  auto x = makeVariable<double>(Values{2.0}, Variances{4.0}, units::m);
   const auto zero = makeVariable<double>(Values{0.0});
   const auto expected = pow(x, zero);
   EXPECT_EQ(x /= x, expected);
