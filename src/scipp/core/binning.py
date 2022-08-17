e463ce2c5debf4b5d06ddef06856d6bf7d09822e
Neil Vaytet <neil.vaytet@esss.se>
2022-08-17 16:56:04 +0200

    maybe it is enough to just return first coord found

--- src/scipp/core/binning.py
+++ src/scipp/core/binning.py
@@ -118,17 +118,23 @@ def _get_coord(x, name):
     if isinstance(x, Variable):
         return x
     if isinstance(x, _cpp.Dataset):
-        cmin = None
-        cmax = None
+        # Coords in the dataset are aligned, so it is enough to return the first one
+        coord = None
         for da in x.values():
-            c = _get_coord(da, name)
-            cmin = c.min() if cmin is None else min(cmin, c.min())
-            cmax = c.max() if cmax is None else max(cmin, c.max())
-        coord = _cpp.concat([cmin, cmax], dim='dummy')
+            if name in da.meta:
+                coord = _get_coord(da, name)
+                break
+        # cmin = None
+        # cmax = None
+        # for da in x.values():
+        #     c = _get_coord(da, name)
+        #     cmin = c.min() if cmin is None else min(cmin, c.min())
+        #     cmax = c.max() if cmax is None else max(cmin, c.max())
+        # coord = _cpp.concat([cmin, cmax], dim='dummy')
     else:
         event_coord = x.bins.meta.get(name) if x.bins is not None else None
         coord = x.meta.get(name, event_coord)
-        _require_coord(name, coord)
+    _require_coord(name, coord)
     return coord
 
 
