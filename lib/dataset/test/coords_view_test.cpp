8199d2e2185077ca892bd129ce29c791e779b6d2
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-08-23 10:25:59 +0200

    Remove unused include

--- lib/dataset/test/coords_view_test.cpp
+++ lib/dataset/test/coords_view_test.cpp
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
-#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <numeric>
