cd3e40de8ebabc75f1ead937a4063d608b400905
Neil Vaytet <neil.vaytet@esss.se>
2022-08-25 13:43:16 +0200

    add unit tests

---
+++ tests/experimental/plotting/kwargs_test.py
@@ -0,0 +1,87 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+
+import scipp as sc
+import numpy as np
+from ...factory import make_dense_data_array, make_dense_dataset
+
+
+def _get_first_child(plot):
+    children = plot._children
+    keys = list(children.keys())
+    return children[keys[0]]
+
+
+def _get_line(plot):
+    return _get_first_child(plot)._line
+
+
+def _get_mesh(plot):
+    return _get_first_child(plot)._mesh
+
+
+def test_kwarg_linecolor():
+    da = make_dense_data_array(ndim=1)
+    p = da.plot(color='r')
+    _ = p._to_widget()
+    assert _get_line(p).get_color() == 'r'
+    p = da.plot(c='b')
+    _ = p._to_widget()
+    assert _get_line(p).get_color() == 'b'
+
+
+def test_kwarg_linestyle():
+    da = make_dense_data_array(ndim=1)
+    p = da.plot(linestyle='solid')
+    _ = p._to_widget()
+    assert _get_line(p).get_linestyle() == '-'
+    p = da.plot(ls='dashed')
+    _ = p._to_widget()
+    assert _get_line(p).get_linestyle() == '--'
+
+
+def test_kwarg_linewidth():
+    da = make_dense_data_array(ndim=1)
+    p = da.plot(linewidth=3)
+    _ = p._to_widget()
+    assert _get_line(p).get_linewidth() == 3
+    p = da.plot(lw=5)
+    _ = p._to_widget()
+    assert _get_line(p).get_linewidth() == 5
+
+
+def test_kwarg_marker():
+    da = make_dense_data_array(ndim=1)
+    p = da.plot(marker='+')
+    _ = p._to_widget()
+    assert _get_line(p).get_marker() == '+'
+
+
+def test_kwarg_norm():
+    da = make_dense_data_array(ndim=1)
+    p = da.plot(norm='log')
+    _ = p._to_widget()
+    assert p._ax.get_yscale() == 'log'
+
+
+def test_kwarg_scale():
+    da = make_dense_data_array(ndim=1)
+    p = da.plot(scale={'xx': 'log'})
+    _ = p._to_widget()
+    assert p._ax.get_xscale() == 'log'
+    assert p._ax.get_yscale() == 'linear'
+
+
+def test_kwarg_cmap():
+    da = make_dense_data_array(ndim=2)
+    p = da.plot(cmap='magma')
+    _ = p._to_widget()
+    assert _get_mesh(p).get_cmap().name == 'magma'
+
+
+def test_kwarg_scale_2d():
+    da = make_dense_data_array(ndim=2)
+    p = da.plot(scale={'xx': 'log', 'yy': 'log'})
+    _ = p._to_widget()
+    assert p._ax.get_xscale() == 'log'
+    assert p._ax.get_yscale() == 'log'
--- tests/experimental/plotting/wrappers_test.py
+++ tests/experimental/plotting/wrappers_test.py
@@ -7,37 +7,37 @@ from ...factory import make_dense_data_array, make_dense_dataset
 
 
 def test_plot_ndarray():
-    sc.plot(np.arange(50.))
+    sc.plot(np.arange(50.))._to_widget()
 
 
 def test_plot_variable():
-    sc.plot(sc.arange('x', 50.))
+    sc.plot(sc.arange('x', 50.))._to_widget()
 
 
 def test_plot_data_array():
-    sc.plot(make_dense_data_array(ndim=1))
+    sc.plot(make_dense_data_array(ndim=1))._to_widget()
     da = make_dense_data_array(ndim=2)
-    sc.plot(da)
-    da.plot()
+    sc.plot(da)._to_widget()
+    da.plot()._to_widget()
 
 
 def test_plot_data_array_missing_coords():
-    sc.data.table_xyz(100).plot()
+    sc.data.table_xyz(100).plot()._to_widget()
 
 
 def test_plot_dataset():
     ds = make_dense_dataset(ndim=1)
-    sc.plot(ds)
+    sc.plot(ds)._to_widget()
 
 
 def test_plot_dict_of_ndarrays():
-    sc.plot({'a': np.arange(50.), 'b': np.arange(60.)})
+    sc.plot({'a': np.arange(50.), 'b': np.arange(60.)})._to_widget()
 
 
 def test_plot_dict_of_variables():
-    sc.plot({'a': sc.arange('x', 50.), 'b': sc.arange('x', 60.)})
+    sc.plot({'a': sc.arange('x', 50.), 'b': sc.arange('x', 60.)})._to_widget()
 
 
 def test_plot_dict_of_data_arrays():
     ds = make_dense_dataset(ndim=1)
-    sc.plot({'a': ds['a'], 'b': ds['b']})
+    sc.plot({'a': ds['a'], 'b': ds['b']})._to_widget()
