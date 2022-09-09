bfa6b0de2229dadb0b361822369bcfbd25a07882
Simon Heybrock <simon.heybrock@ess.eu>
2022-09-09 09:37:14 +0200

    Use Python type names instead of double, float, ...

--- lib/variable/variable_instantiate_map_elements.cpp
+++ lib/variable/variable_instantiate_map_elements.cpp
@@ -12,41 +12,41 @@
 namespace scipp::variable {
 
 // Used internally in implementation of grouping and binning
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_double_to_int64_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_float64_to_int64,
                                    std::unordered_map<double, int64_t>)
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_double_to_int32_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_float64_to_int32,
                                    std::unordered_map<double, int32_t>)
 
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_float_to_int64_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_float32_to_int64,
                                    std::unordered_map<float, int64_t>)
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_float_to_int32_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_float32_to_int32,
                                    std::unordered_map<float, int32_t>)
 
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_int64_to_int64_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_int64_to_int64,
                                    std::unordered_map<int64_t, int64_t>)
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_int64_to_int32_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_int64_to_int32,
                                    std::unordered_map<int64_t, int32_t>)
 
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_int32_to_int64_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_int32_to_int64,
                                    std::unordered_map<int32_t, int64_t>)
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_int32_to_int32_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_int32_to_int32,
                                    std::unordered_map<int32_t, int32_t>)
 
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_bool_to_int64_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_bool_to_int64,
                                    std::unordered_map<bool, int64_t>)
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_bool_to_int32_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_bool_to_int32,
                                    std::unordered_map<bool, int32_t>)
 
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_string_to_int64_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_string_to_int64,
                                    std::unordered_map<std::string, int64_t>)
-INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_string_to_int32_t,
+INSTANTIATE_ELEMENT_ARRAY_VARIABLE(unordered_map_string_to_int32,
                                    std::unordered_map<std::string, int32_t>)
 
 INSTANTIATE_ELEMENT_ARRAY_VARIABLE(
-    unordered_map_datetime64_to_int64_t,
+    unordered_map_datetime64_to_int64,
     std::unordered_map<core::time_point, int64_t>)
 INSTANTIATE_ELEMENT_ARRAY_VARIABLE(
-    unordered_map_datetime64_to_int32_t,
+    unordered_map_datetime64_to_int32,
     std::unordered_map<core::time_point, int32_t>)
 
 INSTANTIATE_ELEMENT_ARRAY_VARIABLE(SubbinSizes, core::SubbinSizes)
