115e444cbdd0a88a8d54abebb362cdae15cdc401
Simon Heybrock <simon.heybrock@ess.eu>
2022-09-30 10:30:06 +0200

    Comments

--- src/scipp/core/bin_remapping.py
+++ src/scipp/core/bin_remapping.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
 # @author Simon Heybrock
-from typing import Dict, Union
+from typing import Dict
 from .._scipp import core as _cpp
 from .cpp_classes import DataArray, Variable, Dataset
 from .like import empty_like
@@ -26,11 +26,11 @@ def reduced_masks(da: DataArray, dim: str) -> Dict[str, Variable]:
     return {name: mask.copy() for name, mask in _reduced(da.masks, dim).items()}
 
 
-def _copy_dict_for_overwrite(mapping: Dict[str, Variable]):
+def _copy_dict_for_overwrite(mapping: Dict[str, Variable]) -> Dict[str, Variable]:
     return {name: copy_for_overwrite(var) for name, var in mapping.items()}
 
 
-def copy_for_overwrite(obj: Union[Variable, DataArray, Dataset]):
+def copy_for_overwrite(obj: VariableLikeType) -> VariableLikeType:
     """
     Copy a Scipp object for overwriting.
 
@@ -45,9 +45,15 @@ def copy_for_overwrite(obj: Union[Variable, DataArray, Dataset]):
                          coords=_copy_dict_for_overwrite(obj.coords),
                          masks=_copy_dict_for_overwrite(obj.masks),
                          attrs=_copy_dict_for_overwrite(obj.attrs))
+    ds = Dataset(coords=_copy_dict_for_overwrite(obj.coords))
+    for name, da in ds:
+        ds[name] = DataArray(copy_for_overwrite(obj.data),
+                             masks=_copy_dict_for_overwrite(obj.masks),
+                             attrs=_copy_dict_for_overwrite(obj.attrs))
+    return ds
 
 
-def hide_masked_and_reduce_meta(da: DataArray, dim: str):
+def hide_masked_and_reduce_meta(da: DataArray, dim: str) -> DataArray:
     if (mask := irreducible_mask(da.masks, dim)) is not None:
         # Avoid using boolean indexing since it would result in (partial) content
         # buffer copy. Instead index just begin/end and reuse content buffer.
@@ -68,10 +74,11 @@ def hide_masked_and_reduce_meta(da: DataArray, dim: str):
 
 
 def _concat_bins_variable(var: Variable, dim: str) -> Variable:
+    # We want to write data from all bins along dim to a contiguous chunk in the
+    # content buffer. This will then allow us to create new, larger bins covering the
+    # respective input bins. We use `cumsum` after moving `dim` to the innermost dim.
+    # This will allow us to setup offsets for the new contiguous layout.
     sizes = var.bins.size()
-    # subbin sizes
-    # cumsum performed with the remove dim as innermost, such that we map all merged
-    # bins into the same output bin
     out_dims = [d for d in var.dims if d != dim]
     out_end = cumsum(sizes.transpose(out_dims + [dim])).transpose(var.dims)
     out_begin = out_end - sizes
@@ -82,8 +89,11 @@ def _concat_bins_variable(var: Variable, dim: str) -> Variable:
         end=out_end,
     )
 
+    # Copy all bin contents, performing the actual reordering with the content buffer.
     out[...] = var
 
+    # Setup output indices. This will have the "merged" bins, referening the new
+    # contigous layout in the content buffer.
     out_sizes = sizes.sum(dim)
     out_end = cumsum(out_sizes)
     out_begin = out_end - out_sizes
