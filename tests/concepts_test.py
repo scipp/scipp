efe602a6451adf00c655ab02c9e400f19f599f1d
Simon Heybrock <simon.heybrock@ess.eu>
2022-10-07 09:03:51 +0200

    Test concepts.irreducible_mask

--- tests/concepts_test.py
+++ tests/concepts_test.py
@@ -18,3 +18,40 @@ def test_concrete_dims_given_dim_list_returns_dim_tuple():
 def test_concrete_dims_given_none_returns_obj_dims():
     var = sc.empty(dims=('xx', 'yy'), shape=(2, 2))
     assert concepts.concrete_dims(var, None) == ('xx', 'yy')
+
+
+def test_irreducible_mask_returns_union_of_relevant_masks():
+    da = sc.DataArray(sc.empty(dims=('xx', 'yy'), shape=(2, 3)))
+    da.masks['x'] = sc.array(dims=['xx'], values=[False, True])
+    da.masks['y'] = sc.array(dims=['yy'], values=[False, True, False])
+    da.masks['xy'] = sc.array(dims=['xx', 'yy'],
+                              values=[[False, False, False], [False, False, True]])
+    assert sc.identical(concepts.irreducible_mask(da, 'xx'),
+                        da.masks['x'] | da.masks['xy'])
+    assert sc.identical(concepts.irreducible_mask(da, 'yy'),
+                        da.masks['y'] | da.masks['xy'])
+    assert sc.identical(concepts.irreducible_mask(da, ('xx', 'yy')),
+                        da.masks['x'] | da.masks['y'] | da.masks['xy'])
+
+
+def test_irreducible_mask_returns_None_if_all_masks_unrelated():
+    da = sc.DataArray(sc.empty(dims=('xx', 'yy'), shape=(2, 3)))
+    da.masks['x'] = sc.array(dims=['xx'], values=[False, True])
+    assert concepts.irreducible_mask(da, 'yy') is None
+
+
+def test_irreducible_mask_returns_copy_if_single_mask_matches():
+    da = sc.DataArray(sc.empty(dims=('xx', 'yy'), shape=(2, 3)))
+    da.masks['x'] = sc.array(dims=['xx'], values=[False, True])
+    mask = concepts.irreducible_mask(da, 'xx')
+    mask.values[0] = True
+    assert not da.masks['x'].values[0]
+
+
+def test_irreducible_mask_does_not_include_0d_mask():
+    da = sc.DataArray(sc.empty(dims=('xx', 'yy'), shape=(2, 3)))
+    da.masks['0d'] = sc.scalar(True)
+    assert concepts.irreducible_mask(da, 'xx') is None
+    assert concepts.irreducible_mask(da, 'yy') is None
+    assert concepts.irreducible_mask(da, ('xx', 'yy')) is None
+    assert concepts.irreducible_mask(da, None) is None
