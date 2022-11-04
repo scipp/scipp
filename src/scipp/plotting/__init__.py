e07ba3fe43b4d24b6cbc018ef812aa59d5b96cd9
Neil Vaytet <neil.vaytet@esss.se>
2022-11-04 18:24:57 +0100

    use get in case key does not exist

--- src/scipp/plotting/__init__.py
+++ src/scipp/plotting/__init__.py
@@ -160,7 +160,7 @@ def legacy_plot(*args, **kwargs):
 
 
 def plot(*args, **kwargs):
-    if config['use_plopp']:
+    if config.get('use_plopp'):
         import plopp
         return plopp.plot(*args, **kwargs)
     else:
