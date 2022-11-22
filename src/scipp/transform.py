7a59c0852931b7dc5d243b0ec12bc33ce43df1d8
Simon Heybrock <simon.heybrock@ess.eu>
2022-11-22 14:00:31 +0100

    Cache the numba cfunc

--- src/scipp/transform.py
+++ src/scipp/transform.py
@@ -11,7 +11,8 @@ from ._scipp.core import experimental_transform
 def _as_numba_cfunc(function, unit_func=None):
     dtype = 'double'
     narg = len(signature(function).parameters)
-    cfunc = numba.cfunc(dtype + '(' + ','.join([dtype] * narg) + ')')(function)
+    cfunc = numba.cfunc(dtype + '(' + ','.join([dtype] * narg) + ')',
+                        cache=True)(function)
     cfunc.unit_func = function if unit_func is None else unit_func
     return cfunc
 
