fc03390e080f4725c0fa18f97f83feb55574ff73
Simon Heybrock <simon.heybrock@ess.eu>
2022-11-22 13:29:33 +0100

    Simplify

--- src/scipp/experimental/transform.py
+++ src/scipp/experimental/transform.py
@@ -12,25 +12,19 @@ notice.
 """
 from .. import core
 
-
-def transform_kernel(dtype='float64'):
-    if dtype != 'float64':
-        raise RuntimeError('Only float64 arguments supported at this point')
-    else:
-        dtype = 'double'
-    import numba
-    from inspect import signature
-
-    def decorator(function, unit_kernel=None):
-        narg = len(signature(function).parameters)
-        cfunc = numba.cfunc(dtype + '(' + ','.join([dtype] * narg) + ')')(function)
-        cfunc.unit_func = function if unit_kernel is None else unit_kernel
-        return cfunc
-
-    return decorator
+import numba
+from inspect import signature
 
 
-def transform(kernel, *args, unit_kernel=None):
+def _as_numba_cfunc(function, unit_kernel=None):
+    dtype = 'double'
+    narg = len(signature(function).parameters)
+    cfunc = numba.cfunc(dtype + '(' + ','.join([dtype] * narg) + ')')(function)
+    cfunc.unit_func = function if unit_kernel is None else unit_kernel
+    return cfunc
+
+
+def transform(func, unit_func=None, dtype='float64', auto_convert_dtypes=False):
     """Transform one or more variables using a custom kernel.
 
     Only variables with dtype=float64 are supported.
@@ -40,6 +34,15 @@ def transform(kernel, *args, unit_kernel=None):
                    a function decorated using `@scipp.experimental.transform_kernel()`
     """
     from .._scipp.core import experimental_transform
-    if not hasattr(kernel, 'address'):
-        kernel = transform_kernel()(kernel, unit_kernel=unit_kernel)
-    return experimental_transform(kernel, *args)
+    if dtype != 'float64':
+        raise RuntimeError('Only float64 arguments supported at this point')
+    else:
+        dtype = 'double'
+    func = _as_numba_cfunc()(func, unit_func=unit_func)
+
+    def func(*args):
+        if auto_convert_dtypes:
+            args = [arg.to(dtype='float64', copy=False) for arg in args]
+        return experimental_transform(func, *args)
+
+    return func
