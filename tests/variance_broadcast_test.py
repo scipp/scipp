d7a4b7e26f52a15dde689b504081041a44467742
Simon Heybrock <simon.heybrock@ess.eu>
2022-11-30 07:28:21 +0100

    More tests

--- tests/variance_broadcast_test.py
+++ tests/variance_broadcast_test.py
@@ -53,6 +53,14 @@ def test_inplace_op_with_implicit_variance_broadcast_of_bins_raises():
         xy + x
 
 
+def test_broadcast_of_dense_without_variances_to_bins_works():
+    table = sc.data.table_xyz(100)
+    table.variances = table.values
+    binned = table.bin(x=4)
+    dense = sc.full(sizes=binned.sizes, value=2.0)
+    assert sc.identical(dense * binned, binned * 2.0)
+
+
 def test_op_with_explicit_variance_broadcast_raises():
     var = sc.linspace('x', 0.0, 1.0, 4)
     scalar = sc.scalar(1.0, variance=0.1)
@@ -72,3 +80,18 @@ def test_inplace_op_with_explicit_variance_broadcast_raises():
 def test_can_copy_broadcast_with_variances():
     var = sc.scalar(1.0, variance=0.1).broadcast(sizes={'x': 4})
     assert sc.identical(var.copy(), var)
+
+
+def test_can_extract_values_of_broadcast_with_variances():
+    var = sc.scalar(1.0, variance=0.1).broadcast(sizes={'x': 4})
+    assert sc.identical(sc.values(var), sc.ones(dims=['x'], shape=[4]))
+
+
+def test_can_extract_variances_of_broadcast_with_variances():
+    var = sc.scalar(1.0, variance=4.0).broadcast(sizes={'x': 4})
+    assert sc.identical(sc.variances(var), sc.full(dims=['x'], shape=[4], value=4.0))
+
+
+def test_can_extract_stddevs_of_broadcast_with_variances():
+    var = sc.scalar(1.0, variance=4.0).broadcast(sizes={'x': 4})
+    assert sc.identical(sc.stddevs(var), sc.full(dims=['x'], shape=[4], value=2.0))
