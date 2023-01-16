22b49fb2b82006f2e910239b1c6e0cde9cd29a3d
YooSunyoung <luysunyoung9@gmail.com>
2023-01-16 13:27:51 +0100

    Update error throwing logic to catch all non-boolean or unit-specified masks in the binary operation between .

--- lib/dataset/dataset.cpp
+++ lib/dataset/dataset.cpp
@@ -273,31 +273,25 @@ bool Dataset::is_readonly() const noexcept { return m_readonly; }
 typename Masks::holder_type union_or(const Masks &currentMasks,
                                      const Masks &otherMasks) {
   typename Masks::holder_type out;
-  for (const auto &masks : {currentMasks, otherMasks}) {
-    for (const auto &[key, item] : masks) {
-      if (item.dtype() != core::dtype<bool>) {
-        std::string optional_message =
-            " Cannot combine non-boolean mask '" + key + "' in operation";
-        except::throw_mismatch_error(core::dtype<bool>, item.dtype(),
-                                     optional_message);
-      } else if (item.unit() != scipp::units::none) {
-        std::string optional_message =
-            " Cannot combine a unit-specified mask '" + key + "' in operation";
-        except::throw_mismatch_error(scipp::units::none, item.unit(),
-                                     optional_message);
-      }
-    }
-  }
 
   for (const auto &[key, item] : currentMasks)
     out.insert_or_assign(key, copy(item));
   for (const auto &[key, item] : otherMasks) {
-    if (!currentMasks.contains(key))
+    if (!currentMasks.contains(key)) {
       out.insert_or_assign(key, copy(item));
-    else if (out[key].dims().includes(item.dims()))
+    } else if (item.dtype() != core::dtype<bool> ||
+               out[key].dtype() != core::dtype<bool>) {
+      throw except::TypeError(" Cannot combine non-boolean mask '" + key +
+                              "' in operation");
+    } else if (item.unit() != scipp::units::none ||
+               out[key].unit() != scipp::units::none) {
+      throw except::UnitError(" Cannot combine a unit-specified mask '" + key +
+                              "' in operation");
+    } else if (out[key].dims().includes(item.dims())) {
       out[key] |= item;
-    else
+    } else {
       out[key] = out[key] | item;
+    }
   }
   return out;
 }
@@ -305,22 +299,6 @@ typename Masks::holder_type union_or(const Masks &currentMasks,
 void union_or_in_place(Masks &currentMasks, const Masks &otherMasks) {
   using core::to_string;
   using units::to_string;
-
-  for (const auto &masks : {currentMasks, otherMasks}) {
-    for (const auto &[key, item] : masks) {
-      if (item.dtype() != core::dtype<bool>) {
-        std::string optional_message =
-            " Cannot combine non-boolean mask '" + key + "' in operation";
-        except::throw_mismatch_error(core::dtype<bool>, item.dtype(),
-                                     optional_message);
-      } else if (item.unit() != scipp::units::none) {
-        std::string optional_message =
-            " Cannot combine a unit-specified mask '" + key + "' in operation";
-        except::throw_mismatch_error(scipp::units::none, item.unit(),
-                                     optional_message);
-      }
-    }
-  }
 
   for (const auto &[key, item] : otherMasks) {
     const auto it = currentMasks.find(key);
@@ -332,6 +310,16 @@ void union_or_in_place(Masks &currentMasks, const Masks &otherMasks) {
       throw except::DimensionError("Cannot update mask '" + to_string(key) +
                                    "' via slice since the mask is implicitly "
                                    "broadcast along the slice dimension.");
+    } else if (it != currentMasks.end() &&
+               (item.dtype() != core::dtype<bool> ||
+                currentMasks[key].dtype() != core::dtype<bool>)) {
+      throw except::TypeError(" Cannot combine non-boolean mask '" + key +
+                              "' in operation");
+    } else if (it != currentMasks.end() &&
+               (item.unit() != scipp::units::none ||
+                currentMasks[key].unit() != scipp::units::none)) {
+      throw except::UnitError(" Cannot combine a unit-specified mask '" + key +
+                              "' in operation");
     }
   }
 
