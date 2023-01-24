6929acfd643e184c37d12341f192e79bf2e76085
Simon Heybrock <simon.heybrock@ess.eu>
2023-01-24 11:34:58 +0100

    unused code

--- lib/dataset/variable_instantiate_dataset.cpp
+++ lib/dataset/variable_instantiate_dataset.cpp
@@ -9,16 +9,6 @@
 
 namespace scipp::variable {
 
-namespace {
-std::string format_data_array(const DataArray &da) {
-  auto s = "DataArray(dims=" + to_string(da.dims()) +
-           ", dtype=" + to_string(da.dtype());
-  if (da.unit() != units::none)
-    s += ", unit=" + to_string(da.unit());
-  return s + ')';
-}
-} // namespace
-
 template <>
 std::string Formatter<DataArray>::format(const Variable &var) const {
   if (var.dims().volume() == 1)
