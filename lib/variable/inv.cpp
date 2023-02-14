ff702e54c084730f02791248c74bc162a75bee64
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-02-14 11:42:10 +0100

    Invert the unit of rotations, too

--- lib/variable/inv.cpp
+++ lib/variable/inv.cpp
@@ -17,19 +17,34 @@ constexpr auto inv =
                core::transform_flags::expect_no_variance_arg<0>,
                core::transform_flags::expect_no_variance_arg<1>,
                [](const auto &transform) { return transform.inverse(); },
-               [](const units::Unit &u) {
-                 // This is not correct for linear transforms (Eigen::Matrix3d).
-                 // But that case is handled separately by the caller.
-                 return u;
+               [](const units::Unit &) {
+                 // The resulting unit depends on the dtype;
+                 // the calling code assigns it.
+                 return units::none;
                }};
 }
 
+namespace {
+bool is_transform_with_translation(const Variable &var) {
+  const auto dt = var.dtype();
+  return dt == dtype<Eigen::Affine3d> || dt == dtype<scipp::core::Translation>;
+}
+
+// Translations: The unit stays the same because translations are additive.
+// Affine transforms: The unit applies only to the translation part, see above.
+// Linear transforms: Can scale the input, the unit is multiplicative.
+// Rotations: A unit is ill-defined, but use 1/u to cancel out any unit
+//            in case the user sets one manually.
+auto result_unit(const Variable &var) {
+  return is_transform_with_translation(var) ? var.unit()
+                                            : units::one / var.unit();
+}
+
+} // namespace
+
 Variable inv(const Variable &var) {
-  const auto result_unit = (var.dtype() == dtype<Eigen::Matrix3d>)
-                               ? units::one / var.unit()
-                               : var.unit();
   auto res = variable::transform(var, element::inv, "inverse");
-  res.setUnit(result_unit);
+  res.setUnit(result_unit(var));
   return res;
 }
 } // namespace scipp::variable
