1c830bc6ce7a160b55e1f743696db212be10d21a
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-03-20 16:43:45 +0100

    Add versionadded in as_vectors

--- src/scipp/spatial/__init__.py
+++ src/scipp/spatial/__init__.py
@@ -58,6 +58,8 @@ def as_vectors(x: Variable, y: Variable, z: Variable) -> Variable:
     ------
     scipp.DTypeError
         If the dtypes of inputs are not ``float64``.
+
+    .. versionadded:: RELEASE_PLACEHOLDER
     """
     return _call_cpp_func(_core_cpp.geometry.as_vectors, x, y, z)
 
