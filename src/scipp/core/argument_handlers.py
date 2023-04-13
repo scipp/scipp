133fc3e72b727b3a69538b211b83c647141547ff
YooSunyoung <luysunyoung9@gmail.com>
2023-04-13 08:34:41 +0200

    Add type hints in dict-like arguments merging helper function

--- src/scipp/core/argument_handlers.py
+++ src/scipp/core/argument_handlers.py
@@ -1,10 +1,16 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 
-from typing import Dict, Optional
+from typing import Dict, Optional, TypeVar
+
+from ..typing import Variable
+
+_ValueType = TypeVar('_ValueType', str, Variable)
 
 
-def combine_dict_args(arg: Optional[Dict], /, **kwargs):
+def combine_dict_args(
+    arg: Optional[Dict[str, _ValueType]] = None, /, **kwargs
+) -> Dict[str, _ValueType]:
     pos_dict = {} if arg is None else arg
 
     overlapped = set(pos_dict).intersection(kwargs)
