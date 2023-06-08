c93a2a0004095ce02600daca7b60da27bf429bc2
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-06-08 13:47:14 +0200

    Support parsing units from pandas column headers

--- src/scipp/compat/pandas_compat.py
+++ src/scipp/compat/pandas_compat.py
@@ -3,33 +3,49 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, Callable, Literal, Optional, Tuple, Union
 
-from ..core import DataArray, Dataset, array
+from ..core import DataArray, Dataset, Unit, array
 from ..typing import VariableLike
+from ..units import default_unit
 
 if TYPE_CHECKING:
     import pandas as pd
 
 
-def from_pandas_series(se: pd.Series, *, include_index: bool = True) -> DataArray:
+def from_pandas_series(
+    se: pd.Series, *, include_index: bool = True, head_parser: HeadParserArg = None
+) -> DataArray:
     row_index = se.axes[0]
     row_index_name = "row" if row_index.name is None else str(row_index.name)
-    name = "" if se.name is None else str(se.name)
+    name, unit = (
+        ("", default_unit)
+        if se.name is None
+        else _parse_head(str(se.name), head_parser)
+    )
 
     coords = (
         {row_index_name: array(dims=[row_index_name], values=row_index)}
         if include_index
         else {}
     )
+
+    if se.dtype == "string":
+        # se.to_numpy() and np.array(se.values) produce an array of dtype=object
+        # when the series contains strings.
+        values = se.to_numpy(dtype=str)
+    else:
+        values = se.to_numpy()
     return DataArray(
-        data=array(values=se.values, dims=[row_index_name]),
+        data=array(values=values, dims=[row_index_name], unit=unit),
         coords=coords,
         name=name,
     )
 
 
-def from_pandas_dataframe(df: pd.DataFrame, *, include_index: bool = True) -> Dataset:
+def from_pandas_dataframe(
+    df: pd.DataFrame, *, include_index: bool = True, head_parser: HeadParserArg = None
+) -> Dataset:
     import pandas as pd
 
     row_index = df.axes[0]
@@ -41,22 +57,28 @@ def from_pandas_dataframe(df: pd.DataFrame, *, include_index: bool = True) -> Da
         return Dataset(
             data={
                 row_index_name: from_pandas_series(
-                    pd.Series(df), include_index=include_index
+                    pd.Series(df), include_index=include_index, head_parser=head_parser
                 )
             }
         )
 
     sc_data = {}
     for column_name in df.axes[1]:
-        sc_data[f"{column_name}"] = from_pandas_series(
-            pd.Series(df[column_name]), include_index=include_index
+        da = from_pandas_series(
+            pd.Series(df[column_name]),
+            include_index=include_index,
+            head_parser=head_parser,
         )
+        sc_data[da.name] = da
 
     return Dataset(data=sc_data)
 
 
 def from_pandas(
-    pd_obj: Union[pd.DataFrame, pd.Series], *, include_index: bool = True
+    pd_obj: Union[pd.DataFrame, pd.Series],
+    *,
+    include_index: bool = True,
+    head_parser: HeadParserArg = None,
 ) -> VariableLike:
     """Converts a pandas.DataFrame or pandas.Series object into a
     scipp Dataset or DataArray respectively.
@@ -67,6 +89,19 @@ def from_pandas(
         The Dataframe or Series to convert.
     include_index:
         If True, the row index is included in the output as a coordinate.
+    head_parser:
+        Parses each column name to extract a name and unit for each data array.
+        By default, it returns the column name and uses the default unit.
+        Builtin parsers can be specified by name:
+
+        - ``"bracket"``: Parses strings of the form ``name [unit]`` where ``name``
+          be any string that does not contain the character ``[``.
+          Whitespace between the name and unit is removed.
+          Both name and unit, including brackets, are optional.
+
+        When implementing a parser, make sure to return ``sc.units.default_unit``
+        if the unit is not specified instead of ``sc.Unit("")`` to get a sensible
+        result for non-numeric columns.
 
     Returns
     -------
@@ -76,8 +111,53 @@ def from_pandas(
     import pandas as pd
 
     if isinstance(pd_obj, pd.DataFrame):
-        return from_pandas_dataframe(pd_obj, include_index=include_index)
+        return from_pandas_dataframe(
+            pd_obj, include_index=include_index, head_parser=head_parser
+        )
     elif isinstance(pd_obj, pd.Series):
-        return from_pandas_series(pd_obj, include_index=include_index)
+        return from_pandas_series(
+            pd_obj, include_index=include_index, head_parser=head_parser
+        )
     else:
         raise ValueError(f"from_pandas: cannot convert type '{type(pd_obj)}'")
+
+
+HeadParser = Callable[[str], Tuple[str, Unit]]
+HeadParserArg = Optional[Union[Literal["bracket"], HeadParser]]
+
+
+def parse_bracket_head(head: str) -> Tuple[str, Unit]:
+    import re
+
+    m = re.match(r"^([^[]*)(?:\[([^[]*)])?$", head)
+    if m is None:
+        raise ValueError(
+            f"Column header does not conform to pattern `name [unit]`: {head}"
+        )
+
+    if m.lastindex == 2:
+        name = m[1].rstrip()
+        unit = Unit(m[2]) if m[2] else default_unit
+    else:
+        name = m[1]
+        unit = default_unit
+    return name, unit
+
+
+_HEAD_PARSERS = {
+    "bracket": parse_bracket_head,
+}
+
+
+def _parse_head(head: str, parser: HeadParserArg) -> Tuple[str, Unit]:
+    if parser is None:
+        return head, default_unit
+    if callable(parser):
+        return parser(head)
+    if (parser := _HEAD_PARSERS.get(parser)) is not None:
+        return parser(head)
+    else:
+        raise ValueError(
+            f"Unknown head parser '{parser}', "
+            f"supported builtin parsers: {list(_HEAD_PARSERS.keys())}."
+        )
--- tests/compat/pandas_compat_test.py
+++ tests/compat/pandas_compat_test.py
@@ -1,7 +1,10 @@
 import pandas
+import pytest
 
 import scipp as sc
 from scipp.compat import from_pandas
+from scipp.compat.pandas_compat import parse_bracket_head
+from scipp.testing import assert_identical
 
 
 def _make_reference_da(row_name, row_coords, values, dtype="int64"):
@@ -26,7 +29,7 @@ def _make_2d_reference_ds(row_name, row_coords, data, dtype="int64"):
             for key, value in data.items()
         },
         coords={
-            row_name: sc.Variable(dims=[row_name], values=row_coords, dtype=dtype),
+            row_name: sc.Variable(dims=[row_name], values=row_coords),
         },
     )
 
@@ -166,3 +169,115 @@ def test_2d_dataframe_with_named_axes():
     )
 
     assert sc.identical(sc_ds, reference_ds)
+
+
+def test_2d_dataframe_does_not_parse_units_by_default():
+    pd_df = pandas.DataFrame(data={"col1 [m]": (1, 2), "col2 [one]": (6, 3)})
+
+    sc_ds = from_pandas(pd_df)
+
+    reference_ds = _make_2d_reference_ds(
+        "row", [0, 1], data={"col1 [m]": (1, 2), "col2 [one]": (6, 3)}
+    )
+
+    assert_identical(sc_ds, reference_ds)
+
+
+def test_2d_dataframe_parse_units_brackets():
+    pd_df = pandas.DataFrame(data={"col1 [m]": (1, 2), "col2 [one]": (6, 3)})
+
+    sc_ds = from_pandas(pd_df, head_parser="bracket")
+
+    reference_ds = _make_2d_reference_ds(
+        "row", [0, 1], data={"col1": (1, 2), "col2": (6, 3)}
+    )
+    reference_ds["col1"].unit = "m"
+    reference_ds["col2"].unit = "one"
+
+    assert_identical(sc_ds, reference_ds)
+
+
+def test_2d_dataframe_parse_units_brackets_string_dtype():
+    pd_df = pandas.DataFrame(
+        data={"col1 [m]": ("a", "b"), "col2": ("c", "d")}, dtype="string"
+    )
+
+    sc_ds = from_pandas(pd_df, head_parser="bracket")
+
+    reference_ds = _make_2d_reference_ds(
+        "row",
+        [0, 1],
+        data={"col1": ("a", "b"), "col2": ("c", "d")},
+        dtype="str",
+    )
+    reference_ds["col1"].unit = "m"
+    reference_ds["col2"].unit = None
+
+    assert_identical(sc_ds, reference_ds)
+
+
+def test_parse_bracket_head_whitespace():
+    name, unit = parse_bracket_head("")
+    assert name == ""
+    assert unit == sc.units.default_unit
+
+    name, unit = parse_bracket_head(" ")
+    assert name == " "
+    assert unit == sc.units.default_unit
+
+
+def test_parse_bracket_head_only_name():
+    name, unit = parse_bracket_head("a name 123")
+    assert name == "a name 123"
+    assert unit == sc.units.default_unit
+
+    name, unit = parse_bracket_head(" padded name  ")
+    assert name == " padded name  "
+    assert unit == sc.units.default_unit
+
+
+def test_parse_bracket_head_only_unit():
+    name, unit = parse_bracket_head("[m]")
+    assert name == ""
+    assert unit == "m"
+
+    name, unit = parse_bracket_head(" [kg]")
+    assert name == ""
+    assert unit == "kg"
+
+
+def test_parse_bracket_head_name_and_unit():
+    name, unit = parse_bracket_head("the name [s]")
+    assert name == "the name"
+    assert unit == "s"
+
+    name, unit = parse_bracket_head("title[A]")
+    assert name == "title"
+    assert unit == "A"
+
+
+def test_parse_bracket_head_empty_unit():
+    name, unit = parse_bracket_head("name []")
+    assert name == "name"
+    assert unit == sc.units.default_unit
+
+
+def test_parse_bracket_head_dimensionless():
+    name, unit = parse_bracket_head("name [one]")
+    assert name == "name"
+    assert unit == "one"
+
+    name, unit = parse_bracket_head("name [dimensionless]")
+    assert name == "name"
+    assert unit == "one"
+
+
+def test_parse_bracket_head_complex_unit():
+    name, unit = parse_bracket_head("name [m / s**2]")
+    assert name == "name"
+    assert unit == "m/s**2"
+
+
+def test_parse_bracket_head_bad_string():
+    with pytest.raises(ValueError):
+        parse_bracket_head("too [many] [brackets]")
