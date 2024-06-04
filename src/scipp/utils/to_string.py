d44ceec73d671370177bd3aec40b20e6dd387576
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-06-04 14:38:26 +0200

    Remove unused function

--- src/scipp/utils/to_string.py
+++ src/scipp/utils/to_string.py
@@ -4,25 +4,6 @@
 # @author Neil Vaytet
 
 
-def name_with_unit(var=None, name=None, log=False):
-    """
-    Make a column title or axis label with "Name [unit]".
-    """
-    text = ""
-    if name is not None:
-        text = name
-    elif var is not None:
-        text = str(var.dims[-1])
-
-    if log:
-        text = "log\u2081\u2080(" + text + ")"
-    if var is not None:
-        unit = var.unit if var.bins is None else var.bins.constituents["data"].unit
-        if unit is not None:
-            text += f" [{unit}]"
-    return text
-
-
 def value_to_string(val, precision=3, max_str_len=80):
     """
     Convert a number to a human readable string.
