08f57469336d315313f4b4e8ec38963319ca7ebd
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-06-12 09:32:21 +0200

    Remove unused function

--- src/scipp/visualization/colors.py
+++ src/scipp/visualization/colors.py
@@ -26,31 +26,3 @@ def rgb_to_hex(rgb: npt.NDArray[np.int64]) -> str:
             h = f"{h}0"
         hex_value.append(h)
     return "#" + "".join(hex_value)
-
-
-def make_random_color(
-    fmt: str = 'rgb',
-) -> npt.NDArray[np.int64] | npt.NDArray[np.float64] | str:
-    """
-    Generate a random color.
-    Possible output formats are:
-
-      - 'rgb' (default): (255, 255, 100)
-      - 'dec': (1.0, 1.0, 0.392)
-      - 'rgba': (1.0, 1.0, 0.392, 1.0)
-      - 'hex': #ffff64
-    """
-    rgb = np.random.randint(0, 255, 3)
-    if fmt == 'rgb':
-        return rgb
-    elif fmt == 'dec':
-        return rgb.astype(float) / 255.0
-    elif fmt == 'rgba':
-        return np.concatenate([rgb.astype(float) / 255.0, [1.0]])
-    elif fmt == 'hex':
-        return rgb_to_hex(rgb)
-    else:
-        raise RuntimeError(
-            f"Unknown color format {fmt}. Possible choices are: "
-            "rgb, dec, rgba, and hex."
-        )
