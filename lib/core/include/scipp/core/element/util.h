75308cc6626dcc6f4eb91098e8099d9eb798514a
jokasimr <johanneskasimir@gmail.com>
2024-09-04 12:11:27 +0200

    fix: make islinspace unit none (#3533)
    * fix: make islinspace unit none
    
    * fix: same for isarange

--- lib/core/include/scipp/core/element/util.h
+++ lib/core/include/scipp/core/element/util.h
@@ -95,13 +95,13 @@ constexpr auto islinspace =
                         scipp::span<const int64_t>, scipp::span<const int32_t>,
                         scipp::span<const time_point>>,
                transform_flags::expect_no_variance_arg<0>,
-               [](const units::Unit &) { return units::one; },
+               [](const units::Unit &) { return units::none; },
                [](const auto &range) { return numeric::islinspace(range); }};
 
 constexpr auto isarange =
     overloaded{arg_list<scipp::span<const int64_t>, scipp::span<const int32_t>>,
                transform_flags::expect_no_variance_arg<0>,
-               [](const units::Unit &) { return units::one; },
+               [](const units::Unit &) { return units::none; },
                [](const auto &range) { return numeric::isarange(range); }};
 
 constexpr auto zip =
