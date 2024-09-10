64af9b909893e242a6c60fb09f67cf53daa8ea8d
Neil Vaytet <neil.vaytet@ess.eu>
2024-09-10 14:03:16 +0200

    use dummy version number when renaming wheels so that pip>24.1 and uv are happy

--- .github/workflows/rename-dev-wheels.py
+++ .github/workflows/rename-dev-wheels.py
@@ -4,7 +4,10 @@ import os
 for filename in glob.glob('dist/*whl'):
     # We remove the version number, dev number, and Git hash to ensure we have a
     # predictable URL of the uploaded release asset that downstream projects can use.
+    # We have to use a dummy version number as this is required by
+    # pip (https://github.com/pypa/pip/issues/12063) and
+    # uv (https://github.com/astral-sh/uv/issues/5478)
     pkg, _, remainder = filename.split('-', 2)
-    target = f'{pkg}-nightly-{remainder}'
+    target = f'{pkg}-9999.99.99-{remainder}'
     print(f'Renaming wheel {filename} to {target}')
     os.rename(filename, target)
