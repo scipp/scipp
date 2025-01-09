89d63b8d695aa25105724726d91d27f85cdf6fc1
Mridul Seth <git@mriduls.com>
2025-01-09 13:36:59 +0100

    windows debug

--- lib/core/test/element_logical_test.cpp
+++ lib/core/test/element_logical_test.cpp
@@ -8,54 +8,60 @@
 using namespace scipp;
 using namespace scipp::core::element;
 
-TEST(LogicalTest, accepts_only_bool) {
+class LogicalTest : public ::testing::Test {
+protected:
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+TEST_F(LogicalTest, accepts_only_bool) {
   static_assert(std::is_same_v<decltype(logical)::types, std::tuple<bool>>);
   static_assert(
       std::is_same_v<decltype(logical_inplace)::types, std::tuple<bool>>);
 }
 
-TEST(LogicalTest, logical_unit) {
+TEST_F(LogicalTest, logical_unit) {
   EXPECT_EQ(logical(units::none), units::none);
   EXPECT_THROW(logical(units::m), except::UnitError);
 }
 
-// TEST(LogicalTest, logical_inplace_unit) {
-//   auto u = units::none;
-//   EXPECT_NO_THROW(logical_inplace(u, units::none));
-//   EXPECT_EQ(u, units::none);
-//   EXPECT_THROW(logical_inplace(u, units::m), except::UnitError);
-//   u = units::m;
-//   EXPECT_THROW(logical_inplace(u, units::none), except::UnitError);
-//   EXPECT_THROW(logical_inplace(u, units::m), except::UnitError);
-// }
+TEST_F(LogicalTest, logical_inplace_unit) {
+  auto u = units::none;
+  EXPECT_NO_THROW(logical_inplace(u, units::none));
+  EXPECT_EQ(u, units::none);
+  EXPECT_THROW(logical_inplace(u, units::m), except::UnitError);
+  u = units::m;
+  EXPECT_THROW(logical_inplace(u, units::none), except::UnitError);
+  EXPECT_THROW(logical_inplace(u, units::m), except::UnitError);
+}
 
-TEST(LogicalTest, and) {
+TEST_F(LogicalTest, and) {
   EXPECT_EQ(logical_and(true, true), true);
   EXPECT_EQ(logical_and(true, false), false);
   EXPECT_EQ(logical_and(false, true), false);
   EXPECT_EQ(logical_and(false, false), false);
 }
 
-TEST(LogicalTest, or) {
+TEST_F(LogicalTest, or) {
   EXPECT_EQ(logical_or(true, true), true);
   EXPECT_EQ(logical_or(true, false), true);
   EXPECT_EQ(logical_or(false, true), true);
   EXPECT_EQ(logical_or(false, false), false);
 }
 
-TEST(LogicalTest, xor) {
+TEST_F(LogicalTest, xor) {
   EXPECT_EQ(logical_xor(true, true), false);
   EXPECT_EQ(logical_xor(true, false), true);
   EXPECT_EQ(logical_xor(false, true), true);
   EXPECT_EQ(logical_xor(false, false), false);
 }
 
-TEST(LogicalTest, not) {
+TEST_F(LogicalTest, not) {
   EXPECT_EQ(logical_not(true), false);
   EXPECT_EQ(logical_not(false), true);
 }
 
-TEST(LogicalTest, and_equals) {
+TEST_F(LogicalTest, and_equals) {
   for (const auto &a : {true, false})
     for (const auto &b : {true, false}) {
       bool x = a;
@@ -64,7 +70,7 @@ TEST(LogicalTest, and_equals) {
     }
 }
 
-TEST(LogicalTest, or_equals) {
+TEST_F(LogicalTest, or_equals) {
   for (const auto &a : {true, false})
     for (const auto &b : {true, false}) {
       bool x = a;
@@ -73,7 +79,7 @@ TEST(LogicalTest, or_equals) {
     }
 }
 
-TEST(LogicalTest, xor_equals) {
+TEST_F(LogicalTest, xor_equals) {
   for (const auto &a : {true, false})
     for (const auto &b : {true, false}) {
       bool x = a;
