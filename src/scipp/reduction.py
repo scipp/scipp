aad5cb0de7a7f7ac4187104d0369d443265139f5
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-01-24 15:12:48 +0100

    Fix simple type errors

--- src/scipp/reduction.py
+++ src/scipp/reduction.py
@@ -2,74 +2,78 @@
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 # @author Simon Heybrock
 import uuid
-from collections.abc import Iterable
+from collections.abc import Iterable, Sequence
+from typing import Generic, TypeVar
 
-from .core import concat, reduction
-from .typing import VariableLike
+from .core import DataArray, Dataset, Variable, concat, reduction
+
+_O = TypeVar("_O", Variable, DataArray, Dataset)
 
 
-class BinsReducer:
-    def __init__(self, obj: VariableLike, dim: str):
-        self._obj = obj
+class BinsReducer(Generic[_O]):
+    def __init__(self, obj: _O, dim: str) -> None:
+        self._obj: _O = obj
         self._dim = dim
 
-    def concat(self):
+    def concat(self) -> _O:
         """Element-wise 'concat' across bins of inputs passed to :py:func:`scipp.reduce`."""  # noqa: E501
+        if self._obj.bins is None:
+            raise ValueError("Input must be binned")
         return self._obj.bins.concat(self._dim)
 
 
-class Reducer:
-    def __init__(self, x: list[VariableLike]):
+class Reducer(Generic[_O]):
+    def __init__(self, x: Sequence[_O]) -> None:
         self._dim = uuid.uuid4().hex
         # concat in init avoids repeated costly step in case of multiple reductions
-        self._obj = concat(x, dim=self._dim)
+        self._obj: _O = concat(x, dim=self._dim)
 
     @property
-    def bins(self):
+    def bins(self) -> BinsReducer[_O]:
         return BinsReducer(self._obj, self._dim)
 
-    def all(self):
+    def all(self) -> _O:
         """Element-wise 'all' across inputs passed to :py:func:`scipp.reduce`."""
         return reduction.all(self._obj, self._dim)
 
-    def any(self):
+    def any(self) -> _O:
         """Element-wise 'any' across inputs passed to :py:func:`scipp.reduce`."""
         return reduction.any(self._obj, self._dim)
 
-    def max(self):
+    def max(self) -> _O:
         """Element-wise 'max' across inputs passed to :py:func:`scipp.reduce`."""
         return reduction.max(self._obj, self._dim)
 
-    def min(self):
+    def min(self) -> _O:
         """Element-wise 'min' across inputs passed to :py:func:`scipp.reduce`."""
         return reduction.min(self._obj, self._dim)
 
-    def sum(self):
+    def sum(self) -> _O:
         """Element-wise 'sum' across inputs passed to :py:func:`scipp.reduce`."""
         return reduction.sum(self._obj, self._dim)
 
-    def mean(self):
+    def mean(self) -> _O:
         """Element-wise 'mean' across inputs passed to :py:func:`scipp.reduce`."""
         return reduction.mean(self._obj, self._dim)
 
-    def nanmax(self):
+    def nanmax(self) -> _O:
         """Element-wise 'nanmax' across inputs passed to :py:func:`scipp.reduce`."""
         return reduction.nanmax(self._obj, self._dim)
 
-    def nanmin(self):
+    def nanmin(self) -> _O:
         """Element-wise 'nanmin' across inputs passed to :py:func:`scipp.reduce`."""
         return reduction.nanmin(self._obj, self._dim)
 
-    def nansum(self):
+    def nansum(self) -> _O:
         """Element-wise 'nansum' across inputs passed to :py:func:`scipp.reduce`."""
         return reduction.nansum(self._obj, self._dim)
 
-    def nanmean(self):
+    def nanmean(self) -> _O:
         """Element-wise 'nanmean' across inputs passed to :py:func:`scipp.reduce`."""
         return reduction.nanmean(self._obj, self._dim)
 
 
-def reduce(x: Iterable[VariableLike]) -> Reducer:
+def reduce(x: Iterable[_O]) -> Reducer[_O]:
     """Create helper object for reduction operations over list or tuple of inputs.
 
     Usage examples:
--- src/scipp/utils/profile.py
+++ src/scipp/utils/profile.py
@@ -3,25 +3,30 @@
 # @author Simon Heybrock
 import os
 import time as _time
+from collections.abc import Callable
+from typing import ParamSpec, TypeVar
+
+_P = ParamSpec('_P')
+_R = TypeVar('_R')
 
 
-def time(method):
+def time(method: Callable[_P, _R]) -> Callable[_P, _R]:
     """Decorator that prints function run time."""
 
-    def timed(*args, **kw):
-        indent = time.counter
-        time.counter += 1
+    def timed(*args: _P.args, **kw: _P.kwargs) -> _R:
+        indent = time.counter  # type: ignore[attr-defined]
+        time.counter += 1  # type: ignore[attr-defined]
         # Not using print() since output may be swallowed in Jupyter notebooks
         os.write(1, f'{"  " * indent}enter {method.__name__}\n'.encode())
         start = _time.time()
         result = method(*args, **kw)
         end = _time.time()
         ms = (end - start) * 1000
-        time.counter -= 1
+        time.counter -= 1  # type: ignore[attr-defined]
         os.write(1, f'{"  " * indent}exit {method.__name__}  {ms} ms\n'.encode())
         return result
 
     return timed
 
 
-time.counter = 0
+time.counter = 0  # type: ignore[attr-defined]
