e46c4f5d821d11624b915707204605fc1f3bf73d
Mridul Seth <mail@mriduls.com>
2025-02-10 13:45:05 +0100

    MAINT: Upload nightlies with dev0 to keep the pin stable downstream

---
+++ .github/workflows/rename_nightly_wheel.py
@@ -0,0 +1,10 @@
+import glob
+import os
+
+for filename in glob.glob('dist/*whl'):
+    # Rename nightly wheels to the standard PyPI complaint X.X.X.dev0
+    pkg, version, remainder = filename.split('-', 2)
+    version = version.split('dev')[0] + 'dev0'
+    target = f'{pkg}-nightly-{remainder}'
+    print(f'Renaming wheel {filename} to {target}')
+    os.rename(filename, target)
