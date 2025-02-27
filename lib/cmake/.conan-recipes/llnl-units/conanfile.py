ce9aae65abfa93d41dd28e238a9f97b380e44fab
Mridul Seth <git@mriduls.com>
2025-02-27 11:23:47 +0100

    Update LLNL-Units to version 0.12.1

--- lib/cmake/.conan-recipes/llnl-units/conanfile.py
+++ lib/cmake/.conan-recipes/llnl-units/conanfile.py
@@ -5,13 +5,13 @@ from conans import CMake, ConanFile, tools
 CMAKE_PROJECT_STR = """project(
     ${UNITS_CMAKE_PROJECT_NAME}
     LANGUAGES C CXX
-    VERSION 0.12.0
+    VERSION 0.12.1
 )"""
 
 
 class UnitsConan(ConanFile):
     name = "LLNL-Units"
-    version = "0.12.0"
+    version = "0.12.1"
     license = "BSD-3"
     url = "https://github.com/llnl/units"
     homepage = "https://units.readthedocs.io"
