b5b0244f3ea134980e8866f5e968e8522a8e87f7
Johannes Kasimir <johanneskasimir@gmail.com>
2025-04-22 13:03:40 +0200

    fix: change concatenate alignment handling

--- lib/dataset/shape.cpp
+++ lib/dataset/shape.cpp
@@ -117,10 +117,10 @@ DataArray concat(const scipp::span<const DataArray> das, const Dim dim) {
                        concat_maps(map(das, get_masks), dim));
   const auto &coords = map(das, get_coords);
   for (auto &&[d, coord] : concat_maps(coords, dim)) {
-    coord.set_aligned(d == dim || std::any_of(coords.begin(), coords.end(),
-                                              [&d_ref = d](auto &_) {
-                                                return _.contains(d_ref);
-                                              }));
+    coord.set_aligned(
+        std::all_of(coords.begin(), coords.end(), [&d_ref = d](auto &_) {
+          return _.contains(d_ref) && _[d_ref].is_aligned();
+        }));
     out.coords().set(d, std::move(coord));
   }
   return out;
