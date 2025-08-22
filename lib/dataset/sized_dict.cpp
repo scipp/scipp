277f75084f6f6886a6fd1f69f748bd153f8ab991
Johannes Kasimir <johanneskasimir@gmail.com>
2025-08-22 14:47:17 +0200

    fix: improve error message

--- lib/dataset/sized_dict.cpp
+++ lib/dataset/sized_dict.cpp
@@ -405,6 +405,12 @@ template <class Key, class Value>
 bool SizedDict<Key, Value>::is_edges(const Key &key,
                                      const std::optional<Dim> dim) const {
   const auto &val = this->at(key);
+  if (!dim.has_value() && val.dims().size() > 1) {
+    throw except::DimensionError(
+        "Coord has multiple dimensions, but no dimension was specified. Use "
+        "the second argument to specify what dimension to check for "
+        "bin-edges.");
+  }
   return core::is_edges(m_sizes, val.dims(),
                         dim.has_value() ? *dim : val.dim());
 }
