4732e300fb21d62a3f20e9020df3d216abe21b82
Mridul Seth <git@mriduls.com>
2025-10-08 16:56:38 +0200

    run patch with scikit-build

--- tools/patch_conan.py
+++ tools/patch_conan.py
@@ -13,9 +13,9 @@ def patch_conan() -> bool:
     content = conf_file.read_text()
     old_version_string = """apple-clang: &apple_clang
             version: ["5.0", "5.1", "6.0", "6.1", "7.0", "7.3", "8.0", "8.1", "9.0", "9.1",
-                      "10.0", "11.0", "12.0", "13", "13.0", "13.1", "14", "14.0", "15", "15.0"]"""  # noqa: E501
+                      "10.0", "11.0", "12.0", "13", "13.0", "13.1", "14", "14.0", "15", "15.0", "16", "16.0"]"""  # noqa: E501
     new_version_string = """apple-clang: &apple_clang
-            version: ["17", "17.0"]"""
+            version: ["15", "15.0", "16", "16.0","17", "17.0"]"""
     content = content.replace(old_version_string, new_version_string)
     conf_file.write_text(content)
     print(f"Patched Conan config file {conf_file}")
