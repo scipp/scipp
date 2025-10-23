c0e3f7116c56632d297c0c7cb0c936fc2d435702
Mridul Seth <git@mriduls.com>
2025-10-23 09:29:55 +0200

    Use CPM for pulling in units, remove conan from conda build path (#3777)
    * Use CPM for pulling in units, remove conan from conda build path
    
    * remove triggers
    
    * whitespace
    
    * cpm_llnl_units
    
    * test
    
    * pop out the thread error
    
    * Apply automatic formatting
    
    * run only for osx
    
    * Update ci-macos.yml
    
    * Update libboost-headers version to 1.89.0
    
    * try to pin point where it went wrong
    
    * Update tbb version to 2022.2.0
    
    * try with pybind11
    
    * Update tbb and libboost-headers versions
    
    * clean up
    
    * clean up
    
    * rename SKIP_CONAN to SKIP_REMOTE_SOURCES

--- lib/cmake/scipp-conan.cmake
+++ lib/cmake/scipp-conan.cmake
@@ -2,33 +2,28 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 # ~~~
-execute_process(
-  COMMAND conan export .
-  WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/.conan-recipes/llnl-units"
-                    COMMAND_ECHO STDOUT
-)
-
 # Conan dependencies
-if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/conan.cmake")
-  message(
-    STATUS
-      "Downloading conan.cmake from https://github.com/conan-io/cmake-conan"
-  )
-  file(
-    DOWNLOAD
-    "https://raw.githubusercontent.com/conan-io/cmake-conan/6e5369d13720f22e07e31bb6b9018dbe60529fea/conan.cmake"
-    "${CMAKE_CURRENT_BINARY_DIR}/conan.cmake"
-    EXPECTED_HASH
-      SHA256=6abed7382c98a76b447675b30baff6a0b2f5d263041f6eb2fb682b80adaa2555
-    TLS_VERIFY ON
-  )
-endif()
-
-include(${CMAKE_CURRENT_BINARY_DIR}/conan.cmake)
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_BINARY_DIR})
-list(APPEND CMAKE_PREFIX_PATH ${CMAKE_CURRENT_BINARY_DIR})
 
 if(SKBUILD)
+  if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/conan.cmake")
+    message(
+      STATUS
+        "Downloading conan.cmake from https://github.com/conan-io/cmake-conan"
+    )
+    file(
+      DOWNLOAD
+      "https://raw.githubusercontent.com/conan-io/cmake-conan/6e5369d13720f22e07e31bb6b9018dbe60529fea/conan.cmake"
+      "${CMAKE_CURRENT_BINARY_DIR}/conan.cmake"
+      EXPECTED_HASH
+        SHA256=6abed7382c98a76b447675b30baff6a0b2f5d263041f6eb2fb682b80adaa2555
+      TLS_VERIFY ON
+    )
+  endif()
+
+  include(${CMAKE_CURRENT_BINARY_DIR}/conan.cmake)
+  list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_BINARY_DIR})
+  list(APPEND CMAKE_PREFIX_PATH ${CMAKE_CURRENT_BINARY_DIR})
+
   # The deploy generator is used to install dependencies into a know location.
   # The RUNTIME_DEPENDENCIES DIRECTORIES install option is then used to find the
   # dependencies. This is actually required only for Windows, since on Linux and
@@ -40,41 +35,26 @@ if(SKBUILD)
     boost/1.86.0
     eigen/3.4.0
     gtest/1.15.0
-    LLNL-Units/0.13.1
     pybind11/2.13.6
     onetbb/2021.12.0
     OPTIONS
     benchmark:shared=False
     boost:header_only=True
     gtest:shared=False
-    LLNL-Units:shared=False
-    LLNL-Units:fPIC=True
-    LLNL-Units:base_type=uint64_t
     GENERATORS
     cmake_find_package_multi
     deploy
   )
-else()
-  conan_cmake_configure(
-    REQUIRES
-    LLNL-Units/0.13.1
-    OPTIONS
-    LLNL-Units:shared=False
-    LLNL-Units:fPIC=True
-    LLNL-Units:base_type=uint64_t
-    GENERATORS
-    cmake_find_package_multi
+
+  conan_cmake_autodetect(conan_settings)
+  if(DEFINED CMAKE_OSX_ARCHITECTURES)
+    if("${CMAKE_OSX_ARCHITECTURES}" STREQUAL "arm64")
+      # For Apple M1
+      set(conan_settings "${conan_settings};arch=armv8")
+    endif()
+  endif()
+  conan_cmake_install(
+    PATH_OR_REFERENCE ${CMAKE_CURRENT_BINARY_DIR} SETTINGS ${conan_settings}
+    BUILD outdated
   )
 endif()
-
-conan_cmake_autodetect(conan_settings)
-if(DEFINED CMAKE_OSX_ARCHITECTURES)
-  if("${CMAKE_OSX_ARCHITECTURES}" STREQUAL "arm64")
-    # For Apple M1
-    set(conan_settings "${conan_settings};arch=armv8")
-  endif()
-endif()
-conan_cmake_install(
-  PATH_OR_REFERENCE ${CMAKE_CURRENT_BINARY_DIR} SETTINGS ${conan_settings}
-  BUILD outdated
-)
---
+++ lib/cmake/scipp-cpm.cmake
@@ -0,0 +1,50 @@
+# ~~~
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
+# ~~~
+
+# download CPM.cmake
+file(
+  DOWNLOAD
+  https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.42.0/CPM.cmake
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake
+  EXPECTED_HASH
+    SHA256=2020b4fc42dba44817983e06342e682ecfc3d2f484a581f11cc5731fbe4dce8a
+)
+
+include(${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake)
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_BINARY_DIR})
+list(APPEND CMAKE_PREFIX_PATH ${CMAKE_CURRENT_BINARY_DIR})
+
+# Configure units library build type based on DYNAMIC_LIB option
+if(DYNAMIC_LIB)
+  set(UNITS_BUILD_SHARED "ON")
+  set(UNITS_BUILD_STATIC "OFF")
+  set(UNITS_BUILD_SHARED_LIBS "ON")
+else()
+  set(UNITS_BUILD_SHARED "OFF")
+  set(UNITS_BUILD_STATIC "ON")
+  set(UNITS_BUILD_SHARED_LIBS "OFF")
+endif()
+
+cpmaddpackage(
+  NAME
+  units
+  GITHUB_REPOSITORY
+  llnl/units
+  VERSION
+  0.13.1
+  OPTIONS
+  "UNITS_PROJECT_NAME units"
+  "SKBUILD OFF"
+  "UNITS_INSTALL ON"
+  "BUILD_SHARED_LIBS ${UNITS_BUILD_SHARED_LIBS}"
+  "UNITS_BUILD_STATIC_LIBRARY ${UNITS_BUILD_STATIC}"
+  "UNITS_BUILD_SHARED_LIBRARY ${UNITS_BUILD_SHARED}"
+  "UNITS_BUILD_OBJECT_LIBRARY OFF"
+  "UNITS_BUILD_CONVERTER_APP OFF"
+  "UNITS_BUILD_WEBSERVER OFF"
+  "UNITS_BASE_TYPE uint64_t"
+  "UNITS_ENABLE_TESTS OFF"
+  "CMAKE_CXX_STANDARD 20"
+)
