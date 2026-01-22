8221a3c220ab18820754e3e0f27737795fbf7354
Mridul Seth <git@mriduls.com>
2026-01-22 11:05:07 +0100

    Move old benchmark script to asv setup

--- benchmarks/variable.py
+++ benchmarks/variable.py
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+from typing import ClassVar
+
+import numpy as np
+
 import scipp as sc
 
 
@@ -23,3 +27,19 @@ class Variable:
 
     def time_variable_non_inplace_operation(self) -> None:
         self.var1 + self.var2
+
+
+class AssignFromNumpy:
+    """
+    Benchmark assigning values from numpy arrays to scipp Variables.
+    """
+
+    params: ClassVar[list[int]] = [10**5, 10**6, 10**7, 10**8]
+    param_names: ClassVar[list[str]] = ['size']
+
+    def setup(self, size: int) -> None:
+        self.array = np.arange(size, dtype=np.float64)
+        self.var = sc.Variable(dims=['x'], values=self.array)
+
+    def time_assign_1d(self, size: int) -> None:
+        self.var.values = self.array
